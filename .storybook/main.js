918f4e436499c2bec1bf505d34f969442aba6835
Westly Wright <westly.wright@gmail.com>
2020-01-16 12:16:59 -0700

    Add storybookjs docs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-backgrounds/register'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-backgrounds/register', '@storybook/addon-docs'],
   presets: ['@storybook/preset-scss']
 };
