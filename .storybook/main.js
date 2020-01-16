200d77d3b941ea8aece57367d23b1476ecf83b47
Westly Wright <westly.wright@gmail.com>
2020-01-16 14:37:41 -0700

    add design systems

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../**/*.stories.js'],
+  stories: ['../stories/**/*.stories.js', '../components/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-backgrounds/register', '@storybook/addon-docs'],
   presets: ['@storybook/preset-scss']
 };
---
+++ stories/DesignSystems.stories.js
@@ -0,0 +1,5 @@
+
+export default {
+  title: 'Design Systems',
+  components: {},
+};
