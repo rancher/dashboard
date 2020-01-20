1c15f5ee85f53dd316a4a545b0fdd6bf4c417973
Cody Jackson <cody@rancher.com>
2020-01-20 12:09:10 -0700

    Defining stories for some components
    I also refactored PercentageBar so that it can  be a resusable
    component. The reusable component is then thinly wrapped by
    the formatter.

---
+++ components/PercentageBar.vue
@@ -0,0 +1,68 @@
+<script>
+/**
+ * A percentage bar which can be used to display how much a resource is being consumed.
+ */
+export default {
+  props: {
+    /**
+    * A value representing the percentage to be displayed. *Must be a value between 0 and 1*.
+    */
+    value: {
+      type:      Number,
+      required:  true,
+      validator: (value) => {
+        return value >= 0 && value <= 1;
+      }
+    }
+  },
+  data() {
+    return { NUMBER_OF_TICKS: 10 };
+  },
+  methods: {
+    getTickBackgroundClass(i) {
+      const valuePercentage = this.value;
+      const barPercentage = i / this.NUMBER_OF_TICKS;
+
+      if (valuePercentage < barPercentage) {
+        return 'bg-darker';
+      }
+
+      if (barPercentage <= 0.6) {
+        return 'bg-success';
+      }
+
+      if (barPercentage <= 0.8) {
+        return 'bg-info';
+      }
+
+      return 'bg-error';
+    }
+  }
+};
+</script>
+
+<template>
+  <span>
+    <span class="percentage">{{ value }}%</span>
+    <span class="bar">
+      <span v-for="i in NUMBER_OF_TICKS" :key="i" class="tick" :class="getTickBackgroundClass(i)">&nbsp;</span>
+    </span>
+  </span>
+</template>
+
+<style lang='scss'>
+  .percentage {
+      vertical-align: middle;
+  }
+  .bar {
+      vertical-align: middle;
+      margin-left: 3px;
+    .tick {
+        display: inline-block;
+        overflow: hidden;
+        margin-right: 3px;
+        width: 3px;
+        font-size: 1.2em;
+    }
+  }
+</style>
--- components/formatter/PercentageBar.vue
+++ components/formatter/PercentageBar.vue
@@ -1,67 +1,34 @@
 <script>
+import PercentageBarComponent from '../PercentageBar';
+
+/**
+ * This is a wrapper around the PercentageBar component which provides the interface
+ * that formatters are expected to have.
+ */
 export default {
-  props: {
+  components: { PercentageBarComponent },
+  props:      {
     value: {
       type:     String,
       default: ''
     },
     row: {
       type:     Object,
-      required: true
+      default: () => {}
     },
     col: {
       type:     Object,
       default: () => {}
     },
   },
-  data() {
-    return { NUMBER_OF_TICKS: 10 };
-  },
-  methods: {
-    getTickBackgroundClass(i) {
-      const valuePercentage = Number.parseFloat(this.value) / 100;
-      const barPercentage = i / this.NUMBER_OF_TICKS;
-
-      if (valuePercentage < barPercentage) {
-        return 'bg-darker';
-      }
-
-      if (barPercentage <= 0.6) {
-        return 'bg-success';
-      }
-
-      if (barPercentage <= 0.8) {
-        return 'bg-info';
-      }
-
-      return 'bg-error';
+  computed: {
+    percentage() {
+      return Number.parseFloat(this.value) / 100;
     }
   }
 };
 </script>
 
 <template>
-  <span>
-    <span class="percentage">{{ value }}%</span>
-    <span class="bar">
-      <span v-for="i in NUMBER_OF_TICKS" :key="i" class="tick" :class="getTickBackgroundClass(i)">&nbsp;</span>
-    </span>
-  </span>
+  <PercentageBarComponent :value="percentage" />
 </template>
-
-<style lang='scss'>
-  .percentage {
-      vertical-align: middle;
-  }
-  .bar {
-      vertical-align: middle;
-      margin-left: 3px;
-    .tick {
-        display: inline-block;
-        overflow: hidden;
-        margin-right: 3px;
-        width: 3px;
-        font-size: 1.2em;
-    }
-  }
-</style>
