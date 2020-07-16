c6785e248d754ed21f89a966c36ea1fe4a9bea7a
Westly Wright <westly.wright@gmail.com>
2020-07-16 15:12:19 -0700

    dropdown button on masthead

--- components/ResourceList/Masthead.vue
+++ components/ResourceList/Masthead.vue
@@ -41,27 +41,95 @@ export default {
       {{ typeDisplay }} <Favorite :resource="resource" />
     </h1>
     <div class="actions">
-      <nuxt-link
-        v-if="isYamlCreatable"
-        :to="yamlCreateLocation"
-        tag="button"
-        type="button"
-        class="btn bg-primary mr-10"
-      >
-        {{ t('resourceList.head.createFromYaml') }}
-      </nuxt-link>
-      <nuxt-link
-        v-if="isCreatable"
-        :to="createLocation"
-        tag="button"
-        type="button"
-        class="btn bg-primary"
-      >
-        {{ t('resourceList.head.create') }}
-      </nuxt-link>
+      <div class="dropdown-button btn bg-primary p-0">
+        <nuxt-link
+          v-if="isCreatable"
+          :to="createLocation"
+          class="btn bg-transparent create-button"
+        >
+          {{ t("resourceList.head.create") }}
+        </nuxt-link>
+
+        <v-popover
+          placement="bottom"
+          container=".actions"
+          offset="10"
+          :popper-options="{modifiers: { flip: { enabled: false } } }"
+        >
+          <button class="icon-container btn bg-transparent">
+            <i class="icon icon-chevron-down" />
+          </button>
+
+          <template slot="popover">
+            <ul class="list-unstyled menu" style="margin: -1px;">
+              <nuxt-link
+                v-if="isYamlCreatable"
+                :to="yamlCreateLocation"
+                tag="li"
+                class="hand"
+              >
+                {{ t("resourceList.head.createFromYaml") }}
+              </nuxt-link>
+            </ul>
+          </template>
+        </v-popover>
+      </div>
     </div>
   </header>
 </template>
 
-<style lang='scss'>
+<style lang="scss">
+  .actions {
+    .v-popover {
+      .text-right {
+        margin-top: 5px;
+      }
+      .trigger {
+        height: 100%;
+        .icon-container {
+          height: 100%;
+          padding: 15px 10px 10px 10px;
+        }
+      }
+    }
+    .dropdown-button {
+      background: var(--accent-btn);
+      border: solid thin var(--link-text);
+      color: var(--link-text);
+
+      .create-button {
+        border-right: 1px solid var(--link-text);
+        margin-top: 10px;
+        margin-bottom: 10px;
+        padding: 5px 40px;
+
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+    .popover {
+      .popover-inner {
+        color: var(--dropdown-text);
+        background-color: var(--dropdown-bg);
+        border: 1px solid var(--dropdown-border);
+        padding: 0px;
+
+        LI {
+          padding: 10px 40px;
+
+          &.divider {
+            padding: 0;
+            border-bottom: 1px solid var(--dropdown-divider);
+          }
+
+          &:not(.divider):hover {
+            background-color: var(--dropdown-hover-bg);
+            color: var(--dropdown-hover-text);
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
 </style>
