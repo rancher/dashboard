6b94cd5be044809486cc496a739ebaf06f7206e1
Westly Wright <westly.wright@gmail.com>
2020-07-21 11:24:11 -0700

    Move dd button styles to tooltip for ssr
    load order of dd button styles on list page got wonky on SSR reload so I moved
    the styles to an earlier load.
    v-tooltip also put the tooltip into the dom when SSR'ing so I've added a display
    none to pull the item out of flow so actions box doesn't get huge and small.

--- components/ButtonDropdown.vue
+++ components/ButtonDropdown.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 <template>
   <div class="dropdown-button-group">
-    <div class="dropdown-button btn bg-primary p-0">
+    <div class="dropdown-button btn bg-primary">
       <slot name="button-content">
         <button class="btn bg-transparent"></button>
       </slot>
@@ -34,84 +34,3 @@ export default {
     </div>
   </div>
 </template>
-
-<style lang="scss">
-  .dropdown-button-group {
-    .v-popover {
-      .text-right {
-        margin-top: 5px;
-      }
-      .trigger {
-        height: 100%;
-        .icon-container {
-          height: 100%;
-          padding: 15px 10px 10px 10px;
-        }
-      }
-    }
-    .dropdown-button {
-      background: var(--accent-btn);
-      border: solid thin var(--link-text);
-      color: var(--link-text);
-
-      .create-button {
-        &:not(:only-child) {
-          border-right: 1px solid var(--link-text);
-        }
-
-        margin-top: 10px;
-        margin-bottom: 10px;
-        padding: 5px 40px;
-
-        &:focus {
-          outline: none;
-        }
-      }
-    }
-    .popover {
-      border: none;
-    }
-    .tooltip {
-      &[x-placement^="bottom"] {
-        .tooltip-arrow {
-          border-bottom-color: var(--dropdown-border);
-
-          &:after {
-            border-bottom-color: var(--dropdown-bg);
-          }
-        }
-      }
-
-      .tooltip-inner {
-        color: var(--dropdown-text);
-        background-color: var(--dropdown-bg);
-        border: 1px solid var(--dropdown-border);
-        padding: 0px;
-        text-align: left;
-
-        LI {
-          padding: 10px 50px 10px 20px;
-
-          &.divider {
-            padding-top: 0px;
-            padding-bottom: 0px;
-
-            > .divider-inner {
-              padding: 0;
-              border-bottom: 1px solid var(--dropdown-divider);
-              width: 125%;
-              margin: 0 auto;
-            }
-          }
-
-          &:not(.divider):hover {
-            background-color: var(--dropdown-hover-bg);
-            color: var(--dropdown-hover-text);
-            cursor: pointer;
-          }
-        }
-
-      }
-    }
-  }
-</style>
