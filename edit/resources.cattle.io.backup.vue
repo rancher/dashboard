297304eefea720454f046baea604fdde28e09962
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2020-09-22 09:24:39 -0700

    fix encryption config name typo

--- edit/resources.cattle.io.backup.vue
+++ edit/resources.cattle.io.backup.vue
@@ -190,7 +190,7 @@ export default {
             </div>
             <div v-if="useEncryption" class="row mt-10">
               <div class="col span-6">
-                <LabeledSelect v-model="value.spec.encryptionConfigName" :mode="mode" :options="encryptionSecretNames" :label="t('backupRestoreOperator.encryptionConfigName.label')" />
+                <LabeledSelect v-model="value.spec.encryptionConfigSecretName" :mode="mode" :options="encryptionSecretNames" :label="t('backupRestoreOperator.encryptionConfigName.label')" />
               </div>
             </div>
           </div>
