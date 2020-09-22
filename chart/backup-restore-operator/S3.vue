1739fcacbd9d52dc8e01beab0e67d41f7b3f7d4d
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2020-09-22 13:01:16 -0700

    fix endpointCA input in s3 config

--- chart/backup-restore-operator/S3.vue
+++ chart/backup-restore-operator/S3.vue
@@ -81,7 +81,7 @@ export default {
       </div>
       <div class="col span-6">
         <LabeledInput v-model="value.endpointCA" :mode="mode" type="multiline" :label="t('backupRestoreOperator.s3.endpointCA')" />
-        <FileSelector v-if="mode!=='view'" class="btn btn-sm role-primary mt-5" :mode="mode" :label="t('generic.readFromFile')" @selected="e=>$set(s3, 'endpointCA', e)" />
+        <FileSelector v-if="mode!=='view'" class="btn btn-sm role-primary mt-5" :mode="mode" :label="t('generic.readFromFile')" @selected="e=>$set(value, 'endpointCA', e)" />
       </div>
     </div>
   </div>
