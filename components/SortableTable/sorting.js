65172a0d3442b86304f9071884dca81b60019b8b
Vincent Fiduccia <vincent@rancher.com>
2020-09-26 02:38:10 -0700

    Fix default table sort

--- components/SortableTable/sorting.js
+++ components/SortableTable/sorting.js
@@ -39,43 +39,39 @@ export default {
     let sortBy = null;
     let descending = false;
 
-    const defaultSortCol = this.headers.find(x => x.defaultSort);
+    this._defaultSortBy = this.defaultSortBy;
 
-    if (defaultSortCol) {
-      sortBy = defaultSortCol.sort;
-      descending = defaultSortCol.defaultSort;
-    } else {
-      this._defaultSortBy = this.defaultSortBy;
+    // Try to find a reasonable default sort
+    if ( !this._defaultSortBy ) {
+      const markedColumn = this.headers.find(x => !!x.defaultSort);
+      const nameColumn = this.headers.find( x => x.name === 'name');
 
-      // Try to find a reasonable default sort
-      if ( !this._defaultSortBy ) {
-        const nameColumn = this.headers.find( x => x.name === 'name');
+      if ( markedColumn ) {
+        this._defaultSortBy = markedColumn.name;
+      } else if ( nameColumn ) {
+        // Use the name column if there is one
+        this._defaultSortBy = nameColumn.name;
+      } else {
+        // The first column that isn't state
+        const first = this.headers.filter( x => x.name !== 'state' )[0];
 
-        if ( nameColumn ) {
-          // Use the name column if there is one
-          this._defaultSortBy = nameColumn.name;
+        if ( first ) {
+          this._defaultSortBy = first.name;
         } else {
-          // The first column that isn't state
-          const first = this.headers.filter( x => x.name !== 'state' )[0];
-
-          if ( first ) {
-            this._defaultSortBy = first.name;
-          } else {
-            // I give up
-            this._defaultSortBy = 'id';
-          }
+          // I give up
+          this._defaultSortBy = 'id';
         }
       }
-
-      sortBy = this.$route.query.sort;
-
-      // If the sort column doesn't exist or isn't specified, use default
-      if ( !sortBy || !this.headers.find(x => x.name === sortBy ) ) {
-        sortBy = this._defaultSortBy;
-      }
-
-      descending = (typeof this.$route.query.desc) !== 'undefined';
     }
+
+    sortBy = this.$route.query.sort;
+
+    // If the sort column doesn't exist or isn't specified, use default
+    if ( !sortBy || !this.headers.find(x => x.name === sortBy ) ) {
+      sortBy = this._defaultSortBy;
+    }
+
+    descending = (typeof this.$route.query.desc) !== 'undefined';
 
     return {
       sortBy,
