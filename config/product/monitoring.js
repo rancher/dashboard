c7709a73939e4f8e7d1286d092ded85dc4acb7e9
Westly Wright <westly.wright@gmail.com>
2020-09-29 11:21:36 -0700

    Fix alertmanager and prometheuse table headers ages

--- config/product/monitoring.js
+++ config/product/monitoring.js
@@ -1,12 +1,13 @@
 import { DSL } from '@/store/type-map';
 import { MONITORING } from '@/config/types';
+import { STATE, NAME as NAME_COL, AGE } from '@/config/table-headers';
 
 export const NAME = 'monitoring';
 export const CHART_NAME = 'rancher-monitoring';
 
 export function init(store) {
   const {
-    product, basicType, virtualType, weightType
+    product, basicType, virtualType, weightType, headers
   } = DSL(store, NAME);
   const {
     ALERTMANAGER,
@@ -44,4 +45,42 @@ export function init(store) {
   weightType(SERVICEMONITOR, 104, true);
   weightType(PODMONITOR, 103, true);
   weightType(PROMETHEUSRULE, 102, true);
+
+  headers(ALERTMANAGER, [
+    STATE,
+    NAME_COL,
+    {
+      name:     'version',
+      labelKey: 'tableHeaders.version',
+      sort:     'spec.version',
+      value:    'spec.version'
+    },
+    {
+      name:      'replicas',
+      labelKey:  'tableHeaders.replicas',
+      value:     'spec.replicas',
+      sort:      'spec.replicas',
+      formatter: 'Number',
+    },
+    AGE
+  ]);
+
+  headers(PROMETHEUSE, [
+    STATE,
+    NAME_COL,
+    {
+      name:     'version',
+      labelKey: 'tableHeaders.version',
+      sort:     'spec.version',
+      value:    'spec.version'
+    },
+    {
+      name:      'replicas',
+      labelKey:  'tableHeaders.replicas',
+      value:     'spec.replicas',
+      sort:      'spec.replicas',
+      formatter: 'Number',
+    },
+    AGE
+  ]);
 }
