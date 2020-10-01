64a574dc7c4ab71f7ac75cfa90fe4f163a77fa39
Cody Jackson <cody@rancher.com>
2020-10-01 12:28:01 -0700

    Removing deploy to constrolplane nodes
    rancher/dashboard#1558

--- chart/logging/index.vue
+++ chart/logging/index.vue
@@ -1,24 +1,9 @@
 <script>
-import Checkbox from '@/components/form/Checkbox';
 import { mapGetters } from 'vuex';
 import LabeledInput from '@/components/form/LabeledInput';
-import { isEqual } from 'lodash';
-
-const TOLERATIONS = [
-  {
-    key:    'node-role.kubernetes.io/controlplane',
-    value:  'true',
-    effect: 'NoSchedule',
-  },
-  {
-    key:    'node-role.kubernetes.io/etcd',
-    value:  'true',
-    effect: 'NoExecute'
-  }
-];
 
 export default {
-  components: { Checkbox, LabeledInput },
+  components: { LabeledInput },
   props:      {
     value: {
       type:    Object,
@@ -28,10 +13,6 @@ export default {
     }
   },
 
-  data() {
-    return { deployToControlPlane: isEqual(this.value.fluentbit_tolerations, TOLERATIONS) };
-  },
-
   computed: {
     ...mapGetters(['currentCluster']),
     provider() {
@@ -39,22 +20,9 @@ export default {
     }
   },
 
-  watch: {
-    deployToControlPlane(newValue) {
-      if (!newValue) {
-        if (this.value.fluentbit_tolerations) {
-          this.value.fluentbit_tolerations = [];
-        }
-      } else {
-        this.value.fluentbit_tolerations = TOLERATIONS;
-      }
-    }
-  },
-
   mounted() {
     this.value.additionalLoggingSources = this.value.additionalLoggingSources || {};
     this.value.additionalLoggingSources[this.provider] = this.value.additionalLoggingSources[this.provider] || {};
-
     this.value.additionalLoggingSources[this.provider].enabled = true;
   },
 };
@@ -62,12 +30,7 @@ export default {
 
 <template>
   <div class="logging">
-    <div class="row">
-      <div class="col span-6">
-        <Checkbox v-model="deployToControlPlane" :label="t('logging.install.deployToControlPlane')" />
-      </div>
-    </div>
-    <div v-if="provider === 'k3s'" class="row mt-10">
+    <div v-if="provider === 'k3s'" class="row">
       <div class="col span-6">
         <LabeledInput v-model="value.additionalLoggingSources.k3s.container_engine" :label="t('logging.install.k3sContainerEngine')" />
       </div>
