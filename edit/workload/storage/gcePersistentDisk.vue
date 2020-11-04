4116f20453732f467bf9de351d287d2609c67504
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2020-11-04 11:58:12 -0700

    add fstype field to workload stoeage

--- edit/workload/storage/gcePersistentDisk.vue
+++ edit/workload/storage/gcePersistentDisk.vue
@@ -24,12 +24,19 @@ export default {
 </script>
 
 <template>
-  <div class="row">
-    <div class="col span-6">
-      <LabeledInput v-model="value.gcePersistentDisk.pdName" :mode="mode" :label="t('workload.storage.csi.pdName')" />
+  <div>
+    <div class="row mb-10">
+      <div class="col span-6">
+        <LabeledInput v-model="value.gcePersistentDisk.pdName" :mode="mode" :label="t('workload.storage.csi.pdName')" />
+      </div>
+      <div class="col span-6">
+        <LabeledInput v-model.number="value.gcePersistentDisk.partition" :mode="mode" :label="t('workload.storage.csi.partition')" />
+      </div>
     </div>
-    <div class="col span-6">
-      <LabeledInput v-model.number="value.gcePersistentDisk.partition" :mode="mode" :label="t('workload.storage.csi.partition')" />
+    <div class="row">
+      <div class="col span-6">
+        <LabeledInput v-model="value.gcePersistentDisk.fsType" :mode="mode" :label="t('workload.storage.csi.fsType')" />
+      </div>
     </div>
   </div>
 </template>
