b9e0fa5e3661a7b734311e5d7b63b9a37e9e6300
lvuch <laurenvuchetich@gmail.com>
2020-11-06 15:39:39 -0700

    WIP design system setup

---
+++ pages/design-system/index.vue
@@ -0,0 +1,25 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div>
+    <h1>Design System</h1>
+    <h2>List</h2>
+    <h2>Create</h2>
+    <h2>Detail</h2>
+    <nav>
+      <a href="/">List</a>
+      <a href="/">Create</a>
+      <a href="/">Detail</a>
+    </nav>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+  nav {
+    a {
+      color: red;
+    }
+  }
+</style>
