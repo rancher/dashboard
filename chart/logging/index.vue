c26347acf6d0c0ac5d6aa973dd6144f28cff9e79
Cody Jackson <sxenog@gmail.com>
2020-11-30 23:55:08 -0700

    Allowing users to configure additionalLoggingSources on the logging chart
    rancher/dashboard#1851

--- chart/logging/index.vue
+++ chart/logging/index.vue
@@ -1,9 +1,10 @@
 <script>
 import { mapGetters } from 'vuex';
 import LabeledInput from '@/components/form/LabeledInput';
+import Checkbox from '@/components/form/Checkbox';
 
 export default {
-  components: { LabeledInput },
+  components: { Checkbox, LabeledInput },
   props:      {
     value: {
       type:    Object,
@@ -30,9 +31,14 @@ export default {
 
 <template>
   <div class="logging">
-    <div v-if="provider === 'k3s'" class="row">
+    <div v-if="provider === 'k3s'" class="row mb-20">
       <div class="col span-6">
         <LabeledInput v-model="value.additionalLoggingSources.k3s.container_engine" :label="t('logging.install.k3sContainerEngine')" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col span-6">
+        <Checkbox v-model="value.additionalLoggingSources[provider].enabled" :label="t('logging.install.enableAdditionalLoggingSources')" />
       </div>
     </div>
   </div>
