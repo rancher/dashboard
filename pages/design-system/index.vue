fdfb9afa31b23553f6b345867186fda84200dc78
lvuch <laurenvuchetich@gmail.com>
2020-12-04 11:22:57 -0700

    WIP

--- pages/design-system/index.vue
+++ pages/design-system/index.vue
@@ -1,5 +1,18 @@
 <script>
-export default { layout: 'unauthenticated' };
+import LabeledInput from '@/components/form/LabeledInput';
+import LabeledSelect from '@/components/form/LabeledSelect';
+import TextAreaAutoGrow from '@/components/form/TextAreaAutoGrow';
+import UnitInput from '@/components/form/UnitInput';
+import Select from '@/components/form/Select';
+import SecretSelector from '@/components/form/SecretSelector';
+import InputWithSelect from '@/components/form/InputWithSelect';
+
+export default {
+  layout:     'unauthenticated',
+  components: {
+    LabeledInput, LabeledSelect, TextAreaAutoGrow, UnitInput, Select, SecretSelector, InputWithSelect
+  },
+};
 </script>
 
 <template>
@@ -19,6 +32,65 @@ export default { layout: 'unauthenticated' };
         </nuxt-link>
       </nav>
     </section>
+
+    <LabeledInput
+      :label="foo"
+      mode="edit"
+    />
+
+    <LabeledSelect
+      :label="foo"
+      mode="edit"
+    />
+
+    <TextAreaAutoGrow
+      v-if="valueMultiline"
+      ref="value"
+      :placeholder="placeholder here"
+      mode="edit"
+      @paste="onPaste(idx, $event)"
+      @input="queueUpdate"
+    />
+
+    <input
+      v-else
+      ref="value"
+      :placeholder="placeholder here"
+      :disabled="isView"
+      @paste="onPaste(idx, $event)"
+      @input="queueUpdate"
+    />
+
+    <UnitInput
+      mode="view"
+      :value="string"
+      :label="foo"
+      :suffix="selected"
+    />
+
+    <Select
+      :options="['foo','bar']"
+      mode="edit"
+      @input="e=>updateEffect(e, rule)"
+    />
+
+    <SecretSelector
+      mode="edit"
+      :disabled="disabled"
+      :label="foo"
+      :show-key-selector="true"
+    />
+
+    <InputWithSelect
+      :text-value="maxSurge"
+      :select-before-text="false"
+      :select-value="surgeUnits"
+      :text-label="foo"
+      mode="edit"
+      type="number"
+      :options="['foo','bar']"
+      @input="e=>updateWithUnits(e, 'maxSurge')"
+    />
   </div>
 </template>
 
