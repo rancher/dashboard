50f41b082a88267bc84ac4720b091354ab9049f2
lvuch <laurenvuchetich@gmail.com>
2021-01-13 14:16:26 -0700

    2183

--- layouts/default.vue
+++ layouts/default.vue
@@ -331,7 +331,10 @@ export default {
                             "state-banner state-banner";
       grid-template-columns: auto min-content;
       margin-bottom: 20px;
-      align-items: center;
+      // align-items: center;
+      // min-height: 48px;
+      // background: orange;
+      align-content: center;
       min-height: 48px;
 
       .type-banner {
@@ -344,12 +347,14 @@ export default {
 
       .title {
         grid-area: title;
+        align-self: center;
       }
 
       .actions-container {
         grid-area: actions;
-        height: 100%;
         margin-left: 8px;
+        // background: red;
+        align-self: center;
       }
     }
 
