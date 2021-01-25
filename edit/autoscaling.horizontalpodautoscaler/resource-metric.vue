b723b5bcf0c264bf525cc3dd0d8410af709498d8
Westly Wright <westly.wright@gmail.com>
2021-01-25 16:24:25 -0700

    New Feature HPA
    rancher/dashboard#2187

---
+++ edit/autoscaling.horizontalpodautoscaler/resource-metric.vue
@@ -0,0 +1,62 @@
+<script>
+import LabeledSelect from '@/components/form/LabeledSelect';
+import MetricTarget from '@/edit/autoscaling.horizontalpodautoscaler/metric-target';
+
+export const DEFAULT_RESOURCE_METRIC = {
+  type:     'Resource',
+  resource: {
+    name:   'CPU',
+    target: {
+      type:               'Utilization',
+      averageUtilization: 50,
+    },
+  },
+};
+
+export default {
+  components: { LabeledSelect, MetricTarget },
+  props:      {
+    value: {
+      type:    Object,
+      default: () => ({
+        name:   '',
+        target: {}
+      }),
+    },
+
+    mode: {
+      type:    String,
+      default: 'create',
+    },
+  },
+
+  data() {
+    return {
+      resourceTypes: [
+        { label: this.t('hpa.types.cpu'), value: 'cpu' },
+        { label: this.t('hpa.types.memory'), value: 'memory' },
+      ],
+    };
+  },
+
+  computed: {},
+};
+</script>
+
+<template>
+  <div class="resource-metric">
+    <div class="row mb-20">
+      <div class="col span-6">
+        <LabeledSelect
+          v-model="value.name"
+          :mode="mode"
+          :label="t('hpa.metrics.headers.resource')"
+          :options="resourceTypes"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <MetricTarget v-model="value.target" :mode="mode" type="resource" />
+    </div>
+  </div>
+</template>
