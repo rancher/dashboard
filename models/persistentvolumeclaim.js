e2674cace1c36b6b896ac0e71c94b411467323bd
Richard Cox <richard.cox@suse.com>
2021-02-23 15:08:48 +0000

    Ensure PVC clone retains Request Storage & Access Modes values - these were overwritten by the model's applyDefaults and a default component value - we now handle clone in both cases

--- models/persistentvolumeclaim.js
+++ models/persistentvolumeclaim.js
@@ -1,10 +1,17 @@
+import { _CLONE } from '@/config/query-params';
 import Vue from 'vue';
 
 export default {
   applyDefaults() {
-    return () => {
+    return (_, realMode) => {
+      const accessModes = realMode === _CLONE ? this.spec.accessModes : [];
+      const storage = realMode === _CLONE ? this.spec.resources.requests.storage : null;
+
       Vue.set(this, 'spec', {
-        accessModes: [], storageClassName: '', volumeName: '', resources: { requests: { storage: null } }
+        accessModes,
+        storageClassName: '',
+        volumeName:       '',
+        resources:        { requests: { storage } }
       });
     };
   },
