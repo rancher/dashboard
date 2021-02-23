83f487eaaf313c0eccb05e3b6c6efae37b1e7283
Richard Cox <ricox@suse.com>
2021-02-23 11:06:43 +0000

    Best effort to improve bulk remove of tokens
    Move any ui session token to the back of the delete queue

--- models/management.cattle.io.token.js
+++ models/management.cattle.io.token.js
@@ -2,8 +2,45 @@ import day from 'dayjs';
 
 export default {
 
+  isUISession() {
+    return () => {
+      // Leave as a function such that it's not treated as a property
+      const labels = this.metadata?.labels;
+      const kind = labels ? labels['authn.management.cattle.io/kind'] : '';
+
+      return kind === 'session';
+    };
+  },
+
+  bulkRemove() {
+    // There's no way to exclude the current users ui session from this bulk remove (which causes the session to expire, user logged out and
+    // fail any subsequent api's to be removed). Do a best effort to move any ui session (user could be logged in via another
+    // method/browser) to the back of the queue.
+    return (resources = this) => this.$dispatch('promptRemove', resources.sort((a, b) => {
+      if (a.isUISession() && !b.isUISession()) {
+        return 1;
+      }
+      if (!a.isUISession() && b.isUISession()) {
+        return -1;
+      }
+
+      return 0;
+    }));
+  },
+
   _availableActions() {
-    return this._standardActions.filter(x => ['promptRemove', 'viewInApi'].includes(x.action));
+    return this._standardActions.reduce((res, action) => {
+      if (action.action === 'viewInApi') {
+        res.push(action);
+      } else if (action.action === 'promptRemove') {
+        res.push({
+          ...action,
+          bulkAction: 'bulkRemove',
+        });
+      }
+
+      return res;
+    }, []);
   },
 
   state() {
