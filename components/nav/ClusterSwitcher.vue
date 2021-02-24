b45f3caab8fd4854cac43d9380460c3651f35b90
Westly Wright <westly.wright@gmail.com>
2021-02-24 09:55:54 -0700

    convert cluster switcher to appened dropdown
    so we can ensure that the positioning is always correct and doesn't flow off right screen when cluster names are long

--- components/nav/ClusterSwitcher.vue
+++ components/nav/ClusterSwitcher.vue
@@ -1,14 +1,16 @@
 <script>
+import { createPopper } from '@popperjs/core';
 import { MANAGEMENT } from '@/config/types';
 import { sortBy } from '@/utils/sort';
 import { findBy } from '@/utils/array';
 import { mapGetters, mapState } from 'vuex';
 import Select from '@/components/form/Select';
+import $ from 'jquery';
 
 export default {
   components: { Select },
 
-  computed:   {
+  computed: {
     ...mapState(['isMultiCluster']),
     ...mapGetters(['currentCluster']),
 
@@ -53,6 +55,51 @@ export default {
     focus() {
       this.$refs.select.focusSearch();
     },
+    withPopper(dropdownList, component, { width }) {
+      /**
+       * We need to explicitly define the dropdown width since
+       * it is usually inherited from the parent with CSS.
+       */
+      const componentWidth = $(component.$parent.$el).width();
+
+      dropdownList.style['min-width'] = `${ componentWidth }px`;
+      dropdownList.style.width = 'min-content';
+      dropdownList.className += ' cluster-switcher-container';
+
+      /**
+       * Here we position the dropdownList relative to the $refs.toggle Element.
+       *
+       * The 'offset' modifier aligns the dropdown so that the $refs.toggle and
+       * the dropdownList overlap by 1 pixel.
+       *
+       * The 'toggleClass' modifier adds a 'drop-up' class to the Vue Select
+       * wrapper so that we can set some styles for when the dropdown is placed
+       * above.
+       */
+      const popper = createPopper(component.$refs.toggle, dropdownList, {
+        placement: 'bottom-end',
+        modifiers: [
+          {
+            name:    'offset',
+            options: { offset: [0, 2] },
+          },
+          {
+            name:    'toggleClass',
+            enabled: true,
+            phase:   'write',
+            fn({ state }) {
+              component.$el.setAttribute('x-placement', state.placement);
+            },
+          },
+        ],
+      });
+
+      /**
+       * To prevent memory leaks Popper needs to be destroyed.
+       * If you return function, it will be called just before dropdown is removed from DOM.
+       */
+      return () => popper.destroy();
+    },
   },
 };
 </script>
@@ -63,16 +110,21 @@ export default {
       ref="select"
       key="cluster"
       v-model="value"
-      :append-to-body="false"
+      :append-to-body="true"
+      :popper-override="withPopper"
+      placement="bottom"
       :searchable="true"
       :selectable="(option) => option.ready"
       :clearable="false"
       :options="options"
-      :close-on-select="false"
     >
       <template #selected-option="opt">
         <span class="cluster-label-container">
-          <img v-if="currentCluster" class="cluster-switcher-os-logo" :src="currentCluster.providerOsLogo" />
+          <img
+            v-if="currentCluster"
+            class="cluster-switcher-os-logo"
+            :src="currentCluster.providerOsLogo"
+          />
           <span class="cluster-label">
             {{ opt.label }}
           </span>
@@ -89,7 +141,11 @@ export default {
       <template #option="opt">
         <span class="dropdown-option">
           <span class="logo">
-            <img v-if="opt.osLogo" class="cluster-switcher-os-logo" :src="opt.osLogo" />
+            <img
+              v-if="opt.osLogo"
+              class="cluster-switcher-os-logo"
+              :src="opt.osLogo"
+            />
           </span>
           <span class="content">
             <b v-if="opt === value">{{ opt.label }}</b>
@@ -109,8 +165,24 @@ export default {
   </div>
 </template>
 
+<style lang="scss">
+// should be outside scoped css, class is dynamic and added by withPopper so scoped + ::v-deep doesn't work
+.cluster-switcher-container {
+  &.vs__dropdown-menu {
+    .vs__dropdown-option {
+      padding: 3px 20px 3px 8px;
+    }
+  }
+}
+</style>
+
 <style lang="scss" scoped>
-.filter {
+.cluster-switcher-os-logo {
+  height: 16px;
+  display: inline-block;
+  vertical-align: middle;
+}
+.cluster-switcher-container {
   .cluster-label-container {
     width: 100%;
     display: grid;
@@ -119,8 +191,13 @@ export default {
     align-content: center;
   }
 
-  .cluster-switcher-os-logo {
-    height: 16px;
+  // matches the padding a layout of the option (and logo/content) to the selected option so it doesn't look off
+  .dropdown-option {
+    display: grid;
+    width: 100%;
+    grid-template-columns: 25px fit-content(100%);
+    align-items: center;
+    align-content: center;
   }
 }
 
@@ -133,18 +210,6 @@ export default {
 
   .vs__selected {
     width: 100%;
-  }
-
-  // matches the padding a layout of the option (and logo/content) to the selected option so it doesn't look off
-  .vs__dropdown-option  {
-    padding: 3px 20px 3px 8px;
-    .dropdown-option {
-      display: grid;
-      width: 100%;
-      grid-template-columns: 25px fit-content(100%);
-      align-items: center;
-      align-content: center;
-    }
   }
 
   &.vs--disabled .vs__actions {
