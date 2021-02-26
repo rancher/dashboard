403b9b9c7de8d8ce457caa3f45a1cbb665254a2b
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2021-02-26 10:54:14 -0700

    hide tls and auth fields from email and webhook receivers

--- edit/monitoring.coreos.com.receiver/types/email.vue
+++ edit/monitoring.coreos.com.receiver/types/email.vue
@@ -1,11 +1,13 @@
 <script>
 import LabeledInput from '@/components/form/LabeledInput';
 import Checkbox from '@/components/form/Checkbox';
-import TLS from '../tls';
+// import TLS from '../tls';
 
 export default {
   components: {
-    Checkbox, LabeledInput, TLS
+    Checkbox,
+    LabeledInput,
+    // TLS
   },
   props:      {
     mode: {
@@ -18,7 +20,7 @@ export default {
     }
   },
   data() {
-    this.$set(this.value, 'http_config', this.value.http_config || {});
+    // this.$set(this.value, 'http_config', this.value.http_config || {});
     this.$set(this.value, 'send_resolved', this.value.send_resolved || false);
     this.$set(this.value, 'require_tls', this.value.require_tls || false);
 
@@ -66,6 +68,6 @@ export default {
         <LabeledInput v-model="value.auth_password" :mode="mode" label="Password" type="password" autocomplete="password" />
       </div>
     </div>
-    <TLS v-model="value.http_config" class="mb-20" :mode="mode" />
+    <!-- <TLS v-model="value.http_config" class="mb-20" :mode="mode" /> -->
   </div>
 </template>
