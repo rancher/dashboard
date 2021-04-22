e2afd39518500eaedc60b41a20754b55e97e4957
Neil MacDougall <neil_macdougall@hotmail.com>
2021-04-22 14:35:07 +0100

    WIP

---
+++ pages/manager/rkeTemplates.vue
@@ -0,0 +1,13 @@
+<script>
+import EmberPage from '@/components/EmberPage';
+
+export default {
+  components: {
+    EmberPage
+  },
+};
+</script>
+
+<template>
+  <EmberPage src="/g/rke-templates/index" :fixed="true" />
+</template>
