539f564b9a1b0d40029ddc72d7af6a167caa16ee
Neil MacDougall <macdougall.neil@gmail.com>
2021-04-22 13:54:46 +0100

    Add support for Release Notes and Getting Started pages

---
+++ content/docs/en-us/getting-started.md
@@ -0,0 +1,6 @@
+---
+title: Getting Started
+---
+# Gettting Started
+
+Welcome to Rancher Dashboard.
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -40,15 +40,13 @@ export default {
     const setting = this.$store.getters['management/byId'](MANAGEMENT.SETTING, 'server-version');
     const fullVersion = setting?.value || 'unknown';
 
+    this.fullVersion = fullVersion;
     this.seenWhatsNewAlready = compare(lastSeenNew, fullVersion) >= 0 && !!lastSeenNew;
-
-    // Hide the release notes message - maybe use a close box instead?
-    await this.$store.dispatch('prefs/set', { key: SEEN_WHATS_NEW, value: fullVersion });
   },
 
   data() {
     return {
-      HIDE_HOME_PAGE_CARDS, clusters: [], seenWhatsNewAlready: false
+      HIDE_HOME_PAGE_CARDS, clusters: [], seenWhatsNewAlready: false, fullVersion: ''
     };
   },
 
@@ -187,7 +185,12 @@ export default {
     },
 
     showWhatsNew() {
-      this.$router.push({ name: 'release-notes' });
+      // Update the value, so that the message goes away
+      const setting = this.$store.getters['management/byId'](MANAGEMENT.SETTING, 'server-version');
+      const fullVersion = setting?.value || 'unknown';
+      this.$store.dispatch('prefs/set', { key: SEEN_WHATS_NEW, value: fullVersion });
+
+      this.$router.push({name: 'docs-doc', params: {doc: 'release-notes'}});
     },
   }
 };
@@ -224,14 +227,15 @@ export default {
           <SimpleBox
             id="migration"
             class="panel"
-            :title="t('landing.migration.title')"
+            :title="t('landing.gettingStarted.title')"
             :pref="HIDE_HOME_PAGE_CARDS"
             pref-key="migrationTip"
           >
-            {{ t('landing.migration.body') }}
+            {{ t('landing.gettingStarted.body') }}
             <br />
-            <!-- Unhide this when docs are added -->
-            <a class="hide pull-right" href="#">{{ t('landing.learnMore') }}</a>
+            <nuxt-link :to="{name: 'docs-doc', params: {doc: 'getting-started'}}" class="pull-right">
+              {{ t('landing.learnMore') }}
+            </nuxt-link>
           </SimpleBox>
           <div class="row panel">
             <div class="col span-12">
