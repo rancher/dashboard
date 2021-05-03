acc3c8804c33e0c3755f4078a356123979b06b70
lvuch <laurenvuchetich@gmail.com>
2021-05-03 13:56:04 -0700

    rearrange and tidy

--- pages/auth/setup.vue
+++ pages/auth/setup.vue
@@ -3,12 +3,12 @@ import { randomStr } from '@/utils/string';
 import LabeledInput from '@/components/form/LabeledInput';
 import CopyToClipboard from '@/components/CopyToClipboard';
 import AsyncButton from '@/components/AsyncButton';
-import BrandImage from '@/components/BrandImage';
 import { SETUP } from '@/config/query-params';
 import { NORMAN, MANAGEMENT } from '@/config/types';
 import { findBy } from '@/utils/array';
 import Checkbox from '@/components/form/Checkbox';
 import { getVendor, getProduct } from '@/config/private-label';
+import RadioGroup from '@/components/form/RadioGroup';
 import { allHash } from '@/utils/promise';
 
 export default {
@@ -27,7 +27,7 @@ export default {
   },
 
   components: {
-    AsyncButton, CopyCode, LabeledInput, CopyToClipboard, Checkbox, BrandImage
+    AsyncButton, LabeledInput, CopyToClipboard, Checkbox, RadioGroup
   },
 
   async asyncData({ route, req, store }) {
@@ -206,112 +206,49 @@ export default {
 <template>
   <div class="setup">
     <div class="row">
-      <div class="col span-6 pr-40 pl-40">
+      <div class="col span-6">
         <h1 class="text-center">
           {{ t('setup.welcome', {product}) }}
         </h1>
 
         <template v-if="step===1">
-          <p class="text-center mb-20 mt-20">
+          <p class="text-center mb-40 mt-20">
             <t k="setup.setPassword" :raw="true" />
           </p>
 
           <!-- For password managers... -->
           <input type="hidden" name="username" autocomplete="username" :value="username" />
-
-          <div class="row">
-            <div class="col span-8">
-              <div class="mb-20">
-                <LabeledInput
-                  ref="password"
-                  v-model.trim="password"
-                  :type="useRandom ? 'text' : 'password'"
-                  :disabled="useRandom"
-                  label-key="setup.newPassword"
-                >
-                  <template v-if="useRandom" #suffix>
-                    <div class="addon" style="padding: 0 0 0 12px;">
-                      <CopyToClipboard :text="password" class="btn-sm" />
-                    </div>
-                  </template>
-                </LabeledInput>
-              </div>
-              <LabeledInput
-                v-show="!useRandom"
-                v-model.trim="confirm"
-                autocomplete="new-password"
-                type="password"
-                label-key="setup.confirmPassword"
-              />
-            </div>
-            <div class="col">
-              <RadioGroup v-model="useRandom" name="password-mode" :options="[{label: t('setup.useRandom'), value: true}, {label: t('setup.useManual'), value: false}]" />
-            </div>
+          <div class="mb-20">
+            <RadioGroup v-model="useRandom" name="password-mode" :options="[{label: t('setup.useRandom'), value: true}, {label: t('setup.useManual'), value: false}]" />
           </div>
-
-          <div class="checkbox mt-20">
-            <<<<<<< HEAD
-            <Checkbox v-model="telemetry" label="Allow collection of anonymous statistics to help us improve Rio" type="checkbox" />
-            <v-popover placement="right">
-              <i class="icon icon-info" />
-              <span slot="popover">
-                Rancher Labs would like to collect a bit of anonymized information<br />
-                about the configuration of your installation to help make Rio better.<br /><br />
-                Your data will not be shared with anyone else, and no information about<br />
-                what specific resources or endpoints you are deploying is included.<br />
-                Once enabled you can view exactly what data will be sent at <code>/v1-telemetry</code>.<br /><br />
-                <a href="https://rancher.com/docs/rancher/v2.x/en/faq/telemetry/" target="_blank">More Info</a>
-              </span>
-            </v-popover>
+          <div class="mb-20">
+            <LabeledInput
+              ref="password"
+              v-model.trim="password"
+              :type="useRandom ? 'text' : 'password'"
+              :disabled="useRandom"
+              label-key="setup.newPassword"
+            >
+              <template v-if="useRandom" #suffix>
+                <div class="addon" style="padding: 0 0 0 12px;">
+                  <CopyToClipboard :text="password" class="btn-sm" />
+                </div>
+              </template>
+            </LabeledInput>
           </div>
+          <LabeledInput
+            v-show="!useRandom"
+            v-model.trim="confirm"
+            autocomplete="new-password"
+            type="password"
+            label-key="setup.confirmPassword"
+          />
 
-          <div class="mt-20">
-            <button v-if="!firstLogin" type="button" class="btn bg-default mr-20" @click="skipPassword">
-              Skip
-            </button>
-            <AsyncButton key="passwordSubmit" type="submit" mode="continue" :disabled="passwordSubmitDisabled" @click="finishPassword" />
-          </div>
-        </template>
-      </div>
-      <div class="col span-6">
-        <BrandImage file-name="setup-step-one.svg" />
-      </div>
-    </div>
-    </form>
+          <hr class="mt-40 mb-40 " />
 
-    <form v-if="step === 2">
-      <div class="row">
-        <div class="col span-6">
-          <h1 class="mb-20">
-            GitHub Integration
-          </h1>
-          <p class="text-muted mb-40">
-            Rio can optionally integrate with GitHub to read resources from
-            repositories and automatically update them when code is pushed.
-          </p>
-          <div class="mt-20">
-            <div>
-              <label class="radio">
-                <input
-                  v-model="kind"
-                  type="radio"
-                  value="public"
-                > Use public GitHub.com
-              </label>
-            </div>
-            <div>
-              <label class="radio">
-                <input
-                  v-model="kind"
-                  type="radio"
-                  value="enterprise"
-                > Use a private GitHub Enterprise installation
-              </label>
-            </div>
-            =======
+          <div class="checkbox">
             <Checkbox v-model="telemetry" :label="t('setup.telemetry.label')" type="checkbox" />
             <i v-tooltip="{content:t('setup.telemetry.tip', {}, true), delay: {hide:500}, autoHide: false}" class="icon icon-info" />
-            >>>>>>> initial setup flow
           </div>
           <div class="checkbox">
             <Checkbox v-model="eula" label-key="setup.eula" type="checkbox" />
@@ -320,11 +257,7 @@ export default {
           <div class="text-center mt-20">
             <AsyncButton key="passwordSubmit" type="submit" mode="continue" :disabled="passwordSubmitDisabled" @click="finishPassword" />
           </div>
-        </div>
-      </div>
-    </form>
-  </div>
-</template>
+        </template>
 
         <template v-else>
           <p>
@@ -339,54 +272,11 @@ export default {
             </button>
             <AsyncButton type="submit" mode="edit" @click="setServerUrl" />
           </div>
-<<<<<<< HEAD
-        </div>
-        <div class="col span-6">
-          <BrandImage file-name="setup-step-one.svg" />
-        </div>
-      </div>
-    </form>
-
-    <form v-if="step === 3">
-      <div class="row">
-        <div class="col span-6">
-          <h1>GitHub Integration, Part Deux</h1>
-          <p class="text-muted mb-20 mt-20">
-            Who should be able to login?
-          </p>
-          <div>
-            <label v-if="me" class="principal">
-              <input type="checkbox" checked disabled />
-              <img :src="me.avatarSrc" width="40" height="40" />
-              <span class="login">
-                {{ me.loginName }}
-              </span>
-            </label>
-
-            <label v-for="org in orgs" :key="org.id" class="principal">
-              <input type="checkbox" :checked="(githubConfig.allowedPrincipalIds || []).includes(org.id)" :value="org.id" @click="togglePrincipal" />
-              <img :src="org.avatarSrc" width="40" height="40" />
-              <span class="login">
-                Members of <b>{{ org.loginName }}</b>
-              </span>
-            </label>
-          </div>
-          <div class="mt-20">
-            <AsyncButton key="setAuthorized" type="submit" mode="done" @click="setAuthorized" />
-          </div>
-        </div>
-        <div class="col span-6">
-          <BrandImage file-name="setup-step-one.svg" />
-        </div>
-      </div>
-    </form>
-=======
         </template>
       </div>
 
       <div class="col span-6 landscape" />
     </div>
->>>>>>> initial setup flow
   </div>
 </template>
 
@@ -413,6 +303,10 @@ export default {
       align-items: center;
       & .checkbox {
         margin: auto
+      }
+
+      .span-6 {
+        padding: 0 60px;
       }
     }
 
