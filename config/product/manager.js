e35fc2e121c27083666ef88c07a711f568e0b83d
Neil MacDougall <neil_macdougall@hotmail.com>
2021-05-05 11:10:04 +0100

    Refactor for reuse in other places

--- config/product/manager.js
+++ config/product/manager.js
@@ -86,13 +86,7 @@ export function init(store) {
   basicType([
     'rke-templates',
     'rke-drivers',
-  ], 'RKE');
-
-  basicType([
-    'global-dns-entries',
-    'global-dns-providers',
-    'catalogs',
-  ], 'Legacy Configuration');
+  ], 'RKE Configuration');
 
   // TODO: RKE
   virtualType({
