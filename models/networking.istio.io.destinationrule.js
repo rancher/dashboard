0e10f6d1071e5419c8129c1362f538c412c84ed2
Neill Somerville <neill.somerville@gmail.com>
2021-07-08 03:21:19 -0700

    spec building

--- models/networking.istio.io.destinationrule.js
+++ models/networking.istio.io.destinationrule.js
@@ -6,8 +6,58 @@ export default {
     return () => {
       Vue.set(
         this, 'spec', {
+          host:          '',
           subsets:       [],
-          trafficPolicy: {}
+          trafficPolicy: {
+
+            loadBalancer: {
+              // simple: 'ROUND_ROBIN',
+              // consistentHash:
+              // localityLbSetting:
+            },
+            connectionPool: {
+              tcp:  {
+                // maxConnections:
+                // connectTimeout:
+                // tcpKeepalive:
+              },
+              http: {
+                // http1MaxPendingRequests:
+                // http2MaxRequests:
+                // maxRequestsPerConnection:
+                // maxRetries:
+                // idleTimeout:
+                // h2UpgradePolicy:
+                // useClientProtocol:
+
+              },
+            },
+            outlierDetection: {
+              // consecutiveGatewayErrors:
+              // consecutive5xxErrors:
+              // interval:
+              // baseEjectionTime:
+              // maxEjectionPercent:
+              // minHealthPercent:
+            },
+
+            tls: {
+              // mode:
+              // clientCertificate:
+              // privateKey:
+              // caCertificates:
+              // credentialName:
+              // subjectAltNames:
+              // sni:
+            },
+            // portLevelSettings:{
+            // - port: '',
+            //     number: 80
+            //   loadBalancer: '',
+            //     simple: LEAST_CONN
+            // }
+
+          }
         });
     };
   },
