68ca892c6e97f2c5ddfe8c6a72e203783e3ebfda
Neil MacDougall <nmacdougall@suse.com>
2021-07-13 14:41:00 +0100

    Fix yaml diff in helm install

--- pages/c/_cluster/apps/charts/install.vue
+++ pages/c/_cluster/apps/charts/install.vue
@@ -541,6 +541,7 @@ export default {
         this.showValuesComponent = false;
         this.showQuestions = false;
 
+        this.updateValue(this.valuesYaml);
         this.showDiff = true;
         break;
       }
@@ -577,6 +578,9 @@ export default {
   },
 
   methods: {
+    updateValue(value) {
+      this.$refs.yaml.updateValue(value);
+    },
 
     async loadValuesComponent() {
       // TODO: Remove RELEASE_NAME. This is only in until the component annotation is added to the OPA Gatekeeper chart
