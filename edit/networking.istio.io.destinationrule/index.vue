f9cbf9eb96cd280897b71023113bcc0b5df2dec7
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2021-07-16 08:52:13 -0700

    account for undefined nested props

--- edit/networking.istio.io.destinationrule/index.vue
+++ edit/networking.istio.io.destinationrule/index.vue
@@ -7,6 +7,7 @@ import Tab from '@/components/Tabbed/Tab';
 import Tabbed from '@/components/Tabbed';
 import KeyValue from '@/components/form/KeyValue';
 import ArrayListGrouped from '@/components/form/ArrayListGrouped';
+import { get, set } from '@/utils/object';
 import LoadBalancer from './LoadBalancer';
 
 export default {
@@ -23,15 +24,16 @@ export default {
 
   mixins: [CreateEditView],
 
-  data() {
-    if (this.mode === 'edit') {
-      const specWithDefaults = Object.assign({}, this.value.spec, this.value.defaultSpec);
-
-      this.$set(this.value, 'spec', specWithDefaults);
+  created() {
+    if (!get(this.value, 'spec.trafficPolicy.loadBalancer')) {
+      set(this.value, 'spec.trafficPolicy.loadBalancer', { simple: 'ROUND_ROBIN' });
     }
-
-    return {};
   },
+
+  methods: {
+    get,
+    set
+  }
 };
 </script>
 
@@ -109,59 +111,64 @@ export default {
           <div class="row">
             <div class="col span-4 mb-10">
               <LabeledInput
-
-                v-model="value.spec.trafficPolicy.connectionPool.http.http1MaxPendingRequests"
+                :value="get(value,'spec.trafficPolicy.connectionPool.http.http1MaxPendingRequests')"
                 :label="t('istio.destinationRule.connectionPool.http1MaxPendingRequests.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.connectionPool.http1MaxPendingRequests.help')"
                 :placeholder="t('istio.destinationRule.connectionPool.http1MaxPendingRequests.placeholder')"
+                @input="set(value,'spec.trafficPolicy.connectionPool.http.http1MaxPendingRequests', $event)"
               />
             </div>
             <div class="col span-4 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.connectionPool.http.http2MaxRequests"
+                :value="get(value, 'spec.trafficPolicy.connectionPool.http.http2MaxRequests')"
                 :label="t('istio.destinationRule.connectionPool.http2MaxRequests.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.connectionPool.http2MaxRequests.help')"
                 :placeholder="t('istio.destinationRule.connectionPool.http2MaxRequests.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.connectionPool.http.http2MaxRequests', $event)"
               />
             </div>
             <div class="col span-4 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.connectionPool.http.maxRequestsPerConnection"
+                :value="get(value, 'spec.trafficPolicy.connectionPool.http.maxRequestsPerConnection')"
                 :label="t('istio.destinationRule.connectionPool.maxRequestsPerConnection.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.connectionPool.maxRequestsPerConnection.help')"
                 :placeholder="t('istio.destinationRule.connectionPool.maxRequestsPerConnection.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.connectionPool.http.maxRequestsPerConnection', $event)"
               />
             </div>
           </div>
           <div class="row">
             <div class="col span-4 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.connectionPool.http.maxRetries"
+                :value="get(value, 'spec.trafficPolicy.connectionPool.http.maxRetries')"
                 :label="t('istio.destinationRule.connectionPool.maxRetries.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.connectionPool.maxRetries.help')"
                 :placeholder="t('istio.destinationRule.connectionPool.maxRetries.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.connectionPool.http.maxRetries', $event)"
               />
             </div>
             <div class="col span-4 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.connectionPool.tcp.duration"
+                :value="get(value, 'spec.trafficPolicy.connectionPool.tcp.duration')"
                 :label="t('istio.destinationRule.connectionPool.connectTimeout.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.connectionPool.connectTimeout.help')"
                 :placeholder="t('istio.destinationRule.connectionPool.connectTimeout.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.connectionPool.tcp.duration', $event)"
               />
             </div>
             <div class="col span-4 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.connectionPool.tcp.maxConnections"
+                :value="get(value,'spec.trafficPolicy.connectionPool.tcp.maxConnections')"
                 :label="t('istio.destinationRule.connectionPool.maxConnections.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.connectionPool.maxConnections.help')"
                 :placeholder="t('istio.destinationRule.connectionPool.maxConnections.placeholder')"
+                @input="set(value,'spec.trafficPolicy.connectionPool.tcp.maxConnections', $event)"
               />
             </div>
           </div>
@@ -174,40 +181,44 @@ export default {
           <div class="row">
             <div class="col span-6 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.outlierDetection.baseEjectionTime"
+                :value="get(value, 'spec.trafficPolicy.outlierDetection.baseEjectionTime')"
                 :label="t('istio.destinationRule.outlierDetection.baseEjectionTime.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.outlierDetection.baseEjectionTime.help')"
                 :placeholder="t('istio.destinationRule.outlierDetection.baseEjectionTime.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.outlierDetection.baseEjectionTime', $event)"
               />
             </div>
             <div class="col span-6 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.outlierDetection.consecutiveErrors"
+                :value="get(value, 'spec.trafficPolicy.outlierDetection.consecutiveErrors')"
                 :label="t('istio.destinationRule.outlierDetection.consecutiveErrors.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.outlierDetection.consecutiveErrors.help')"
                 :placeholder="t('istio.destinationRule.outlierDetection.consecutiveErrors.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.outlierDetection.consecutiveErrors', $event)"
               />
             </div>
           </div>
           <div class="row">
             <div class="col span-6 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.outlierDetection.interval"
+                :value="get(value,'spec.trafficPolicy.outlierDetection.interval')"
                 :label="t('istio.destinationRule.outlierDetection.interval.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.outlierDetection.interval.help')"
                 :placeholder="t('istio.destinationRule.outlierDetection.interval.placeholder')"
+                @input="set(value,'spec.trafficPolicy.outlierDetection.interval', $event)"
               />
             </div>
             <div class="col span-6 mb-10">
               <LabeledInput
-                v-model="value.spec.trafficPolicy.outlierDetection.maxEjectionPercent"
+                :value="get(value, 'spec.trafficPolicy.outlierDetection.maxEjectionPercent')"
                 :label="t('istio.destinationRule.outlierDetection.maxEjectionPercent.label')"
                 :mode="mode"
                 :tooltip="t('istio.destinationRule.outlierDetection.maxEjectionPercent.help')"
                 :placeholder="t('istio.destinationRule.outlierDetection.maxEjectionPercent.placeholder')"
+                @input="set(value, 'spec.trafficPolicy.outlierDetection.maxEjectionPercent', $event)"
               />
             </div>
           </div>
