b3fe9435c44d41c4c4e266b25d542b2208cf53cf
n313893254 <n313893254@126.com>
2021-07-22 15:42:02 +0800

    HARVESTER:fix:mutil cluster

--- store/index.js
+++ store/index.js
@@ -84,10 +84,6 @@ export const getters = {
     return getters['management/byId'](MANAGEMENT.CLUSTER, state.clusterId);
   },
 
-  currentVirtualCluster(state, getters) {
-    return getters['management/byId'](MANAGEMENT.CLUSTER, state.virtualClusterId);
-  },
-
   currentProduct(state, getters) {
     const active = getters['type-map/activeProducts'];
 
