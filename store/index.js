4395c72e714088e13759bbfa22a59a624501a64c
n313893254 <n313893254@126.com>
2021-07-23 18:31:50 +0800

    HARVESTER:fix:can not load virtual

--- store/index.js
+++ store/index.js
@@ -652,14 +652,14 @@ export const actions = {
 
   async loadVirtual({
     state, commit, dispatch, getters
-  }, id) {
+  }, { id, product, oldProduct }) {
     const isMultiCluster = getters['isMultiCluster'];
 
     if (isMultiCluster && id === 'local') {
       return;
     }
 
-    if ( state.clusterId && state.clusterId === id ) {
+    if ( state.clusterId && state.clusterId === id && oldProduct === VIRTUAL) {
       // Do nothing, we're already connected/connecting to this cluster
       return;
     }
@@ -717,18 +717,10 @@ export const actions = {
 
   async resetStore({
     state, commit, dispatch, getters
-  }, {
-    id, store, oldProduct, product
-  }) {
+  }, { id, store }) {
     if ( state.clusterId && id && store) {
       await dispatch(`${ store }/unsubscribe`);
       commit(`${ store }/reset`);
-    }
-
-    if (oldProduct !== product) {
-      if (product === VIRTUAL || oldProduct === VIRTUAL) {
-        commit('setCluster', null);
-      }
     }
   },
 
