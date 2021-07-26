fa99c8c28ca922a94061e366bbd4536f0703d6bd
Cody Jackson <cody@rancher.com>
2021-07-26 12:33:56 -0700

    Adding tables

---
+++ components/formatter/Feedback.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  components: { },
+  props:      {
+    row: {
+      type:     Object,
+      required: true,
+    },
+    options: {
+      type:    [Object, String],
+      default: null,
+    },
+  },
+};
+</script>
+
+<template>
+  <span>
+    <button class="btn role-primary error">
+      <i class="icon icon-error" />
+    </button>
+  </span>
+</template>
