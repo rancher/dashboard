6ff31647b72d7192462e99486653a4dc0729d507
Phillip Rak <rak.phillip@gmail.com>
2021-08-16 12:17:31 -0700

    Check for upper and lower case in uri

--- utils/validators/setting.js
+++ utils/validators/setting.js
@@ -5,7 +5,7 @@ const httpsKeys = [
 export function isHttps(value, getters, errors, validatorArgs, displayKey) {
   const key = validatorArgs[0];
 
-  if (httpsKeys.includes(key) && !value.startsWith('https://')) {
+  if (httpsKeys.includes(key) && !value.toLowerCase().startsWith('https://')) {
     errors.push(getters['i18n/t']('validation.setting.serverUrl.https'));
   }
 
