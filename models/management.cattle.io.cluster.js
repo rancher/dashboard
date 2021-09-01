7cff6ce2da65a390674dd34bcbd503b627b7ec1d
Neill Somerville <neill.somerville@gmail.com>
2021-08-31 19:49:36 -0700

    translation update

--- models/management.cattle.io.cluster.js
+++ models/management.cattle.io.cluster.js
@@ -143,7 +143,6 @@ export default {
 
   kubernetesVersion() {
     return this.kubernetesVersionRaw || this.$rootGetters['i18n/t']('generic.provisioning');
-    // return this.kubernetesVersionRaw || this.$rootGetters['i18n/t']('generic.provisioning');
   },
 
   kubernetesVersionBase() {
