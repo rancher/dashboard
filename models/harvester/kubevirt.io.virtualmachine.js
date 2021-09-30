dad33843de690fbc98f3353433c3445814d4760e
n313893254 <n313893254@126.com>
2021-09-30 15:45:30 +0800

    HARVESTER:Fix VM is off it shows VMI status

--- models/harvester/kubevirt.io.virtualmachine.js
+++ models/harvester/kubevirt.io.virtualmachine.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { load } from 'js-yaml';
-import { colorForState } from '@/plugins/steve/resource-instance';
+import ResourceInstance, { colorForState } from '@/plugins/steve/resource-instance';
 import { POD, NODE, HCI } from '@/config/types';
 import { findBy } from '@/utils/array';
 import { get } from '@/utils/object';
@@ -724,5 +724,13 @@ export default {
     const memory = this?.spec?.template?.spec?.domain?.resources?.requests?.memory || 0;
 
     return parseInt(memory);
-  }
+  },
+
+  stateDescription() {
+    if (this.isOff) {
+      return '';
+    }
+
+    return ResourceInstance.stateDescription.call(this);
+  },
 };
