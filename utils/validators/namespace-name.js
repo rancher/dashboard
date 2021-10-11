fad8e6ef9b61948fa4e4fcd20853e6bac9a34bf1
Catherine Luse <catherine.luse@gmail.com>
2021-10-11 01:00:43 -0700

    Add namespace name regex validator

---
+++ utils/validators/namespace-name.js
@@ -0,0 +1,7 @@
+export function namespaceName(pathValue, getters, errors, validatorArgs, displayKey) {
+  if ((pathValue || '').match(/[a-z0-9]([-a-z0-9]*[a-z0-9])?/)) {
+    errors.push(getters['i18n/t']('validation.namespaceName'));
+  }
+
+  return errors;
+}
