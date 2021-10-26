83cde6db7e97becb5cfd90a1998da861ea84fa14
n313893254 <n313893254@126.com>
2021-10-26 16:34:39 +0800

    HARVESTER: Change disk state 'active' to 'healty'

--- components/formatter/HarvesterDiskState.vue
+++ components/formatter/HarvesterDiskState.vue
@@ -2,7 +2,7 @@
 import BadgeState from '@/components/BadgeState';
 import { stateDisplay } from '@/plugins/steve/resource-class';
 
-const ACTIVE = 'active';
+const ACTIVE = 'healthy';
 const WARNING = 'warning';
 
 export default {
