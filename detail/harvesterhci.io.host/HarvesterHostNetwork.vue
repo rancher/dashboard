23f353d102680815384166a32cb9f2fcefbf17e5
n313893254 <n313893254@126.com>
2021-10-27 18:18:12 +0800

    HARVESTER:Add image uploading warning banner

---
+++ detail/harvesterhci.io.host/HarvesterHostNetwork.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'HarvesterHostNetwork',
+
+  components: {},
+
+  props: {
+    value: {
+      type:     Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {}
+};
+</script>
+
+<template>
+  <h3>Network</h3>
+</template>
+
+<style lang="scss" scoped>
+#host-instances {
+  ::v-deep thead th {
+    vertical-align: middle;
+  }
+
+  ::v-deep .state {
+    display: flex;
+
+    .vmstate {
+      margin-right: 6px;
+    }
+  }
+}
+</style>
