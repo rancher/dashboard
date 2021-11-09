838ae59cb7c8568072d4742d79290d7bed850084
Richard Cox <richard.cox@suse.com>
2021-11-09 17:28:17 +0000

    Fix issue when entering Epinio section from outside of product

--- products/epinio/pages/c/_cluster/index.vue
+++ products/epinio/pages/c/_cluster/index.vue
@@ -1,9 +1,89 @@
 <script lang="ts">
-export default { };
+import Vue from 'vue';
+
+import Loading from '@/components/Loading.vue';
+import ResourceTable from '@/components/ResourceTable.vue';
+import { EPINIO_MGMT_STORE, EPINIO_TYPES } from '@/products/epinio/types';
+import Resource from '@/plugins/core-store/resource-class';
+
+interface Data {
+  clusters: Resource[],
+}
+
+// Data, Methods, Computed, Props
+export default Vue.extend<Data, any, any, any>({
+  components: { Loading, ResourceTable },
+
+  layout: 'plain',
+
+  async fetch() {
+    this.clusters = await this.$store.dispatch(`${ EPINIO_MGMT_STORE }/findAll`, { type: EPINIO_TYPES.INSTANCE });
+
+    // if (this.clusters.length === 1) {
+    //   // TODO: RC Epinio cluster --> Epinio
+    //   this.$router.replace(createEpinioRoute(`c-cluster-resource`, { cluster: this.clusters[0].id, resource: EPINIO_TYPES.APP }));
+    // }
+  },
+
+  data() {
+    return {
+      clusters:       [],
+      clustersSchema: this.$store.getters[`${ EPINIO_MGMT_STORE }/schemaFor`](EPINIO_TYPES.INSTANCE)
+    };
+  },
+
+  computed: {
+    cluster(): string {
+      return this.$route.params.cluster;
+    },
+
+    product(): string {
+      return this.$route.params.product;
+    }
+  },
+
+});
 </script>
 
 <template>
-  <div>
-    Cluster Dashboard
+  <Loading v-if="$fetchState.pending" mode="main" />
+  <div v-else-if="clusters.length === 0" class="root">
+    <h2>{{ t('epinio.instances.none.header') }}</h2>
+    <p>{{ t('epinio.instances.none.description') }}</p>
+  </div>
+  <div v-else class="root">
+    <div class="epinios-table">
+      <h2>{{ t('epinio.instances.header') }}</h2>
+
+      <ResourceTable
+        :rows="clusters"
+        :schema="clustersSchema"
+        :table-actions="false"
+      >
+        <template #cell:pick="{row}">
+          <n-link class="btn btn-sm role-primary" :to="{name: 'ext-epinio-c-cluster-applications', params: {cluster: row.id}}">
+            {{ t('landing.clusters.explore') }}
+          </n-link>
+        </template>
+      </ResourceTable>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+div.root {
+  align-items: center;
+  padding-top: 50px;
+  display: flex;
+
+  .epinios-table {
+    & > h4 {
+      padding-top: 50px;
+      padding-bottom : 20px;
+    }
+    min-width: 60%;
+  }
+}
+
+</style>
--- products/epinio/pages/index.vue
+++ products/epinio/pages/index.vue
@@ -1,92 +1,18 @@
 <script lang="ts">
-import Vue from 'vue';
+import { createEpinioRoute } from '@/products/epinio/utils/custom-routing';
+import { BLANK_CLUSTER } from '@/store';
+export default {
+  middleware({ redirect }: any ) {
+    return redirect(
+      createEpinioRoute(`c-cluster`, { cluster: BLANK_CLUSTER }),
+    );
+  }
+};
 
-import Loading from '@/components/Loading.vue';
-import ResourceTable from '@/components/ResourceTable.vue';
-import { EPINIO_MGMT_STORE, EPINIO_TYPES } from '@/products/epinio/types';
-import Resource from '@/plugins/core-store/resource-class';
-
-interface Data {
-  clusters: Resource[],
-}
-
-// Data, Methods, Computed, Props
-export default Vue.extend<Data, any, any, any>({
-  components: { Loading, ResourceTable },
-
-  layout: 'plain',
-
-  async fetch() {
-    this.clusters = await this.$store.dispatch(`${ EPINIO_MGMT_STORE }/findAll`, { type: EPINIO_TYPES.INSTANCE });
-
-    if (this.clusters.length === 1 ) {
-      // TODO: RC
-      // this.$router.replace({
-      //   name:   createEpinioRoute(`c-cluster`),
-      //   params: { cluster: this.clusters[0].id }
-      // });
-    }
-  },
-
-  data() {
-    return {
-      clusters:       [],
-      clustersSchema: this.$store.getters[`${ EPINIO_MGMT_STORE }/schemaFor`](EPINIO_TYPES.INSTANCE)
-    };
-  },
-
-  computed: {
-    cluster(): string {
-      return this.$route.params.cluster;
-    },
-
-    product(): string {
-      return this.$route.params.product;
-    }
-  },
-
-});
 </script>
 
 <template>
-  <Loading v-if="$fetchState.pending" mode="main" />
-  <div v-else-if="clusters.length === 0" class="root">
-    <h2>{{ t('epinio.instances.none.header') }}</h2>
-    <p>{{ t('epinio.instances.none.description') }}</p>
-  </div>
-  <div v-else class="root">
-    <div class="epinios-table">
-      <h2>{{ t('epinio.instances.header') }}</h2>
-
-      <ResourceTable
-        :rows="clusters"
-        :schema="clustersSchema"
-        :table-actions="false"
-      >
-        <template #cell:pick="{row}">
-          <n-link class="btn btn-sm role-primary" :to="{name: 'ext-epinio-c-cluster-applications', params: {cluster: row.id}}">
-            {{ t('landing.clusters.explore') }}
-          </n-link>
-        </template>
-      </ResourceTable>
-    </div>
+  <div>
+    Cluster Dashboard
   </div>
 </template>
-
-<style lang="scss" scoped>
-
-div.root {
-  align-items: center;
-  padding-top: 50px;
-  display: flex;
-
-  .epinios-table {
-    & > h4 {
-      padding-top: 50px;
-      padding-bottom : 20px;
-    }
-    min-width: 60%;
-  }
-}
-
-</style>
