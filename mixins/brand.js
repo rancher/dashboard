ee0e4d1ff06b6eb1e895c1311533c9c6331c9a72
Shavindra <shavindra@users.noreply.github.com>
2021-11-24 09:45:10 +0100

    Remove CSS rule paser.

--- mixins/brand.js
+++ mixins/brand.js
@@ -2,7 +2,7 @@ import { MANAGEMENT } from '@/config/types';
 import { getVendor } from '@/config/private-label';
 import { SETTING } from '@/config/settings';
 import { findBy } from '@/utils/array';
-import { createCssVars, getCssThemeVarsFromDocument } from '@/utils/color';
+import { createCssVars } from '@/utils/color';
 
 export default {
   async fetch() {
@@ -21,7 +21,7 @@ export default {
     },
 
     themeVars() {
-      return getCssThemeVarsFromDocument(`theme-${ this.$store.getters['prefs/theme'] }`);
+      return { headerTextColor: window.getComputedStyle(document.body).getPropertyValue('--header-text-color') };
     },
 
     color() {
--- utils/color.js
+++ utils/color.js
@@ -13,8 +13,6 @@ Primary color classes from _light.scss
 
 */
 
-import { listCssCustomProperties } from '~/utils/css-rule-parser';
-
 const Color = require('color');
 
 export function createCssVars(color, theme = 'light', name = 'primary') {
@@ -76,31 +74,4 @@ function contrastColor(color, contrastOptions = LIGHT_CONTRAST_COLORS) {
   }
 
   return out;
-}
-
-/**
- * Filters the imported CSS vars from the document based on the theme.
- *
- * @param {('theme-light' | 'theme-dark' )} themeName - Theme name to filter by
- *
- * @description - listCssCustomProperties will retrieve all the custom css vars associated with that theme.
- *                It will then translate the kebab-cased vars to camelCase.
- *                e.g. --header-text-color to headerTextColor.
- *
- * @example
- * // returns { primaryText: #000000 }
- * getScssThemeVars('thme-light')
- *
- * @example
- * // returns { primaryText: #B6B6C2 }
- * getScssThemeVars('theme-dark')
- */
-export function getCssThemeVarsFromDocument(themeName = 'theme-light') {
-  return listCssCustomProperties(themeName).reduce((cssMap, curr) => {
-    const key = curr[0].replace('--', '').replace(/-./g, x => x[1].toUpperCase());
-
-    cssMap[key] = curr[1];
-
-    return cssMap;
-  }, {});
 }
