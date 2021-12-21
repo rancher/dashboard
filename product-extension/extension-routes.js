2061ccf9ab008c02bc6455e8bfc2a132a2693791
Richard Cox <richard.cox@suse.com>
2021-12-21 10:31:07 +0000

    Disable Epinio

--- product-extension/extension-routes.js
+++ product-extension/extension-routes.js
@@ -1,9 +1,12 @@
-import epinioRoutes from '../products/epinio/routes';
+// *************************************************************
+// import epinioRoutes from '../products/epinio/routes';
 
-// Note - This content needs to be in a separate file than the core `extensions` one ... otherwise all file references lose modules... which unwraps quickly
-const extensions = [
-  epinioRoutes
-];
+// // Note - This content needs to be in a separate file than the core `extensions` one ... otherwise all file references lose modules... which unwraps quickly
+// const extensions = [
+//   epinioRoutes
+// ];
+// *************************************************************
+const extensions = [];
 
 export default {
 
--- product-extension/extensions.js
+++ product-extension/extensions.js
@@ -1,9 +1,14 @@
-import { EPINIO_PRODUCT_NAME } from '@/products/epinio/types';
-import epinio from '@/products/epinio/index';
+// *************************************************************
+// Uncomment to enable EPINIO
+// import { EPINIO_PRODUCT_NAME } from '@/products/epinio/types';
+// import epinio from '@/products/epinio/index';
 
-// Note - Extensions need to be directly imported here rather than importing this file and applying themselves to this file
-// (extensions are required by `store/index`, which executes after anything imported here)
-const extensions = { [EPINIO_PRODUCT_NAME]: epinio };
+// //Note - Extensions need to be directly imported here rather than importing this file and applying themselves to this file
+// // (extensions are required by `store/index`, which executes after anything imported here)
+// const extensions = { [EPINIO_PRODUCT_NAME]: epinio };
+// *************************************************************
+
+const extensions = {};
 
 export default {
 
