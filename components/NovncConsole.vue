67f3f233bcabbf721ee6e970bbd09776509d09fe
n313893254 <n313893254@126.com>
2022-01-12 17:48:12 +0800

    HARVESTER: Add vnc error message

--- components/NovncConsole.vue
+++ components/NovncConsole.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <div ref="view"></div>
+    <div v-if="connected && disconnected">
+      <main class="error">
+        <div class="text-center">
+          <BrandImage file-name="error-desert-landscape.svg" width="900" height="300" />
+          <h1>
+            Warning
+          </h1>
+          <h2 class="text-secondary mt-20">
+            {{ t('vncConsole.error.message') }}
+          </h2>
+        </div>
+      </main>
+    </div>
+    <div
+      ref="view"
+    />
   </div>
 </template>
 
 <script>
 import RFB from '@novnc/novnc/core/rfb';
+import BrandImage from '@/components/BrandImage';
 
 export default {
   props: {
@@ -15,13 +31,28 @@ export default {
     }
   },
 
+  components: { BrandImage },
+
   data() {
-    return { rfb: null };
+    return {
+      rfb:          null,
+      connected:    false,
+      disconnected: false,
+    };
   },
 
   mounted() {
     this.$nextTick(() => {
-      this.rfb = new RFB(this.$refs.view, this.url);
+      const rfb = new RFB(this.$refs.view, this.url);
+
+      rfb.addEventListener('connect', () => {
+        this.connected = true;
+      });
+      rfb.addEventListener('disconnect', (e) => {
+        this.disconnected = true;
+      });
+
+      this.rfb = rfb;
     });
   },
 
@@ -40,3 +71,23 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .error {
+    overflow: hidden;
+
+    .row {
+      align-items: center;
+    }
+
+    h1 {
+      font-size: 5rem;
+    }
+
+    .desert-landscape {
+      img {
+        max-width: 100%;
+      }
+    }
+  }
+</style>
