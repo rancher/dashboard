9f1315365ef49b515baf2405925699dd832cb850
Josh Soref <jsoref@users.noreply.github.com>
2022-01-26 17:07:34 -0500

    spelling: toggle
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -182,7 +182,7 @@ export default {
       :width="340"
       @opened="dialogOpened"
     >
-      <Card :show-highlight-border="false" class="toogle-support">
+      <Card :show-highlight-border="false" class="toggle-support">
         <template #title>
           {{ isRemoveDialog? t('support.subscription.removeTitle') : t('support.subscription.addTitle') }}
         </template>
@@ -216,7 +216,7 @@ export default {
   grid-template-columns: 70% 30%;
 }
 
-.toogle-support {
+.toggle-support {
     height: 100%;
 
     &.card-container {
