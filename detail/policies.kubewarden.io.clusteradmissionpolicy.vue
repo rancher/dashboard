5a023ffbc69709c1a5d9b8f3931ec0480f60db52
Jordon Leach <jordonleach@gmail.com>
2022-02-04 10:02:56 -0500

    add development url for cluster metrics testing

--- detail/policies.kubewarden.io.clusteradmissionpolicy.vue
+++ detail/policies.kubewarden.io.clusteradmissionpolicy.vue
@@ -1,7 +1,7 @@
 <script>
 import { mapGetters } from 'vuex';
 import { monitoringStatus } from '@/utils/monitoring';
-import { allDashboardsExist } from '@/utils/grafana';
+// import { allDashboardsExist } from '@/utils/grafana';
 import CreateEditView from '@/mixins/create-edit-view';
 import DashboardMetrics from '@/components/DashboardMetrics';
 import ResourceTabs from '@/components/form/ResourceTabs';
@@ -26,11 +26,15 @@ export default {
     },
   },
 
-  async fetch() {
-    this.CLUSTER_METRICS_DETAIL_URL = `/k8s/clusters/${ this.currentCluster.id }/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/kubewarden`;
-    this.CLUSTER_METRICS_SUMMARY_URL = `/k8s/clusters/${ this.currentCluster.id }/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/kubewarden`;
+  fetch() {
+    // If developing locally you need to remove /k8s/clusters/${ this.currentCluster } from the url
+    this.CLUSTER_METRICS_DETAIL_URL = `/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
+    this.CLUSTER_METRICS_SUMMARY_URL = `/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
 
-    this.showPolicyMetrics = await allDashboardsExist(this.$store.dispatch, this.currentCluster.id, [this.CLUSTER_METRICS_DETAIL_URL, this.CLUSTER_METRICS_SUMMARY_URL]);
+    // this.CLUSTER_METRICS_DETAIL_URL = `/k8s/clusters/${ this.currentCluster }/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
+    // this.CLUSTER_METRICS_SUMMARY_URL = `/k8s/clusters/${ this.currentCluster }/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
+
+    // this.showPolicyMetrics = await allDashboardsExist(this.$store.dispatch, this.currentCluster.id, [this.CLUSTER_METRICS_DETAIL_URL, this.CLUSTER_METRICS_SUMMARY_URL]);
   },
 
   data() {
