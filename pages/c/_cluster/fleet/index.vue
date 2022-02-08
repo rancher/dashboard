9c74a843b59ca5982b05f1598274a5a15868c5d2
Alexandre Alves <aalves@AlexandresMBP2.lan>
2022-02-08 17:55:58 +0000

    poc rendering resource table of git repos

--- pages/c/_cluster/fleet/index.vue
+++ pages/c/_cluster/fleet/index.vue
@@ -1,10 +1,21 @@
 <script>
 import Loading from '@/components/Loading';
 import { FLEET } from '@/config/types';
+import ResourceTable from '@/components/ResourceTable';
+import Link from '@/components/formatter/Link';
+import Shortened from '@/components/formatter/Shortened';
+import {
+  AGE,
+  STATE,
+  NAME,
+  FLEET_SUMMARY
+} from '@/config/table-headers';
 
 export default {
   name:       'ListGitRepo',
-  components: { Loading },
+  components: {
+    Loading, ResourceTable, Link, Shortened
+  },
 
   async fetch() {
     const store = this.$store;
@@ -15,6 +26,7 @@ export default {
 
   data() {
     return {
+      schema:          {},
       gitRepos:        null,
       fleetWorkspaces: null
     };
@@ -36,6 +48,41 @@ export default {
       });
 
       return finalData;
+    },
+    headers() {
+      const out = [
+        STATE,
+        NAME,
+        {
+          name:     'repo',
+          labelKey: 'tableHeaders.repo',
+          value:    'repoDisplay',
+          sort:     'repoDisplay',
+          search:   ['spec.repo', 'status.commit'],
+        },
+        {
+          name:     'target',
+          labelKey: 'tableHeaders.target',
+          value:    'targetInfo.modeDisplay',
+          sort:     ['targetInfo.modeDisplay', 'targetInfo.cluster', 'targetInfo.clusterGroup'],
+        },
+        {
+          name:      'clustersReady',
+          labelKey:  'tableHeaders.clustersReady',
+          value:     'status.readyClusters',
+          sort:      'status.readyClusters',
+          search:    false,
+        },
+        FLEET_SUMMARY,
+        AGE
+      ];
+
+      return out;
+    },
+  },
+  methods: {
+    parseTargetMode(row) {
+      return row.targetInfo?.mode === 'clusterGroup' ? this.t('fleet.gitRepo.warningTooltip.clusterGroup') : this.t('fleet.gitRepo.warningTooltip.cluster');
     }
   },
 };
@@ -59,9 +106,62 @@ export default {
             <li v-for="(repo, ri) in ws" :key="ri">
               {{ repo.nameDisplay }}
             </li>
+            <ResourceTable
+              v-bind="$attrs"
+              :schema="schema"
+              :headers="headers"
+              :rows="ws"
+              key-field="_key"
+              v-on="$listeners"
+            >
+              <template #cell:repo="{row}">
+                <Link
+                  :row="row"
+                  :value="row.spec.repo"
+                  label-key="repoDisplay"
+                  before-icon-key="repoIcon"
+                  url-key="spec.repo"
+                />
+                <template v-if="row.commitDisplay">
+                  <div class="text-muted">
+                    <Shortened long-value-key="status.commit" :row="row" :value="row.commitDisplay" />
+                  </div>
+                </template>
+              </template>
+
+              <template #cell:clustersReady="{row}">
+                <span v-if="!row.clusterInfo" class="text-muted">&mdash;</span>
+                <span v-else-if="row.clusterInfo.unready" class="text-warning">{{ row.clusterInfo.ready }}/{{ row.clusterInfo.total }}</span>
+                <span v-else class="cluster-count-info">
+                  {{ row.clusterInfo.ready }}/{{ row.clusterInfo.total }}
+                  <i
+                    v-if="!row.clusterInfo.total"
+                    v-tooltip.bottom="parseTargetMode(row)"
+                    class="icon icon-warning"
+                  />
+                </span>
+              </template>
+
+              <template #cell:target="{row}">
+                {{ row.targetInfo.modeDisplay }}
+              </template>
+            </ResourceTable>
           </ul>
         </li>
       </ul>
     </section>
   </div>
 </template>
+
+<style lang="scss">
+.cluster-count-info {
+  display: flex;
+  align-items: center;
+
+  i {
+    margin-left: 5px;
+    font-size: 22px;
+    color: var(--warning);
+  }
+}
+</style>
