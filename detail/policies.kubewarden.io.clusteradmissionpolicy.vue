776f5d5f0fb44767749228447bf20a82ac7857d4
Jordon Leach <jordonleach@gmail.com>
2022-02-09 14:18:41 -0500

    fix metrics tab conditional

--- detail/policies.kubewarden.io.clusteradmissionpolicy.vue
+++ detail/policies.kubewarden.io.clusteradmissionpolicy.vue
@@ -1,14 +1,22 @@
 <script>
 import { mapGetters } from 'vuex';
+import { _CREATE } from '@/config/query-params';
 import { monitoringStatus } from '@/utils/monitoring';
-// import { allDashboardsExist } from '@/utils/grafana';
+import { dashboardExists } from '@/utils/grafana';
 import CreateEditView from '@/mixins/create-edit-view';
 import DashboardMetrics from '@/components/DashboardMetrics';
 import ResourceTabs from '@/components/form/ResourceTabs';
 import ResourceTable from '@/components/ResourceTable';
 import Tab from '@/components/Tabbed/Tab';
 
+// The uid in the proxy `r3Pw-107z` is setup in the configmap for the kubewarden dashboard
+// It's the generic uid from the json here: https://grafana.com/grafana/dashboards/15314
+const POLICY_METRICS_DETAIL_URL = `/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
+const JAEGER_PROXY = `/api/v1/namespaces/jaeger/services/http:all-in-one-query:16686/proxy/api/traces?operation=/api/traces&service=jaeger-query`;
+
 export default {
+  name: 'ClusterAdmissionPolicy',
+
   components: {
     DashboardMetrics, ResourceTabs, ResourceTable, Tab
   },
@@ -17,29 +25,23 @@ export default {
 
   props: {
     mode: {
-      default: 'create',
       type:    String,
+      default: _CREATE,
     },
     resource: {
       type:    String,
       default: null
     },
     value: {
-      required: true,
       type:     Object,
+      required: true,
     },
   },
 
   async fetch() {
     const inStore = this.$store.getters['currentStore'](this.resource);
 
-    // If on a local server with metrics or developing locally you need to remove /k8s/clusters/${ this.currentCluster } from the url
-    this.POLICY_METRICS_DETAIL_URL = `/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
-
-    // For downstream clusters
-    // this.POLICY_METRICS_DETAIL_URL = `/k8s/clusters/${ this.currentCluster }/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/r3Pw-1O7z/kubewarden?orgId=1&refresh=30s`;
-    // this.showPolicyMetrics = await allDashboardsExist(this.$store.dispatch, this.currentCluster.id, [this.POLICY_METRICS_DETAIL_URL, this.POLICY_METRICS_SUMMARY_URL]);
-    const JAEGER_PROXY = `/k8s/clusters/${ this.currentCluster.id }/api/v1/namespaces/jaeger/services/http:all-in-one-query:16686/proxy/api/traces?operation=/api/traces&service=jaeger-query`;
+    this.showPolicyMetrics = this.monitoringStatus.installed && await dashboardExists(this.$store, this.currentCluster.id, POLICY_METRICS_DETAIL_URL);
 
     this.traces = await this.$store.dispatch(`${ inStore }/request`, { url: JAEGER_PROXY });
   },
@@ -47,10 +49,10 @@ export default {
   data() {
     return {
       showPolicyMetrics:          false,
-      showPolicyTracing:          true,
+      showPolicyTracing:          false,
       traces:                     null,
-      POLICY_METRICS_DETAIL_URL:  null,
-      // POLICY_METRICS_SUMMARY_URL: ''
+      POLICY_METRICS_DETAIL_URL,
+      JAEGER_PROXY
     };
   },
 
