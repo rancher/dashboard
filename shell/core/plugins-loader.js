4f62020dedefd81d3ed0255f31b89dff71143578
Neil MacDougall <nmacdougall@suse.com>
2022-02-10 13:38:05 +0000

    Fix coer.js version

--- shell/core/plugins-loader.js
+++ shell/core/plugins-loader.js
@@ -19,11 +19,13 @@ export default function({
     dynamicLoader.default($plugin);
   }
 
-  // The libraries we build have Vue externalised, so we need to expose Vue as a global for
-  // them to pick up - see: https://cli.vuejs.org/guide/build-targets.html#library
-  window.Vue = Vue;
+  if (process.client) {
+    // The libraries we build have Vue externalised, so we need to expose Vue as a global for
+    // them to pick up - see: https://cli.vuejs.org/guide/build-targets.html#library
+    window.Vue = Vue;
 
-  // Global libraries - allows us to externalise these to reduce package bundle size
-  window.__jszip = JSZip;
-  window.__jsyaml = jsyaml;
+    // Global libraries - allows us to externalise these to reduce package bundle size
+    window.__jszip = JSZip;
+    window.__jsyaml = jsyaml;
+  }
 }
