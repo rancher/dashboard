check-spelling/rancher-dashboard

spelling: embedded
Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

utils/grafana.js
@@ -42,8 +42,8 @@ export async function dashboardExists(dispatch, clusterId, embedUrl, store = 'cl
   }
 }
 
-export async function allDashboardsExist(dispatch, clusterId, embededUrls, store = 'cluster') {
-  const existPromises = embededUrls.map(url => dashboardExists(dispatch, clusterId, url, store));
+export async function allDashboardsExist(dispatch, clusterId, embeddedUrls, store = 'cluster') {
+  const existPromises = embeddedUrls.map(url => dashboardExists(dispatch, clusterId, url, store));
 
   return (await Promise.all(existPromises)).every(exists => exists);
 }

rancher/dashboard

utils/grafana.js
@@ -42,8 +42,8 @@ export async function dashboardExists(store, clusterId, embedUrl, storeName = 'c
   }
 }
 
-export async function allDashboardsExist(store, clusterId, embededUrls, storeName = 'cluster') {
-  const existPromises = embededUrls.map(url => dashboardExists(store, clusterId, url, storeName));
+export async function allDashboardsExist(store, clusterId, embeddedUrls, storeName = 'cluster') {
+  const existPromises = embeddedUrls.map(url => dashboardExists(store, clusterId, url, storeName));
 
   return (await Promise.all(existPromises)).every(exists => exists);
 }