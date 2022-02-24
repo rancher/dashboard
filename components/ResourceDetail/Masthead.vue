de4b0a8ab5017c492d8e47a1187f5adb587f70d5
Neil MacDougall <nmacdougall@suse.com>
2022-02-24 16:44:14 +0000

    Fix buf where extra dash is shown in masthead

--- components/ResourceDetail/Masthead.vue
+++ components/ResourceDetail/Masthead.vue
@@ -100,10 +100,6 @@ export default {
       return null;
     },
 
-    resourceSubtypeString() {
-      return this.resourceSubtype?.length ? `${ this.resourceSubtype } -` : this.resourceSubtype;
-    },
-
     namespaceLocation() {
       if (!this.isNamespace) {
         return {
@@ -329,7 +325,7 @@ export default {
             </nuxt-link>
             <span v-else>{{ parent.displayName }}:</span>
             <span v-if="value.detailPageHeaderActionOverride && value.detailPageHeaderActionOverride(realMode)">{{ value.detailPageHeaderActionOverride(realMode) }}</span>
-            <t v-else :k="'resourceDetail.header.' + realMode" :subtype="resourceSubtypeString" :name="value.nameDisplay" />
+            <t v-else :k="'resourceDetail.header.' + realMode" :subtype="resourceSubtype" :name="value.nameDisplay" />
             <BadgeState v-if="!isCreate && parent.showState" class="masthead-state" :value="value" />
           </h1>
         </div>
