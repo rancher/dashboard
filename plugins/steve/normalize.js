2720947fcde1848e9b73f1c6c97d637ace56bd92
Richard Cox <richard.cox@suse.com>
2022-03-04 16:55:48 +0000

    Retry machine pool scaling when there are conflicts - When scaling a machine pool there's lots of status updates to the cluster - This means hitting a 409 conflict error when trying to scale an already scaling pool is likely - There already exists a way to check for actual conflicts and retrying the save, though this was only for the CreateEditView component - That process has now become generic and is used when scaling pools - Alos fix bug where scale errors weren't caught - fixes #5029

--- plugins/steve/normalize.js
+++ plugins/steve/normalize.js
@@ -1,5 +1,6 @@
 import { SCHEMA } from '@/config/types';
 import isObject from 'lodash/isObject';
+import { applyChangeset, changeset, changesetConflicts } from '~/utils/object';
 
 export const KEY_FIELD_FOR = {
   [SCHEMA]:  '_id',
@@ -113,4 +114,34 @@ function dropCattleKeys(obj) {
       delete obj[key];
     }
   });
+}
+
+// Detect and resolve conflicts from a 409 response.
+// If they are resolved, return a false-y value
+// Else they can't be resolved, return an array of errors to show to the user.
+export function handleConflict(initialValueJSON, value, liveValue, rootGetters) {
+  const orig = cleanForDiff(initialValueJSON);
+  const user = cleanForDiff(value.toJSON());
+  const cur = cleanForDiff(liveValue.toJSON());
+
+  const bgChange = changeset(orig, cur);
+  const userChange = changeset(orig, user);
+  const actualConflicts = changesetConflicts(bgChange, userChange);
+
+  console.log('Background Change', bgChange); // eslint-disable-line no-console
+  console.log('User Change', userChange); // eslint-disable-line no-console
+  console.log('Conflicts', actualConflicts); // eslint-disable-line no-console
+
+  value.metadata.resourceVersion = liveValue.metadata.resourceVersion;
+  applyChangeset(value, bgChange);
+
+  if ( actualConflicts.length ) {
+    // Stop the save and let the user inspect and continue editing
+    const out = [rootGetters['i18n/t']('validation.conflict', { fields: actualConflicts.join(', '), fieldCount: actualConflicts.length })];
+
+    return out;
+  } else {
+    // The save can continue
+    return false;
+  }
 }
