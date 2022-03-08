e21c6a5f804a3de916d9d0f7d9dd77c88fd4bc2c
Shavindra <shavindra@users.noreply.github.com>
2022-03-08 08:42:32 +0100

    Fix PR comments.

--- detail/fleet.cattle.io.gitrepo.vue
+++ detail/fleet.cattle.io.gitrepo.vue
@@ -103,7 +103,7 @@ export default {
     >
       {{ t('fleet.fleetSummary.noClustersGitRepo') }}
     </Banner>
-    <ResourceTabs v-model="value" mode="view" class="mt-20">
+    <ResourceTabs v-model="value" mode="view" class="mt-20" :need-related="false">
       <Tab label="Bundles" name="bundles" :weight="30">
         <FleetBundles :value="value" />
       </Tab>
