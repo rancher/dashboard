da50bbcbf9347635078dd5c42ea27f1d684b882d
Phillip Rak <rak.phillip@gmail.com>
2022-03-16 16:54:04 -0700

    Improve exported types
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/types/index.d.ts
+++ pkg/rancher-components/types/index.d.ts
@@ -1,5 +1,7 @@
 import { VueConstructor } from 'vue'
 
+export const Badge: VueConstructor
 export const Banner: VueConstructor
+export const Card: VueConstructor
 export const Checkbox: VueConstructor
 export const LabeledTooltip: VueConstructor
