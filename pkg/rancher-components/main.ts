66885f97c7f185bda156ffafea69a45a93fdf1c7
Phillip Rak <rak.phillip@gmail.com>
2022-03-16 15:42:15 -0700

    Rename main.js to main.ts
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ pkg/rancher-components/main.ts
@@ -0,0 +1,19 @@
+// import * as components from './index'
+
+// const ComponentLibrary = {
+//   install (Vue) {
+//     for (const componentName in components) {
+//       const component = components[componentName]
+
+//       Vue.component(component.name, component)
+//     }
+//   }
+// }
+
+// export default ComponentLibrary
+
+// if (typeof window !== 'undefined' && window.Vue) {
+//   window.Vue.use(ComponentLibrary)
+// }
+
+export * from './index'
