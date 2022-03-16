d26a673ea98cf1761a6cc7739a00d204fc37c1bf
Phillip Rak <rak.phillip@gmail.com>
2022-03-16 16:53:26 -0700

    Add labeled tooltip component
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/types/index.d.ts
+++ pkg/rancher-components/types/index.d.ts
@@ -2,3 +2,4 @@ import { VueConstructor } from 'vue'
 
 export const Banner: VueConstructor
 export const Checkbox: VueConstructor
+export const LabeledTooltip: VueConstructor
--- pkg/rancher-components/vue.config.js
+++ pkg/rancher-components/vue.config.js
@@ -3,7 +3,10 @@ module.exports = {
     extract: false,
     loaderOptions: {
       sass: {
-        prependData: '@import "/assets/styles/_mixins.scss";'
+        prependData: `
+          @import "/assets/styles/_mixins.scss"; 
+          @import "/assets/styles/_variables.scss";
+        `
       }
     }
   }
