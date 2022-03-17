aec1c4360729643cbf35856011dd550c5836e34e
Phillip Rak <rak.phillip@gmail.com>
2022-03-17 09:48:41 -0700

    Add Labeled Input
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/types/index.d.ts
+++ pkg/rancher-components/types/index.d.ts
@@ -4,6 +4,7 @@ export const Badge: VueConstructor
 export const Banner: VueConstructor
 export const Card: VueConstructor
 export const Checkbox: VueConstructor
+export const LabeledInput: VueConstructor
 export const LabeledTooltip: VueConstructor
 export const RadioButton: VueConstructor
 export const RadioGroup: VueConstructor
