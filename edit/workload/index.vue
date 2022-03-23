8e46ceabc81c0ea9ae16bb2e2f2ebdad86c11486
Giuseppe Leo <giuseppe.leo@suse.com>
2022-03-23 11:34:00 +0100

    Make CRU footer actions stick to the bottom (#5340)
    * Make CRU footer actions stick to the bottom
    
    * Fill content of CRU views to always move actions to the bottom
    
    * Remove ellipsis for change workspace label in clusters actions

--- edit/workload/index.vue
+++ edit/workload/index.vue
@@ -821,7 +821,7 @@ export default {
 <template>
   <Loading v-if="$fetchState.pending" />
 
-  <form v-else>
+  <form v-else class="filled-height">
     <CruResource
       :validation-passed="true"
       :selected-subtype="type"
