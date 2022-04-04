a0031d9abd4b20bdaf1d26534b4dc2f030ebf019
Catherine Luse <catherine.luse@gmail.com>
2022-04-04 10:48:45 -0700

    NeuVector link uses cattle-neuvector-system namespace

--- pages/c/_cluster/neuvector/index.vue
+++ pages/c/_cluster/neuvector/index.vue
@@ -28,7 +28,7 @@ export default {
         iconSrc:     this.neuvectorImgSrc,
         label:       'neuvector.overview.linkedList.neuvector.label',
         description:   'neuvector.overview.linkedList.neuvector.description',
-        link:        `/k8s/clusters/${ this.currentCluster.id }/api/v1/namespaces/neuvector-system/services/https:neuvector-service-webui:8443/proxy`
+        link:        `/k8s/clusters/${ this.currentCluster.id }/api/v1/namespaces/cattle-neuvector-system/services/https:neuvector-service-webui:8443/proxy`
       },
     ];
   }
