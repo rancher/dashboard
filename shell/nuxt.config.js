fa352dcc51a3b774c6dd0325082d11a2a49aea4d
Richard Cox <richard.cox@suse.com>
2022-04-06 13:18:25 +0100

    Revert "Remove frontmatter-markdown-loader reference from nuxt-config"
    This reverts commit 29ef6f2f7be26b3692ab1eeb41bc80c26f56a14f.

--- shell/nuxt.config.js
+++ shell/nuxt.config.js
@@ -442,6 +442,17 @@ export default function(dir, _appConfig) {
             skipEmptyLines: true
           },
         });
+
+        // Prevent warning in log with the md files in the content folder
+        config.module.rules.push({
+          test:    /\.md$/,
+          use:  [
+            {
+              loader:  'frontmatter-markdown-loader',
+              options: { mode: ['body'] }
+            }
+          ]
+        });
       },
 
       // extractCSS:   true,
