b073ee3e0bdac8187c96b1ae205abf32d1361486
cnotv <giuseppe.leo@suse.com>
2022-04-12 15:23:50 +0200

    Add tests for ArrayListGrouped

--- components/form/ArrayListGrouped.vue
+++ components/form/ArrayListGrouped.vue
@@ -75,7 +75,7 @@ export default {
         v-if="canRemoveRow(scope.row, scope.i)"
         type="button"
         class="btn role-link close btn-sm"
-        :data-testid="`remove-row-${scope.i}`"
+        :data-testid="`remove-item-${scope.i}`"
         @click="scope.remove"
       >
         <i class="icon icon-2x icon-x" />
