rancher/dashboard

commit d62f333ee695beb476356df8e257aaed321d0997

Migrating RBAC to use the norman API

config/types.js
@@ -9,13 +9,24 @@ export const STEVE = { PREFERENCE: 'userpreference' };
 // Old APIs via Norman
 // Base: /v3
 export const NORMAN = {
-  AUTH_CONFIG: 'authconfig',
-  CLUSTER:                       'cluster',
-  PRINCIPAL:   'principal',
-  USER:        'user',
-  TOKEN:       'token',
-  GROUP:       'group',
-  SPOOFED:     { GROUP_PRINCIPAL: 'group.principal' }
+  AUTH_CONFIG:                   'authconfig',
+  ETCD_BACKUP:                   'etcdbackup',
+  CLUSTER_TOKEN:                 'clusterregistrationtoken',
+  CLUSTER_ROLE_TEMPLATE_BINDING: 'clusterRoleTemplateBinding',
+  CLOUD_CREDENTIAL:              'cloudcredential',
+  GLOBAL_ROLE:                   'globalRole',
+  GLOBAL_ROLE_BINDING:           'globalRoleBinding',
+
+  GROUP:                         'group',
+  // Note - This allows access to node resources, not schema's or custom components (both are accessed via 'type' which clashes with kube node)
+  NODE:                          'node',
+  PRINCIPAL:                     'principal',
+  PROJECT:                       'project',
+  PROJECT_ROLE_TEMPLATE_BINDING: 'projectRoleTemplateBinding',
+  SPOOFED:                       { GROUP_PRINCIPAL: 'group.principal' },
+  ROLE_TEMPLATE:                 'roleTemplate',
+  TOKEN:                         'token',
+  USER:                          'user',
 };
 
 // Public (via Norman)

components/GlobalRoleBindings.vue
@@ -70,7 +70,7 @@ export default {
         this.sortedRoles.custom = this.sortedRoles.custom.sort(sort);
 
         if (!this.isCreate) {
-          this.globalRoleBindings = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.GLOBAL_ROLE_BINDING });
+          this.globalRoleBindings = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.GLOBAL_ROLE_BINDING, opt: { force: true } });
         }
 
         this.update();
@@ -234,6 +234,10 @@ export default {
       // Ensure roles are added before removed (in case by removing one user is unable to add another)
       await this.saveAddedRoles(userId);
       await this.saveRemovedRoles();
+      await this.$store.dispatch('management/findAll', {
+        type: MANAGEMENT.GLOBAL_ROLE_BINDING,
+        opt:  { force: true }
+      }, { force: true });
     },
     confirmUserCanLogIn() {
       const allRolesRules = [];
@@ -313,7 +317,7 @@ export default {
                   :value-when-true="role.id"
                   :disabled="!!assignOnlyRoles[role.id]"
                   :label="role.nameDisplay"
-                  :description="role.description"
+                  :description="role.descriptionDisplay"
                   :mode="mode"
                   @input="checkboxChanged"
                 >

components/auth/RoleDetailEdit.vue
@@ -334,6 +334,8 @@ export default {
         v-model="value"
         :namespaced="isNamespaced"
         :mode="mode"
+        name-key="displayName"
+        description-key="description"
         label="Name"
       />
       <div v-if="isRancherType" class="row">

models/management.cattle.io.globalrole.js
@@ -1,5 +1,5 @@
 import { DESCRIPTION } from '@/config/labels-annotations';
-import { SCHEMA } from '@/config/types';
+import { SCHEMA, NORMAN } from '@/config/types';
 import { CATTLE_API_GROUP, SUBTYPE_MAPPING } from '@/models/management.cattle.io.roletemplate';
 import { uniq } from '@/utils/array';
 import Vue from 'vue';
@@ -33,8 +33,8 @@ export default {
     return this.$rootGetters['i18n/withFallback'](`rbac.globalRoles.role.${ this.id }.label`, this.displayName || this.metadata?.name || this.id);
   },
 
-  description() {
-    return this.metadata?.annotations?.[DESCRIPTION] || this.$rootGetters['i18n/withFallback'](`rbac.globalRoles.role.${ this.id }.description`, this.t(`rbac.globalRoles.unknownRole.description`));
+  descriptionDisplay() {
+    return this.description || this.metadata?.annotations?.[DESCRIPTION] || this.$rootGetters['i18n/withFallback'](`rbac.globalRoles.role.${ this.id }.description`, this.t(`rbac.globalRoles.unknownRole.description`));
   },
 
   isSpecial() {
@@ -76,6 +76,45 @@ export default {
 
   doneOverride() {
     return this.listLocation;
-  }
+  },
 
+  parentLocationOverride() {
+    return this.listLocation;
+  },
+
+  basicNorman() {
+    if (this.id) {
+      return this.$dispatch(`rancher/find`, { id: this.id, type: NORMAN.GLOBAL_ROLE }, { root: true });
+    }
+
+    return this.$dispatch(`rancher/create`, { type: NORMAN.GLOBAL_ROLE, name: this.displayName }, { root: true });
+  },
+
+  async norman() {
+    const norman = await this.basicNorman;
+
+    norman.rules = this.rules;
+    norman.newUserDefault = this.newUserDefault;
+    norman.id = this.id;
+    norman.name = this.displayName;
+    norman.description = this.description;
+
+    return norman;
+  },
+
+  save() {
+    return async() => {
+      const norman = await this.norman;
+
+      return norman.save();
+    };
+  },
+
+  remove() {
+    return async() => {
+      const norman = await this.norman;
+
+      await norman.remove();
+    };
+  }
 };

models/management.cattle.io.globalrolebinding.js
@@ -23,4 +23,38 @@ export default {
     return this.user;
   },
 
+  basicNorman() {
+    if (this.id) {
+      return this.$dispatch(`rancher/find`, { id: this.id, type: NORMAN.GLOBAL_ROLE_BINDING }, { root: true });
+    }
+
+    return this.$dispatch(`rancher/create`, { type: NORMAN.GLOBAL_ROLE_BINDING, name: this.displayName }, { root: true });
+  },
+
+  async norman() {
+    const norman = await this.basicNorman;
+
+    norman.globalRoleId = this.globalRoleName;
+    norman.userId = this.userName;
+    norman.groupPrincipalId = this.groupPrincipalName;
+
+    return norman;
+  },
+
+  save() {
+    return async() => {
+      const norman = await this.norman;
+
+      return norman.save();
+    };
+  },
+
+  remove() {
+    return async() => {
+      const norman = await this.norman;
+
+      await norman.remove();
+    };
+  }
+
 };

models/management.cattle.io.roletemplate.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { get } from '@/utils/object';
 import { DESCRIPTION } from '@/config/labels-annotations';
+import { NORMAN } from '@/config/types';
 import Role from './rbac.authorization.k8s.io.role';
 
 export const CATTLE_API_GROUP = '.cattle.io';
@@ -300,6 +301,47 @@ export default {
 
   doneOverride() {
     return this.listLocation;
-  }
+  },
 
+  parentLocationOverride() {
+    return this.listLocation;
+  },
+
+  basicNorman() {
+    if (this.id) {
+      return this.$dispatch(`rancher/find`, { id: this.id, type: NORMAN.ROLE_TEMPLATE }, { root: true });
+    }
+
+    return this.$dispatch(`rancher/create`, { type: NORMAN.ROLE_TEMPLATE, name: this.displayName }, { root: true });
+  },
+
+  async norman() {
+    const norman = await this.basicNorman;
+
+    norman.rules = this.rules;
+    norman.locked = this.locked;
+    norman.clusterCreatorDefault = this.clusterCreatorDefault || false;
+    norman.projectCreatorDefault = this.projectCreatorDefault || false;
+    norman.context = this.context;
+    norman.description = this.description;
+    norman.roleTemplateIds = this.roleTemplateNames;
+
+    return norman;
+  },
+
+  save() {
+    return async() => {
+      const norman = await this.norman;
+
+      return norman.save();
+    };
+  },
+
+  remove() {
+    return async() => {
+      const norman = await this.norman;
+
+      await norman.remove();
+    };
+  }
 };

commit 27570624fa46c27ef680d174765e3d1e908bdbb8

Fix issue where norman cluster type was unknown - Regression from #5632 - Caused by d62f333ee

config/types.js
@@ -12,6 +12,7 @@ export const NORMAN = {
   APP:                           'app',
   AUTH_CONFIG:                   'authconfig',
   ETCD_BACKUP:                   'etcdbackup',
+  CLUSTER:                       'cluster',
   CLUSTER_TOKEN:                 'clusterregistrationtoken',
   CLUSTER_ROLE_TEMPLATE_BINDING: 'clusterRoleTemplateBinding',
   CLOUD_CREDENTIAL:              'cloudcredential',