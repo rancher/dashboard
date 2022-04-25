rancher/dashboard

commit ebc9927756a558fbcb242fd43cc5db796c834dbf

Add new forms for receivers using AlertmanagerConfig spec

components/SortableTable/index.vue
@@ -271,6 +271,16 @@ export default {
       type:    Function,
       default: null,
     },
+
+    /**
+     * Allows you to link to a custom detail page for data that
+     * doesn't have a class model. For example, a receiver configuration
+     * block within an AlertmanagerConfig resource.
+     */
+    getCustomDetailLink: {
+      type:    Function,
+      default: null
+    }
   },
 
   data() {
@@ -703,7 +713,7 @@ export default {
   <div ref="container">
     <div :class="{'titled': $slots.title && $slots.title.length}" class="sortable-table-header">
       <slot name="title" />
-      <div v-if="showHeaderRow" class="fixed-header-actions">
+      <div v-if="showHeaderRow" class="fixed-header-actions" :class="{'fixed-header-actions': true, button: true}">
         <div :class="bulkActionsClass" class="bulk">
           <slot name="header-left">
             <template v-if="tableActions">
@@ -762,7 +772,7 @@ export default {
           <slot name="header-middle" />
         </div>
 
-        <div v-if="search || ($slots['header-right'] && $slots['header-right'].length)" class="search">
+        <div v-if="search || ($slots['header-right'] && $slots['header-right'].length)" class="search row">
           <slot name="header-right" />
           <input
             v-if="search"
@@ -772,6 +782,7 @@ export default {
             class="input-sm search-box"
             :placeholder="t('sortableTable.search')"
           >
+          <slot name="header-button" />
         </div>
       </div>
     </div>
@@ -889,13 +900,14 @@ export default {
                           :row-key="row.key"
                         />
                         <component
-                          :is="col.component"
-                          v-else-if="col.component"
-                          :value="col.value"
-                          :row="row.row"
-                          :col="col.col"
-                          v-bind="col.col.formatterOpts"
-                          :row-key="row.key"
+                          :is="col.formatter"
+                          v-if="col.formatter"
+                          :value="valueFor(row,col)"
+                          :row="row"
+                          :col="col"
+                          v-bind="col.formatterOpts"
+                          :row-key="get(row,keyField)"
+                          :get-custom-detail-link="getCustomDetailLink"
                         />
                         <component
                           :is="col.col.formatter"
@@ -1249,6 +1261,9 @@ $spacing: 10px;
     display: flex;
     align-items: center;
   }
+}
+.fixed-header-actions.button{
+  grid-template-columns: [bulk] auto [middle] min-content [search] minmax(min-content, 350px);
 }
 
 .fixed-header-actions {

components/nav/WindowManager/index.vue
@@ -111,6 +111,7 @@ export default {
       doc.addEventListener('touchstart', this.dragEnd, true);
 
       const eventY = event.screenY;
+
       const rect = boundingRect(event.target);
       const offset = eventY - rect.top;
 

edit/monitoring.coreos.com.alertmanagerconfig/index.vue
@@ -1,0 +1,172 @@
+<script>
+import CruResource from '@/components/CruResource';
+
+import CreateEditView from '@/mixins/create-edit-view';
+import Tabbed from '@/components/Tabbed';
+import Tab from '@/components/Tabbed/Tab';
+import { MONITORING } from '@/config/types';
+import Loading from '@/components/Loading';
+import NameNsDescription from '@/components/form/NameNsDescription';
+import { EDITOR_MODES } from '@/components/YamlEditor';
+import { RECEIVERS_TYPES } from '@/models/monitoring.coreos.com.receiver';
+import RouteConfig from './routeConfig';
+import ResourceTable from '@/components/ResourceTable';
+import { _VIEW, _CREATE, _EDIT, _CONFIG } from '@/config/query-params';
+
+export default {
+  components: {
+    CruResource,
+    Loading,
+    NameNsDescription,
+    ResourceTable,
+    RouteConfig,
+    Tab,
+    Tabbed,
+  },
+
+  mixins: [CreateEditView],
+
+  fetch() {},
+
+  data() {
+    this.value.applyDefaults();
+    const defaultReceiverValues = {};
+    const receiverSchema = this.$store.getters['cluster/schemaFor'](MONITORING.SPOOFED.ALERTMANAGERCONFIG_RECEIVER_SPEC);
+    const routeSchema = this.$store.getters['cluster/schemaFor'](MONITORING.SPOOFED.ALERTMANAGERCONFIG_ROUTE_SPEC);
+    const receiverOptions = this.value.spec.receivers.map(receiver => receiver.name);
+
+    return {
+      config:             _CONFIG,
+      createReceiverLink: {
+        name:   'c-cluster-monitoring-alertmanagerconfig-alertmanagerconfigid-receiver',
+        params: {
+          cluster:              this.$store.getters['clusterId'],
+          alertmanagerconfigid: this.value.id
+        },
+        query: { mode: _CREATE }
+      },
+      defaultReceiverValues,
+      editReceiverLink: {
+        name:   'c-cluster-monitoring-alertmanagerconfig-alertmanagerconfigid-receiver',
+        params: {
+          cluster:              this.$store.getters['clusterId'],
+          alertmanagerconfigid: this.value.id
+        },
+        query: { mode: _EDIT }
+      },
+      receiverTableHeaders: [
+        {
+          name:          'name',
+          labelKey:      'tableHeaders.name',
+          value:         'name',
+          sort:          ['nameSort'],
+          formatter:     'LinkDetail',
+          canBeVariable: true,
+        }
+        // Add more columns
+      ],
+      newReceiverType:      null,
+      receiverOptions,
+      receiverTypes:        RECEIVERS_TYPES,
+      routeSchema,
+      receiverSchema,
+      view:                 _VIEW,
+    };
+  },
+
+  computed: {
+
+    editorMode() {
+      if ( this.mode === _VIEW ) {
+        return EDITOR_MODES.VIEW_CODE;
+      }
+
+      return EDITOR_MODES.EDIT_CODE;
+    },
+  },
+  methods: {
+
+    translateReceiverTypes() {
+      return this.receiverTypes.map((receiverType) => {
+        return {
+          ...receiverType,
+          label: this.t(receiverType.label)
+        };
+      });
+    },
+    addNewReceiver(name) {
+      // const existingReceiversOfSelectedType = this.value.spec.receivers[this.newReceiverType] || []
+
+      // this.value.spec.receivers[this.newReceiverType] = this.value.spec.receivers;
+    },
+    getReceiverDetailLink(receiverData) {
+      return {
+        name:   'c-cluster-monitoring-alertmanagerconfig-alertmanagerconfigid-receiver',
+        params: {
+          cluster:              this.$store.getters['clusterId'],
+          alertmanagerconfigid: this.value.id
+        },
+        query: {
+          mode: _VIEW, receiverName: receiverData.name, currentView: _CONFIG
+        }
+      };
+    },
+  }
+};
+</script>
+
+<template>
+  <Loading v-if="$fetchState.pending" />
+  <CruResource
+    v-else
+    class="route"
+    :done-route="doneRoute"
+    :errors="errors"
+    :mode="mode"
+    :resource="value"
+    :subtypes="[]"
+    :cancel-event="true"
+    @error="e=>errors = e"
+    @finish="save"
+    @cancel="done"
+  >
+    <NameNsDescription v-model="value" :mode="mode" :namespaced="isNamespaced" />
+
+    <Tabbed>
+      <Tab :label="t('monitoring.route.label')" :weight="1" name="route">
+        <RouteConfig :value="value.spec.route" :mode="mode" :receiver-options="receiverOptions" />
+      </Tab>
+      <Tab :label="t('alertmanagerConfigReceiver.receivers')" :weight="2" name="receivers">
+        <ResourceTable
+          :headers="receiverTableHeaders"
+          :schema="receiverSchema"
+          :rows="value.spec.receivers"
+          :get-custom-detail-link="getReceiverDetailLink"
+          :table-actions="false"
+        >
+          <template #header-button>
+            <nuxt-link v-if="createReceiverLink && createReceiverLink.name" :to="createReceiverLink">
+              <button class="btn role-primary">
+                Create Receiver
+              </button>
+            </nuxt-link>
+          </template>
+        </ResourceTable>
+      </Tab>
+    </Tabbed>
+  </CruResource>
+</template>
+
+<style lang="scss" scoped>
+  h3 {
+    margin-top: 2em;
+  }
+  input {
+    margin-top: 1em;
+  }
+  .route {
+    &[real-mode=view] .label {
+      color: var(--input-label);
+    }
+  }
+</style>

pages/c/_cluster/monitoring/alertmanagerconfig/_alertmanagerconfigid/index.vue
@@ -1,0 +1,12 @@
+<script>
+import ResourceDetail from '@/components/ResourceDetail';
+
+export default {
+  name:       'AlertmanagerConfig',
+  components: { ResourceDetail },
+};
+</script>
+
+<template>
+  <ResourceDetail />
+</template>

pages/c/_cluster/monitoring/alertmanagerconfig/index.vue
@@ -1,0 +1,24 @@
+<script>
+
+export default {
+  name: 'AlertmanagerConfig',
+
+  data() {
+    return {};
+  },
+
+};
+</script>
+
+<template>
+  <div>AlertmanagerConfig</div>
+</template>
+
+<style lang='scss' scoped>
+.header{
+  display: flex;
+  H1{
+    flex: 1;
+  }
+}
+</style>

assets/translations/en-us.yaml
@@ -2873,6 +2873,16 @@ monitoringReceiver:
     sendResolved:
       label: Enable send resolved alerts
 
+alertmanagerConfigReceiver:
+  name: Receiver Name
+  addButton: Add Receiver
+  receivers: Receivers
+  namespaceWarning: Could not render the secret selector because no namespace was found.
+  receiverTypes: "The following receiver types can be edited in forms: Email, Slack, PagerDuty, Opsgenie and Webhook. For other receiver types, edit the AlertmanagerConfig YAML."
+  slack:
+    keyId: Key Id from Secret
+    webhookUrl: Webhook URL
+    apiUrlTooltip: The secret's key that contains the Slack webhook URL. The secret needs to be in the same namespace as the AlertmanagerConfig object and accessible by the Prometheus Operator.
 monitoringRoute:
   groups:
     label: Group By

components/ActionMenu.vue
@@ -9,15 +9,69 @@ const CALC = 'calculate';
 const SHOW = 'show';
 
 export default {
+  props: {
+    customActions: {
+      // Custom actions can be used if you need the action
+      // menu to work for something that is not a Kubernetes
+      // resource, for example, a receiver within an
+      // AlertmanagerConfig.
+
+      // This prop can also be used to avoid
+      // a dependency on Vuex. For now, this component can have
+      // its state controlled by either props OR by Vuex, but if it
+      // gets unwieldy, it could later be split into two components,
+      // one with the dependency on Vuex and one without.
+      type:    Array,
+      default: () => {
+        return [];
+      }
+    },
+    open: {
+      // Use this prop to show and hide the action menu if
+      // you want to avoid an unnecessary dependency on Vuex.
+
+      // Note: There are known issues with performance if this component
+      // is included with every row of a table, so don't do that.
+      // Instead the ActionMenu component can be included once on a page,
+      // and then if you click on a list item, that can change
+      // the menu's target so that it can open in different locations.
+      type:    Boolean,
+      default: false
+    },
+    useCustomTargetElement: {
+      // The custom target element can be a
+      // variable in the component state of a list or detail page
+      // if you don't want a dependency on Vuex.
+      // Then when an action menu button is clicked, it can emit an event
+      // that triggers the target to be set to the clicked element,
+      // so that the dropdown menu can open where the context menu
+      // was clicked.
+      // This flag tells the component to look for and use the
+      // custom target element.
+      type:    Boolean,
+      default: false
+    },
+    customTargetElement: {
+      type:    HTMLElement,
+      default: null
+    },
+    customTargetEvent: {
+      // The event details from the user's click can be used
+      // for positioning the menu on the page.
+      type:    PointerEvent,
+      default: null
+    },
+  },
+
   data() {
-    return {
-      phase: HIDDEN,
-      style: {}
-    };
+    return { phase: HIDDEN, style: {} };
   },
 
   computed: {
     ...mapGetters({
+      // Use either these Vuex getters
+      // OR the props to set the action menu state,
+      // but don't use both.
       targetElem:  'action-menu/elem',
       targetEvent: 'action-menu/event',
       shouldShow:  'action-menu/showing',
@@ -27,7 +81,13 @@ export default {
     showing() {
       return this.phase !== HIDDEN;
     },
+    menuOptions() {
+      if (this.customActions.length > 0) {
+        return this.customActions;
+      }
 
+      return this.options;
+    },
   },
 
   watch: {
@@ -48,6 +108,15 @@ export default {
       },
     },
 
+    open() {
+      // This component has a timing issue where the
+      // mounted size of the expanded menu is used to
+      // calculate where its position should be. That means
+      // it won't work if the style is a computed property,
+      // so we put a watcher here to update the style instead.
+      this.updateStyle();
+    },
+
     '$route.path'(val, old) {
       this.hide();
     }
@@ -55,23 +124,35 @@ export default {
 
   methods: {
     hide() {
-      this.$store.commit('action-menu/hide');
+      if (this.useCustomTargetElement) {
+        // If the show/hide state is controlled
+        // by props, emit an event to close the menu.
+        this.$emit('close');
+      } else {
+        // If the show/hide state is controlled
+        // by Vuex, mutate the store to close the menu.
+        this.$store.commit('action-menu/hide');
+      }
     },
 
     updateStyle() {
-      if ( this.phase === SHOW ) {
+      if ( this.phase === SHOW || this.open ) {
         const menu = $('.menu', this.$el)[0];
-        const event = this.targetEvent;
-        const elem = this.targetElem;
+        let elem;
 
-        this.style = fitOnScreen(menu, event || elem, {
+        if (this.useCustomTargetElement) {
+          elem = this.customTargetElement;
+        } else {
+          elem = this.targetElem;
+        }
+
+        this.style = fitOnScreen(menu, elem, {
           overlapX:  true,
           fudgeX:    elem ? 4 : 0,
           fudgeY:    elem ? 4 : 0,
           positionX: (elem ? AUTO : CENTER),
           positionY: AUTO,
         });
-
         this.style.visibility = 'visible';
       } else {
         this.style = {};
@@ -79,15 +160,29 @@ export default {
     },
 
     execute(action, event, args) {
-      const opts = { alt: isAlternate(event) };
-
       if (action.disabled) {
         return;
       }
 
-      this.$store.dispatch('action-menu/execute', {
-        action, args, opts
-      });
+      if (this.useCustomTargetElement) {
+        // If the state of this component is controlled
+        // by props instead of Vuex, we assume you wouldn't want
+        // the mutation to have a dependency on Vuex either.
+        // So in that case we use events to execute actions instead.
+        // If an action list item is clicked, this
+        // component emits that event, then we assume the parent
+        // component will execute the action.
+        this.$emit(action.action);
+      } else {
+        // If the state of this component is controlled
+        // by Vuex, mutate the store when an action is clicked.
+        const opts = { alt: isAlternate(event) };
+
+        this.$store.dispatch('action-menu/execute', {
+          action, args, opts
+        });
+      }
+
       this.hide();
     },
 
@@ -99,14 +194,20 @@ export default {
 </script>
 
 <template>
-  <div v-if="showing">
+  <div v-if="showing || open">
     <div class="background" @click="hide" @contextmenu.prevent></div>
     <ul class="list-unstyled menu" :style="style">
-      <li v-for="opt in options" :key="opt.action" :disabled="opt.disabled" :class="{divider: opt.divider}" @click="execute(opt, $event)">
+      <li
+        v-for="opt in menuOptions"
+        :key="opt.action"
+        :disabled="opt.disabled"
+        :class="{divider: opt.divider}"
+        @click="execute(opt, $event)"
+      >
         <i v-if="opt.icon" :class="{icon: true, [opt.icon]: true}" />
         <span v-html="opt.label" />
       </li>
-      <li v-if="!hasOptions(options)" class="no-actions">
+      <li v-if="!hasOptions(menuOptions)" class="no-actions">
         <span v-t="'sortableTable.noActions'" />
       </li>
     </ul>
@@ -124,6 +225,7 @@ export default {
     top: 0;
     left: 0;
     z-index: z-index('dropdownContent');
+    min-width: 145px;
 
     color: var(--dropdown-text);
     background-color: var(--dropdown-bg);

components/CruResource.vue
@@ -92,8 +92,10 @@ export default {
     }
   },
 
-  data() {
+  data(props) {
     const yaml = this.createResourceYaml();
+
+    console.log('done route in alertmanagerConfig ', props.doneRoute);
 
     return {
       isCancelModal: false,

commit cc8167b1fb2d9d6cf8bfb2600c89690856a1a27c

Fix duplicate cells in some tables - caused by https://github.com/rancher/dashboard/commit/ebc9927756a558fbcb242fd43cc5db796c834dbf#diff-1a2277d92947d83bba624b418c7879f3fa45d44df0bb93f741aa3ae7c4b7df43R903 - reverted seection that was changes & added new property in correct place

components/SortableTable/index.vue
@@ -900,14 +900,13 @@ export default {
                           :row-key="row.key"
                         />
                         <component
-                          :is="col.formatter"
-                          v-if="col.formatter"
-                          :value="valueFor(row,col)"
-                          :row="row"
-                          :col="col"
-                          v-bind="col.formatterOpts"
-                          :row-key="get(row,keyField)"
-                          :get-custom-detail-link="getCustomDetailLink"
+                          :is="col.component"
+                          v-else-if="col.component"
+                          :value="col.value"
+                          :row="row.row"
+                          :col="col.col"
+                          v-bind="col.col.formatterOpts"
+                          :row-key="row.key"
                         />
                         <component
                           :is="col.col.formatter"
@@ -917,6 +916,7 @@ export default {
                           :col="col.col"
                           v-bind="col.col.formatterOpts"
                           :row-key="row.key"
+                          :get-custom-detail-link="getCustomDetailLink"
                         />
                         <template v-else-if="col.value !== ''">
                           {{ col.formatted }}