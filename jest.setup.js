3fec0678eabf5e0c042f79d6376742ba66564cec
cnotv <giuseppe.leo@suse.com>
2022-04-27 22:49:14 +0200

    Add global directives for unit tests in Jest

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,11 @@
 import { config } from '@vue/test-utils';
+import { directiveSsr as t } from '@/plugins/i18n';
 
 config.mocks['$store'] = { getters: { 'i18n/t': jest.fn() } };
+config.directives = { t };
+
+// Overrides some components
+// config.stubs['my-component'] = { template: "<div></div> "};
 
 /**
  * Global configuration for Jest tests
@@ -12,7 +17,8 @@ beforeAll(() => {
  * Common initialization for each test, required for resetting states
  */
 beforeEach(() => {
-
+  // jest.resetModules(); // Use this function inside your test if you need to remove any cache stored
+  // jest.clearAllMocks(); // Use this function inside your test if you need to reset mocks
 });
 
 /**
