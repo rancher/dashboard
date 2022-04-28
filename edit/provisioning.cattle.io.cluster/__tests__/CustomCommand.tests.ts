2060da674f94f22dd02e25d417ba57a2883c2197
cnotv <giuseppe.leo@suse.com>
2022-04-28 18:41:24 +0200

    Add test for custom command generated from tokens

--- edit/provisioning.cattle.io.cluster/__tests__/CustomCommand.tests.ts
+++ edit/provisioning.cattle.io.cluster/__tests__/CustomCommand.tests.ts
@@ -1,37 +1,42 @@
-import { mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import CustomCommand from '@/edit/provisioning.cattle.io.cluster/CustomCommand.vue';
+import VTooltip from 'v-tooltip';
+
+const localVue = createLocalVue();
+
+localVue.use(VTooltip);
 
 describe('component: CustomCommand', () => {
-  it('should add more items', () => {
+  it('should return linux commands with the right flags based on cluster information', () => {
     const token = 'MY_TOKEN';
     const ip = 'MY_IP';
     const checkSum = 'MY_CHECKSUM';
     const wrapper = mount(CustomCommand, {
-      mocks: {
-        propsData: {
-          cluster:      {},
-          clusterToken: {
-            insecureWindowsNodeCommand: ` curl.exe --insecure -fL ${ ip }/wins-agent-install.ps1 -o install.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; ./install.ps1 -Server ${ ip } -Label 'cattle.io / os=windows' -Token ${ token } -Worker -CaChecksum ${ checkSum }`,
-            windowsNodeCommand:         `curl.exe -fL ${ ip }/wins-agent-install.ps1 -o install.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; ./install.ps1 -Server ${ ip } -Label 'cattle.io/os=windows' -Token ${ token } -Worker -CaChecksum ${ checkSum }`
-          }
-        },
-        data: {
-          address:         '',
-          controlPlane:    true,
-          etcd:            true,
-          insecure:        false,
-          internalAddress: '',
-          labels:          {},
-          nodeName:        '',
-          taints:          [],
-          worker:          true,
-        },
-        $store: { getters: { 'i18n/t': jest.fn() } }
+      propsData: {
+        cluster:      {},
+        clusterToken: {
+          insecureNodeCommand: ` curl --insecure -fL ${ ip }/system-agent-install.sh | sudo  sh -s - --server ${ ip } --label 'cattle.io/os=linux' --token ${ token } --ca-checksum ${ checkSum }`,
+          nodeCommand:         ` curl -fL ${ ip }/system-agent-install.sh | sudo  sh -s - --server ${ ip } --label 'cattle.io/os=linux' --token ${ token } --ca-checksum ${ checkSum }`
+        }
       },
+      localVue,
+      // stubs: { CopyCode: { template: '<div></div> ' } },
+      data:     () => ({
+        address:         '',
+        controlPlane:    true,
+        etcd:            true,
+        insecure:        false,
+        internalAddress: '',
+        labels:          {},
+        nodeName:        '',
+        taints:          [],
+        worker:          true,
+      }),
     });
-    const value = `curl -fL ${ ip }/system-agent-install.sh | sudo  sh -s - --server ${ ip } --label 'cattle.io/os=linux' --token ${ token } --ca-checksum ${ checkSum } --etcd --controlplane --worker`;
-    const element = wrapper.find('code').element;
 
-    expect(element.textContent).toBe(value);
+    const value = `curl -fL ${ ip }/system-agent-install.sh | sudo  sh -s - --server ${ ip } --label 'cattle.io/os=linux' --token ${ token } --ca-checksum ${ checkSum } --etcd --controlplane --worker`;
+    const element = wrapper.find('#copiedLinux').element;
+
+    expect(element.textContent).toContain(value);
   });
 });
