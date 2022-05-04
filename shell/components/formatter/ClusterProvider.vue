d9a6aa145a96004c43ba78349c45532679c2ecfb
Neil MacDougall <nmacdougall@suse.com>
2022-05-04 21:19:57 +0100

    Improve presentation of cluster provide on Cluster Management list

---
+++ shell/components/formatter/ClusterProvider.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    row: {
+      type:     Object,
+      required: true
+    },
+  }
+};
+</script>
+
+<template>
+  <div>
+    <template v-if="row.machineProvider">
+    {{ row.machineProviderDisplay }}
+    </template>
+    <template v-else-if="row.isCustom">
+      {{ t('cluster.provider.custom') }}
+    </template>
+    <template v-else>
+      {{ t('cluster.provider.imported') }}
+    </template>
+    <div class="text-muted">
+      {{ row.provisionerDisplay }}
+    </div>
+  </div>
+</template>
