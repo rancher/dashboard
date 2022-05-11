55ce0a9da318dfa0bb5c09d9fa5efcdb170405c4
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2022-05-11 14:03:10 +0100

    Can't create an Opaque secret using plain text as value (#5918)
    * fix issue where user could not create opaque secret from edit config interface
    
    * Add `canEncode` to cover c&p
    
    Co-authored-by: Alexandre Alves <aalves@Alexandres-MBP.lan>
    Co-authored-by: Richard Cox <richard.cox@suse.com>

--- shell/components/form/KeyValue.vue
+++ shell/components/form/KeyValue.vue
@@ -277,6 +277,7 @@ export default {
         [this.keyName]:   '',
         [this.valueName]: '',
         binary:           false,
+        canEncode:        this.handleBase64,
         supported:        true
       });
     }
@@ -322,7 +323,8 @@ export default {
         [this.valueName]: value,
       };
 
-      obj.binary = this.displayValuesAsBinary;
+      obj.binary = false;
+      obj.canEncode = this.handleBase64;
       obj.supported = true;
       this.rows.push(obj);
       this.queueUpdate();
@@ -445,6 +447,7 @@ export default {
         [this.keyName]:   (split[0] || '').trim(),
         [this.valueName]: (split[1] || '').trim(),
         supported:        true,
+        canEncode:        this.handleBase64,
         binary:           this.displayValuesAsBinary
       }));
 
