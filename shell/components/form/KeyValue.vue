a9167a34ddf21918c47bb2a9e8ba35d6cc1bef68
Richard Cox <richard.cox@suse.com>
2022-05-11 12:05:42 +0100

    Add `canEncode` to cover c&p

--- shell/components/form/KeyValue.vue
+++ shell/components/form/KeyValue.vue
@@ -447,6 +447,7 @@ export default {
         [this.keyName]:   (split[0] || '').trim(),
         [this.valueName]: (split[1] || '').trim(),
         supported:        true,
+        canEncode:        this.handleBase64,
         binary:           this.displayValuesAsBinary
       }));
 
