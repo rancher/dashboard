9baa303444ba379186395b7dfbc432c2e876f0c2
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-05-18 09:27:16 +0100

    setting up routing for os elemental plugin

--- pkg/os-elemental/index.ts
+++ pkg/os-elemental/index.ts
@@ -1,9 +1,19 @@
 import { importTypes } from '@rancher/auto-import';
+import { IPlugin } from '@shell/core/types';
+import elementalRouting from './routing/elemental-routing';
 
 // Init the package
-export default function($plugin) {
+export default function($plugin: IPlugin) {
   // Auto-import model, detail, edit from the folders
   importTypes($plugin);
 
+  // Provide plugin metadata from package.json
+  $plugin.metadata = require('./package.json');
+
+  // Load a product
   $plugin.addProduct(require('./product'));
+
+  // Add Vue Routes
+  $plugin.addRoutes(elementalRouting);
+  console.log('LOADING NIINTIDSADSAADSASd');
 }
