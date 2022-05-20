9fd4c7ad6a19a53aa0babc98015228ced3d09de4
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-05-20 09:59:05 +0100

    rename pkg to elemental + fix basic routing + add simple translations

---
+++ pkg/elemental/pages/MachineInventories.vue
@@ -0,0 +1,13 @@
+<script>
+export default { name: 'MachineInventories' };
+</script>
+
+<template>
+  <div>
+    <h1>MachineInventories!!!</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pkg/elemental/product.js
@@ -0,0 +1,51 @@
+import { ELEMENTAL_PRODUCT_NAME, ELEMENTAL_TYPES } from './types';
+import { createElementalRoute, rootElementalRoute } from './utils/custom-routing';
+
+export function init($plugin, store) {
+  const { product, virtualType, basicType } = $plugin.DSL(store, $plugin.name);
+
+  // app in sidebar
+  product({
+    icon:                'gear',
+    inStore:             'management',
+    removable:           false,
+    showClusterSwitcher: false,
+    to:                  rootElementalRoute()
+  });
+
+  virtualType({
+    label:        store.getters['i18n/t']('elemental.menuLabels.dashboard'),
+    icon:         'folder',
+    group:        'Root',
+    namespaced:   false,
+    name:         ELEMENTAL_TYPES.DASHBOARD,
+    weight:       3,
+    route:        rootElementalRoute()
+  });
+
+  virtualType({
+    label:        store.getters['i18n/t']('elemental.menuLabels.operations'),
+    icon:         'folder',
+    group:        'Root',
+    namespaced:   false,
+    name:         ELEMENTAL_TYPES.OPERATIONS,
+    weight:       2,
+    route:        createElementalRoute(ELEMENTAL_TYPES.OPERATIONS, { product: ELEMENTAL_PRODUCT_NAME })
+  });
+
+  virtualType({
+    label:        store.getters['i18n/t']('elemental.menuLabels.machineInventories'),
+    icon:         'folder',
+    group:        'Root',
+    namespaced:   false,
+    name:         ELEMENTAL_TYPES.MACHINE_INVENTORIES,
+    weight:       1,
+    route:        createElementalRoute(ELEMENTAL_TYPES.MACHINE_INVENTORIES, { product: ELEMENTAL_PRODUCT_NAME })
+  });
+
+  basicType([
+    ELEMENTAL_TYPES.DASHBOARD,
+    ELEMENTAL_TYPES.OPERATIONS,
+    ELEMENTAL_TYPES.MACHINE_INVENTORIES,
+  ]);
+}
