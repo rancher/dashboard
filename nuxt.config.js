1a39a3ccb82eeefb83d999ad02c1e397e3bc5983
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-05-23 15:21:17 +0100

    exclude elemental pkg by default

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import config from './shell/nuxt.config';
 
 export default config(__dirname, {
-  excludes: ['epinio'],
+  excludes: ['epinio', 'elemental'],
   // excludes: ['fleet', 'example']
   // autoLoad: ['fleet', 'example']
 });
