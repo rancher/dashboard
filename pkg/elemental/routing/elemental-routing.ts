eee8090bdc8486fb702ea57a54637fce4bfcd6c1
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-05-24 17:14:33 +0100

    add default routes and view for creating and viewing details of a resource

--- pkg/elemental/routing/elemental-routing.ts
+++ pkg/elemental/routing/elemental-routing.ts
@@ -3,6 +3,8 @@ import { ELEMENTAL_PRODUCT_NAME } from '../types';
 import Dashboard from '../pages/index.vue';
 import Operations from '../pages/Operations.vue';
 import ListElementalResource from '../pages/resource/index.vue';
+import CreateElementalResource from '../pages/resource/create.vue';
+import ElementalResourceDetails from '../pages/resource/_id.vue';
 
 const routes: RouteConfig[] = [
   {
@@ -19,6 +21,16 @@ const routes: RouteConfig[] = [
     name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource`,
     path:      `/:product/c/:cluster/:resource`,
     component: ListElementalResource,
+  },
+  {
+    name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource-create`,
+    path:      `/:product/c/:cluster/:resource/create`,
+    component: CreateElementalResource,
+  },
+  {
+    name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource-id`,
+    path:      `/:product/c/:cluster/:resource/id`,
+    component: ElementalResourceDetails,
   },
 ];
 
