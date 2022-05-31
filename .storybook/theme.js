3816ad68cb184beab34b825dcf2625d69b7c0eb1
bisht-richa <richa.bisht@suse.com>
2022-05-31 13:34:27 +0200

    Changed files name

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,5 +3,5 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'Rancher Storybook',
-  brandImage: 'https://raw.githubusercontent.com/rancher/dashboard/master/assets/images/pl/rancher-logo.svg',
+  brandImage: 'https://raw.githubusercontent.com/rancher/dashboard/b099206b8f7d3da19218afe1cda5feb00810eb8e/shell/assets/images/pl/rancher-logo.svg',
 });
