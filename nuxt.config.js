10cdf833154743b10b06f5e9e0a66ecdde959b87
scures <scurescu@suse.com>
2022-06-01 09:34:42 +0200

    Excludes packages from .env file instead of hardcoded

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import config from './shell/nuxt.config';
 
 export default config(__dirname, {
-  excludes: ['epinio', 'rancher-components'],
+  excludes: [...process.env.EXCLUDES_PKG.split(',')],
   // excludes: ['fleet', 'example']
   // autoLoad: ['fleet', 'example']
 });
