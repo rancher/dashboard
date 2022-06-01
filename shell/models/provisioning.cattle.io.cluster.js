6693c588c01015d17e8b65234d701f2790f1aeac
Catherine Luse <catherine.luse@gmail.com>
2022-06-01 10:58:11 -0700

    Re-enable RKE2 encryption key rotation

--- shell/models/provisioning.cattle.io.cluster.js
+++ shell/models/provisioning.cattle.io.cluster.js
@@ -113,8 +113,7 @@ export default class ProvCluster extends SteveModel {
         action:     'rotateEncryptionKey',
         label:      this.$rootGetters['i18n/t']('nav.rotateEncryptionKeys'),
         icon:       'icon icon-refresh',
-        // Disabling encryption key rotation for RKE2 for now because it was removed from v2.6.5
-        enabled:    (this.isRke1 && this.mgmt?.hasAction('rotateEncryptionKey') && ready) // || canEditRKE2cluster
+        enabled:    canEditRKE2cluster || (this.isRke1 && this.mgmt?.hasAction('rotateEncryptionKey') && ready)
       }, {
         action:     'saveAsRKETemplate',
         label:      this.$rootGetters['i18n/t']('nav.saveAsRKETemplate'),
