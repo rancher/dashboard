02667d68b883f545649dcbf8ee67a10e74169ecb
bisht-richa <richa.bisht@suse.com>
2022-06-07 16:23:47 +0200

    Hide canvas tab from toolbar, some minor updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,11 +18,14 @@ module.exports = {
       name: '@storybook/addon-docs',
       options: { mdxBabelOptions: { babelrc: true, configFile: true } },
     },
+<<<<<<< HEAD
   ],
 
   staticDirs: [
     'public',
     '../shell/assets'
+=======
+>>>>>>> 7f15921e5 (Hide canvas tab from toolbar, some minor updates)
   ],
 
   webpackFinal: async (config, { configType }) => {
