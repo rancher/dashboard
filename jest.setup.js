f230fcfd81b1f0ca242120ec9c932c1dc271ba2f
cnotv <giuseppe.leo@suse.com>
2022-06-08 15:36:33 +0200

    Correct vue-select import in Vue within Jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -2,12 +2,13 @@ import { config } from '@vue/test-utils';
 import { directiveSsr as t } from '@shell/plugins/i18n';
 import VTooltip from 'v-tooltip';
 import VModal from 'vue-js-modal';
-import vueSelect from 'vue-select';
+import vSelect from 'vue-select';
 
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
-Vue.use(VTooltip).use(VModal).use(vueSelect);
+Vue.use(VTooltip).use(VModal);
+Vue.component('v-select', vSelect);
 
 config.mocks['$store'] = { getters: { 'i18n/t': jest.fn() } };
 config.directives = { t };
