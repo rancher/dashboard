830b59d0a9cc95b629d8cb85fbb73c06f6a3265d
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-06-08 14:17:38 +0100

    working on cluster creation + working version of dashboard view for elemental

--- shell/edit/provisioning.cattle.io.cluster/rke2.vue
+++ shell/edit/provisioning.cattle.io.cluster/rke2.vue
@@ -327,6 +327,10 @@ export default {
       return this.value.spec.rkeConfig;
     },
 
+    isElementalCluster() {
+      return this.provider === 'elemental';
+    },
+
     advancedTitleAlt() {
       const machineSelectorLength = this.rkeConfig.machineSelectorConfig.length;
 
@@ -395,7 +399,7 @@ export default {
       const showK3s = k3s.length && !existingRke2;
       const out = [];
 
-      if ( showRke2 ) {
+      if ( showRke2 && this.provider !== 'elemental' ) {
         if ( showK3s ) {
           out.push({ kind: 'group', label: this.t('cluster.provider.rke2') });
         }
@@ -538,7 +542,7 @@ export default {
     },
 
     showCisProfile() {
-      return (this.provider === 'custom' || this.provider === 'elemental') && ( this.serverArgs.profile || this.agentArgs.profile );
+      return (this.provider === 'custom' || this.isElementalCluster) && ( this.serverArgs.profile || this.agentArgs.profile );
     },
 
     registryOptions() {
@@ -551,7 +555,7 @@ export default {
     },
 
     needCredential() {
-      if ( this.provider === 'custom' || this.provider === 'import' || this.provider === 'elemental' || this.mode === _VIEW ) {
+      if ( this.provider === 'custom' || this.provider === 'import' || this.isElementalCluster || this.mode === _VIEW ) {
         return false;
       }
 
@@ -747,6 +751,7 @@ export default {
     defaultVersion() {
       const all = this.versionOptions.filter(x => !!x.value);
       const first = all[0]?.value;
+
       const preferred = all.find(x => x.value === this.defaultRke2)?.value;
 
       const rke2 = this.filterAndMap(this.rke2Versions, null);
@@ -1089,7 +1094,7 @@ export default {
     },
 
     validationPassed() {
-      return (this.provider === 'custom' || this.provider === 'elemental' || !!this.credentialId);
+      return (this.provider === 'custom' || this.isElementalCluster || !!this.credentialId);
     },
 
     cancelCredential() {
@@ -1101,7 +1106,7 @@ export default {
     done() {
       let routeName = 'c-cluster-product-resource';
 
-      if ( this.mode === _CREATE && (this.provider === 'import' || this.provider === 'custom' || this.provider === 'elemental') ) {
+      if ( this.mode === _CREATE && (this.provider === 'import' || this.provider === 'custom' || this.isElementalCluster) ) {
         // Go show the registration command
         routeName = 'c-cluster-product-resource-namespace-id';
       }
