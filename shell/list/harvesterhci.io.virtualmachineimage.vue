eea42971f6bc04f25e5eb1c2fa3d32d4bfe67aea
wujun <897415845@qq.com>
2022-06-08 16:31:03 +0800

    HARVESTER: filter labels

--- shell/list/harvesterhci.io.virtualmachineimage.vue
+++ shell/list/harvesterhci.io.virtualmachineimage.vue
@@ -1,6 +1,8 @@
 <script>
 import ResourceTable from '@shell/components/ResourceTable';
 import { Banner } from '@components/Banner';
+import FilterLabel from '@shell/components/FilterLabel';
+import { defaultTableSortGenerationFn } from '@shell/components/ResourceTable.vue';
 
 export default {
   name: 'ListHarvesterImage',
@@ -8,6 +10,7 @@ export default {
   components: {
     ResourceTable,
     Banner,
+    FilterLabel
   },
 
   props:      {
@@ -21,11 +24,73 @@ export default {
     },
   },
 
+  data() {
+    return {
+      searchLabels: [],
+      filterRows:   []
+    };
+  },
+
   computed: {
     uploadingImages() {
       return this.$store.getters['harvester-common/uploadingImages'] || [];
     },
+
+    optionLabels() {
+      const labels = this.rows.map((row) => {
+        return Object.keys(row.labels);
+      });
+
+      return Array.from(new Set(labels.flat()));
+    },
   },
+
+  methods: {
+    update() {
+      const map = {};
+
+      this.searchLabels.map((v) => {
+        map[`${ v.key }`] = v.value;
+      });
+    },
+
+    calcValueOptions(key) {
+      const valueOptions = [];
+
+      this.rows.map((row) => {
+        const isExistValue = valueOptions.find(value => value.label === row.labels[key]);
+
+        if (Object.keys(row.labels).includes(key) && key && row.labels[key] && !isExistValue) {
+          valueOptions.push({
+            value: row.labels[key],
+            label: row.labels[key]
+          });
+        }
+      });
+
+      return valueOptions;
+    },
+
+    changeRows(filterRows, searchLabels) {
+      this.$set(this, 'filterRows', filterRows);
+      this.$set(this, 'searchLabels', searchLabels);
+    },
+
+    removeAll() {
+      this.$set(this, 'searchLabels', []);
+    },
+
+    sortGenerationFn() {
+      let base = defaultTableSortGenerationFn(this.schema, this.$store);
+
+      this.searchLabels.map((label) => {
+        base += label.key;
+        base += label.value;
+      });
+
+      return base;
+    },
+  }
 };
 </script>
 
@@ -38,10 +103,15 @@ export default {
     />
     <ResourceTable
       v-bind="$attrs"
-      :rows="rows"
+      :rows="filterRows"
       :schema="schema"
+      :sort-generation-fn="sortGenerationFn"
       key-field="_key"
       v-on="$listeners"
-    />
+    >
+      <template #more-header-middle>
+        <FilterLabel ref="filterLabel" :rows="rows" @changeRows="changeRows" />
+      </template>
+    </ResourceTable>
   </div>
 </template>
