2a99b3ad66d9bdeaa848d19ce70c3161626e2893
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-06-15 17:23:02 +0100

    add missing string to translation file

--- shell/edit/provisioning.cattle.io.cluster/rke2.vue
+++ shell/edit/provisioning.cattle.io.cluster/rke2.vue
@@ -1058,7 +1058,7 @@ export default {
       const finalPools = [];
 
       for ( const entry of this.machinePools ) {
-        console.log('machine pool entry on save', entry);
+        console.log('*** machine pool entry config on save ***', entry.config);
         if ( entry.remove ) {
           continue;
         }
@@ -1613,7 +1613,7 @@ export default {
             </Tab>
           </template>
           <div v-if="!unremovedMachinePools.length">
-            You do not have any machine pools defined, click the plus to add one.
+            {{ t('cluster.machinePool.noPoolsDisclaimer') }}
           </div>
         </Tabbed>
         <div class="spacer" />
