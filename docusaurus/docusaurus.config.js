952a1f7b500dfc754d4e4912004b5703cb60de1b
bisht-richa <richa.bisht@suse.com>
2022-06-16 14:19:35 +0200

    Added docusaurus

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -34,15 +34,29 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+<<<<<<< HEAD
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           showLastUpdateTime: true,
+=======
+          sidebarPath: require.resolve('./sidebars.js'),
+>>>>>>> 1c4899ebd (Added docusaurus)
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+<<<<<<< HEAD
         blog: false,
+=======
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+>>>>>>> 1c4899ebd (Added docusaurus)
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -54,23 +68,87 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
+<<<<<<< HEAD
         logo: {
           alt: 'Rancher Logo',
           src: 'img/rancher-logo.svg',
+=======
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+>>>>>>> 1c4899ebd (Added docusaurus)
         },
         items: [
           {
             type: 'doc',
+<<<<<<< HEAD
             docId: 'plugins',
             position: 'left',
             label: 'Documentation',
+=======
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+>>>>>>> 1c4899ebd (Added docusaurus)
           },
         ],
       },
       footer: {
         style: 'dark',
+<<<<<<< HEAD
         links: [],
         copyright: `Copyright © ${new Date().getFullYear()} Rancher. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks.`,
+=======
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+>>>>>>> 1c4899ebd (Added docusaurus)
       },
       prism: {
         theme: lightCodeTheme,
