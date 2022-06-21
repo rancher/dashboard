a21b603f26f64ddafd3f3160bdd7041e2adf00d9
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2022-06-21 08:09:38 -0700

    fix loading imported dev cluster

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import config from './shell/nuxt.config';
 
 // Excludes the following plugins if there's no .env file.
-const excludes = process.env.EXCLUDES_PKG || 'epinio, rancher-components';
+const excludes = process.env.EXCLUDES_PKG || 'epinio, rancher-components, harvester';
 
 export default config(__dirname, {
   excludes: excludes.replace(/\s/g, '').split(','),
