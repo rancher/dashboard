11edae7b6743f059191cd6e5a4dc8028435f7bd5
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2022-06-22 10:04:40 -0700

    fix harvester pkg routing

--- shell/middleware/authenticated.js
+++ shell/middleware/authenticated.js
@@ -238,7 +238,6 @@ export default async function({
   }
   // Load stuff
   await applyProducts(store, $plugin);
-
   // Setup a beforeEach hook once to keep track of the current product
   if ( !beforeEachSetup ) {
     beforeEachSetup = true;
