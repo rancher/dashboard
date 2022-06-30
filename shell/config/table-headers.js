a7dbff0a555581029fbed06f113ceab6cbb79fe2
Neil MacDougall <nmacdougall@suse.com>
2022-06-30 17:04:21 +0100

    Update some columns not to be filterable

--- shell/config/table-headers.js
+++ shell/config/table-headers.js
@@ -151,6 +151,7 @@ export const NODE = {
   name:          'node',
   labelKey:      'tableHeaders.node',
   value:         'spec.nodeName',
+  getValue:      row => row.spec?.nodeName,
   sort:          'spec.nodeName',
   formatter:     'LinkName',
   formatterOpts: { type: NODE_TYPE },
@@ -161,6 +162,7 @@ export const NODE_NAME = {
   labelKey:  'tableHeaders.nodeName',
   sort:      'name',
   value:     'name',
+  getValue:  row => row.name,
   formatter: 'LinkDetail',
 };
 
@@ -265,7 +267,7 @@ export const POD_IMAGES = {
   value:     'imageNames',
   getValue:  row => row.imageNames,
   sort:      'imageNames',
-  search:    'imageNames',
+  // search:    'imageNames',
   formatter: 'PodImages'
 };
 
@@ -276,6 +278,7 @@ export const POD_RESTARTS = {
   delayLoading: true,
   value:        'restartCount',
   getValue:     row => row.restartCount,
+  search:    false,
 };
 
 export const ENDPOINTS = {
@@ -645,6 +648,7 @@ export const WORKLOAD_HEALTH_SCALE = {
   width:        150,
   skipSelect:   true,
   delayLoading: true,
+  search:       false,
 };
 
 export const FLEET_SUMMARY = {
