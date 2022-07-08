886238dea0a75ca139df8d64afe801cdf5cac019
Neil MacDougall <nwmac@users.noreply.github.com>
2022-07-08 17:40:20 +0100

    Errors are not displaying when standard user attempts to create a project with non-default Pod Security Policy (#6278)

--- shell/models/management.cattle.io.project.js
+++ shell/models/management.cattle.io.project.js
@@ -74,7 +74,7 @@ export default class Project extends HybridModel {
 
     const newValue = await norman.save({ replace: forceReplaceOnReq });
 
-    newValue.doAction('setpodsecuritypolicytemplate', { podSecurityPolicyTemplateId: this.spec.podSecurityPolicyTemplateId || null });
+    await newValue.doAction('setpodsecuritypolicytemplate', { podSecurityPolicyTemplateId: this.spec.podSecurityPolicyTemplateId || null });
 
     await this.$dispatch('management/findAll', { type: MANAGEMENT.PROJECT, opt: { force: true } }, { root: true });
 
