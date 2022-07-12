4a8a11a12ccb7fdd20668f878d5920b20f139668
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2022-07-12 09:56:00 -0700

    members routing

--- pkg/harvester/pages/c/_cluster/members/index.vue
+++ pkg/harvester/pages/c/_cluster/members/index.vue
@@ -1,6 +1,21 @@
 <script>
 import ExplorerMembers from '@shell/components/ExplorerMembers';
-export default { components: { ExplorerMembers } };
+import { HARVESTER_NAME } from '@shell/config/product/harvester-manager';
+import { MANAGEMENT } from '@shell/config/types';
+export default {
+  components: { ExplorerMembers },
+  computed:   {
+    createLocationOverride() {
+      return {
+        name:   `${ HARVESTER_NAME }-c-cluster-resource-create`,
+        params: {
+          cluster:  this.$store.getters['currentCluster']?.id,
+          resource: MANAGEMENT.CLUSTER_ROLE_TEMPLATE_BINDING
+        }
+      };
+    }
+  }
+};
 </script>
 
-<template><ExplorerMembers v-bind="$attrs" /></template>
+<template><ExplorerMembers :create-location-override="createLocationOverride" v-bind="$attrs" /></template>
