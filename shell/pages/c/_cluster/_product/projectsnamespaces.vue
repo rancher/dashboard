d7b2d1db9751ea7f7d7925deb74dfa6aca1433e8
Richard Cox <richard.cox@suse.com>
2022-07-15 14:36:21 +0100

    Split out project namespace into it's own component (2 or 2)

---
+++ shell/pages/c/_cluster/_product/projectsnamespaces.vue
@@ -0,0 +1,6 @@
+<script>
+import ExplorerProjectsNamespaces from '@shell/components/ExplorerProjectsNamespaces';
+export default { components: { ExplorerProjectsNamespaces } };
+</script>
+
+<template><ExplorerProjectsNamespaces v-bind="$attrs" /></template>
