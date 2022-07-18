e90ecbe9e2a773b19e9526998c98b9cffabb147f
Shavindra <shavindra@users.noreply.github.com>
2022-07-18 22:43:00 +0100

    Fix tabs.

--- shell/edit/workload/types/Deployment.vue
+++ shell/edit/workload/types/Deployment.vue
@@ -1,58 +1,36 @@
 <script>
-
+import CreateEditView from '@shell/mixins/create-edit-view';
 import WorkLoadMixin from '@shell/edit/workload/mixins/workload';
 
-const DEPLOYMENT_TABS = {
-  DEPLOYMENT: 'deployment',
-  PODS: 'pods'
-}
-
 export default {
-  name:       'CruWorkload',
+  name:       'WorkloadGeneric',
+  mixins: [CreateEditView, WorkLoadMixin],
+  
   data() {
     return {
-      mainTabs: [{
-          name: DEPLOYMENT_TABS.DEPLOYMENT,
-          weight: 99,
-        },
-        {
-          name: DEPLOYMENT_TABS.PODS,
-          weight: 98,
-      }]
+      selectedName: null
     }
   },
   computed: {
-    deploymentTabs() {
-      return this.mainTabs.concat(this.containerTabs)
-    },
-
-    containerTabs() {
-      return this.podTemplateSpec.containers.map((c,i)=>{
-        return {
-          name: c.name,
-          weight: 97 - i, // 99, 98 = DEPLOYMENT_TABS
-          label: c.name,
-          type: 'container'
-        }
-      })
+    sdContainer() {
+      return this.containerOptions.find((c)=>c.name === this.selectedName)
     }
   },
   methods: {
     changed(tab) {
-      const containerType = this.containerTabs.find((c) => c.name === tab.tab._props.name && !!tab.tab.__active)
-      console.log(containerType)
-      if(containerType) {
-        this.selectContainer(containerType)
+      this.selectedName = tab.selectedName
+      const container = this.containerOptions.find((c)=>c.name === tab.selectedName)
+      console.log(container)
+      if( container ) {
+        this.selectContainer(container)
       }
     }
-  },
-  mixins: [WorkLoadMixin],
+  }
 };
 </script>
 
 <template>
   <Loading v-if="$fetchState.pending" />
-
   <form v-else class="filled-height">
     <CruResource
       :validation-passed="true"
@@ -114,122 +92,9 @@ export default {
           </div>
         </div>
       </div>
+      <Tabbed @changed="changed">
+        <Tab v-for="(tab, i) in allContainers" :key="i" :label="tab.label || t(`workload.container.titles.${tab.name}`)" :name="tab.name" :weight="tab.weight">
 
-      <Tabbed :key="containerChange + 100" @changed="changed">
-        <Tab v-for="(tab, i) in deploymentTabs" :key="i" :label="tab.label || t(`workload.container.titles.${tab.name}`)" :name="tab.name" :weight="tab.weight">
-          {{tab.type}}
-          <Labels v-if="tab.name === 'deployment' " v-model="value" :mode="mode" />
-          <div v-if="tab.name === 'pods' " >
-            <Tabbed :key="containerChange" :side-tabs="true"> 
-            <Tab :label="t('workload.storage.title')" name="storage" :weight="tabWeightMap['storage']">
-              <Storage
-                v-model="podTemplateSpec"
-                :namespace="value.metadata.namespace"
-                :register-before-hook="registerBeforeHook"
-                :mode="mode"
-                :secrets="namespacedSecrets"
-                :config-maps="namespacedConfigMaps"
-                :container="container"
-                :save-pvc-hook-name="savePvcHookName"
-                @removePvcForm="clearPvcFormState"
-              />
-            </Tab>
-            <Tab :label="t('workload.container.titles.resources')" name="resources" :weight="tabWeightMap['resources']">
-              <h3 class="mb-10">
-                <t k="workload.scheduling.titles.limits" />
-              </h3>
-              <ContainerResourceLimit v-model="flatResources" :mode="mode" :show-tip="false" />
-              <template>
-                <div class="spacer"></div>
-                <div>
-                  <h3 class="mb-10">
-                    <t k="workload.scheduling.titles.tolerations" />
-                  </h3>
-                  <div class="row">
-                    <Tolerations v-model="podTemplateSpec.tolerations" :mode="mode" />
-                  </div>
-                </div>
-
-                <div>
-                  <div class="spacer"></div>
-                  <h3 class="mb-10">
-                    <t k="workload.scheduling.titles.priority" />
-                  </h3>
-                  <div class="row">
-                    <div class="col span-6">
-                      <LabeledInput v-model.number="podTemplateSpec.priority" :mode="mode" :label="t('workload.scheduling.priority.priority')" />
-                    </div>
-                    <div class="col span-6">
-                      <LabeledInput v-model="podTemplateSpec.priorityClassname" :mode="mode" :label="t('workload.scheduling.priority.className')" />
-                    </div>
-                  </div>
-                </div>
-              </template>
-            </Tab>
-            <Tab :label="t('workload.container.titles.podScheduling')" name="podScheduling" :weight="tabWeightMap['podScheduling']">
-              <PodAffinity :mode="mode" :value="podTemplateSpec" :nodes="allNodeObjects" />
-            </Tab>
-            <Tab :label="t('workload.container.titles.nodeScheduling')" name="nodeScheduling" :weight="tabWeightMap['nodeScheduling']">
-              <NodeScheduling :mode="mode" :value="podTemplateSpec" :nodes="allNodes" />
-            </Tab>
-            <Tab :label="t('workload.container.titles.upgrading')" name="upgrading" :weight="tabWeightMap['upgrading']">
-              <Job v-if="isJob || isCronJob" v-model="spec" :mode="mode" :type="type" />
-              <Upgrading v-else v-model="spec" :mode="mode" :type="type" />
-            </Tab>
-            <Tab v-if="!isInitContainer" :label="t('workload.container.titles.healthCheck')" name="healthCheck" :weight="tabWeightMap['healthCheck']">
-              <HealthCheck v-model="healthCheck" :mode="mode" />
-            </Tab>
-            <Tab :label="t('workload.container.titles.securityContext')" name="securityContext" :weight="tabWeightMap['securityContext']">
-              <Security v-model="container.securityContext" :mode="mode" />
-              <div class="spacer"></div>
-              <div>
-                <h3>{{ t('workload.container.security.podFsGroup') }}</h3>
-                <div class="row">
-                  <div class="col span-6">
-                    <LabeledInput v-model.number="podFsGroup" type="number" :mode="mode" :label="t('workload.container.security.fsGroup')" />
-                  </div>
-                </div>
-              </div>
-            </Tab>
-            <Tab :label="t('workload.container.titles.networking')" name="networking" :weight="tabWeightMap['networking']">
-              <Networking v-model="podTemplateSpec" :mode="mode" />
-            </Tab>
-            <Tab v-if="isStatefulSet" :label="t('workload.container.titles.volumeClaimTemplates')" name="volumeClaimTemplates" :weight="tabWeightMap['volumeClaimTemplates']">
-              <VolumeClaimTemplate v-model="spec" :mode="mode" />
-            </Tab>
-            <Tab name="labels" label-key="generic.labelsAndAnnotations" :weight="tabWeightMap['labels']">
-              <Labels v-model="value" :mode="mode" />
-              <div class="spacer"></div>
-
-              <div>
-                <h3>{{ t('workload.container.titles.podLabels') }}</h3>
-                <div class="row mb-20">
-                  <KeyValue
-                    key="labels"
-                    v-model="podLabels"
-                    :add-label="t('labels.addLabel')"
-                    :mode="mode"
-                    :read-allowed="false"
-                    :protip="false"
-                  />
-                </div>
-                <div class="spacer"></div>
-                <h3>{{ t('workload.container.titles.podAnnotations') }}</h3>
-                <div class="row">
-                  <KeyValue
-                    key="annotations"
-                    v-model="podAnnotations"
-                    :add-label="t('labels.addAnnotation')"
-                    :mode="mode"
-                    :read-allowed="false"
-                    :protip="false"
-                  />
-                </div>
-              </div>
-            </Tab>
-          </Tabbed>
-          </div>
-          <div v-if="tab.type === 'container'"> 
               <div>
                 <div :style="{'align-items':'center'}" class="row mb-20">
                   <div class="col span-6">
@@ -310,9 +175,150 @@ export default {
                 <h3>{{ t('workload.container.titles.lifecycle') }}</h3>
                 <LifecycleHooks v-model="container.lifecycle" :mode="mode" />
               </div>
-          </div> 
+        </Tab>
+        <Tab :label="'Deployments'" :name="'deployments'" :weight="99">
+          <Labels v-model="value" :mode="mode" />
+        </Tab>
+        <Tab :label="'Pods'" :name="'pods'" :weight="99">
+          <div>
+            <Tabbed :key="containerChange" :side-tabs="true"> 
+            <Tab :label="t('workload.storage.title')" name="storage" :weight="tabWeightMap['storage']">
+              <Storage
+                v-model="podTemplateSpec"
+                :namespace="value.metadata.namespace"
+                :register-before-hook="registerBeforeHook"
+                :mode="mode"
+                :secrets="namespacedSecrets"
+                :config-maps="namespacedConfigMaps"
+                :container="container"
+                :save-pvc-hook-name="savePvcHookName"
+                @removePvcForm="clearPvcFormState"
+              />
+            </Tab>
+            <Tab :label="t('workload.container.titles.resources')" name="resources" :weight="tabWeightMap['resources']">
+              <h3 class="mb-10">
+                <t k="workload.scheduling.titles.limits" />
+              </h3>
+              <ContainerResourceLimit v-model="flatResources" :mode="mode" :show-tip="false" />
+              <template>
+                <div class="spacer"></div>
+                <div>
+                  <h3 class="mb-10">
+                    <t k="workload.scheduling.titles.tolerations" />
+                  </h3>
+                  <div class="row">
+                    <Tolerations v-model="podTemplateSpec.tolerations" :mode="mode" />
+                  </div>
+                </div>
+
+                <div>
+                  <div class="spacer"></div>
+                  <h3 class="mb-10">
+                    <t k="workload.scheduling.titles.priority" />
+                  </h3>
+                  <div class="row">
+                    <div class="col span-6">
+                      <LabeledInput v-model.number="podTemplateSpec.priority" :mode="mode" :label="t('workload.scheduling.priority.priority')" />
+                    </div>
+                    <div class="col span-6">
+                      <LabeledInput v-model="podTemplateSpec.priorityClassname" :mode="mode" :label="t('workload.scheduling.priority.className')" />
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </Tab>
+            <Tab :label="t('workload.container.titles.podScheduling')" name="podScheduling" :weight="tabWeightMap['podScheduling']">
+              <PodAffinity :mode="mode" :value="podTemplateSpec" :nodes="allNodeObjects" />
+            </Tab>
+            <Tab :label="t('workload.container.titles.nodeScheduling')" name="nodeScheduling" :weight="tabWeightMap['nodeScheduling']">
+              <NodeScheduling :mode="mode" :value="podTemplateSpec" :nodes="allNodes" />
+            </Tab>
+            <Tab :label="t('workload.container.titles.upgrading')" name="upgrading" :weight="tabWeightMap['upgrading']">
+              <Job v-if="isJob || isCronJob" v-model="spec" :mode="mode" :type="type" />
+              <Upgrading v-else v-model="spec" :mode="mode" :type="type" />
+            </Tab>
+            <!-- <Tab v-if="!isInitContainer" :label="t('workload.container.titles.healthCheck')" name="healthCheck" :weight="tabWeightMap['healthCheck']">
+              <HealthCheck v-model="healthCheck" :mode="mode" />
+            </Tab> -->
+            <Tab :label="t('workload.container.titles.securityContext')" name="securityContext" :weight="tabWeightMap['securityContext']">
+              <Security v-model="container.securityContext" :mode="mode" />
+              <div class="spacer"></div>
+              <div>
+                <h3>{{ t('workload.container.security.podFsGroup') }}</h3>
+                <div class="row">
+                  <div class="col span-6">
+                    <LabeledInput v-model.number="podFsGroup" type="number" :mode="mode" :label="t('workload.container.security.fsGroup')" />
+                  </div>
+                </div>
+              </div>
+            </Tab>
+            <Tab :label="t('workload.container.titles.networking')" name="networking" :weight="tabWeightMap['networking']">
+              <Networking v-model="podTemplateSpec" :mode="mode" />
+            </Tab>
+            <Tab v-if="isStatefulSet" :label="t('workload.container.titles.volumeClaimTemplates')" name="volumeClaimTemplates" :weight="tabWeightMap['volumeClaimTemplates']">
+              <VolumeClaimTemplate v-model="spec" :mode="mode" />
+            </Tab>
+            <Tab name="labels" label-key="generic.labelsAndAnnotations" :weight="tabWeightMap['labels']">
+              <Labels v-model="value" :mode="mode" />
+              <div class="spacer"></div>
+
+              <div>
+                <h3>{{ t('workload.container.titles.podLabels') }}</h3>
+                <div class="row mb-20">
+                  <KeyValue
+                    key="labels"
+                    v-model="podLabels"
+                    :add-label="t('labels.addLabel')"
+                    :mode="mode"
+                    :read-allowed="false"
+                    :protip="false"
+                  />
+                </div>
+                <div class="spacer"></div>
+                <h3>{{ t('workload.container.titles.podAnnotations') }}</h3>
+                <div class="row">
+                  <KeyValue
+                    key="annotations"
+                    v-model="podAnnotations"
+                    :add-label="t('labels.addAnnotation')"
+                    :mode="mode"
+                    :read-allowed="false"
+                    :protip="false"
+                  />
+                </div>
+              </div>
+            </Tab>
+          </Tabbed>
+          </div>
         </Tab>
       </Tabbed>
     </CruResource>
   </form>
 </template>
+
+<style lang='scss'>
+.container-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.type-placeholder {
+  color: white;
+  font-size: 2.5em;
+  height: 100%;
+  width: 100%;
+  background-color: var(--primary);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.type-description {
+  color: var(--input-label);
+}
+
+.next-dropdown {
+  display: inline-block;
+}
+</style>
