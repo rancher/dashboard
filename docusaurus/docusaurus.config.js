adff20b0230d4adb9e9100f734db6de5b01068e5
bisht-richa <richa.bisht@suse.com>
2022-07-18 10:19:54 +0200

    Fixes in docusaurus.config.js

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -19,7 +19,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bisht-richa', // Usually your GitHub org/user name.
   projectName: 'dashboard', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
 
 
   // Even if you don't use internalization, you can use this field to set useful
