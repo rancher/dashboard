ac27b6018e038b0bb05f3f760448c5a2e46bef56
Neil MacDougall <nmacdougall@suse.com>
2022-07-18 16:46:56 +0100

    Performance: Cache pods by namespace

--- shell/models/workload.js
+++ shell/models/workload.js
@@ -1,4 +1,5 @@
 import { findBy, insertAt } from '@shell/utils/array';
+import { matches } from '@shell/utils/selector';
 import {
   TARGET_WORKLOADS, TIMESTAMP, UI_MANAGED, HCI as HCI_LABELS_ANNOTATIONS, CATTLE_PUBLIC_ENDPOINTS
 } from '@shell/config/labels-annotations';
