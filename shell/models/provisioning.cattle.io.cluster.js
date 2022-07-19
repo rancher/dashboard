9427509f1adf8b3ed3d17e171af9962187f6acb4
Catherine Luse <catherine.luse@gmail.com>
2022-07-18 20:48:33 -0700

    Only label imported clusters as imported

--- shell/models/provisioning.cattle.io.cluster.js
+++ shell/models/provisioning.cattle.io.cluster.js
@@ -200,6 +200,8 @@ export default class ProvCluster extends SteveModel {
   }
 
   get isImported() {
+    // As of Rancher v2.6.7, this returns false for imported K3s clusters,
+    // in which this.provisioner is `k3s`.
     return this.provisioner === 'imported';
   }
 
@@ -220,6 +222,8 @@ export default class ProvCluster extends SteveModel {
   }
 
   get isImportedK3s() {
+    // As of Rancher v2.6.7, this returns false for imported K3s clusters,
+    // in which this.provisioner is `k3s`.
     return this.isImported && this.mgmt?.status?.provider === 'k3s';
   }
 
