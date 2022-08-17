396dfbbcdb580c9e6998f48b9c5c482fc9888cc3
Richard Cox <richard.cox@suse.com>
2022-08-17 15:04:18 +0100

    Remove testid from generic create page - this is a page at the top level of vue - any custom test id would need to come from params (cluster, product and/or resource)

--- shell/pages/c/_cluster/_product/_resource/create.vue
+++ shell/pages/c/_cluster/_product/_resource/create.vue
@@ -4,18 +4,9 @@ import ResourceDetail from '@shell/components/ResourceDetail';
 export default {
   name:       'ClusterResourceCreate',
   components: { ResourceDetail },
-  data() {
-    return {
-      /**
-       * Inherited global identifier prefix for tests
-       * Define a term based on the parent component to avoid conflicts on multiple components
-       */
-      componentTestid: 'cluster-manager-create'
-    };
-  }
 };
 </script>
 
 <template>
-  <ResourceDetail :component-testid="componentTestid" />
+  <ResourceDetail />
 </template>
