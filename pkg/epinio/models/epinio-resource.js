08c1ed26e5dc2c470d07101beb0029a6a21d1e4d
scures <scurescu@suse.com>
2022-08-17 10:33:12 +0200

    Closes apps logs on namespace removal

--- pkg/epinio/models/epinio-resource.js
+++ pkg/epinio/models/epinio-resource.js
@@ -49,12 +49,45 @@ export default class EpinioResource extends Resource {
     opt.method = 'delete';
 
     try {
+      if (this.type === 'namespaces') {
+        await this._closeLogsOnDelete(this.id);
+      }
       const res = await this.$dispatch('request', { opt, type: this.type });
 
       console.log('### Resource Remove', this.type, this.id, res);// eslint-disable-line no-console
       this.$dispatch('remove', this);
     } catch (e) {
       throw epinioExceptionToErrorsArray(e);
+    }
+  }
+
+  // Close APPS logs on namespace deletions
+  async _closeLogsOnDelete(namespace) {
+    const namespaces = await this.$dispatch('findAll', { type: this.type, opt: { force: true } });
+
+    // Find new namespace
+    const current = namespaces.filter(n => n.id === namespace)[0];
+
+    if (current.apps.length) {
+      const allTabs = await this.$rootGetters['wm/allTabs'];
+
+      current.apps.map((e) => {
+        const appShellId = `epinio-${ namespace }/${ e }-app-shell`;
+        const appLogId = `epinio-${ namespace }/${ e }-app-logs`;
+
+        if ( allTabs.length > 0 ) {
+          allTabs.map((el) => {
+            const stagingLog = `epinio-${ namespace }/${ e }-logs-`;
+
+            if (el.id.startsWith(stagingLog)) {
+              this.$dispatch('wm/close', el.id, { root: true });
+            }
+          });
+        }
+
+        this.$dispatch('wm/close', appLogId, { root: true });
+        this.$dispatch('wm/close', appShellId, { root: true });
+      });
     }
   }
 }
