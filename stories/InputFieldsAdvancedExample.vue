7a3764675fd062e701a38b8ca2b650d8904939a4
Richard Cox <richard.cox@suse.com>
2022-08-23 15:12:30 +0100

    Fix merge conflicts

--- stories/InputFieldsAdvancedExample.vue
+++ stories/InputFieldsAdvancedExample.vue
@@ -5,11 +5,7 @@ import Taints from '@shell/components/form/Taints.vue';
 import Tolerations from '@shell/components/form/Tolerations.vue';
 import Upgrading from '@shell/edit/workload/Upgrading.vue';
 import Security from '@shell/components/form/Security.vue';
-<<<<<<< HEAD
 import LabeledInput from '@components/Form/LabeledInput/LabeledInput.vue';
-=======
-import LabeledInput from '@/pkg/rancher-components/src/components/Form/LabeledInput/LabeledInput.vue';
->>>>>>> import path fixes
 import LabeledSelect from '@shell/components/form/LabeledSelect.vue';
 
 export default {
