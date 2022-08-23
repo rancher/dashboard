5193cf42af1b38609f6138407ef6a4e0e6bd0f3a
Neil MacDougall <nwmac@users.noreply.github.com>
2022-08-23 10:04:21 +0100

    Remove window.v debugging code (#6684)
    * Remove window.v debugging code
    
    * Remove lookup debugging tool which also added items to the global window object

--- shell/nuxt.config.js
+++ shell/nuxt.config.js
@@ -570,7 +570,6 @@ export default function(dir, _appConfig) {
       path.join(NUXT_SHELL, 'plugins/global-formatters'),
       path.join(NUXT_SHELL, 'plugins/trim-whitespace'),
       { src: path.join(NUXT_SHELL, 'plugins/extend-router') },
-      { src: path.join(NUXT_SHELL, 'plugins/lookup'), ssr: false },
       { src: path.join(NUXT_SHELL, 'plugins/console'), ssr: false },
       { src: path.join(NUXT_SHELL, 'plugins/int-number'), ssr: false },
       { src: path.join(NUXT_SHELL, 'plugins/nuxt-client-init'), ssr: false },
