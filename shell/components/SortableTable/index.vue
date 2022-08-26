b26af7d73802da0119187a5a4145e5ad6f7657db
Alexandre Alves <aalves@Alexandres-MBP.lan>
2022-08-26 12:13:43 +0100

    bump timeout

--- shell/components/SortableTable/index.vue
+++ shell/components/SortableTable/index.vue
@@ -387,7 +387,7 @@ export default {
       if (old && !neu) {
         this.manualRefreshTimer = setTimeout(() => {
           this.watcherUpdateLiveAndDelayed(neu, old);
-        }, 500);
+        }, 1000);
       }
     }
   },
