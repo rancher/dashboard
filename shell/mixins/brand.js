rancher/dashboard

commit 771f168a5b33f970f47bcefb272cfa199b3d10be

Allow the backend to override the theme - Used in non-rancher world where the ability to select theme isn't supported - If no `ui-theme` present resort to previous method of working out theme - https://github.com/epinio/ui/issues/125

shell/mixins/brand.js
@@ -5,15 +5,15 @@ import { findBy } from '@shell/utils/array';
 import { createCssVars } from '@shell/utils/color';
 
 export default {
-  async fetch() {
-    this.globalSettings = await this.$store.getters['management/all'](MANAGEMENT.SETTING);
-  },
-
   data() {
-    return { globalSettings: [], brandCookie: null };
+    return { brandCookie: null };
   },
 
   computed: {
+    globalSettings() {
+      return this.$store.getters['management/all'](MANAGEMENT.SETTING);
+    },
+
     brand() {
       const setting = findBy(this.globalSettings, 'id', SETTING.BRAND);
 
@@ -33,6 +33,13 @@ export default {
     },
 
     theme() {
+      const setting = findBy(this.globalSettings, 'id', SETTING.THEME);
+
+      // This handles cases where the settings update after the page loads (like on log out)
+      if (setting?.value) {
+        return setting?.value;
+      }
+
       return this.$store.getters['prefs/theme'];
     }
   },

shell/config/settings.js
@@ -53,6 +53,11 @@ export const SETTING = {
   PRIMARY_COLOR:                    'ui-primary-color',
   LINK_COLOR:                       'ui-link-color',
   COMMUNITY_LINKS:                  'ui-community-links',
+  /**
+   * Allow the backend to force a light/dark theme. Used in non-rancher world and results in the theme used
+   * both pre and post log in. If not present defaults to the usual process
+   */
+  THEME:                            'ui-theme',
   SYSTEM_NAMESPACES:                 'system-namespaces'
 };
 

shell/store/prefs.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import { MANAGEMENT, STEVE } from '@shell/config/types';
 import { clone } from '@shell/utils/object';
 import { SETTING } from '@shell/config/settings';
+import { findBy } from '~/shell/utils/array';
 
 const definitions = {};
 
@@ -163,7 +164,13 @@ export const getters = {
     return definition.options.slice();
   },
 
-  theme: (state, getters) => {
+  theme: (state, getters, rootState, rootGetters) => {
+    const setting = rootGetters['management/byId'](MANAGEMENT.SETTING, SETTING.THEME);
+
+    if (setting?.value) {
+      return setting?.value;
+    }
+
     let theme = getters['get'](THEME);
     const pcs = getters['get'](PREFERS_SCHEME);
 

commit 733caceb3637fa9945bc52e875f09c4c822304c4

Fix application of branding, color and theme from settings (#6706)
- The watchers for settings apply them
- This was not happening as they never changed from the initial value
- Caused by https://github.com/rancher/dashboard/commit/771f168a5b33f970f47bcefb272cfa199b3d10be
- Revert to the initial way. This may break epinio but that can be fixed later on

shell/mixins/brand.js
@@ -12,16 +12,15 @@ export default {
         this.apps = await this.$store.dispatch('management/findAll', { type: CATALOG.APP });
       }
     } catch (e) {}
+
+    this.globalSettings = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.SETTING });
   },
 
   data() {
-    return { apps: [] };
+    return { apps: [], globalSettings: [] };
   },
 
   computed: {
-    globalSettings() {
-      return this.$store.getters['management/all'](MANAGEMENT.SETTING);
-    },
 
     brand() {
       const setting = findBy(this.globalSettings, 'id', SETTING.BRAND);