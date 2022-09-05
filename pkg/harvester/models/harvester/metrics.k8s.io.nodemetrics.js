316d875f7449578d808123db1ba1b7f055d7d92a
Richard Cox <richard.cox@suse.com>
2022-09-05 18:11:08 +0100

    Fix some of the missing node metrics - Previously the harvester store was a steve store with access to all steve models - Now the harvester store pretends to be a steve store - Importantly it doesn't have access to the default dashboard models   - This is done to reduce bundle size (otherwise would import them all)   - So TODO check that all models used by harvester plugin have their own direct versions

---
+++ pkg/harvester/models/harvester/metrics.k8s.io.nodemetrics.js
@@ -0,0 +1,5 @@
+import NodeMetric from '@shell/models/metrics.k8s.io.nodemetrics';
+
+// TODO: RC FIX Stats on home page - storage used doesn't look correct
+// TODO: RC FIX other places where the harvester plugin uses steve models
+export default class HciNodeMetric extends NodeMetric { }
