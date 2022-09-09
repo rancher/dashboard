99db7a83797a56fbc2d2f0915978faf4316abe04
bisht-richa <richa.bisht@suse.com>
2022-09-09 09:28:04 +0200

    Removed extra string

--- shell/pages/prefs.vue
+++ shell/pages/prefs.vue
@@ -209,7 +209,7 @@ export default {
         <h4 v-t="'prefs.advanced'" />
         <Checkbox v-model="dev" :label="t('prefs.dev.label', {}, true)" />
         <p class="wrap-text">
-          {{ t('prefs.advancedTooltip', {}, raw=true) }}
+          {{ t('prefs.advancedTooltip') }}
         </p>
         <br>
         <Checkbox v-if="!isSingleProduct" v-model="hideDescriptions" :label="t('prefs.hideDesc.label')" class="mt-10" />
