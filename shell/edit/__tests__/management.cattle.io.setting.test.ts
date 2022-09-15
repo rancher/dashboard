f4b69ea58fae9aeaff4399ce56c5d04c34a5593f
cnotv <giuseppe.leo@suse.com>
2022-09-15 12:49:04 +0200

    Add test for no validation cases

--- shell/edit/__tests__/management.cattle.io.setting.test.ts
+++ shell/edit/__tests__/management.cattle.io.setting.test.ts
@@ -3,24 +3,38 @@ import Settings from '@shell/edit/management.cattle.io.setting.vue';
 
 describe('management.cattle.io.setting should', () => {
   describe('validate input with provided settings', () => {
+    const requiredSetup = () => ({
+      propsData: { value: { value: 'anything' } },
+      // Remove all these mocks after migration to Vue 2.7/3 due mixin logic
+      mocks:     {
+        $store: {
+          getters: {
+            currentStore:              () => 'current_store',
+            'current_store/schemaFor': jest.fn(),
+            'current_store/all':       jest.fn(),
+            'i18n/t':                  jest.fn(),
+            'i18n/exists':             jest.fn(),
+          }
+        },
+        $route:  { query: { AS: '' } },
+        $router: { applyQuery: jest.fn() },
+      }
+    });
+
+    it('allowing to save if no rules', () => {
+      const wrapper = mount(Settings, {
+        data: () => ({ setting: { } }),
+        ...requiredSetup()
+      });
+      const saveButton = wrapper.find('[data-testid="form-save"]').element as HTMLInputElement;
+
+      expect(saveButton.disabled).toBe(false);
+    });
+
     it('allowing to save if valid', () => {
       const wrapper = mount(Settings, {
-        data:      () => ({ setting: { rules: [() => undefined] } }),
-        propsData: { value: { value: 'anything' } },
-        // Remove all these mocks after migration to Vue 2.7/3 due mixin logic
-        mocks:     {
-          $store: {
-            getters: {
-              currentStore:              () => 'current_store',
-              'current_store/schemaFor': jest.fn(),
-              'current_store/all':       jest.fn(),
-              'i18n/t':                  jest.fn(),
-              'i18n/exists':             jest.fn(),
-            }
-          },
-          $route:  { query: { AS: '' } },
-          $router: { applyQuery: jest.fn() },
-        }
+        data: () => ({ setting: { rules: [() => undefined] } }),
+        ...requiredSetup()
       });
       const saveButton = wrapper.find('[data-testid="form-save"]').element as HTMLInputElement;
 
@@ -29,22 +43,8 @@ describe('management.cattle.io.setting should', () => {
 
     it('preventing to save if any error', () => {
       const wrapper = mount(Settings, {
-        data:      () => ({ setting: { rules: [() => 'error'] } }),
-        propsData: { value: { value: 'anything' } },
-        // Remove all these mocks after migration to Vue 2.7/3 due mixin logic
-        mocks:     {
-          $store: {
-            getters: {
-              currentStore:              () => 'current_store',
-              'current_store/schemaFor': jest.fn(),
-              'current_store/all':       jest.fn(),
-              'i18n/t':                  jest.fn(),
-              'i18n/exists':             jest.fn(),
-            }
-          },
-          $route:  { query: { AS: '' } },
-          $router: { applyQuery: jest.fn() },
-        }
+        data: () => ({ setting: { rules: [() => 'error'] } }),
+        ...requiredSetup()
       });
       const saveButton = wrapper.find('[data-testid="form-save"]').element as HTMLInputElement;
 
