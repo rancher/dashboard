9364022cbd00946a6109560441af9207b0cb3677
Robert Patrick <robert.patrick@oracle.com>
2022-09-22 09:43:52 -0500

    adding missing customizations script

---
+++ verrazzano.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Added by Verrazzano
+#
+# This script is used to install Rancher Dashboard customizations
+# where the customization does not require changing the application source code.
+#
+if [ ! -z "${KUBECONFIG}" ]; then
+  export KUBECONFIG
+fi
+
+label_patch="{ \"op\": \"replace\", \"path\": \"/value\", \"value\": \"Verrazzano\" }"
+kubectl patch Setting ui-pl -n cattle-impersonation-system --type json -p "[${label_patch}]"
+
+logo_dir="$(dirname "$0")"/pkg/verrazzano/assets/images
+
+logo_base64=$(cat $logo_dir/verrazzano-light.svg | base64)
+kubectl apply -f - <<EOF
+apiVersion: management.cattle.io/v3
+kind: Setting
+metadata:
+    name: ui-logo-light
+value: "data:image/svg+xml;base64,${logo_base64}"
+EOF
+
+logo_base64=$(cat $logo_dir/verrazzano-dark.svg | base64)
+kubectl apply -f - <<EOF
+apiVersion: management.cattle.io/v3
+kind: Setting
+metadata:
+    name: ui-logo-dark
+value: "data:image/svg+xml;base64,${logo_base64}"
+EOF
