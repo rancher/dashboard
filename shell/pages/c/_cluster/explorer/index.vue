75ad91c287adc138c9bcce4a5cae1672ca3d46b9
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2022-09-26 09:25:37 +0100

    Add PSP Removal banners (#6943)
    * add deprecation banner for PSPs on homepage to be displayed only if the user has any
    
    * revert changes done on home page + update copy for deprecation banner + add deprecation banner to cluster dashboard
    
    * address PR comments
    
    Co-authored-by: Alexandre Alves <aalves@Alexandres-MacBook-Pro.local>

--- shell/pages/c/_cluster/explorer/index.vue
+++ shell/pages/c/_cluster/explorer/index.vue
@@ -111,12 +111,12 @@ export default {
 
     return {
       nodeHeaders,
-      constraints:         [],
-      events:              [],
-      nodeMetrics:         [],
-      showClusterMetrics: false,
-      showK8sMetrics:     false,
-      showEtcdMetrics:    false,
+      constraints:                 [],
+      events:                      [],
+      nodeMetrics:                 [],
+      showClusterMetrics:          false,
+      showK8sMetrics:              false,
+      showEtcdMetrics:             false,
       CLUSTER_METRICS_DETAIL_URL,
       CLUSTER_METRICS_SUMMARY_URL,
       K8S_METRICS_DETAIL_URL,
@@ -124,7 +124,8 @@ export default {
       ETCD_METRICS_DETAIL_URL,
       ETCD_METRICS_SUMMARY_URL,
       clusterCounts,
-      selectedTab:         'cluster-events',
+      selectedTab:                 'cluster-events',
+      displayPspDeprecationBanner: false
     };
   },
 
@@ -136,6 +137,28 @@ export default {
     this.$store.dispatch('cluster/forgetType', ENDPOINTS); // Used by AlertTable to get alerts when v2 monitoring is installed
     this.$store.dispatch('cluster/forgetType', METRIC.NODE);
     this.$store.dispatch('cluster/forgetType', MANAGEMENT.NODE);
+  },
+
+  watch: {
+    // we need to hook up this API call to a watcher because the page logic is based on a getter
+    // this seems reasonable, so that we don't disrupt the optimal loading times of the page
+    currentCluster: {
+      async handler(neu, old) {
+        if (neu && (!old || old.id !== neu.id)) {
+          const major = neu.status?.version?.major ? parseInt(neu.status?.version?.major) : 0;
+          const minor = neu.status?.version?.minor ? parseInt(neu.status?.version?.minor) : 0;
+
+          if (major === 1 && minor >= 21 && minor < 25) {
+            const psps = await this.$store.dispatch('cluster/request', { url: `/k8s/clusters/${ neu.id }/v1/policy.podsecuritypolicies` });
+
+            if (psps && psps.data && psps.data.length) {
+              this.displayPspDeprecationBanner = true;
+            }
+          }
+        }
+      },
+      immediate: true
+    },
   },
 
   computed: {
@@ -378,6 +401,12 @@ export default {
         </div>
       </div>
     </header>
+    <Banner
+      v-if="displayPspDeprecationBanner"
+      color="warning"
+    >
+      <t k="landing.deprecatedPsp" :raw="true" />
+    </Banner>
     <Banner
       v-if="!hideClusterToolsTip"
       :closable="true"
