9943d8cd77a573dc3ed93f785814dfcddfac81d0
Alexandre Alves <aalves@Alexandres-MacBook-Pro.local>
2022-09-27 10:28:20 +0100

    revert deletion of clusterrepo due to CI/CD validation of plugin

---
+++ shell/list/catalog.cattle.io.clusterrepo.vue
@@ -0,0 +1,36 @@
+<script>
+import ResourceTable from '@shell/components/ResourceTable';
+export default {
+  name:       'ListClusterReposApps',
+  components: { ResourceTable },
+  props:      {
+    resource: {
+      type:     String,
+      required: true,
+    },
+    schema: {
+      type:     Object,
+      required: true,
+    },
+    rows: {
+      type:     Array,
+      required: true,
+    },
+  },
+  computed: {
+    loading() {
+      return this.rows.length ? false : this.$fetchState?.pending;
+    }
+  },
+};
+</script>
+
+<template>
+  <div>
+    <ResourceTable
+      :schema="schema"
+      :rows="rows"
+      :loading="loading"
+    />
+  </div>
+</template>
