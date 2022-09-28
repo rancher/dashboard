344fcc96c9c3e406a38448c06f1148462ebaf8c4
scures <scurescu@suse.com>
2022-09-28 13:07:35 +0200

    uses LabeledInput instead of html:input

--- shell/components/PromptRemove.vue
+++ shell/components/PromptRemove.vue
@@ -9,11 +9,13 @@ import { uniq } from '@shell/utils/array';
 import AsyncButton from '@shell/components/AsyncButton';
 import { CATALOG as CATALOG_ANNOTATIONS } from '@shell/config/labels-annotations';
 import { CATALOG } from '@shell/config/types';
+import { LabeledInput } from '@components/Form/LabeledInput';
+
 export default {
   name: 'PromptRemove',
 
   components: {
-    Card, Checkbox, AsyncButton
+    Card, Checkbox, AsyncButton, LabeledInput
   },
   props: {
     /**
@@ -369,14 +371,13 @@ export default {
             </div>
           </template>
         </div>
-        <input
+        <LabeledInput
           v-if="needsConfirm"
           id="confirm"
-          ref="inputRef"
           v-model="confirmName"
+          v-focus
           :data-testid="componentTestid + '-input'"
           type="text"
-          autofocus
         />
         <div class="text-warning mb-10 mt-10">
           {{ warning }}
