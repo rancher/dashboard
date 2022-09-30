e1adf6af9735ad0e5d5bbe61fedb1d8201b8676a
wujun <897415845@qq.com>
2022-09-30 12:32:09 +0800

    HARVESTER: fix select style

--- shell/components/form/LabeledSelect.vue
+++ shell/components/form/LabeledSelect.vue
@@ -398,6 +398,7 @@ export default {
         height: 20px;
         min-height: unset !important;
         padding: 0 0 0 7px !important;
+        white-space: nowrap;
 
         > button {
           height: 20px;
