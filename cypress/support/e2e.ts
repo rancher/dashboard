1c6b5ac6663ce0b56621a423dc820d76cc68f585
Giuseppe Leo <giuseppe.leo@suse.com>
2022-09-30 10:36:36 +0200

    Generate coverage value for both unit and E2E tests (#6690)
    * Correct Jest coverage configuration
    
    * Add coverage upload
    
    * Exclude operationsl scripts from linting
    
    * Rephrase title based on pull request title, run and attempt, with fallback for commits
    
    * Add instrumentation for E2E coverage report
    
    * Add merge logic to the coverage files
    
    * Add instrumentalization for Cypress to generate coverage for E2E
    
    * Download artifacts before merge coverage in CI
    
    * Merge fix
    
    * Copy coverage files before artifact upload
    
    * Add summary after running tests
    
    * Change restriction rules about coverage for pull_request and push
    
    * Add documentation for the coverage
    
    * Correct test documentation folder restructuring
    
    * Add inclusion list for Cypress coverage; add scripts as exclusion
    
    * Extend ignored path to be parsed for Jest to improve performances
    
    * Workflow cleanup
    
    * Checkout current branch head on test runs
    
    * Submit separated coverage with different flags

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,45 +1,5 @@
-
-import { Matcher } from '~/cypress/support/types';
+import '@cypress/code-coverage/support';
 import './commands';
-
-declare global {
-  // eslint-disable-next-line no-unused-vars
-  namespace Cypress {
-    interface Chainable {
-      login(username?: string, password?: string, cacheSession?: boolean): Chainable<Element>;
-      byLabel(label: string,): Chainable<Element>;
-
-      /**
-       *  Wrapper for cy.get() to simply define the data-testid value that allows you to pass a matcher to find the element.
-       * @param id Value used for the data-testid attribute of the element.
-       * @param matcher Matching character used for attribute value:
-       * - `$`: Suffixed with this value
-       * - `^`: Prefixed with this value
-       * - `~`: Contains this value as whitespace separated words
-       * - `*`: Contains this value
-       */
-      getId(id: string, matcher?: Matcher): Chainable<Element>;
-
-      /**
-       *  Wrapper for cy.find() to simply define the data-testid value that allows you to pass a matcher to find the element.
-       * @param id Value used for the data-testid attribute of the element.
-       * @param matcher Matching character used for attribute value:
-       * - `$`: Suffixed with this value
-       * - `^`: Prefixed with this value
-       * - `~`: Contains this value as whitespace separated words
-       * - `*`: Contains this value
-       */
-      findId(id: string, matcher?: Matcher): Chainable<Element>;
-
-      /**
-       * Override user preferences to default values, allowing to pass custom preferences for a deterministic scenario
-       * Leave empty for reset to default values
-       */
-      // eslint-disable-next-line no-undef
-      userPreferences(preferences?: Partial<UserPreferences>): Chainable<null>;
-    }
-  }
-}
 
 // TODO handle redirection errors better?
 // we see a lot of 'error navigation cancelled' uncaught exceptions that don't actually break anything; ignore them here
