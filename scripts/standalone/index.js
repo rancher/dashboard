68895e4579fbbdc0cda6f1c31bb5ac812677a6e1
Shavindra <shavindra@users.noreply.github.com>
2022-10-07 16:57:27 +0100

    merge master.

--- scripts/standalone/index.js
+++ scripts/standalone/index.js
@@ -50,9 +50,7 @@ const proxy = {
   '/p/':             proxyOpts('https://127.0.0.1:8000'),
   '/assets':         proxyOpts('https://127.0.0.1:8000'),
   '/translations':   proxyOpts('https://127.0.0.1:8000'),
-  '/engines-dist':   proxyOpts('https://127.0.0.1:8000'),
-  // Plugin dev
-  '/verdaccio/':     proxyOpts('http://127.0.0.1:4873/-'),
+  '/engines-dist':   proxyOpts('https://127.0.0.1:8000')
 };
 
 const proxies = {};
