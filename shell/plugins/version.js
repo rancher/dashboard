91aefb56122630509f40f091dc59880246f5e70c
Shavindra <shavindra@users.noreply.github.com>
2022-10-07 17:47:42 +0100

    test without /rancherversion

--- shell/plugins/version.js
+++ shell/plugins/version.js
@@ -1,9 +1,9 @@
 import { setIsRancherPrime } from '@shell/config/version';
-export default async function({ $axios }) {
+export default function({ $axios }) {
   try {
-    const response = await $axios.get('/rancherversion');
+    // const response = await $axios.get('/rancherversion');
 
-    setIsRancherPrime(response.data);
+    // setIsRancherPrime(response.data);
   } catch (e) {
     console.log('Failed to fetch Ranhcer Version', e); // eslint-disable-line no-console
   }
