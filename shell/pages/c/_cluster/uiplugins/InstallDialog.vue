382b5178f9c432af5741a6af4d48d7d7568517f4
Neil MacDougall <nmacdougall@suse.com>
2022-10-10 16:04:01 +0100

    Change way we set defautl system registry

--- shell/pages/c/_cluster/uiplugins/InstallDialog.vue
+++ shell/pages/c/_cluster/uiplugins/InstallDialog.vue
@@ -159,7 +159,9 @@ export default {
       const defaultRegistry = this.defaultRegistrySetting?.value || '';
 
       if (isRancherImage && defaultRegistry) {
-        chart.values.global = { cattle: { systemDefaultRegistry: defaultRegistry } };
+        chart.values.global = chart.values.global || {};
+        chart.values.global.cattle = chart.values.global.cattle || {};
+        chart.values.global.cattle.systemDefaultRegistry = defaultRegistry;
       }
 
       // Pass in the system default registry property if set - only if the image is in the rancher org
