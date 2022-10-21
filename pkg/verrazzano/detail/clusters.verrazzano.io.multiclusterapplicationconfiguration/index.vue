17f643d697549c4e6aab3999bd40b876b3a16dca
Mason Taube <mason.taube@oracle.com>
2022-10-21 11:08:03 -0400

    Multi-Cluster App Details page (#62)
    * Query for pods on non-local clusters for multicluster applications
    
    * missing comment
    
    * use watchers for multiclusterapplication pod list

--- pkg/verrazzano/detail/clusters.verrazzano.io.multiclusterapplicationconfiguration/index.vue
+++ pkg/verrazzano/detail/clusters.verrazzano.io.multiclusterapplicationconfiguration/index.vue
@@ -1,5 +1,6 @@
 <script>
 // Added by Verrazzano
+
 import CountGauge from '@shell/components/CountGauge';
 import DashboardMetrics from '@shell/components/DashboardMetrics';
 import Loading from '@shell/components/Loading';
@@ -9,11 +10,18 @@ import Tab from '@shell/components/Tabbed/Tab';
 import VerrazzanoHelper from '@pkg/mixins/verrazzano-helper';
 import V1WorkloadMetrics from '@shell/mixins/v1-workload-metrics';
 
-import { STATE, NAME, NODE, POD_IMAGES } from '@shell/config/table-headers';
-import { POD, WORKLOAD_TYPES } from '@shell/config/types';
+import { STATE, NAME, POD_IMAGES } from '@shell/config/table-headers';
+import { MANAGEMENT, POD, WORKLOAD_TYPES } from '@shell/config/types';
 import { allHash } from '@shell/utils/promise';
 import { mapGetters } from 'vuex';
 import { allDashboardsExist } from '@shell/utils/grafana';
+
+import ClusteredPod from '@pkg/models/clustered-pod';
+
+import Socket, {
+  EVENT_CONNECTED,
+  EVENT_MESSAGE,
+} from '@shell/utils/socket';
 
 const WORKLOAD_METRICS_DETAIL_URL = '/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/rancher-workload-pods-1/rancher-workload-pods?orgId=1';
 const WORKLOAD_METRICS_SUMMARY_URL = '/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/rancher-workload-1/rancher-workload?orgId=1';
@@ -49,41 +57,152 @@ export default {
   mixins: [VerrazzanoHelper, V1WorkloadMetrics],
   data() {
     return {
-      fetchInProgress: true,
-      namespace:       this.value.metadata?.namespace,
-      allPods:         {},
-      allJobs:         [],
-      showMetrics:     false,
+      actionMenuTargetElement:  null,
+      actionMenuTargetEvent:    null,
+      actionMenuIsOpen:        false,
+      fetchInProgress:         true,
+      namespace:               this.value.metadata?.namespace,
+      allPods:                 {},
+      allJobs:                 [],
+      showMetrics:             false,
       WORKLOAD_METRICS_DETAIL_URL,
       WORKLOAD_METRICS_SUMMARY_URL,
+      rowsData:                null,
+      sockets:                 {},
     };
   },
+
+  destroyed() {
+    Object.values(this.sockets).forEach((socket) => {
+      socket.disconnect();
+    });
+
+    this.sockets = {};
+  },
+
   async fetch() {
-    const hash = { allPods: this.$store.dispatch('management/findAll', { type: POD }) };
+    const hash = {
+      allClusters: this.$store.dispatch('management/findAll', {
+        type: MANAGEMENT.CLUSTER,
+        opt:  { url: MANAGEMENT.CLUSTER }
+      })
+    };
 
     if (this.value.type === WORKLOAD_TYPES.CRON_JOB) {
       hash.allJobs = this.$store.dispatch('management/findAll', { type: WORKLOAD_TYPES.JOB });
     }
+
     const res = await allHash(hash);
-
-    if (res.allPods) {
-      const appName = this.value.metadata.name;
-      const filteredPods = res.allPods.filter(pod => pod.metadata.labels['app.oam.dev/name'] === appName);
-
-      this.sortObjectsByNamespace(filteredPods, this.allPods);
-    }
 
     if (res.allJobs) {
       this.allJobs = res.allJobs;
+    }
+
+    this.allPods[this.namespace] = [];
+
+    const appName = this.value.metadata.name;
+
+    if (res.allClusters) {
+      const placementClusters = res.allClusters.filter((cluster) => {
+        return this.value?.spec?.placement?.clusters?.find(c => c.name === cluster.nameDisplay);
+      });
+
+      for (const cluster of placementClusters) {
+        const socket = getSocket.call(this, cluster);
+
+        socket.addEventListener(EVENT_CONNECTED, (e) => {
+          socket.send(JSON.stringify({
+            resourceType:    'pod',
+            resourceVersion: '0'
+          }));
+        });
+
+        socket.addEventListener(EVENT_MESSAGE, (e) => {
+          const event = e.detail;
+
+          if ( event.data) {
+            const msg = JSON.parse(event.data);
+
+            if (msg.name) {
+              if (msg.name === 'resource.change') {
+                const updatedPod = ClusteredPodFactory.call(this, msg.data, cluster);
+
+                const objectIx = this.allPods[updatedPod.metadata.namespace]?.findIndex(podItem => podItem.id === updatedPod.id);
+
+                if (objectIx !== -1) {
+                  this.allPods[updatedPod.metadata.namespace]?.splice(objectIx, 1, updatedPod);
+
+                  this.resetPods();
+                }
+              } else if (msg.name === 'resource.create') {
+                const newPod = ClusteredPodFactory.call(this, msg.data, cluster);
+
+                if (newPod?.metadata?.labels['app.oam.dev/name'] === appName && newPod?.metadata?.namespace === this.namespace) {
+                  this.allPods[newPod.metadata.namespace]?.push(newPod);
+
+                  this.resetPods();
+                }
+              } else if (msg.name === 'resource.remove') {
+                const id = msg.data?.id;
+                const namespace = msg.data?.metadata?.namespace;
+
+                const index = this.allPods[namespace]?.findIndex(podItem => podItem.id === id);
+
+                if (index > -1) {
+                  this.allPods[namespace].splice(index, 1);
+                }
+
+                this.resetPods();
+              }
+            }
+          }
+        });
+
+        socket.connect();
+      }
     }
 
     const isMetricsSupportedKind = METRICS_SUPPORTED_KINDS.includes(this.value.type);
 
     this.showMetrics = isMetricsSupportedKind && await allDashboardsExist(this.$store, this.currentCluster.id, [WORKLOAD_METRICS_DETAIL_URL, WORKLOAD_METRICS_SUMMARY_URL]);
     this.fetchInProgress = false;
+
+    function ClusteredPodFactory(data, cluster) {
+      const inStore = this.$store.getters['currentProduct'].inStore;
+
+      const pod = new ClusteredPod(data, {
+        dispatch: (c, payload) => {
+          return this.$store.dispatch(c.includes('/') ? c : `${ inStore }/${ c }`, payload);
+        },
+        getters: {
+          schemaFor: (type) => {
+            this.$store.getters[`${ inStore }/schemaFor`](type);
+          }
+        },
+        rootGetters: this.$store.getters
+      });
+
+      if (cluster) {
+        pod.cluster = cluster.id;
+        pod.clusterDisplay = cluster.nameDisplay;
+      }
+
+      return pod;
+    }
+
+    function getSocket(cluster) {
+      const clusterId = cluster?.id;
+
+      const subscribeUrl = `/k8s/clusters/${ clusterId }/v1/subscribe`;
+
+      this.sockets[clusterId] = this.sockets[clusterId] ? this.sockets[clusterId] : new Socket(subscribeUrl);
+
+      return this.sockets[clusterId];
+    }
   },
   computed:   {
     ...mapGetters(['currentCluster']),
+    ...mapGetters({ t: 'i18n/t' }),
     isJob() {
       return this.value.type === WORKLOAD_TYPES.JOB;
     },
@@ -144,8 +263,21 @@ export default {
     podHeaders() {
       return [
         STATE,
-        NAME,
-        NODE,
+        {
+          ...NAME,
+          value:            row => row.metadata?.name,
+          formatter:     'ClusterLinkName',
+          formatterOpts: {
+            type:      POD,
+            namespace: this.value.namespace,
+          },
+        },
+        {
+          name:          'cluster',
+          labelKey:      'verrazzano.common.headers.cluster',
+          value:         'clusterDisplay',
+          sort:          ['nameSort'],
+        },
         POD_IMAGES
       ];
     },
@@ -175,6 +307,7 @@ export default {
   methods: {
     resetPods() {
       this.value.pods = this.allPods[this.namespace] || [];
+      this.rowsData = this.value.pods;
     }
   },
   watch: {
@@ -231,15 +364,17 @@ export default {
         />
       </Tab>
       <Tab v-else name="pods" :label="t('tableHeaders.pods')" :weight="4">
-        <SortableTable
-          v-if="value.pods"
-          :rows="value.pods"
-          :headers="podHeaders"
-          key-field="id"
-          :schema="podSchema"
-          :groupable="false"
-          :search="false"
-        />
+        <div>
+          <SortableTable
+            :rows="rowsData"
+            :headers="podHeaders"
+            key-field="id"
+            :schema="podSchema"
+            :search="false"
+            :row-actions="true"
+            :show-groups="false"
+          />
+        </div>
       </Tab>
       <Tab v-if="showMetrics" :label="t('workload.container.titles.metrics')" name="workload-metrics" :weight="3">
         <template #default="props">
