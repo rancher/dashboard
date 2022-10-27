5905a33ba09e6b299d25979a0c126df813ec1fb9
cnotv <giuseppe.leo@suse.com>
2022-10-27 20:51:48 +0200

    Restrict retry for run mode only

--- cypress.config.ts
+++ cypress.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
   projectId:              process.env.TEST_PROJECT_ID,
   defaultCommandTimeout:  process.env.TEST_TIMEOUT ? +process.env.TEST_TIMEOUT : 60000,
   trashAssetsBeforeRuns:  true,
-  retries:                2,
+  retries:               {
+    runMode:  2,
+    openMode: 0
+  },
   experimentalStudio:     true,
   env:                    {
     baseUrl,
