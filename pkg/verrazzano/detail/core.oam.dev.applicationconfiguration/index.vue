32f7514f8f3e8adba945321b09d165e430fe821a
Mason Taube <mason.taube@oracle.com>
2022-11-02 12:01:47 -0400

    use ResourceFetch plugin to query for pods/components

--- pkg/verrazzano/detail/core.oam.dev.applicationconfiguration/index.vue
+++ pkg/verrazzano/detail/core.oam.dev.applicationconfiguration/index.vue
@@ -1,7 +1,6 @@
 <script>
 // Added by Verrazzano
 import CountGauge from '@shell/components/CountGauge';
-import DashboardMetrics from '@shell/components/DashboardMetrics';
 import Loading from '@shell/components/Loading';
 import ResourceTabs from '@shell/components/form/ResourceTabs';
 import SortableTable from '@shell/components/SortableTable';
@@ -12,98 +11,41 @@ import V1WorkloadMetrics from '@shell/mixins/v1-workload-metrics';
 import { STATE, NAME, NODE, POD_IMAGES } from '@shell/config/table-headers';
 import { POD, WORKLOAD_TYPES } from '@shell/config/types';
 import { VZ_COMPONENT } from '@pkg/types';
-import { allHash } from '@shell/utils/promise';
 import { mapGetters } from 'vuex';
-import { allDashboardsExist } from '@shell/utils/grafana';
-
-const WORKLOAD_METRICS_DETAIL_URL = '/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/rancher-workload-pods-1/rancher-workload-pods?orgId=1';
-const WORKLOAD_METRICS_SUMMARY_URL = '/api/v1/namespaces/cattle-monitoring-system/services/http:rancher-monitoring-grafana:80/proxy/d/rancher-workload-1/rancher-workload?orgId=1';
-
-export const WORKLOAD_TYPE_TO_KIND_MAPPING = {
-  // Each deployment creates a replicaset and the metrics are published for a replicaset.
-  [WORKLOAD_TYPES.DEPLOYMENT]:             'ReplicaSet',
-  [WORKLOAD_TYPES.CRON_JOB]:               'CronJob',
-  [WORKLOAD_TYPES.DAEMON_SET]:             'DaemonSet',
-  [WORKLOAD_TYPES.JOB]:                    'Job',
-  [WORKLOAD_TYPES.STATEFUL_SET]:           'StatefulSet',
-  [WORKLOAD_TYPES.REPLICA_SET]:            'ReplicaSet',
-  [WORKLOAD_TYPES.REPLICATION_CONTROLLER]: 'ReplicationController',
-};
-
-const METRICS_SUPPORTED_KINDS = [
-  WORKLOAD_TYPES.DAEMON_SET,
-  WORKLOAD_TYPES.REPLICA_SET,
-  WORKLOAD_TYPES.STATEFUL_SET,
-  WORKLOAD_TYPES.DEPLOYMENT
-];
+import ResourceFetch from '@shell/mixins/resource-fetch';
 
 export default {
   name:       'VerrazzanoApplicationDetail',
   components: {
     CountGauge,
-    DashboardMetrics,
     Loading,
     ResourceTabs,
     SortableTable,
     Tab,
   },
-  mixins: [VerrazzanoHelper, V1WorkloadMetrics],
+  mixins: [VerrazzanoHelper, V1WorkloadMetrics, ResourceFetch],
   data() {
     return {
-      fetchInProgress: true,
-      namespace:       this.value.metadata?.namespace,
-      allPods:         {},
-      allComponents:   {},
-      allJobs:         [],
-      showMetrics:     false,
-      WORKLOAD_METRICS_DETAIL_URL,
-      WORKLOAD_METRICS_SUMMARY_URL,
+      fetchInProgress:    true,
+      namespace:          this.value.metadata?.namespace,
+      allPods:            {},
+      displayPods:        [],
+      allComponents:      {},
+      componentResources: null,
+      podResources:       null,
     };
   },
-  async fetch() {
-    const hash = { allPods: this.$store.dispatch('management/findAll', { type: POD }) };
+  fetch() {
+    const self = this;
 
-    if (this.value.type === WORKLOAD_TYPES.CRON_JOB) {
-      hash.allJobs = this.$store.dispatch('management/findAll', { type: WORKLOAD_TYPES.JOB });
-    }
-
+    this.$fetchType(POD).then((pods) => {
+      self.podResources = pods;
+    });
     if (this.$store.getters['cluster/schemaFor'](VZ_COMPONENT)) {
-      hash.allComponents = this.$store.dispatch('management/findAll', { type: VZ_COMPONENT });
+      this.$fetchType(VZ_COMPONENT).then((components) => {
+        self.componentResources = components;
+      });
     }
-
-    const res = await allHash(hash);
-
-    if (res.allPods) {
-      const appName = this.value.metadata.name;
-      const filteredPods = res.allPods.filter(pod => pod.metadata.labels['app.oam.dev/name'] === appName);
-
-      this.sortObjectsByNamespace(filteredPods, this.allPods);
-      this.resetPods();
-    }
-
-    if (res.allComponents) {
-      const filteredComponents = this.value?.status?.workloads ? [] : res.allComponents;
-
-      if (this.value?.status?.workloads) {
-        res.allComponents.forEach((component) => {
-          if (this.value.status.workloads.find(workload => workload.componentName === component.name)) {
-            filteredComponents.push(component);
-          }
-        });
-      }
-
-      this.sortObjectsByNamespace(filteredComponents, this.allComponents);
-      this.resetComponents();
-    }
-
-    if (res.allJobs) {
-      this.allJobs = res.allJobs;
-    }
-
-    const isMetricsSupportedKind = METRICS_SUPPORTED_KINDS.includes(this.value.type);
-
-    this.showMetrics = isMetricsSupportedKind && await allDashboardsExist(this.$store, this.currentCluster.id, [WORKLOAD_METRICS_DETAIL_URL, WORKLOAD_METRICS_SUMMARY_URL]);
-    this.fetchInProgress = false;
   },
   computed:   {
     ...mapGetters(['currentCluster']),
@@ -131,30 +73,6 @@ export default {
     componentSchema() {
       return this.$store.getters['cluster/schemaFor']('core.oam.dev.component');
     },
-    container() {
-      return this.podTemplateSpec?.containers[0];
-    },
-    jobSchema() {
-      return this.$store.getters['cluster/schemaFor'](WORKLOAD_TYPES.JOB);
-    },
-    jobHeaders() {
-      return this.$store.getters['type-map/headersFor'](this.jobSchema);
-    },
-    totalRuns() {
-      if (!this.value.jobs) {
-        return;
-      }
-
-      return this.value.jobs.reduce((total, job) => {
-        const { status = {} } = job;
-
-        total += (status.active || 0);
-        total += (status.succeeded || 0);
-        total += (status.failed || 0);
-
-        return total;
-      }, 0);
-    },
     podRestarts() {
       return this.value.pods.reduce((total, pod) => {
         const { status:{ containerStatuses = [] } } = pod;
@@ -178,38 +96,51 @@ export default {
         POD_IMAGES
       ];
     },
-    graphVarsWorkload() {
-      return this.value.type === WORKLOAD_TYPES.DEPLOYMENT ? this.value.replicaSetId : this.value.shortId;
-    },
-    graphVars() {
-      return {
-        namespace: this.value.namespace,
-        kind:      WORKLOAD_TYPE_TO_KIND_MAPPING[this.value.type],
-        workload:  this.graphVarsWorkload
-      };
-    },
+
     showPodGaugeCircles() {
       const podGauges = Object.values(this.value.podGauges);
       const total = this.value.pods.length;
 
       return !podGauges.find(pg => pg.count === total);
     },
-    showJobGaugeCircles() {
-      const jobGauges = Object.values(this.value.jobGauges);
-      const total = this.isCronJob ? this.totalRuns : this.value.pods.length;
-
-      return !jobGauges.find(jg => jg.count === total);
-    }
   },
   methods: {
     resetPods() {
       this.value.pods = this.allPods[this.namespace] || [];
+      this.displayPods = this.value.pods;
     },
     resetComponents() {
       this.value.relatedComponents = this.allComponents[this.namespace] || [];
     }
   },
   watch: {
+    componentResources() {
+      if (this.componentResources) {
+        const filteredComponents = this.value?.status?.workloads ? [] : this.componentResources;
+
+        if (this.value?.status?.workloads) {
+          this.componentResources.forEach((component) => {
+            if (this.value.status.workloads.find(workload => workload.componentName === component.name)) {
+              filteredComponents.push(component);
+            }
+          });
+        }
+
+        this.sortObjectsByNamespace(filteredComponents, this.allComponents);
+        this.resetComponents();
+      }
+    },
+    podResources() {
+      if (this.podResources) {
+        const appName = this.value.metadata.name;
+        const filteredPods = this.podResources.filter(pod => pod.metadata.labels['app.oam.dev/name'] === appName);
+
+        this.allPods = {};
+        this.sortObjectsByNamespace(filteredPods, this.allPods);
+        this.resetPods();
+      }
+    },
+
     fetchInProgress() {
       this.resetPods();
       this.resetComponents();
@@ -227,21 +158,10 @@ export default {
   <Loading v-if="$fetchState.pending" />
   <div v-else>
     <h3>
-      {{ isJob || isCronJob ? t('workload.detailTop.runs') :t('verrazzano.common.titles.applicationDetails') }}
+      {{ t('verrazzano.common.titles.applicationDetails') }}
     </h3>
-    <div v-if="value.pods || value.jobGauges" class="gauges mb-20" :class="{'gauges__pods': !!value.pods}">
-      <template v-if="value.jobGauges">
-        <CountGauge
-          v-for="(group, key) in value.jobGauges"
-          :key="key"
-          :total="isCronJob? totalRuns : value.pods.length"
-          :useful="group.count || 0"
-          :graphical="showJobGaugeCircles"
-          :primary-color-var="`--sizzle-${group.color}`"
-          :name="t(`workload.gaugeStates.${key}`)"
-        />
-      </template>
-      <template v-else>
+    <div v-if="value.pods" class="gauges mb-20" :class="{'gauges__pods': !!value.pods}">
+      <template>
         <CountGauge
           v-for="(group, key) in value.podGauges"
           :key="key"
@@ -254,55 +174,27 @@ export default {
       </template>
     </div>
     <ResourceTabs :value="value">
-      <Tab v-if="isCronJob" name="jobs" :label="t('tableHeaders.jobs')" :weight="4">
+      <Tab name="pods" :label="t('tableHeaders.pods')" :weight="4">
         <SortableTable
-          :rows="value.jobs"
-          :headers="jobHeaders"
+          v-if="displayPods"
+          :rows="displayPods"
+          :headers="podHeaders"
           key-field="id"
-          :schema="jobSchema"
-          :show-groups="false"
+          :schema="podSchema"
+          :groupable="false"
           :search="false"
         />
       </Tab>
-      <div v-else>
-        <Tab name="pods" :label="t('tableHeaders.pods')" :weight="4">
-          <SortableTable
-            v-if="value.pods"
-            :rows="value.pods"
-            :headers="podHeaders"
-            key-field="id"
-            :schema="podSchema"
-            :groupable="false"
-            :search="false"
-          />
-        </Tab>
-        <Tab name="Components">
-          <SortableTable
-            v-if="value.relatedComponents"
-            :rows="value.relatedComponents"
-            :headers="componentHeaders"
-            key-field="id"
-            :schema="componentSchema"
-            :groupable="false"
-            :search="false"
-          />
-        </Tab>
-      </div>
-      <Tab v-if="showMetrics" :label="t('workload.container.titles.metrics')" name="workload-metrics" :weight="3">
-        <template #default="props">
-          <DashboardMetrics
-            v-if="props.active"
-            :detail-url="WORKLOAD_METRICS_DETAIL_URL"
-            :summary-url="WORKLOAD_METRICS_SUMMARY_URL"
-            :vars="graphVars"
-            graph-height="550px"
-          />
-        </template>
-      </Tab>
-      <Tab v-if="v1MonitoringUrl" name="v1Metrics" :label="t('node.detail.tab.metrics')" :weight="10">
-        <div id="ember-anchor">
-          <EmberPage inline="ember-anchor" :src="v1MonitoringUrl" />
-        </div>
+      <Tab name="Components">
+        <SortableTable
+          v-if="value.relatedComponents"
+          :rows="value.relatedComponents"
+          :headers="componentHeaders"
+          key-field="id"
+          :schema="componentSchema"
+          :groupable="false"
+          :search="false"
+        />
       </Tab>
     </ResourceTabs>
   </div>
