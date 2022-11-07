4b79968f985e99594ca72348b048d7ebe8f7839a
LiuYan <361112237@qq.com>
2022-11-07 15:53:26 +0800

    feat(logout): support keycloak saml-logout

--- shell/pages/auth/logout.vue
+++ shell/pages/auth/logout.vue
@@ -1,10 +1,25 @@
 <script>
+import { findBy } from '@shell/utils/array';
+import { NORMAN } from '@shell/config/types';
 
 export default {
   layout: 'unauthenticated',
 
   async asyncData({ redirect, store, router }) {
-    await store.dispatch('auth/logout', null, { root: true });
+    let principal;
+
+    try {
+      const principals = await store.dispatch('rancher/findAll', {
+        type: NORMAN.PRINCIPAL,
+        opt:  { url: '/v3/principals', force: true }
+      });
+
+      principal = findBy(principals, 'me', true);
+    } catch (e) {
+      // Maybe not Rancher
+    }
+
+    await store.dispatch('auth/logout', { provider: principal?.provider ?? '' }, { root: true });
   }
 };
 </script>
