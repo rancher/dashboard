1f105250039727f9d89cf630e701638091ae7712
bisht-richa <richa.bisht@suse.com>
2022-11-14 16:43:41 +0100

    Added content

---
+++ stories/WIP/Tab.stories.js
@@ -0,0 +1,28 @@
+import Tab from '@shell/components/Tabbed/Tab';
+import Tabbed from '@shell/components/Tabbed';
+
+export default {
+  title:       'WIP/Tabs',
+  component:   Tabbed,
+};
+
+export const Story = (args, { argTypes }) => ({
+  components: { Tabbed, Tab },
+  props:      Object.keys(argTypes),
+  data() {
+    return { isActive: true };
+  },
+  template:   `
+      <Tabbed v-bind="$props">
+        <Tab name="test1" badge="0" label="new">
+          <div name="test1">Test one content</div>
+        </Tab>
+        <Tab name="test2" error="true" label="new2"><div name="test1">Test two content</div></Tab>
+      </Tabbed>
+    `
+});
+
+Story.story = { name: 'Tabs' };
+Story.args = {
+  label: 'test'
+};
