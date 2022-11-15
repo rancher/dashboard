aa6ba96bd97cb1ff62934d141bb2a566c9a09810
scures <scurescu@suse.com>
2022-11-15 15:14:42 +0100

    Adds a Storybook page for the GrawlManager

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,24 +14,70 @@ require('../shell/plugins/i18n');
 require('../shell/plugins/v-select');
 require('../shell/plugins/tooltip');
 
-//const store = require('./store');
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+// Defines namespaced modules for the Store.
+const growl = {
+  namespaced: true,
+  state: {
+    stack: [{
+      id: 1,
+      color: 'success',
+      icon: 'checkmark',
+      timeout: 5000,
+      title: 'Success',
+      message: 'This is a success message'
+    },
+    {
+      id: 2,
+      color: 'info',
+      icon: 'info',
+      timeout: 5000,
+      title: 'Info',
+      message: 'This is an info message'
+    },
+    {
+      id: 3,
+      color: 'warning',
+      icon: 'warning',
+      timeout: 5000,
+      title: 'Warning',
+      message: 'This is an warning message'
+    },
+    {
+      id: 4,
+      color: 'error',
+      icon: 'error',
+      timeout: 5000,
+      title: 'Warning',
+      message: 'This is an warning message'
+    },
+    ],
+  },
+  getters: require("../shell/store/growl.js").getters,
+  actions: require("../shell/store/growl.js").actions,
+  mutations: require("../shell/store/growl.js").mutations
+}
+
+export const store = new Vuex.Store({
   getters: {
     'i18n/t': state => (key, args) => {
       const msg = get(i18nStrings, key) || key;
 
-      if ( msg?.includes('{')) {
+      if (msg?.includes('{')) {
         const formatter = new IntlMessageFormat(msg, state.selected);
         return formatter.format(args);
       }
 
       return msg;
     }
+  },
+  modules: {
+    growl
   }
 });
+
 
 const storePlugin = {
   store,
@@ -43,7 +89,7 @@ const storePlugin = {
 Vue.use(storePlugin);
 
 export const parameters = {
-  previewTabs: { 
+  previewTabs: {
     canvas: { hidden: false },
   },
   actions: { argTypesRegex: "^on[A-Z].*" },
