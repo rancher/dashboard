5b88f70fc35940538cfd4b40cab176d62a7dbc21
bisht-richa <richa.bisht@suse.com>
2022-11-16 13:51:34 +0100

    Corrected test

--- pkg/rancher-components/src/components/Card/Card.test.ts
+++ pkg/rancher-components/src/components/Card/Card.test.ts
@@ -5,28 +5,33 @@ describe('component: Card', () => {
   const title = 'Card title';
   const body = 'Card body';
 
-
-    const wrapper = mount(Card, {
-        propsData: { title, body },
-        slots: { 
-          title: '<div>Card title</div>',
-          body: '<div>Card body</div>',
-        }
-    });
-
   it('should have a card title', () => {
+    const wrapper = mount(Card, {
+      propsData: { title },
+      slots: { 
+        title: '<div>Card title</div>',
+      }
+    });
+  
     const element = wrapper.find('[data-testid="card-title-slot"]')
     expect(element.exists()).toBe(true);
     expect(element.text()).toBe(title)
   });
 
   it('should have a card body', () => {
+    const wrapper = mount(Card, {
+      propsData: { body },
+      slots: { 
+        body: '<div>Card body</div>',
+      }
+    });
     const element = wrapper.find('[data-testid="card-body-slot"]')
     expect(element.exists()).toBe(true);
     expect(element.text()).toBe(body)
   });
 
-  it('should have a card actions', () => {
+  it('should display the default card actions', () => {
+    const wrapper = mount(Card)
     const element = wrapper.find('[data-testid="card-actions-slot"]')
     expect(element.exists()).toBe(true);
   });
