33b5b302cdcea06b69f468485ada3d32729a724f
huacheng <cheng@HuachengdeMacBook-Pro.local>
2022-11-18 14:10:37 +0800

    fix(cluster registry): Name the registry secret with the domain name

--- shell/edit/provisioning.cattle.io.cluster/RegistryConfigs.vue
+++ shell/edit/provisioning.cattle.io.cluster/RegistryConfigs.vue
@@ -104,7 +104,18 @@ export default {
         });
       }
       this.clusterRegisterBeforeHook(wrapFn, ...args);
-    }
+    },
+    generateName(row) {
+      const hostnameReg = /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/;
+      const match = hostnameReg.exec(row?.value?.hostname) || [];
+      const hostname = match[0];
+
+      if (hostname) {
+        return `${ hostname }-`;
+      } else {
+        return 'registryconfig-auth-';
+      }
+    },
   }
 };
 </script>
@@ -144,7 +155,7 @@ export default {
               :vertical="true"
               :namespace="value.metadata.namespace"
               :mode="mode"
-              generate-name="registryconfig-auth-"
+              :generate-name="generateName(row)"
             />
           </div>
           <div class="col span-6">
