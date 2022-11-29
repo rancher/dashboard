b0326ce9c320c2842b7cc56efb8ee8ef5d7f7386
bisht-richa <richa.bisht@suse.com>
2022-11-29 12:04:09 +0100

    Added new stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,7 +13,8 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
-    "storybook-dark-mode"
+    "storybook-dark-mode",
+    "@storybook/addon-actions"
   ],
 
   staticDirs: [
