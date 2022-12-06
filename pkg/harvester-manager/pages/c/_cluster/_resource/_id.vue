539429696f9a8f86b2f83d5e2c13126950232401
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2022-12-06 09:02:49 -0700

    linting

--- pkg/harvester-manager/pages/c/_cluster/_resource/_id.vue
+++ pkg/harvester-manager/pages/c/_cluster/_resource/_id.vue
@@ -2,8 +2,8 @@
 import ResourceDetail from '@shell/components/ResourceDetail';
 
 export default {
-  name:        'HarvesterMgrResourceId',
-  components:  { ResourceDetail },
+  name:       'HarvesterMgrResourceId',
+  components: { ResourceDetail },
 };
 </script>
 
