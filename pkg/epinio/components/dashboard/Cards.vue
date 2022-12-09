fee8aff85cc7bbdefcb6263d7a64475e5c5f544a
scures <scurescu@suse.com>
2022-12-09 08:25:01 +0100

    Disables the wave effect and reduces the size of the loading spinner

--- pkg/epinio/components/dashboard/Cards.vue
+++ pkg/epinio/components/dashboard/Cards.vue
@@ -151,40 +151,41 @@ export default {
   align-items: center;
   place-content: center;
 
-  &::after {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    opacity: 0.1;
-    transform: translateX(-100%);
-    background-image: linear-gradient(
-      90deg,
-      rgba(#fff, 0) 0,
-      rgba(#fff, 0.2) 20%,
-      rgba(#fff, 0.5) 60%,
-      rgba(#fff, 0)
-    );
-    animation: shimmer 4s infinite;
-    content: '';
-  }
+  // INFO: Disable shimmer effect for now
+  // &::after {
+  //   position: absolute;
+  //   top: 0;
+  //   right: 0;
+  //   bottom: 0;
+  //   left: 0;
+  //   opacity: 0.1;
+  //   transform: translateX(-100%);
+  //   background-image: linear-gradient(
+  //     90deg,
+  //     rgba(#fff, 0) 0,
+  //     rgba(#fff, 0.2) 20%,
+  //     rgba(#fff, 0.5) 60%,
+  //     rgba(#fff, 0)
+  //   );
+  //   animation: shimmer 4s infinite;
+  //   content: '';
+  // }
+
+  // @keyframes shimmer {
+  //   100% {
+  //     transform: translateX(100%);
+  //   }
+  // }
 
   .animate-spin {
     opacity: 0.5;
-    font-size: 60px;
+    font-size: 24px;
     animation: spin 5s linear infinite;
   }
 
   @keyframes spin {
     100% {
       transform: rotate(360deg);
-    }
-  }
-
-  @keyframes shimmer {
-    100% {
-      transform: translateX(100%);
     }
   }
 }
