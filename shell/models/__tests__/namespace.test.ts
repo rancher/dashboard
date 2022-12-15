5339d9da45e2c2ef32c0f54ad564be1fde9b2a9d
cnotv <giuseppe.leo@suse.com>
2022-12-15 13:01:12 +0100

    Add list of cases to be covered for the model tests

---
+++ shell/models/__tests__/namespace.test.ts
@@ -0,0 +1,19 @@
+describe('class Namespace', () => {
+  it.todo('should check if isSystem');
+  it.todo('should check if isFleetManaged');
+  it.todo('should return the projectId');
+  it.todo('should return the project');
+  it.todo('should return the project name with i18n');
+  it.todo('should return the projectNameSort');
+  it.todo('should check if istioInstalled');
+  it.todo('should check if injectionEnabled');
+  it.todo('should enable Istio auto injection');
+  it.todo('should disable Istio auto injection');
+  it.todo('should disableAutoInjection');
+  it.todo('should check if confirmRemove');
+  it.todo('should return the listLocation');
+  it.todo('should return _detailLocation with a name');
+  it.todo('should return the resourceQuota');
+  it.todo('should set the resourceQuota as reactive Vue property');
+  it.todo('should reset project with cleanForNew');
+});
