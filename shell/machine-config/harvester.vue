e4e1620f5c11b5c539e44327e9f197eea3814f93
Qingyang Nong <n313893254@126.com>
2023-01-04 07:30:10 +0800

    HARVESTER: Remove External Harvester Cloud Credentials Support (#7827)

--- shell/machine-config/harvester.vue
+++ shell/machine-config/harvester.vue
@@ -96,12 +96,7 @@ export default {
 
       const url = `/k8s/clusters/${ clusterId }/v1`;
 
-      const isImportCluster =
-        this.credential.decodedData.clusterType === 'imported';
-
-      this.isImportCluster = isImportCluster;
-
-      if (clusterId && isImportCluster) {
+      if (clusterId) {
         const res = await allHashSettled({
           namespaces: this.$store.dispatch('cluster/request', { url: `${ url }/${ NAMESPACE }s` }),
           images:     this.$store.dispatch('cluster/request', { url: `${ url }/${ HCI.IMAGE }s` }),
@@ -244,7 +239,6 @@ export default {
 
     return {
       credential:         null,
-      isImportCluster:    false,
       vmAffinity,
       userData,
       networkData,
@@ -413,7 +407,7 @@ export default {
         const clusterId = get(this.credential, 'decodedData.clusterId');
         const url = `/k8s/clusters/${ clusterId }/v1`;
 
-        if (url && this.isImportCluster) {
+        if (url) {
           const res = await this.$store.dispatch('cluster/request', { url: `${ url }/${ HCI.IMAGE }s` });
 
           this.images = res?.data;
@@ -504,7 +498,6 @@ export default {
 
         <div class="col span-6">
           <LabeledSelect
-            v-if="isImportCluster"
             v-model="value.vmNamespace"
             :mode="mode"
             :options="namespaceOptions"
@@ -516,23 +509,10 @@ export default {
               t('cluster.harvester.machinePool.namespace.placeholder')
             "
           />
-
-          <LabeledInput
-            v-else
-            v-model="value.vmNamespace"
-            label-key="cluster.credential.harvester.namespace"
-            :required="true"
-            :mode="mode"
-            :disabled="namespaceDisabled"
-            :placeholder="
-              t('cluster.harvester.machinePool.namespace.placeholder')
-            "
-          />
         </div>
       </div>
 
       <div
-        v-if="isImportCluster"
         class="row mt-20"
       >
         <div class="col span-6">
@@ -560,33 +540,6 @@ export default {
             :placeholder="
               t('cluster.harvester.machinePool.network.placeholder')
             "
-          />
-        </div>
-      </div>
-
-      <div
-        v-else
-        class="row mt-20"
-      >
-        <div class="col span-6">
-          <LabeledInput
-            v-model="value.imageName"
-            :mode="mode"
-            :required="true"
-            :placeholder="t('cluster.credential.harvester.placeholder')"
-            :disabled="disabledEdit"
-            label-key="cluster.credential.harvester.image"
-          />
-        </div>
-
-        <div class="col span-6">
-          <LabeledInput
-            v-model="value.networkName"
-            :mode="mode"
-            :required="true"
-            :placeholder="t('cluster.credential.harvester.placeholder')"
-            :disabled="disabledEdit"
-            label-key="cluster.credential.harvester.network"
           />
         </div>
       </div>
@@ -633,7 +586,7 @@ export default {
         </h3>
         <div>
           <LabeledSelect
-            v-if="isImportCluster && isCreate"
+            v-if="isCreate"
             v-model="userData"
             class="mb-10"
             :options="userDataOptions"
@@ -656,7 +609,7 @@ export default {
         <h3>{{ t('cluster.credential.harvester.networkData.title') }}</h3>
         <div>
           <LabeledSelect
-            v-if="isImportCluster && isCreate"
+            v-if="isCreate"
             v-model="networkData"
             class="mb-10"
             :options="networkDataOptions"
