7e674f4798d44acf7578b919554eec9b38b614da
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-01-05 15:14:57 +0000

    fix button alignment on masthead details page

--- shell/components/ResourceDetail/Masthead.vue
+++ shell/components/ResourceDetail/Masthead.vue
@@ -481,7 +481,7 @@ export default {
         </div>
       </div>
       <slot name="right">
-        <div class="actions-container">
+        <div class="actions-container align-start">
           <div class="actions">
             <button
               v-if="detailsAction && currentView === DETAIL_VIEW && isView"
