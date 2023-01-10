ace71d4b5e4036b6fa1467b09f7beab06dfebdd4
Richa <richa.bisht@suse.com>
2023-01-10 14:10:56 +0100

    Fixed link alignment in home page whats new section

--- pkg/rancher-components/src/components/Banner/Banner.vue
+++ pkg/rancher-components/src/components/Banner/Banner.vue
@@ -162,6 +162,9 @@ $icon-size: 24px;
     line-height: 20px;
     width: 100%;
     border-left: solid $left-border-size transparent;
+    align-items: center;
+    flex-direction: row;
+    display: flex;
 
     .primary & {
       background: var(--primary);
