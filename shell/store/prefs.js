2be0d3d6845729ee1211d90726867ddd2dc147ac
Richa <richa.bisht@suse.com>
2023-01-10 09:24:22 +0100

    Corrected pref variable name format and update comments

--- shell/store/prefs.js
+++ shell/store/prefs.js
@@ -120,8 +120,8 @@ export const PSP_DEPRECATION_BANNER = create('hide-psp-deprecation-banner', fals
 // Maximum number of clusters to show in the slide-in menu
 export const MENU_MAX_CLUSTERS = create('menu-max-clusters', 4, { options: [2, 3, 4, 5, 6, 7, 8, 9, 10], parseJSON });
 
-// Prompt for confirm node deletion when scaling down node pool in GUI
-export const SCALE_POOL_PROMPT = create('scalePoolPrompt', '', { parseJSON });
+// Prompt for confirm when scaling down node pool in GUI and save the pref
+export const SCALE_POOL_PROMPT = create('scale-pool-prompt', '', { parseJSON });
 // --------------------
 
 const cookiePrefix = 'R_';
