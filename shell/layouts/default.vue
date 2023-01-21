88540e80e0d2e082fda54e9f19305655edea0378
Phillip Rak <rak.phillip@gmail.com>
2023-01-21 08:35:33 -0700

    Move remaining nav css to new component
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/layouts/default.vue
+++ shell/layouts/default.vue
@@ -418,18 +418,6 @@ export default {
 
     NAV {
       grid-area: nav;
-      position: relative;
-      background-color: var(--nav-bg);
-      border-right: var(--nav-border-size) solid var(--nav-border);
-      overflow-y: auto;
-
-      H6, .root.child .label {
-        margin: 0;
-        letter-spacing: normal;
-        line-height: initial;
-
-        A { padding-left: 0; }
-      }
     }
   }
 
