aa8514d8b206daefa8e39e8156cb1e0e6ce0daec
Catherine Luse <catherine.luse@gmail.com>
2023-01-27 05:40:40 -0700

    Avoid showing YAML in form mode (#7924)

--- shell/components/CruResource.vue
+++ shell/components/CruResource.vue
@@ -640,7 +640,7 @@ export default {
       </template>
       <!------ YAML ------>
       <section
-        v-else
+        v-else-if="showYaml"
         class="cru-resource-yaml-container resource-container cru__content"
       >
         <ResourceYaml
