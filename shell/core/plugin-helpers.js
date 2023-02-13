bb754c97c6e8008b749e9d3f72c22a3f96b5454d
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2023-02-13 16:49:12 +0000

    gate some critical methods that were introducing errors on elemental while running on v2.7.0 dashboard (#8148)
    Co-authored-by: Alexandre Alves <aalves@Alexandres-MBP.lan>

--- shell/core/plugin-helpers.js
+++ shell/core/plugin-helpers.js
@@ -94,78 +94,81 @@ function checkExtensionRouteBinding($route, locationConfig) {
 export function getApplicableExtensionEnhancements(pluginCtx, actionType, uiArea, currRoute, translationCtx = pluginCtx) {
   const extensionEnhancements = [];
 
-  const actions = pluginCtx.$plugin.getUIConfig(actionType, uiArea);
+  // gate it so that we prevent errors on older versions of dashboard
+  if (pluginCtx.$plugin?.getUIConfig) {
+    const actions = pluginCtx.$plugin.getUIConfig(actionType, uiArea);
 
-  actions.forEach((action, i) => {
-    if (checkExtensionRouteBinding(currRoute, action.locationConfig)) {
-      // ADD CARD PLUGIN UI ENHANCEMENT
-      if (actionType === ExtensionPoint.CARD) {
-        // intercept to apply translation
-        if (uiArea === CardLocation.CLUSTER_DASHBOARD_CARD && action.labelKey) {
-          actions[i].label = translationCtx.t(action.labelKey);
-        }
-
-      // ADD ACTION PLUGIN UI ENHANCEMENT
-      } else if (actionType === ExtensionPoint.ACTION) {
-        // TABLE ACTION
-        if (uiArea === ActionLocation.TABLE) {
+    actions.forEach((action, i) => {
+      if (checkExtensionRouteBinding(currRoute, action.locationConfig)) {
+        // ADD CARD PLUGIN UI ENHANCEMENT
+        if (actionType === ExtensionPoint.CARD) {
           // intercept to apply translation
-          if (action.labelKey) {
+          if (uiArea === CardLocation.CLUSTER_DASHBOARD_CARD && action.labelKey) {
             actions[i].label = translationCtx.t(action.labelKey);
           }
 
-          // sets the enabled flag to true if omitted on the config
-          if (!Object.keys(action).includes('enabled')) {
-            actions[i].enabled = true;
+        // ADD ACTION PLUGIN UI ENHANCEMENT
+        } else if (actionType === ExtensionPoint.ACTION) {
+          // TABLE ACTION
+          if (uiArea === ActionLocation.TABLE) {
+            // intercept to apply translation
+            if (action.labelKey) {
+              actions[i].label = translationCtx.t(action.labelKey);
+            }
+
+            // sets the enabled flag to true if omitted on the config
+            if (!Object.keys(action).includes('enabled')) {
+              actions[i].enabled = true;
+            }
+
+            // bulkable flag
+            actions[i].bulkable = actions[i].multiple || actions[i].bulkable;
+
+            // populate action identifier to prevent errors
+            if (!actions[i].action) {
+              actions[i].action = `custom-table-action-${ randomStr(10).toLowerCase() }`;
+            }
           }
 
-          // bulkable flag
-          actions[i].bulkable = actions[i].multiple || actions[i].bulkable;
+          // extract simplified shortcut definition on plugin - HEADER ACTION
+          if (uiArea === ActionLocation.HEADER && action.shortcut) {
+            // if it's a string, then assume CTRL for windows and META for mac
+            if (typeof action.shortcut === 'string') {
+              actions[i].shortcutLabel = () => {
+                return isMac ? `(\u2318-${ action.shortcut.toUpperCase() })` : `(Ctrl-${ action.shortcut.toUpperCase() })`;
+              };
+              actions[i].shortcutKey = { windows: ['ctrl', action.shortcut], mac: ['meta', action.shortcut] };
+            // correct check for an Object type in JS... handle the object passed
+            } else if (typeof action.shortcut === 'object' && !Array.isArray(action.shortcut) && action.shortcut !== null) {
+              actions[i].shortcutKey = action.shortcut;
+              const keyboardCombo = isMac ? actions[i].shortcut.mac : actions[i].shortcut.windows ? actions[i].shortcut.windows : [];
+              let scLabel = '';
 
-          // populate action identifier to prevent errors
-          if (!actions[i].action) {
-            actions[i].action = `custom-table-action-${ randomStr(10).toLowerCase() }`;
-          }
-        }
-
-        // extract simplified shortcut definition on plugin - HEADER ACTION
-        if (uiArea === ActionLocation.HEADER && action.shortcut) {
-          // if it's a string, then assume CTRL for windows and META for mac
-          if (typeof action.shortcut === 'string') {
-            actions[i].shortcutLabel = () => {
-              return isMac ? `(\u2318-${ action.shortcut.toUpperCase() })` : `(Ctrl-${ action.shortcut.toUpperCase() })`;
-            };
-            actions[i].shortcutKey = { windows: ['ctrl', action.shortcut], mac: ['meta', action.shortcut] };
-          // correct check for an Object type in JS... handle the object passed
-          } else if (typeof action.shortcut === 'object' && !Array.isArray(action.shortcut) && action.shortcut !== null) {
-            actions[i].shortcutKey = action.shortcut;
-            const keyboardCombo = isMac ? actions[i].shortcut.mac : actions[i].shortcut.windows ? actions[i].shortcut.windows : [];
-            let scLabel = '';
-
-            keyboardCombo.forEach((key, i) => {
-              if (i < keyboardCombo.length - 1) {
-                if (key === 'meta') {
-                  key = '\u2318';
+              keyboardCombo.forEach((key, i) => {
+                if (i < keyboardCombo.length - 1) {
+                  if (key === 'meta') {
+                    key = '\u2318';
+                  } else {
+                    key = ucFirst(key);
+                  }
+                  scLabel += `${ key }`;
+                  scLabel += '-';
                 } else {
-                  key = ucFirst(key);
+                  scLabel += `${ key.toUpperCase() }`;
                 }
-                scLabel += `${ key }`;
-                scLabel += '-';
-              } else {
-                scLabel += `${ key.toUpperCase() }`;
-              }
-            });
+              });
 
-            actions[i].shortcutLabel = () => {
-              return `(${ scLabel })`;
-            };
+              actions[i].shortcutLabel = () => {
+                return `(${ scLabel })`;
+              };
+            }
           }
         }
-      }
 
-      extensionEnhancements.push(actions[i]);
-    }
-  });
+        extensionEnhancements.push(actions[i]);
+      }
+    });
+  }
 
   return extensionEnhancements;
 }
