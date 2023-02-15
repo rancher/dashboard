a01c2ac1e057cda76c3580dfa3d92af4dc5303f7
Qingyang Nong <n313893254@126.com>
2023-02-16 05:16:00 +0800

    [Backport 2.6] Fix kubernetesVersionExtension is '' on the create Harvester storage class page (#8180)
    * HARVESTER: Fix kubernetesVersionExtension is ''
    
    * fix typo in kubernetesVersionExtension regex
    
    ---------
    
    Co-authored-by: Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>

--- shell/pages/c/_cluster/explorer/index.vue
+++ shell/pages/c/_cluster/explorer/index.vue
@@ -396,8 +396,11 @@ export default {
       </div>
       <div>
         <label>{{ t('glance.version') }}: </label>
-        <span v-if="currentCluster.kubernetesVersionExtension" style="font-size: 0.5em">{{ currentCluster.kubernetesVersionExtension }}</span>
         <span>{{ currentCluster.kubernetesVersionBase }}</span>
+        <span
+          v-if="currentCluster.kubernetesVersionExtension"
+          style="font-size: 0.75em"
+        >{{ currentCluster.kubernetesVersionExtension }}</span>
       </div>
       <div>
         <label>{{ t('glance.created') }}: </label>
