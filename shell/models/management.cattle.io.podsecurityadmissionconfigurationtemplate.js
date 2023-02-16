d6e61562fe39963668c5f893b1be7109d8acc5e8
cnotv <giuseppe.leo@suse.com>
2023-02-16 12:36:02 +0100

    Add routing method to the CruResource editor containing Yaml editor as well

--- shell/models/management.cattle.io.podsecurityadmissionconfigurationtemplate.js
+++ shell/models/management.cattle.io.podsecurityadmissionconfigurationtemplate.js
@@ -1,18 +1,4 @@
 import SteveModel from '@shell/plugins/steve/steve-class';
-import { MANAGEMENT } from '@shell/config/types';
 
 export default class PodSecurityAdmissionTemplate extends SteveModel {
-  get listLocation() {
-    return {
-      name:   MANAGEMENT.PSA,
-      params: {
-        product:  MANAGEMENT.PSA,
-        resource: this.type,
-      }
-    };
-  }
-
-  get doneOverride() {
-    return this.listLocation;
-  }
 }
