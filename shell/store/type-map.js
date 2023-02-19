6a876e932c34f1835b604ce4f193b3499f87ee5f
Shavindra <shavindra@users.noreply.github.com>
2023-02-19 21:51:47 +0000

    Fix linting.

--- shell/store/type-map.js
+++ shell/store/type-map.js
@@ -843,7 +843,6 @@ export const getters = {
 
   allTypes(state, getters, rootState, rootGetters) {
     return (product, mode = ALL) => {
-
       const module = findBy(state.products, 'name', product)?.inStore;
       const schemas = rootGetters[`${ module }/all`](SCHEMA);
       const counts = rootGetters[`${ module }/all`](COUNT)?.[0]?.counts || {};
