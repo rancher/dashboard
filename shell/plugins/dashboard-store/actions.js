6a5f976080a68b53dbe7d0b312167a216c6ce3ea
Richard Cox <richard.cox@suse.com>
2023-02-24 15:14:31 +0000

    Persist http response collection revision - Normally we fetch resources and then watch using the revision in that responce - If we get a resource.stop event we'll then try to find the current revision from the store - This looks at the type cache's revision and revisions in each resource - Bug   - the type cache's revision is set updated, which leads to very old revisions being used   - This can cause a `too old` error... which is handeld by refetchin the whole list - Fix   - Ensure we set the collection's revision when we're likely to need it again - Tested   - the different find/load store actions/mutations   - this includes incremental loading

--- shell/plugins/dashboard-store/actions.js
+++ shell/plugins/dashboard-store/actions.js
@@ -53,8 +53,9 @@ export async function loadSchemas(ctx, watch = true) {
 
   commit('loadAll', {
     ctx,
-    type: SCHEMA,
-    data: res.data
+    type:     SCHEMA,
+    data:     res.data,
+    revision: res.revision
   });
 
   if ( watch !== false ) {
@@ -304,6 +305,7 @@ export default {
           ctx,
           type,
           data:      out.data,
+          revision:  out.revision,
           skipHaveAll,
           namespace: opt.namespaced,
         });
@@ -370,8 +372,9 @@ export default {
     commit('loadSelector', {
       ctx,
       type,
-      entries: res.data,
-      selector
+      entries:  res.data,
+      selector,
+      revision: res.revision,
     });
 
     if ( opt.watch !== false ) {
