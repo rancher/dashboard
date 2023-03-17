580f2b36c56dbe8ea2b0c3a45f783dd6d61eed54
Phillip Rak <rak.phillip@gmail.com>
2023-03-16 17:30:01 -0700

    Disallow `v-html`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>
    Conflicts:
    	.eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,7 +31,7 @@ module.exports = {
     'unicorn/no-new-buffer':    'off',
     'vue/html-self-closing':    'off',
     'vue/no-unused-components': 'warn',
-    'vue/no-v-html':            'on',
+    'vue/no-v-html':            'error',
     'wrap-iife':                'off',
 
     'array-bracket-spacing':             'warn',
