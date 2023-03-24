aac5541508248293d12175e9d8bf3ead29c6c270
Dalibor Moc <dalimoc@gmail.com>
2023-03-23 22:45:54 -0400

    JSON upload and swap values

--- pkg/technical-interview/components/tab-3.vue
+++ pkg/technical-interview/components/tab-3.vue
@@ -1,27 +1,87 @@
 <script lang="ts">
 import Vue from 'vue';
 
-// import Tabbed from "@shell/components/Tabbed/index.vue";
-// import Tab from "@shell/components/Tabbed/Tab.vue";
-
 interface Data {}
 
 // Data, Methods, Computed, Props
 export default Vue.extend<Data, any, any, any>({
-  //   components: { Tabbed, Tab },
-
-  //   layout: "plain",
-
   data() {
-    return {};
+    return {
+      originalJson: null,
+      swappedJson:  null,
+    };
   },
 
   computed: {},
 
-  methods: {},
+  methods: {
+    onFileChange(e: any) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        try {
+          this.originalJson = JSON.parse(event?.target?.result as string);
+          this.swappedJson = null;
+        } catch (e) {
+          alert('Invalid JSON file');
+        }
+      };
+      reader.readAsText(file);
+    },
+    swapKeysAndValues() {
+      this.swappedJson = this.swap(this.originalJson);
+    },
+    swap(json: any): any {
+      if (typeof json !== 'object' || Array.isArray(json)) {
+        return json;
+      }
+
+      const result: any = {};
+
+      for (const key in json) {
+        const value = json[key];
+
+        if (
+          typeof value === 'string' ||
+          typeof value === 'number' ||
+          typeof value === 'boolean'
+        ) {
+          result[value.toString()] = key;
+        } else {
+          result[key] = this.swap(value);
+        }
+      }
+
+      return result;
+    },
+  },
 });
 </script>
 
 <template>
-  <div>Tab 3</div>
+  <div>
+    Tab 3
+
+    <div>
+      <input
+        type="file"
+        @change="onFileChange"
+      >
+      <div v-if="originalJson">
+        <h3>Original JSON:</h3>
+        <pre>{{ originalJson }}</pre>
+      </div>
+      <button
+        v-if="originalJson"
+        @click="swapKeysAndValues"
+      >
+        Swap Keys and Values
+      </button>
+      <div v-if="swappedJson">
+        <h3>Swapped JSON:</h3>
+        <pre>{{ swappedJson }}</pre>
+      </div>
+    </div>
+  </div>
 </template>
