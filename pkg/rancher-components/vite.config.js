5abf74d3eddd58ed064672a97bf4ab87034cb9b4
Phillip Rak <rak.phillip@gmail.com>
2023-04-18 14:43:53 -0700

    Remove legacy plugin
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/vite.config.js
+++ pkg/rancher-components/vite.config.js
@@ -1,6 +1,5 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-// import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
@@ -24,10 +23,6 @@ export default defineConfig({
   },
   plugins: [
     vue2(),
-    // legacy({
-    //   targets: ['ie >= 11'],
-    //   additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    // })
   ],
   css: {
     preprocessorOptions: {
