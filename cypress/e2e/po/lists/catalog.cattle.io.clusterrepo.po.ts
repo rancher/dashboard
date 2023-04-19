5fe00614044e0578e1c0dfcd05d4a1889d6bb95f
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-04-19 16:11:18 -0700

    added class for clusterrepo resource list component

--- cypress/e2e/po/lists/catalog.cattle.io.clusterrepo.po.ts
+++ cypress/e2e/po/lists/catalog.cattle.io.clusterrepo.po.ts
@@ -4,4 +4,7 @@ import BaseResourceList from '~/cypress/e2e/po/lists/base-resource-list.po';
  * List component for catalog.cattle.io.clusterrepo resources
  */
 export default class AppRepoListPo extends BaseResourceList {
+  actionMenu(repoName: string) {
+    return this.resourceTable().sortableTable().rowActionMenuOpen(repoName, 7);
+  }
 }
