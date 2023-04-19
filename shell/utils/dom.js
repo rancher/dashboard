61adce989899e36b8458ada617939df577f021c0
Francesco Torchia <francesco.torchia@suse.com>
2023-04-19 16:15:55 +0200

    dom util, getParent function refactoring for readability
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- shell/utils/dom.js
+++ shell/utils/dom.js
@@ -1,5 +1,15 @@
 export function getParent(el, parentSelector) {
-  while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el, parentSelector))) { }
+  el = el?.parentElement;
+
+  if (!el) {
+    return null;
+  }
+
+  const matchFn = el.matches || el.matchesSelector;
+
+  if (!matchFn.call(el, parentSelector)) {
+    return getParent(el, parentSelector);
+  }
 
   return el;
 }
