5e7c2b4a74d8fe85aa8686d3900e0932d20553cb
cnotv <giuseppe.leo@suse.com>
2023-05-10 17:56:15 +0200

    Add transformer for require using Babel

--- shell/babel.config.js
+++ shell/babel.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  presets: [
+module.exports = function(api) {
+  api.cache(true);
+  const presets = [
     [
       '@vue/cli-plugin-babel/preset',
       { useBuiltIns: false }
@@ -8,6 +9,18 @@ module.exports = {
       '@babel/preset-env',
       { targets: { node: 'current' } }
     ]
-  ],
-  env: { test: { presets: [['@babel/env', { targets: { node: 'current' } }]] } }
+  ];
+  const env = { test: { presets: [['@babel/env', { targets: { node: 'current' } }]] } };
+
+  const plugins = [];
+
+  if (process.env.NODE_ENV === 'test') {
+    plugins.push('transform-require-context');
+  }
+
+  return {
+    presets,
+    plugins,
+    env
+  };
 };
