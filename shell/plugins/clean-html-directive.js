ef8ffd23081c4f1a8f5a04a9ffc0f3e5bd412c09
Phillip Rak <rak.phillip@gmail.com>
2023-05-18 15:38:33 -0700

    Create a `v-clean-tooltip` directive
    Update `VCleanTooltip`
    Only purify if `value.content` is string
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>
    Conflicts:
    	shell/initialize/index.js

--- shell/plugins/clean-html-directive.js
+++ shell/plugins/clean-html-directive.js
@@ -17,7 +17,7 @@ const ALLOWED_TAGS = [
   'strong',
 ];
 
-const purifyHTML = value => DOMPurify.sanitize(value, { ALLOWED_TAGS });
+export const purifyHTML = value => DOMPurify.sanitize(value, { ALLOWED_TAGS });
 
 export const cleanHtmlDirective = {
   inserted(el, binding) {
