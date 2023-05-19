43d64c978b3b8ddfe472f980647bc76e14ad4d71
Phillip Rak <rak.phillip@gmail.com>
2023-05-19 10:38:32 -0700

    Create a `v-clean-tooltip` directive
    Update `VCleanTooltip`
    Only purify if `value.content` is string
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ shell/plugins/clean-tooltip-directive.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import { VTooltip } from 'v-tooltip';
+import { purifyHTML } from './clean-html-directive';
+
+function purifyContent(value) {
+  const type = typeof value;
+
+  if (type === 'string') {
+    return purifyHTML(value);
+  } else if (value && type === 'object' && typeof value.content === 'string') {
+    return { ...value, content: purifyHTML(value.content) };
+  } else {
+    return value;
+  }
+}
+
+function bind(el, { value, oldValue, modifiers }) {
+  const purifiedValue = purifyContent(value);
+
+  VTooltip.bind(
+    el,
+    {
+      value: purifiedValue, oldValue, modifiers
+    });
+}
+
+const VCleanTooltip = {
+  ...VTooltip,
+  bind,
+  update: bind,
+};
+
+Vue.directive('clean-tooltip', VCleanTooltip);
