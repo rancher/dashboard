bd67186c596af72dee8be32a20a820aee2548090
scures <scurescu@suse.com>
2023-05-25 17:02:41 +0200

    fix: extends ComponentPo for CardPo

--- cypress/e2e/po/components/card.po.ts
+++ cypress/e2e/po/components/card.po.ts
@@ -1,11 +1,8 @@
+import ComponentPo from '~/cypress/e2e/po/components/component.po';
 import { CypressChainable } from '~/cypress/e2e/po/po.types';
 
-export default class CardPo {
-  private readonly cardSelector: string;
-
-  constructor() {
-    this.cardSelector = '[data-testid="card"]';
-  }
+export default class CardPo extends ComponentPo {
+  private readonly cardSelector = '[data-testid="card"]';
 
   getTitle(): CypressChainable {
     return cy.get(this.cardSelector).get('[data-testid="card-title-slot"]');
