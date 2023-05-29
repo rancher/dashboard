f3625a5874ed719ed2cf8e65cc43e7f1d6835623
huacheng <cheng@HuachengdeMacBook-Pro.local>
2023-05-29 09:47:05 +0800

    test(snapshot): Add backup success prompt

---
+++ shell/plugins/steve/__test__/subscribe.test.js
@@ -0,0 +1,140 @@
+
+import { actions } from '@shell/plugins/steve/subscribe.js';
+
+describe('plugin: subscribe, actions: ws.resource.change', () => {
+  it('should be rke2 growl success', () => {
+    const growlSuccess = jest.fn(t => t);
+    const ctx = {
+      getters: {
+        normalizeType: type => type,
+        typeEntry:     type => '',
+      },
+      dispatch:    growlSuccess,
+      rootGetters: {
+        'management/byId':     (type, id) => ({ nameDisplay: id }),
+        'type-map/optionsFor': type => ({ type }),
+        'i18n/t':              (type, p) => p?.name ? type + p.name : type
+      }
+    };
+    const data = {
+      clusterId: 'test',
+      metadata:  {
+        state:             { error: false },
+        creationTimestamp: new Date()
+      },
+      spec: { clusterName: 'test' },
+      type: 'rke.cattle.io.etcdsnapshot'
+    };
+
+    actions['ws.resource.change'](ctx, { data, type: 'rke.cattle.io.etcdsnapshot' });
+
+    expect(growlSuccess).toHaveBeenCalledWith('growl/success', {
+      title:   'cluster.snapshot.success.title',
+      message: 'cluster.snapshot.success.messagetest',
+      err:     'cluster.snapshot.success.messagetest',
+    }, { root: true });
+  });
+
+  it('should be rke2 growl fail', () => {
+    const growlFail = jest.fn(t => t);
+    const ctx = {
+      getters: {
+        normalizeType: type => type,
+        typeEntry:     type => '',
+      },
+      dispatch:    growlFail,
+      rootGetters: {
+        'management/byId':     (type, id) => ({ nameDisplay: id }),
+        'type-map/optionsFor': type => ({ type }),
+        'i18n/t':              (type, p) => p?.name ? type + p.name : type
+      }
+    };
+    const data = {
+      // clusterId: 'test',
+      metadata: {
+        state:             { error: true },
+        creationTimestamp: new Date()
+      },
+      spec: { clusterName: 'testFail' },
+      type: 'rke.cattle.io.etcdsnapshot'
+    };
+
+    actions['ws.resource.change'](ctx, { data, type: 'rke.cattle.io.etcdsnapshot' });
+
+    expect(growlFail).toHaveBeenCalledWith('growl/fromError', {
+      title:   'cluster.snapshot.fail.title',
+      message: 'cluster.snapshot.fail.messagetestFail',
+      err:     'cluster.snapshot.fail.messagetestFail',
+    }, { root: true });
+  });
+
+  it('should be rke growl success', () => {
+    const growlSuccess = jest.fn(t => t);
+    const ctx = {
+      getters: {
+        normalizeType: type => type,
+        typeEntry:     type => '',
+      },
+      dispatch:    growlSuccess,
+      rootGetters: {
+        'management/byId':     (type, id) => ({ nameDisplay: id }),
+        'type-map/optionsFor': type => ({ type }),
+        'i18n/t':              (type, p) => p?.name ? type + p.name : type
+      }
+    };
+    const data = {
+      annotations: [],
+      status:      {
+        conditions: [{
+          lastUpdateTime: new Date(),
+          type:           'Completed'
+        }]
+      },
+      clusterId: 'test',
+      type:      'etcdBackup',
+      state:     'active'
+    };
+
+    actions['ws.resource.change'](ctx, { data });
+
+    expect(growlSuccess).toHaveBeenCalledWith('growl/success', {
+      title:   'cluster.snapshot.success.title',
+      message: 'cluster.snapshot.success.messagetest',
+    }, { root: true });
+  });
+
+  it('should be rke growl fail', () => {
+    const growlFail = jest.fn(t => t);
+    const ctx = {
+      getters: {
+        normalizeType: type => type,
+        typeEntry:     type => '',
+      },
+      dispatch:    growlFail,
+      rootGetters: {
+        'management/byId':     (type, id) => ({ nameDisplay: id }),
+        'type-map/optionsFor': type => ({ type }),
+        'i18n/t':              (type, p) => p?.name ? type + p.name : type
+      }
+    };
+    const data = {
+      annotations: [],
+      status:      {
+        conditions: [{
+          lastUpdateTime: new Date(),
+          type:           'Completed'
+        }]
+      },
+      clusterId: 'testFail',
+      type:      'etcdBackup',
+      state:     'failed'
+    };
+
+    actions['ws.resource.change'](ctx, { data, type: 'rke.cattle.io.etcdsnapshot' });
+
+    expect(growlFail).toHaveBeenCalledWith('growl/fromError', {
+      title:   'cluster.snapshot.fail.title',
+      message: 'cluster.snapshot.fail.messagetestFail',
+    }, { root: true });
+  });
+});
