773855d5fbca1e62b5bd071c29b04c24eb69147d
Francesco Torchia <francesco.torchia@suse.com>
2023-05-31 13:17:15 +0200

    Fix anti-pattern in version saving
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/epinio/index.ts
+++ pkg/epinio/index.ts
@@ -13,7 +13,7 @@ const onEnter: OnNavToPackage = async(store, config) => {
   const serverVersionSettings = store.getters['management/byId'](MANAGEMENT.SETTING, 'server-version');
   const res = await store.dispatch(`epinio/request`, { opt: { url: `/api/v1/info` } });
 
-  store.commit('epinio/info', res);
+  await store.dispatch('epinio/info');
 
   await store.dispatch('management/load', {
     data: {
