b111df0940c43b58d756306c135fa22e683071cc
Richa <richa.bisht@suse.com>
2023-06-09 17:24:50 +0200

    Added test to a valid integer

--- shell/edit/monitoring.coreos.com.receiver/types/__tests__/email.test.ts
+++ shell/edit/monitoring.coreos.com.receiver/types/__tests__/email.test.ts
@@ -8,6 +8,15 @@ describe('page: Routes and Receivers', () => {
         mode:  'create',
         value: {}
       },
+      mocks: {
+        t:      (text: string) => text, // Mock i18n global function used as alternative to the getter
+        $store: {
+          getters: {
+            'i18n/t':      jest.fn(),
+            'i18n/exists': jest.fn()
+          }
+        }
+      },
     });
 
     const host = wrapper.find('[data-testid="input-email-host"]');
@@ -15,5 +24,31 @@ describe('page: Routes and Receivers', () => {
 
     expect(host.exists()).toBe(true);
     expect(port.exists()).toBe(true);
+  });
+  it('email host and posrt field should have value to a valid integer', () => {
+    const wrapper = mount(Email, {
+      propsData: {
+        mode:  'create',
+        value: {}
+      },
+      mocks: {
+        t:      (text: string) => text, // Mock i18n global function used as alternative to the getter
+        $store: {
+          getters: {
+            'i18n/t':      jest.fn(),
+            'i18n/exists': jest.fn()
+          }
+        }
+      },
+    });
+
+    const host = wrapper.find('[data-testid="input-email-host"]');
+    const port = wrapper.find('[data-testid="input-email-port"]');
+
+    host.setValue('10.2.300.3');
+    port.setValue('8080');
+
+    expect(host.element.value).toStrictEqual('10.2.300.3');
+    expect(port.element.value).toStrictEqual('8080');
   });
 });
