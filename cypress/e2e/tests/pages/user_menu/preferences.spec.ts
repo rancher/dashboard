78da214cca0e535320552be8217b12d113ca40b0
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-06-12 13:58:46 -0700

    removed .only

--- cypress/e2e/tests/pages/user_menu/preferences.spec.ts
+++ cypress/e2e/tests/pages/user_menu/preferences.spec.ts
@@ -16,7 +16,7 @@ describe('User can update their preferences', () => {
     cy.login();
   });
 
-  it.only('Can navigate to Preferences Page', { tags: ['@adminUser', '@standardUser'] }, () => {
+  it('Can navigate to Preferences Page', { tags: ['@adminUser', '@standardUser'] }, () => {
     /*
     Open user menu and navigate to Preferences page
     Verify url includes endpoint '/prefs'
