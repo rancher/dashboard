91cabd73f250e028c3223460e1dbebdf85728ab3
Neil MacDougall <nmacdougall@suse.com>
2023-06-15 15:08:38 +0100

    Remove unused var

--- cypress/e2e/tests/pages/cluster-manager.spec.ts
+++ cypress/e2e/tests/pages/cluster-manager.spec.ts
@@ -16,7 +16,6 @@ const runPrefix = `e2e-test-${ runTimestamp }`;
 
 // File specific consts
 const { baseUrl } = Cypress.config();
-const clusterRequestBase = `${ baseUrl }/v1/provisioning.cattle.io.clusters/fleet-default`;
 const clusterNamePartial = `${ runPrefix }-create`;
 const rke2CustomName = `${ clusterNamePartial }-rke2-custom`;
 const importGenericName = `${ clusterNamePartial }-import-generic`;
