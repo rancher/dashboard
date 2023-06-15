c39c6b369b7532c76a73e117628277d1a32eeaff
Francesco Torchia <francesco.torchia@suse.com>
2023-06-15 19:06:28 +0200

    Remove manifest from Chart & image exports
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/epinio/dialog/ExportAppDialog.vue
+++ pkg/epinio/dialog/ExportAppDialog.vue
@@ -55,6 +55,7 @@ export default {
         await resource.createManifest();
       } else {
         const partsData = await resource?.applicationParts
+          .filter(part => part !== APPLICATION_PARTS.MANIFEST)
           .reduce(async(acc, part) => ({
             ...await acc,
             [part]: await resource.fetchPart(part),
