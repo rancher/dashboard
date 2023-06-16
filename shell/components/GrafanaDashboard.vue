bdcaa45ffa781bafdbf63a78f1f64934a30786a0
Dai Yuzeng <cridequeger@yahoo.com>
2023-06-16 17:56:47 +0800

    HARVESTER: keep the param referer on the newly opened grafana page.

--- shell/components/GrafanaDashboard.vue
+++ shell/components/GrafanaDashboard.vue
@@ -253,10 +253,16 @@ export default {
       v-if="!loading && !error"
       class="external-link"
     >
+      <!-- https://github.com/harvester/harvester-installer/pull/512/files -->
+      <!-- It is necessary to include the parameter referer when accessing the Grafana page. -->
+      <!-- This parameter is required by the backend to identify the origin of the request from which cluster -->
+      <!-- The matching mechanism as follows: -->
+      <!-- ~.*/k8s/clusters/(c-m-.+)/.* -->
+      <!-- ~.*/dashboard/harvester/c/(c-m-.+)/.* -->
       <a
         :href="grafanaUrl"
         target="_blank"
-        rel="noopener noreferrer nofollow"
+        rel="noopener nofollow"
       >{{ t('grafanaDashboard.grafana') }} <i class="icon icon-external-link" /></a>
     </div>
   </div>
