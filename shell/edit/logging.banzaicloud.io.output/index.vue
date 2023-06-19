701913f925a9d4377f130fbfda417ae696586748
Richa <richa.bisht@suse.com>
2023-06-19 09:05:20 +0200

    Fixed footer logging cluster create screen

--- shell/edit/logging.banzaicloud.io.output/index.vue
+++ shell/edit/logging.banzaicloud.io.output/index.vue
@@ -258,6 +258,14 @@ export default {
   $logo: 60px;
 
 .output {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  .side-tabs {
+    background: yellow;
+    flex: 1;
+  }
   .provider {
     h1 {
       display: inline-block;
