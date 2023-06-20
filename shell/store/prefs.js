c63762f108fe528c90d2a3dea3094db4a88d8642
Dai Yuzeng <cridequeger@yahoo.com>
2023-06-20 12:50:40 +0800

    HARVESTER: add preferred-shortcut-keys to user preference

--- shell/store/prefs.js
+++ shell/store/prefs.js
@@ -75,6 +75,7 @@ export const HIDE_DESC = create('hide-desc', [], { parseJSON });
 export const HIDE_SENSITIVE = create('hide-sensitive', true, { options: [true, false], parseJSON });
 export const SHOW_PRE_RELEASE = create('show-pre-release', false, { options: [false, true], parseJSON });
 export const SHOW_CHART_MODE = create('chart-mode', 'featured', { parseJSON });
+export const PREFERED_SHORTCUT_KEYS = create('prefered-shortcut-keys', [], { parseJSON });
 
 export const DATE_FORMAT = create('date-format', 'ddd, MMM D YYYY', {
   options: [
