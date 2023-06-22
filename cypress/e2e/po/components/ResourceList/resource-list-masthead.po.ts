0df3eba2b2df7d265b8909b8a80755588120646b
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2023-06-22 09:31:21 +0100

    add e2e tests for users/roles list view for standard user (#9097)
    * redoing the PR
    
    * address PR comments
    
    * address PR comments
    
    ---------
    
    Co-authored-by: Alexandre Alves <aalves@Alexandres-MBP.lan>

--- cypress/e2e/po/components/ResourceList/resource-list-masthead.po.ts
+++ cypress/e2e/po/components/ResourceList/resource-list-masthead.po.ts
@@ -4,4 +4,8 @@ export default class ResourceListMastheadPo extends ComponentPo {
   actions() {
     return this.self().get('.actions-container .actions .btn');
   }
+
+  title() {
+    return this.self().find('.title > h1').invoke('text');
+  }
 }
