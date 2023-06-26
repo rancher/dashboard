03c7391c926517fa0eef09824e0e9323cc945cf9
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-06-26 15:33:02 +0100

    fix issue with Resources Select in globalRoles edit/create interface

--- shell/components/form/Select.vue
+++ shell/components/form/Select.vue
@@ -135,8 +135,8 @@ export default {
       return true;
     },
     getOptionKey(opt) {
-      if (opt?.optionKey) {
-        return get(opt, opt.optionKey);
+      if (this.optionKey) {
+        return get(opt, this.optionKey);
       }
 
       const label = this.getOptionLabel(opt);
