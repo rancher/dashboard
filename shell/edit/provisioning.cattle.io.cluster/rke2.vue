11f7cfe9f19fc55dae4e002838cab9569ae8942e
Giuseppe Leo <giuseppe.leo@suse.com>
2023-07-13 19:52:49 +0200

    [RKE2] Cluster unable to provision after switching back to Back to Form from Edit as YAML (#9264)
    * Cleanup code
    
    * Prevent to JSON clone agent config if undefined
    
    * Add clean-tooltip directive to jest configuration
    
    * Remove agent cleanup from the before hook
    
    * Split fetch methods in parts
    
    * Move condition to parent
    
    * Add tests for agent configuration
    
    * SKip test till resolved extra setup
    
    * Updated code comment based on changes

--- shell/edit/provisioning.cattle.io.cluster/rke2.vue
+++ shell/edit/provisioning.cattle.io.cluster/rke2.vue
@@ -71,7 +71,7 @@ import S3Config from './S3Config';
 import SelectCredential from './SelectCredential';
 import AdvancedSection from '@shell/components/AdvancedSection.vue';
 import { ELEMENTAL_SCHEMA_IDS, KIND, ELEMENTAL_CLUSTER_PROVIDER } from '../../config/elemental-types';
-import AgentConfiguration, { cleanAgentConfiguration } from './AgentConfiguration';
+import AgentConfiguration from './AgentConfiguration';
 import { getApplicableExtensionEnhancements } from '@shell/core/plugin-helpers';
 import { ExtensionPoint, TabLocation } from '@shell/core/types';
 
@@ -158,128 +158,9 @@ export default {
   },
 
   async fetch() {
-    // Check presence of PSP in RKE2, which is where we show the templates
-    this.psps = await this.checkPsps();
-
-    if ( !this.rke2Versions ) {
-      const hash = {
-        rke2Versions: this.$store.dispatch('management/request', { url: '/v1-rke2-release/releases' }),
-        k3sVersions:  this.$store.dispatch('management/request', { url: '/v1-k3s-release/releases' }),
-      };
-
-      if ( this.$store.getters['management/canList'](MANAGEMENT.POD_SECURITY_POLICY_TEMPLATE) ) {
-        hash.allPSPs = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.POD_SECURITY_POLICY_TEMPLATE });
-      }
-
-      if (this.$store.getters['management/canList'](MANAGEMENT.PSA)) {
-        hash.allPSAs = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.PSA });
-      }
-
-      // Get the latest versions from the global settings if possible
-      const globalSettings = await this.$store.getters['management/all'](MANAGEMENT.SETTING) || [];
-      const defaultRke2Setting = globalSettings.find((setting) => setting.id === 'rke2-default-version') || {};
-      const defaultK3sSetting = globalSettings.find((setting) => setting.id === 'k3s-default-version') || {};
-
-      let defaultRke2 = defaultRke2Setting?.value || defaultRke2Setting?.default;
-      let defaultK3s = defaultK3sSetting?.value || defaultK3sSetting?.default;
-
-      // RKE2: Use the channel if we can not get the version from the settings
-      if (!defaultRke2) {
-        hash.rke2Channels = this.$store.dispatch('management/request', { url: '/v1-rke2-release/channels' });
-      }
-
-      // K3S: Use the channel if we can not get the version from the settings
-      if (!defaultK3s) {
-        hash.k3sChannels = this.$store.dispatch('management/request', { url: '/v1-k3s-release/channels' });
-      }
-
-      const res = await allHash(hash);
-
-      this.allPSPs = res.allPSPs || [];
-      this.allPSAs = res.allPSAs || [];
-      this.rke2Versions = res.rke2Versions.data || [];
-      this.k3sVersions = res.k3sVersions.data || [];
-
-      if (!defaultRke2) {
-        const rke2Channels = res.rke2Channels.data || [];
-
-        defaultRke2 = rke2Channels.find((x) => x.id === 'default')?.latest;
-      }
-
-      if (!defaultK3s) {
-        const k3sChannels = res.k3sChannels.data || [];
-
-        defaultK3s = k3sChannels.find((x) => x.id === 'default')?.latest;
-      }
-
-      if ( !this.rke2Versions.length && !this.k3sVersions.length ) {
-        throw new Error('No version info found in KDM');
-      }
-
-      // Store default versions
-      this.defaultRke2 = defaultRke2;
-      this.defaultK3s = defaultK3s;
-    }
-
-    if ( !this.value.spec ) {
-      set(this.value, 'spec', {});
-    }
-
-    if ( !this.value.spec.machineSelectorConfig ) {
-      set(this.value.spec, 'machineSelectorConfig', []);
-    }
-
-    if ( !this.value.spec.machineSelectorConfig.find((x) => !x.machineLabelSelector) ) {
-      this.value.spec.machineSelectorConfig.unshift({ config: {} });
-    }
-
-    if ( this.value.spec.cloudCredentialSecretName ) {
-      await this.$store.dispatch('rancher/findAll', { type: NORMAN.CLOUD_CREDENTIAL });
-      this.credentialId = `${ this.value.spec.cloudCredentialSecretName }`;
-    }
-
-    if ( !this.value.spec.kubernetesVersion ) {
-      set(this.value.spec, 'kubernetesVersion', this.defaultVersion);
-    }
-
-    if ( this.rkeConfig.etcd?.s3?.bucket ) {
-      this.s3Backup = true;
-    }
-
-    if ( !this.rkeConfig.etcd ) {
-      set(this.rkeConfig, 'etcd', {
-        disableSnapshots:     false,
-        s3:                   null,
-        snapshotRetention:    5,
-        snapshotScheduleCron: '0 */5 * * *',
-      });
-    } else if (typeof this.rkeConfig.etcd.disableSnapshots === 'undefined') {
-      const disableSnapshots = !this.rkeConfig.etcd.snapshotRetention && !this.rkeConfig.etcd.snapshotScheduleCron;
-
-      set(this.rkeConfig.etcd, 'disableSnapshots', disableSnapshots);
-    }
-
-    // Namespaces if required - this is mainly for custom provisioners via extensions that want
-    // to allow creating their resources in a different namespace
-    if (this.needsNamespace) {
-      this.allNamespaces = await this.$store.dispatch('management/findAll', { type: NAMESPACE });
-    }
-
-    if ( !this.machinePools ) {
-      await this.initMachinePools(this.value.spec.rkeConfig.machinePools);
-      if ( this.mode === _CREATE && !this.machinePools.length ) {
-        await this.addMachinePool();
-      }
-    }
-
-    if ( this.value.spec.defaultPodSecurityPolicyTemplateName === undefined ) {
-      set(this.value.spec, 'defaultPodSecurityPolicyTemplateName', '');
-    }
-
-    if ( this.value.spec.defaultPodSecurityAdmissionConfigurationTemplateName === undefined ) {
-      set(this.value.spec, 'defaultPodSecurityAdmissionConfigurationTemplateName', '');
-    }
-
+    this.psps = await this.getPsps();
+    await this.fetchRke2Versions();
+    await this.initSpecs();
     await this.initAddons();
     await this.initRegistry();
 
@@ -289,17 +170,7 @@ export default {
       this.userChartValues[key] = value;
     });
 
-    // Ensure we have empty models for the two agent configurations
-
-    // Cluster Agent Configuration
-    if ( !this.value.spec[CLUSTER_AGENT_CUSTOMIZATION]) {
-      set(this.value.spec, CLUSTER_AGENT_CUSTOMIZATION, {});
-    }
-
-    // Fleet Agent Configuration
-    if ( !this.value.spec[FLEET_AGENT_CUSTOMIZATION] ) {
-      set(this.value.spec, FLEET_AGENT_CUSTOMIZATION, {});
-    }
+    this.setAgentConfiguration();
   },
 
   data() {
@@ -1174,7 +1045,6 @@ export default {
   created() {
     this.registerBeforeHook(this.saveMachinePools, 'save-machine-pools');
     this.registerBeforeHook(this.setRegistryConfig, 'set-registry-config');
-    this.registerBeforeHook(this.agentConfigurationCleanup, 'cleanup-agent-config');
     this.registerAfterHook(this.cleanupMachinePools, 'cleanup-machine-pools');
     this.registerAfterHook(this.saveRoleBindings, 'save-role-bindings');
 
@@ -1188,10 +1058,185 @@ export default {
     nlToBr,
     set,
 
+    /**
+     * Initialize all the cluster specs
+     */
+    async initSpecs() {
+      if ( !this.value.spec ) {
+        set(this.value, 'spec', {});
+      }
+
+      if ( !this.value.spec.machineSelectorConfig ) {
+        set(this.value.spec, 'machineSelectorConfig', []);
+      }
+
+      if ( !this.value.spec.machineSelectorConfig.find((x) => !x.machineLabelSelector) ) {
+        this.value.spec.machineSelectorConfig.unshift({ config: {} });
+      }
+
+      if ( this.value.spec.cloudCredentialSecretName ) {
+        await this.$store.dispatch('rancher/findAll', { type: NORMAN.CLOUD_CREDENTIAL });
+        this.credentialId = `${ this.value.spec.cloudCredentialSecretName }`;
+      }
+
+      if ( !this.value.spec.kubernetesVersion ) {
+        set(this.value.spec, 'kubernetesVersion', this.defaultVersion);
+      }
+
+      if ( this.rkeConfig.etcd?.s3?.bucket ) {
+        this.s3Backup = true;
+      }
+
+      if ( !this.rkeConfig.etcd ) {
+        set(this.rkeConfig, 'etcd', {
+          disableSnapshots:     false,
+          s3:                   null,
+          snapshotRetention:    5,
+          snapshotScheduleCron: '0 */5 * * *',
+        });
+      } else if (typeof this.rkeConfig.etcd.disableSnapshots === 'undefined') {
+        const disableSnapshots = !this.rkeConfig.etcd.snapshotRetention && !this.rkeConfig.etcd.snapshotScheduleCron;
+
+        set(this.rkeConfig.etcd, 'disableSnapshots', disableSnapshots);
+      }
+
+      // Namespaces if required - this is mainly for custom provisioners via extensions that want
+      // to allow creating their resources in a different namespace
+      if (this.needsNamespace) {
+        this.allNamespaces = await this.$store.dispatch('management/findAll', { type: NAMESPACE });
+      }
+
+      if ( !this.machinePools ) {
+        await this.initMachinePools(this.value.spec.rkeConfig.machinePools);
+        if ( this.mode === _CREATE && !this.machinePools.length ) {
+          await this.addMachinePool();
+        }
+      }
+
+      if ( this.value.spec.defaultPodSecurityPolicyTemplateName === undefined ) {
+        set(this.value.spec, 'defaultPodSecurityPolicyTemplateName', '');
+      }
+
+      if ( this.value.spec.defaultPodSecurityAdmissionConfigurationTemplateName === undefined ) {
+        set(this.value.spec, 'defaultPodSecurityAdmissionConfigurationTemplateName', '');
+      }
+    },
+
+    /**
+     * Fetch RKE versions and their configurations to be mapped to the form
+     */
+    async fetchRke2Versions() {
+      if ( !this.rke2Versions ) {
+        const hash = {
+          rke2Versions: this.$store.dispatch('management/request', { url: '/v1-rke2-release/releases' }),
+          k3sVersions:  this.$store.dispatch('management/request', { url: '/v1-k3s-release/releases' }),
+        };
+
+        if ( this.$store.getters['management/canList'](MANAGEMENT.POD_SECURITY_POLICY_TEMPLATE) ) {
+          hash.allPSPs = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.POD_SECURITY_POLICY_TEMPLATE });
+        }
+
+        if (this.$store.getters['management/canList'](MANAGEMENT.PSA)) {
+          hash.allPSAs = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.PSA });
+        }
+
+        // Get the latest versions from the global settings if possible
+        const globalSettings = await this.$store.getters['management/all'](MANAGEMENT.SETTING) || [];
+        const defaultRke2Setting = globalSettings.find((setting) => setting.id === 'rke2-default-version') || {};
+        const defaultK3sSetting = globalSettings.find((setting) => setting.id === 'k3s-default-version') || {};
+
+        let defaultRke2 = defaultRke2Setting?.value || defaultRke2Setting?.default;
+        let defaultK3s = defaultK3sSetting?.value || defaultK3sSetting?.default;
+
+        // RKE2: Use the channel if we can not get the version from the settings
+        if (!defaultRke2) {
+          hash.rke2Channels = this.$store.dispatch('management/request', { url: '/v1-rke2-release/channels' });
+        }
+
+        // K3S: Use the channel if we can not get the version from the settings
+        if (!defaultK3s) {
+          hash.k3sChannels = this.$store.dispatch('management/request', { url: '/v1-k3s-release/channels' });
+        }
+
+        const res = await allHash(hash);
+
+        this.allPSPs = res.allPSPs || [];
+        this.allPSAs = res.allPSAs || [];
+        this.rke2Versions = res.rke2Versions.data || [];
+        this.k3sVersions = res.k3sVersions.data || [];
+
+        if (!defaultRke2) {
+          const rke2Channels = res.rke2Channels.data || [];
+
+          defaultRke2 = rke2Channels.find((x) => x.id === 'default')?.latest;
+        }
+
+        if (!defaultK3s) {
+          const k3sChannels = res.k3sChannels.data || [];
+
+          defaultK3s = k3sChannels.find((x) => x.id === 'default')?.latest;
+        }
+
+        if ( !this.rke2Versions.length && !this.k3sVersions.length ) {
+          throw new Error('No version info found in KDM');
+        }
+
+        // Store default versions
+        this.defaultRke2 = defaultRke2;
+        this.defaultK3s = defaultK3s;
+      }
+    },
+
+    cleanAgentConfiguration(model, key) {
+      if (!model || !model[key]) {
+        return;
+      }
+
+      const v = model[key];
+
+      if (Array.isArray(v) && v.length === 0) {
+        delete model[key];
+      } else if (v && typeof v === 'object') {
+        Object.keys(v).forEach((k) => {
+          // delete these auxiliary props used in podAffinity and nodeAffinity that shouldn't be sent to the server
+          if (k === '_namespaceOption' || k === '_namespaces' || k === '_anti' || k === '_id') {
+            delete v[k];
+          }
+
+          // prevent cleanup of "namespaceSelector" when an empty object because it represents all namespaces in pod/node affinity
+          // https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.25/#podaffinityterm-v1-core
+          if (k !== 'namespaceSelector') {
+            this.cleanAgentConfiguration(v, k);
+          }
+        });
+
+        if (Object.keys(v).length === 0) {
+          delete model[key];
+        }
+      }
+    },
+
+    /**
+     * Clean agent configuration objects, so we only send values when the user has configured something
+     */
     agentConfigurationCleanup() {
-      // Clean agent configuration objects, so we only send values when the user has configured something
-      cleanAgentConfiguration(this.value.spec, CLUSTER_AGENT_CUSTOMIZATION);
-      cleanAgentConfiguration(this.value.spec, FLEET_AGENT_CUSTOMIZATION);
+      this.cleanAgentConfiguration(this.value.spec, CLUSTER_AGENT_CUSTOMIZATION);
+      this.cleanAgentConfiguration(this.value.spec, FLEET_AGENT_CUSTOMIZATION);
+    },
+
+    /**
+     * Ensure we have empty models for the two agent configurations
+     */
+    setAgentConfiguration() {
+      // Cluster Agent Configuration
+      if ( !this.value.spec[CLUSTER_AGENT_CUSTOMIZATION]) {
+        set(this.value.spec, CLUSTER_AGENT_CUSTOMIZATION, {});
+      }
+
+      // Fleet Agent Configuration
+      if ( !this.value.spec[FLEET_AGENT_CUSTOMIZATION] ) {
+        set(this.value.spec, FLEET_AGENT_CUSTOMIZATION, {});
+      }
     },
 
     /**
@@ -1518,6 +1563,9 @@ export default {
     },
 
     async _doSaveOverride(btnCb) {
+      // We cannot use the hook, because it is triggered on YAML toggle without restore initialized data
+      this.agentConfigurationCleanup();
+
       if ( this.errors ) {
         clear(this.errors);
       }
@@ -1613,10 +1661,10 @@ export default {
         delete this.value.spec.rkeConfig.machineGlobalConfig.profile;
       }
 
-      // store the current data for fleet and cluster agent so that we can re-apply it later if the save fails
-      // we also have a before hook (check created() hooks) where the cleanup of the data occurs
-      const clusterAgentDeploymentCustomization = JSON.parse(JSON.stringify(this.value.spec[CLUSTER_AGENT_CUSTOMIZATION]));
-      const fleetAgentDeploymentCustomization = JSON.parse(JSON.stringify(this.value.spec[FLEET_AGENT_CUSTOMIZATION]));
+      // Store the current data for fleet and cluster agent so that we can re-apply it later if the save fails
+      // The cleanup occurs before save with agentConfigurationCleanup()
+      const clusterAgentDeploymentCustomization = this.value.spec[CLUSTER_AGENT_CUSTOMIZATION] ? JSON.parse(JSON.stringify(this.value.spec[CLUSTER_AGENT_CUSTOMIZATION])) : null;
+      const fleetAgentDeploymentCustomization = this.value.spec[FLEET_AGENT_CUSTOMIZATION] ? JSON.parse(JSON.stringify(this.value.spec[FLEET_AGENT_CUSTOMIZATION])) : null;
 
       await this.save(btnCb);
 
@@ -2090,10 +2138,9 @@ export default {
     },
 
     /**
-     * Check if current cluster has PSP enabled
-     * Consider exclusively RKE2 provisioned clusters in edit mode
+     * Get provisioned RKE2 cluster PSPs in edit mode
      */
-    async checkPsps() {
+    async getPsps() {
       // As server returns 500 we exclude all the possible cases
       if (
         this.mode !== _CREATE &&
@@ -3025,7 +3072,9 @@ export default {
           label-key="cluster.agentConfig.tabs.cluster"
         >
           <AgentConfiguration
+            v-if="value.spec.clusterAgentDeploymentCustomization"
             v-model="value.spec.clusterAgentDeploymentCustomization"
+            data-testid="rke2-cluster-agent-config"
             type="cluster"
             :mode="mode"
           />
@@ -3037,7 +3086,9 @@ export default {
           label-key="cluster.agentConfig.tabs.fleet"
         >
           <AgentConfiguration
+            v-if="value.spec.fleetAgentDeploymentCustomization"
             v-model="value.spec.fleetAgentDeploymentCustomization"
+            data-testid="rke2-fleet-agent-config"
             type="fleet"
             :mode="mode"
           />
