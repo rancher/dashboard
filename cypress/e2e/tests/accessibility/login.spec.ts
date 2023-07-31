d554ea09a6ec0282717b39bfe49552324bebb2f6
Richa <richa.bisht@suse.com>
2023-07-31 21:15:53 +0200

    Fixed lint

--- cypress/e2e/tests/accessibility/login.spec.ts
+++ cypress/e2e/tests/accessibility/login.spec.ts
@@ -1,10 +1,10 @@
 import { LoginPagePo } from '@/cypress/e2e/po/pages/login-page.po';
 
-describe('Local authentication', { tags: ['@adminUser', '@standardUser'] }, () => {
-  it('Log in with valid credentials', () => {
+describe('Login page a11y testing', { tags: ['@adminUser', '@standardUser'] }, () => {
+  it('wcag21aa test', (wait = 600) => {
     LoginPagePo.goTo();
     cy.injectAxe();
-    cy.wait(600);
+    cy.wait(wait);
     cy.checkPageAccessibility();
   });
 });
