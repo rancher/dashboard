a4bae991f217b2b36b16bb27785d828400ec33b7
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2023-08-08 16:06:49 -0700

    improve login error message display

--- shell/store/auth.js
+++ shell/store/auth.js
@@ -334,10 +334,10 @@ export const actions = {
     } catch (err) {
       if (err._status === 401) {
         return Promise.reject(LOGIN_ERRORS.CLIENT_UNAUTHORIZED);
-      } else if ( err._status >= 400 && err._status <= 499 ) {
-        return Promise.reject(LOGIN_ERRORS.CLIENT);
       } else if (err.message) {
         return Promise.reject(err.message);
+      } else if ( err._status >= 400 && err._status <= 499 ) {
+        return Promise.reject(LOGIN_ERRORS.CLIENT);
       }
 
       return Promise.reject(LOGIN_ERRORS.SERVER);
