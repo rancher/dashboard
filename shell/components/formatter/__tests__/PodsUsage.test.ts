20d97cbd7d7ba50f57743bf2354d23870aa966ad
Sean <sean.mchugh@suse.com>
2023-08-16 09:47:08 -0400

    Now uses the same data as the cluster homepage, delayed load not needed.

--- shell/components/formatter/__tests__/PodsUsage.test.ts
+++ shell/components/formatter/__tests__/PodsUsage.test.ts
@@ -2,37 +2,44 @@ import { mount } from '@vue/test-utils';
 import PodsUsage from '@shell/components/formatter/PodsUsage.vue';
 
 describe('component: PodsUsage', () => {
-  it('should not display values if data is not ready', () => {
-    const wrapper = mount(PodsUsage, {
-      propsData: { row: {} },
-      mocks:     { $store: { dispatch: { 'management/request': jest.fn() } } }
-    });
-
-    const element = wrapper.find('p').element;
-
-    expect(element).toBeUndefined();
-  });
-
-  it('should display spinning icon', () => {
-    const wrapper = mount(PodsUsage, {
-      propsData: { row: {} },
-      mocks:     { $store: { dispatch: { 'management/request': jest.fn() } } }
-    });
-
-    const element = wrapper.find('i').element;
-
-    expect(element).toBeDefined();
-  });
-
   it('should display podsUsage value', () => {
     const wrapper = mount(PodsUsage, {
-      propsData: { row: { isReady: true } },
-      data:      () => ({ loading: false }),
-      mocks:     { $store: { dispatch: { 'management/request': jest.fn() } } }
+      propsData: {
+        row: {
+          mgmt: {
+            status: {
+              requested:   { pods: 10 },
+              allocatable: { pods: 20 }
+            }
+          }
+        }
+      },
+      mocks: { $store: { dispatch: { 'management/request': jest.fn() } } }
     });
 
     const element = wrapper.find('p').element;
 
     expect(element.textContent).toBeDefined();
+    expect(element.textContent).toBe('10/20');
+  });
+  it('should display dash when there are no totalPods', () => {
+    const wrapper = mount(PodsUsage, {
+      propsData: {
+        row: {
+          mgmt: {
+            status: {
+              requested:   { pods: 10 },
+              allocatable: { pods: 0 }
+            }
+          }
+        }
+      },
+      mocks: { $store: { dispatch: { 'management/request': jest.fn() } } }
+    });
+
+    const element = wrapper.find('p').element;
+
+    expect(element.textContent).toBeDefined();
+    expect(element.textContent).toBe('—');
   });
 });
