c0dfc7fdeca0526ed78764a7bbc756e20aca3741
Shavindra <shavindra@users.noreply.github.com>
2023-08-18 10:54:49 +0200

    Refactor tests.

--- cypress/e2e/tests/pages/fleet/dashboard.spec.ts
+++ cypress/e2e/tests/pages/fleet/dashboard.spec.ts
@@ -32,21 +32,14 @@ describe('Fleet Dashboard', { tags: '@adminUser' }, () => {
     const fleetLocalResourceTable = fleetDashboardPage.resourceTable('fleet-local');
     const row = fleetLocalResourceTable.sortableTable().rowElements().eq(0);
 
-    row.find('.bg-success[data-testid="clusters-ready"] span').should('have.text', '1/1');
-  });
+    row.get('.bg-success[data-testid="clusters-ready"]').should('exist');
+    row.get('.bg-success[data-testid="clusters-ready"] span').should('have.text', '1/1');
 
-  it('Should display bundles status', () => {
-    const fleetLocalResourceTable = fleetDashboardPage.resourceTable('fleet-local');
-    const row = fleetLocalResourceTable.sortableTable().rowElements().eq(0);
+    row.get('.bg-success[data-testid="clusters-ready"]').wait(6000).should('exist');
+    row.get('.bg-success[data-testid="bundles-ready"] span').should('have.text', '1/1');
 
-    row.find('.bg-success[data-testid="bundles-ready"] span').should('have.text', '1/1');
-  });
-
-  it('Should display resources status', () => {
-    const fleetLocalResourceTable = fleetDashboardPage.resourceTable('fleet-local');
-    const row = fleetLocalResourceTable.sortableTable().rowElements().eq(0);
-
-    row.find('.bg-success[data-testid="resources-ready"] span').should('have.text', '1/1');
+    row.get('.bg-success[data-testid="clusters-ready"]').wait(6000).should('exist');
+    row.get('.bg-success[data-testid="resources-ready"] span').should('have.text', '1/1');
   });
 
   after(() => {
