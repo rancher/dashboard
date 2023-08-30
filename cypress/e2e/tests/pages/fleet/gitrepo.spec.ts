e060a32adbd3f14ab135d9f369dce1bcd10479d6
Shavindra <shavindra@users.noreply.github.com>
2023-08-30 23:10:29 +0200

    Add E2E

--- cypress/e2e/tests/pages/fleet/gitrepo.spec.ts
+++ cypress/e2e/tests/pages/fleet/gitrepo.spec.ts
@@ -3,59 +3,62 @@ import { FleetDashboardPagePo } from '@/cypress/e2e/po/pages/fleet/fleet-dashboa
 import { gitRepoCreateRequest } from '@/cypress/e2e/blueprints/fleet/gitrepos';
 
 describe('Git Repo', { tags: '@adminUser' }, () => {
-  beforeEach(() => {
-    cy.login();
-  });
 
   describe('Create', () => {
     let gitRepoCreatePage: GitRepoCreatePo;
     const repoList = [];
 
-    beforeEach(() => {
+    
+
+    before(() => {
+      cy.login();
       gitRepoCreatePage = new GitRepoCreatePo('local');
       cy.interceptAllRequests('POST');
+      gitRepoCreatePage.goTo();
 
-    });
-
-    it('Should be able to create a git repo', () => {
       const { name, } = gitRepoCreateRequest.metadata;
       const { repo, branch, paths, helmRepoURLRegex } = gitRepoCreateRequest.spec;
 
-      gitRepoCreatePage.goTo();
       gitRepoCreatePage.setRepoName(name);
       gitRepoCreatePage.setGitRepoUrl(repo);
+      gitRepoCreatePage.selectWorkspace('fleet-default');
       gitRepoCreatePage.setBranchName(branch);
-      gitRepoCreatePage.gitRepoPaths().setValueAtIndex(paths[0], 0);
+      gitRepoCreatePage.helmAuth().createBasicAuth('test', 'test');
       gitRepoCreatePage.setHelmRepoURLRegex(helmRepoURLRegex);
+
+      gitRepoCreatePage.gitRepoPaths().setValueAtIndex(paths[0], 0);
       gitRepoCreatePage.goToNext();
       gitRepoCreatePage.create();
 
       repoList.push(name);
     });
 
-    it('Should be able to create a gitrepo with helm credentials and regex', () => {
+    it('Should be able to create a git repo', () => {
 
+      // First request is for creating credentials
+      let secretName = '';
       cy.wait('@interceptAllRequests0').then(({ request, response }) => {
-        expect(request.body).to.deep.eq(gitRepoCreateRequest);
         expect(response.statusCode).to.eq(201);
-        expect(response.body.metadata.name).to.eq(name);
-        expect(response.body.metadata.namespace).to.eq(namespace);
+        secretName = response.body.metadata.name;
+        expect(secretName).to.not.be.empty;
       });
 
-      cy.wait('@interceptAllRequests1').then(({ request, response }) => {
-        expect(request.body).to.deep.eq(gitRepoCreateRequest);
+      // Second request is for creating the git repo
+      cy.wait('@interceptAllRequests0').then(({ request, response }) => {
+        gitRepoCreateRequest.spec.helmSecretName = secretName;
         expect(response.statusCode).to.eq(201);
+        expect(request.body).to.deep.eq(gitRepoCreateRequest);
       });
-
-    })
+      
+    });
 
     after(() => {
-      const fleetDashboardPage = new FleetDashboardPagePo('local');
+       const fleetDashboardPage = new FleetDashboardPagePo('local');
 
-      fleetDashboardPage.goTo();
-      const fleetLocalResourceTable = fleetDashboardPage.resourceTable('fleet-default');
+       fleetDashboardPage.goTo();
+       const fleetLocalResourceTable = fleetDashboardPage.resourceTable('fleet-default');
 
-      fleetLocalResourceTable.sortableTable().deleteItemWithUI('fleet-e2e-test-gitrepo');
-    });
+       fleetLocalResourceTable.sortableTable().deleteItemWithUI('fleet-e2e-test-gitrepo');
+     });
   });
 });
