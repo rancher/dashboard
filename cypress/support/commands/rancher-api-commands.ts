596185e555c05d4242ed3265cbd89a13ae0463d0
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-09-10 14:59:38 -0700

    add slash

--- cypress/support/commands/rancher-api-commands.ts
+++ cypress/support/commands/rancher-api-commands.ts
@@ -297,7 +297,7 @@ Cypress.Commands.add('setRancherResource', (prefix, resourceType, resourceId, bo
 Cypress.Commands.add('getUserPreferences', () => {
   return cy.request({
     method:  'GET',
-    url:     `${ Cypress.env('api') }v1/userpreferences`,
+    url:     `${ Cypress.env('api') }/v1/userpreferences`,
     headers: {
       'x-api-csrf': token.value,
       Accept:       'application/json'
