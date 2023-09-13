dfd4a06ecb3b7a2db109f4c12254c7b3f8e1e978
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-09-13 10:44:13 -0700

    fix linting

--- cypress/e2e/tests/pages/global-settings/settings-p2.spec.ts
+++ cypress/e2e/tests/pages/global-settings/settings-p2.spec.ts
@@ -4,7 +4,6 @@ import BurgerMenuPo from '@/cypress/e2e/po/side-bars/burger-side-menu.po';
 import ClusterManagerCreateRke2CustomPagePo from '@/cypress/e2e/po/edit/provisioning.cattle.io.cluster/create/cluster-create-rke2-custom.po';
 import AccountPagePo from '@/cypress/e2e/po/pages/account-api-keys.po';
 import ClusterManagerListPagePo from '@/cypress/e2e/po/pages/cluster-manager/cluster-manager-list.po';
-import ProductNavPo from '@/cypress/e2e/po/side-bars/product-side-nav.po';
 import { settings } from '@/cypress/e2e/blueprints/global_settings/settigns-data';
 
 const settingsPage = new SettingsPagePo();
