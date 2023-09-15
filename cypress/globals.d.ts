1047ca57440ae3cc41249ad9e7fe2ffc868cd8e4
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-09-15 09:57:16 -0700

    remove getUserPreferences()

--- cypress/globals.d.ts
+++ cypress/globals.d.ts
@@ -37,7 +37,6 @@ declare namespace Cypress {
 
     getRancherResource(prefix: 'v3' | 'v1', resourceType: string, resourceId: string, expectedStatusCode: string): Chainable;
     setRancherResource(prefix: 'v3' | 'v1', resourceType: string, resourceId: string, body: string): Chainable;
-    getUserPreferences(): Chainable;
 
     /**
      *  Wrapper for cy.get() to simply define the data-testid value that allows you to pass a matcher to find the element.
