b6b1bf2774a8b82cd013c8b0efeabe0f00a0e826
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-09-21 15:22:51 -0700

    small change

--- cypress/e2e/tests/pages/explorer/cluster-tools.spec.ts
+++ cypress/e2e/tests/pages/explorer/cluster-tools.spec.ts
@@ -1,7 +1,7 @@
 import ClusterToolsPagePo from '@/cypress/e2e/po/pages/explorer/cluster-tools.po';
 import ClusterDashboardPagePo from '@/cypress/e2e/po/pages/explorer/cluster-dashboard.po';
-import { InstallChartsPage } from '~/cypress/e2e/po/pages/explorer/install-charts.po';
-import PromptRemove from '~/cypress/e2e/po/prompts/promptRemove.po';
+import { InstallChartsPage } from '@/cypress/e2e/po/pages/explorer/install-charts.po';
+import PromptRemove from '@/cypress/e2e/po/prompts/promptRemove.po';
 
 const clusterTools = new ClusterToolsPagePo('local');
 
