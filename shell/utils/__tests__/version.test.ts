981db834ccd027ec951d2d0a99e3f083f2626f71
cnotv <giuseppe.leo@suse.com>
2023-09-28 14:47:58 +0200

    Add fallback and tests for version comparison not retrieving values

--- shell/utils/__tests__/version.test.ts
+++ shell/utils/__tests__/version.test.ts
@@ -1,4 +1,40 @@
-import { isDevBuild } from '@shell/utils/version';
+import { isDevBuild, compare } from '@shell/utils/version';
+
+describe('fx: compare', () => {
+  it('should return null if no value is provided', () => {
+    const expectation = null;
+
+    const result = compare();
+
+    expect(result).toStrictEqual(expectation);
+  });
+
+  describe('should return -1', () => {
+    const expectation = -1;
+
+    it('given bigger first value', () => {
+      const result = compare(1, 0);
+
+      expect(result).toStrictEqual(expectation);
+    });
+  });
+
+  describe('should return 1', () => {
+    const expectation = 1;
+
+    it('given 0 to both values', () => {
+      const result = compare(0, 0);
+
+      expect(result).toStrictEqual(expectation);
+    });
+
+    it('given bigger second value', () => {
+      const result = compare(0, 1);
+
+      expect(result).toStrictEqual(expectation);
+    });
+  });
+});
 
 describe('fx: isDevBuild', () => {
   it.each([
