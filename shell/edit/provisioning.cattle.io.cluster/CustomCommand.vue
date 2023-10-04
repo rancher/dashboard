07ccd4223b74ccf5d5f0cb6d36a91cca247f9f78
Shavindra <Shavindra@users.noreply.github.com>
2023-10-04 10:57:05 +0200

    Fix custom cluster registration private IPv6 command. (#9830)

--- shell/edit/provisioning.cattle.io.cluster/CustomCommand.vue
+++ shell/edit/provisioning.cattle.io.cluster/CustomCommand.vue
@@ -55,7 +55,7 @@ export default {
       this.controlPlane && out.push('--controlplane');
       this.worker && out.push('--worker');
       this.address && out.push(`--address ${ sanitizeIP(this.address) }`);
-      this.internalAddress && out.push(`--internal-address ${ sanitizeValue(this.internalAddress) }`);
+      this.internalAddress && out.push(`--internal-address ${ sanitizeIP(this.internalAddress) }`);
       this.nodeName && out.push(`--node-name ${ sanitizeValue(this.nodeName) }`);
 
       for ( const key in this.labels ) {
