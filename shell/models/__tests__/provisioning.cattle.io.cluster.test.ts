ee84d7c8748f451f6e8f10f5346f3abc574e3394
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2023-10-06 10:18:52 -0700

    Fixed RKE cluster being shown as Imported

---
+++ shell/models/__tests__/provisioning.cattle.io.cluster.test.ts
@@ -0,0 +1,85 @@
+import ProvCluster from '@shell/models/provisioning.cattle.io.cluster';
+
+describe('class ProvCluster', () => {
+  const importedClusterInfo = {
+    clusterName: 'test', provisioner: 'imported', mgmt: { spec: { gkeConfig: {} } }
+  };
+  const importedGkeClusterInfo = {
+    clusterName: 'test', provisioner: 'rke2', mgmt: { spec: { gkeConfig: { imported: true } } }
+  };
+  const importedAksClusterInfo = {
+    clusterName: 'test', provisioner: 'rke2', mgmt: { spec: { aksConfig: { imported: true } } }
+  };
+  const importedEksClusterInfo = {
+    clusterName: 'test', provisioner: 'rke2', mgmt: { spec: { eksConfig: { imported: true } } }
+  };
+  const notImportedGkeClusterInfo = {
+    clusterName: 'test', provisioner: 'rke2', mgmt: { spec: { gkeConfig: { imported: false } } }
+  };
+  const importedClusterInfoWithProviderForEmberParam = {
+    clusterName: 'test', provisioner: 'rke2', mgmt: { providerForEmberParam: 'import' }
+  };
+  const localClusterInfo = {
+    clusterName: 'test', provisioner: 'imported', mgmt: { isLocal: true, spec: { gkeConfig: {} } }
+  };
+
+  describe('isImported', () => {
+    const testCases = [
+      [importedClusterInfo, true],
+      [importedGkeClusterInfo, true],
+      [importedAksClusterInfo, true],
+      [importedEksClusterInfo, true],
+      [notImportedGkeClusterInfo, false],
+      [importedClusterInfoWithProviderForEmberParam, true],
+      [localClusterInfo, false],
+      [{}, false],
+    ];
+    const resetMocks = () => {
+      // Clear all mock function calls:
+      jest.clearAllMocks();
+    };
+
+    it.each(testCases)('should return the isImported value properly based on the props data', (clusterData: Object, expected: Boolean) => {
+      const cluster = new ProvCluster({});
+
+      jest.spyOn(cluster, 'mgmt', 'get').mockReturnValue(
+        clusterData.mgmt
+      );
+      jest.spyOn(cluster, 'provisioner', 'get').mockReturnValue(
+        clusterData.provisioner
+      );
+
+      expect(cluster.isImported).toBe(expected);
+      resetMocks();
+    }
+    );
+  });
+
+  describe('mgmt', () => {
+    const testCases = [
+      [importedClusterInfo, importedClusterInfo.mgmt],
+      [importedGkeClusterInfo, importedGkeClusterInfo.mgmt],
+      [importedAksClusterInfo, importedAksClusterInfo.mgmt],
+      [importedEksClusterInfo, importedEksClusterInfo.mgmt],
+      [notImportedGkeClusterInfo, notImportedGkeClusterInfo.mgmt],
+      [importedClusterInfoWithProviderForEmberParam, importedClusterInfoWithProviderForEmberParam.mgmt],
+      [localClusterInfo, localClusterInfo.mgmt],
+      [{}, null],
+    ];
+
+    const resetMocks = () => {
+      // Clear all mock function calls:
+      jest.clearAllMocks();
+    };
+
+    it.each(testCases)('should return the isImported value properly based on the props data', (clusterData: Object, expected: Object) => {
+      const clusterMock = jest.fn(() => clusterData.mgmt);
+      const ctx = { rootGetters: { 'management/byId': clusterMock } };
+      const cluster = new ProvCluster({ status: { clusterName: clusterData.clusterName } }, ctx);
+
+      expect(cluster.mgmt).toBe(expected);
+      resetMocks();
+    }
+    );
+  });
+});
