77e353634f2eadad56cf96a1c543812a75fdcf17
cnotv <giuseppe.leo@suse.com>
2023-10-09 22:57:45 +0200

    Replace test with scoped slot property

--- pkg/rancher-components/src/components/Form/Radio/RadioGroup.test.ts
+++ pkg/rancher-components/src/components/Form/Radio/RadioGroup.test.ts
@@ -3,31 +3,24 @@ import { RadioGroup } from './index';
 
 describe('component: RadioGroup', () => {
   describe('when disabled', () => {
-    /**
-     * NOTE: Current setup does not match guidelines for the test of scoped slots
-     * https://test-utils.vuejs.org/guide/advanced/slots.html#Scoped-Slots
-     * This seems not covered till the latest version of the library
-     * https://github.com/vuejs/vue-test-utils
-     */
     it.each([true, false])('should expose disabled slot prop for indexed slots for %p', (disabled) => {
-      const ExampleWrapper = {
-        template: `
-          <RadioGroup
-            :options="[{ label: 'whatever', value: 'whatever' }]"
-            :disabled="${ disabled }"
-            name="whatever"
-          >
-            <template #0="{isDisabled}">
-            <input id="test" :disabled="isDisabled"/>
-            </template>
-          </RadioGroup>
-        `,
-        components: { RadioGroup },
-        data() {
-          return { msg: 'world' };
+      const wrapper = mount(RadioGroup, {
+        propsData: {
+          name:    'whatever',
+          options: [{ label: 'whatever', value: 'whatever' }],
+          disabled
+        },
+        scopedSlots: {
+          0(props: {isDisabled: boolean}) {
+            return this.$createElement('input', {
+              attrs: {
+                id:       'test',
+                disabled: props.isDisabled
+              }
+            });
+          }
         }
-      };
-      const wrapper = mount(ExampleWrapper, {});
+      });
 
       const slot = wrapper.find('#test').element as HTMLInputElement;
 
