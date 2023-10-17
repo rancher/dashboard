1ca6b5b9b5f8b86ca29738bc32e24796fb5f7478
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-10-17 11:43:14 -0700

    update test and po

--- cypress/e2e/po/pages/explorer/cluster-tools.po.ts
+++ cypress/e2e/po/pages/explorer/cluster-tools.po.ts
@@ -17,23 +17,23 @@ export default class ClusterToolsPagePo extends PagePo {
     return cy.get('.grid > .item');
   }
 
-  featureChartCard(index: number): Cypress.Chainable {
+  getCardByIndex(index: number): Cypress.Chainable {
     return this.featureChartCards().eq(index);
   }
 
   goToInstall(index: number) {
-    return this.featureChartCard(index).find('.btn').contains('Install').click();
+    return this.getCardByIndex(index).find('.btn').contains('Install').click();
   }
 
   deleteChart(index: number) {
-    return this.featureChartCard(index).find('.action .btn').eq(0).click();
+    return this.getCardByIndex(index).find('.action .btn').eq(0).click();
   }
 
   editChart(index: number) {
-    return this.featureChartCard(index).find('.action .btn').eq(1).click();
+    return this.getCardByIndex(index).find('.action .btn').eq(1).click();
   }
 
   getChartVersion(index: number) {
-    return this.featureChartCard(index).find('.version');
+    return this.getCardByIndex(index).find('.version');
   }
 }
