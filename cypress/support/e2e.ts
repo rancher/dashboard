37dc981941bf9a055d99252bcf7cd648a22d25e0
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2023-10-18 16:18:38 -0700

    revert change made to exception handling

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -7,7 +7,9 @@ registerCypressGrep();
 
 // TODO handle redirection errors better?
 // we see a lot of 'error navigation cancelled' uncaught exceptions that don't actually break anything; ignore them here
-Cypress.on('uncaught:exception', (e, runnable) => {
+Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from failing the test
-  return false;
+  if (err.message.includes('navigation guard')) {
+    return false;
+  }
 });
