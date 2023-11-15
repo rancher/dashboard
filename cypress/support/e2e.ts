c21de88b464d397b7bf498d5e151f33fab49e814
yonasberhe23 <yonas.berhe@suse.com>
2023-11-15 11:41:56 -0800

    actions on cluster tests (#9946)
    * actions on cluster tests
    
    ---------
    
    Co-authored-by: Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -2,8 +2,10 @@ import '@cypress/code-coverage/support';
 import './commands/commands';
 import './commands/rancher-api-commands.ts';
 import registerCypressGrep from '@cypress/grep/src/support';
+import { addCustomCommand } from 'cypress-delete-downloads-folder';
 
 registerCypressGrep();
+addCustomCommand();
 
 // TODO handle redirection errors better?
 // we see a lot of 'error navigation cancelled' uncaught exceptions that don't actually break anything; ignore them here
