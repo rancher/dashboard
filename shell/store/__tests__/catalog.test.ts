af79b1b52ac06c7fa6a5edcc4223fecb10634d0d
cnotv <giuseppe.leo@suse.com>
2023-12-18 20:02:44 +0100

    Create tests for missing chart filtering cases

--- shell/store/__tests__/catalog.test.ts
+++ shell/store/__tests__/catalog.test.ts
@@ -61,6 +61,31 @@ describe('getters', () => {
         expect(result).toStrictEqual(chart1);
       });
 
+      it.each([
+        {
+          chartName: 'rancher-alerting-drivers',
+          repoType:  'cluster',
+          repoName:  'rancher-charts',
+        },
+        { chartName: 'rancher-alerting-drivers' },
+        { repoType: 'cluster' },
+        { repoName: 'rancher-charts' },
+      ])('given any matching parameter %p', (options) => {
+        const chart1 = {
+          chartName:  'rancher-alerting-drivers',
+          repoType:   'cluster',
+          repoName:   'rancher-charts',
+          deprecated: false
+        };
+        const state = {};
+        const stateGetters = { charts: [chart1] };
+
+        // NOTE: Getters arguments are actually optional
+        const result = (getters.chart as any)(state, stateGetters)(options);
+
+        expect(result).toStrictEqual(chart1);
+      });
+
       it('given key', () => {
         const key = 'repoType/repoName/chartName';
         const chart1 = {
@@ -104,6 +129,38 @@ describe('getters', () => {
 
         // NOTE: Getters arguments are actually optional
         const result = (getters.chart as any)(state, stateGetters)({ chart });
+
+        expect(result).toStrictEqual(chart1);
+      });
+
+      it('given only all matching parameters if provided', () => {
+        const options = {
+          chartName: 'epinio',
+          repoType:  'cluster',
+          repoName:  'rancher-charts',
+        };
+        const common = {
+          chartName:  'epinio',
+          repoType:   'cluster',
+          deprecated: false
+        };
+        const chart1 = {
+          repoName: 'rancher-charts',
+          ...common
+        };
+        const chart2 = {
+          repoName: 'epinio-repo',
+          ...common,
+        };
+        const chart3 = {
+          repoName: 'epinio-extension',
+          ...common,
+        };
+        const state = {};
+        const stateGetters = { charts: [chart2, chart3, chart1] };
+
+        // NOTE: Getters arguments are actually optional
+        const result = (getters.chart as any)(state, stateGetters)(options);
 
         expect(result).toStrictEqual(chart1);
       });
