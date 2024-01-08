2cd4b00d41109c30be134dc83f1d680ce5ecc3f3
Neil MacDougall <nwmac@users.noreply.github.com>
2024-01-08 17:05:25 +0000

    Add grafana resource configuration (#10086)
    * Fix for Monitoring Helm chart sometimes adds empty selector
    
    * Correct close icon styling
    
    * Tweak close icon width
    
    * Fix lint issues
    
    * Monitoring chart: Add support for Grafana resource configuration
    
    * Remove duplicate test from merge issue
    
    * Don't install the charts, use static response
    
    * Fix e2e test

--- cypress/e2e/tests/pages/charts/prometheus.spec.ts
+++ cypress/e2e/tests/pages/charts/prometheus.spec.ts
@@ -4,6 +4,7 @@ import TabbedPo from '@/cypress/e2e/po/components/tabbed.po';
 import { ChartsPage } from '@/cypress/e2e/po/pages/charts.po';
 import LabeledSelectPo from '@/cypress/e2e/po/components/labeled-select.po';
 import CheckboxPo from '@/cypress/e2e/po/components/checkbox-input.po';
+import LabeledInputPo from '@/cypress/e2e/po/components/labeled-input.po';
 
 import { prometheusSpec } from '@/cypress/e2e/blueprints/charts/prometheus-chart';
 
@@ -22,7 +23,7 @@ describe('Charts', { tags: ['@charts', '@adminUser'] }, () => {
       chartsPage.goTo();
     });
 
-    describe('Promethus local provisioner config', () => {
+    describe('Prometheus local provisioner config', () => {
       const provisionerVersion = 'v0.0.24';
 
       // Install the chart and navigate to the edit options page
@@ -111,6 +112,77 @@ describe('Charts', { tags: ['@charts', '@adminUser'] }, () => {
           const monitoringChart = req.request?.body.charts.find((chart: any) => chart.chartName === 'rancher-monitoring');
 
           expect(monitoringChart.values.prometheus).to.deep.equal(prometheusSpec.values.prometheus);
+        });
+      });
+    });
+
+    describe('Grafana resource configuration', () => {
+      beforeEach(() => {
+        cy.login();
+        chartsPage.goTo();
+        cy.intercept('POST', 'v1/catalog.cattle.io.clusterrepos/rancher-charts?action=install', {
+          statusCode: 201,
+          body:       {
+            type:               'chartActionOutput',
+            links:              {},
+            operationName:      'helm-operation-test',
+            operationNamespace: 'fleet-local'
+          }
+        }).as('prometheusChartCreation');
+
+        cy.intercept('GET', '/v1/catalog.cattle.io.operations/fleet-local/helm-operation-test?*', {
+          statusCode: 200,
+          body:       {
+            id:   'fleet-local/helm-operation-test',
+            kind: 'Operation',
+          }
+        });
+      });
+
+      it('Should allow for Grafana resource requests/limits configuration', () => {
+        const tabbedOptions = new TabbedPo();
+
+        // Set Grafana resource request/limits configuration
+        chartsPage.goToInstall().nextPage().editOptions(tabbedOptions, '[data-testid="btn-grafana"');
+
+        const requestsCpu = new LabeledInputPo(`[data-testid="input-grafana-requests-cpu"]`, tabbedOptions.self());
+
+        requestsCpu.checkExists();
+        requestsCpu.checkVisible();
+        requestsCpu.set('123m');
+
+        const requestsMemory = new LabeledInputPo(`[data-testid="input-grafana-requests-memory"]`, tabbedOptions.self());
+
+        requestsMemory.checkExists();
+        requestsMemory.checkVisible();
+        requestsMemory.set('567Mi');
+
+        const limitsCpu = new LabeledInputPo(`[data-testid="input-grafana-limits-cpu"]`, tabbedOptions.self());
+
+        limitsCpu.checkExists();
+        limitsCpu.checkVisible();
+        limitsCpu.set('87m');
+
+        const limitsMemory = new LabeledInputPo(`[data-testid="input-grafana-limits-memory"]`, tabbedOptions.self());
+
+        limitsMemory.checkExists();
+        limitsMemory.checkVisible();
+        limitsMemory.set('123Mi');
+
+        // Click on YAML. In YAML mode, the prometheus selector is present but empty
+        // It should not be sent to the API
+        chartsPage.editYaml();
+
+        chartsPage.installChart();
+
+        cy.wait('@prometheusChartCreation', { requestTimeout: 10000 }).then((req) => {
+          const monitoringChart = req.request?.body.charts.find((chart: any) => chart.chartName === 'rancher-monitoring');
+          const resource = monitoringChart.values.grafana.resources;
+
+          expect(resource.requests.cpu).to.equal('123m');
+          expect(resource.requests.memory).to.equal('567Mi');
+          expect(resource.limits.cpu).to.equal('87m');
+          expect(resource.limits.memory).to.equal('123Mi');
         });
       });
     });
