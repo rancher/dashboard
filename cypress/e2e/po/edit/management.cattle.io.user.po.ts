74467e93997231c4fbcd8e461338ddfa9f0c4bdd
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2024-01-15 08:49:18 +0000

    add retry logic for failing user creation network request (#10261)
    * add retry logic for failing user creation network request
    
    * add failing network request to retry logic + remove commented code
    
    * fix lint issue
    
    * address scenario where global role binding fails and we need to delete the user created to retry
    
    ---------
    
    Co-authored-by: Alexandre Alves <aalves@Alexandres-MacBook-Pro.local>

--- cypress/e2e/po/edit/management.cattle.io.user.po.ts
+++ cypress/e2e/po/edit/management.cattle.io.user.po.ts
@@ -47,6 +47,39 @@ export default class MgmtUserEditPo extends PagePo {
     return new AsyncButtonPo('[data-testid="form-save"]', this.self());
   }
 
+  saveCreateWithErrorRetry(attempt = 1): Cypress.Chainable | null {
+    if (attempt > 3) {
+      return null;
+    }
+
+    cy.intercept('POST', 'v3/users').as('userCreation');
+    cy.intercept('POST', 'v3/globalrolebindings').as('globalRoleBindingsCreation');
+
+    this.saveCreateForm().click();
+
+    // Based on issue https://github.com/rancher/dashboard/issues/10260
+    // main xhr request for user creation
+    cy.wait('@userCreation').then(({ response }) => {
+      if (response?.statusCode !== 201) {
+        cy.wait(1500); // eslint-disable-line cypress/no-unnecessary-waiting
+
+        return this.saveCreateWithErrorRetry(++attempt);
+      }
+
+      const userId = response.body?.id;
+
+      // also covers globalrolebindings fail upon user creation
+      cy.wait('@globalRoleBindingsCreation').then(({ response }) => {
+        if (response?.statusCode !== 201) {
+          cy.deleteRancherResource('v3', 'users', userId); // we need to delete the user previously created
+          cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+
+          return this.saveCreateWithErrorRetry(++attempt);
+        }
+      });
+    });
+  }
+
   saveAndWaitForRequests(method: string, url: any, multipleCalls?: boolean): CypressChainable {
     cy.intercept(method, url).as('request');
     this.saveCreateForm().click();
