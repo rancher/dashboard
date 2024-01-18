1c09fbd83dbc18de148487ef74c28175d5bd3455
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2024-01-18 11:46:42 -0800

    add warning banner when amazon out of tree cloud provider is selected

--- shell/edit/provisioning.cattle.io.cluster/rke2.vue
+++ shell/edit/provisioning.cattle.io.cluster/rke2.vue
@@ -594,6 +594,13 @@ export default {
     },
 
     /**
+     * Display warning about additional configuration needed for cloud provider Amazon if kube >= 1.27
+     */
+    showCloudProviderAmazonAdditionalConfigWarning() {
+      return !!semver.gte(this.value.spec.kubernetesVersion, 'v1.27.0') && this.agentConfig['cloud-provider-name'] === 'aws';
+    },
+
+    /**
      * Kube Version
      */
     selectedVersion() {
@@ -2473,6 +2480,12 @@ export default {
             <span
               v-clean-html="t('cluster.harvester.warning.cloudProvider.incompatible', null, true)"
             />
+          </Banner>
+          <Banner
+            v-if="showCloudProviderAmazonAdditionalConfigWarning"
+            color="warning"
+          >
+            <span v-clean-html="t('cluster.banner.cloudProviderAddConfig', {}, true)" />
           </Banner>
           <div class="row mb-10">
             <div class="col span-6">
