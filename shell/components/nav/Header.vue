19ced97cf947c6b41e9d936a943f58de97ba7dfb
Neil MacDougall <nmacdougall@suse.com>
2024-02-13 14:27:35 +0000

    Remove unused getter

--- shell/components/nav/Header.vue
+++ shell/components/nav/Header.vue
@@ -63,7 +63,6 @@ export default {
   computed: {
     ...mapGetters(['clusterReady', 'isExplorer', 'isRancher', 'currentCluster',
       'currentProduct', 'rootProduct', 'backToRancherLink', 'backToRancherGlobalLink', 'pageActions', 'isSingleProduct', 'isRancherInHarvester', 'showTopLevelMenu']),
-    ...mapGetters('type-map', ['activeProducts']),
 
     appName() {
       return getProduct();
