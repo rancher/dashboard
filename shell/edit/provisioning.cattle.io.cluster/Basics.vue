0dc5b937b0ce06dee24f8f738dcd28eb9a573067
Phillip Rak <rak.phillip@gmail.com>
2024-02-13 11:10:46 -0700

    Revert "Aws In-tree support (#9643)"
    This reverts changes made by  Aws In-tree support (#9643). While testing, it appears that the intent of this PR (to handle upgrade cases and display a banner) can never be triggered.
    
    fixes #10426

--- shell/edit/provisioning.cattle.io.cluster/Basics.vue
+++ shell/edit/provisioning.cattle.io.cluster/Basics.vue
@@ -132,10 +132,6 @@ export default {
       type:     Boolean,
       required: true
     },
-    unsupportedCloudProvider: {
-      type:     Boolean,
-      required: true
-    },
     cloudProviderOptions: {
       type:     Array,
       required: true
@@ -374,7 +370,7 @@ export default {
     },
 
     canNotEditCloudProvider() {
-      const canNotEdit = this.clusterIsAlreadyCreated && !this.unsupportedCloudProvider;
+      const canNotEdit = this.clusterIsAlreadyCreated;
 
       return canNotEdit;
     },
@@ -491,12 +487,6 @@ export default {
       <div class="spacer" />
 
       <div class="col span-12">
-        <Banner
-          v-if="unsupportedCloudProvider"
-          class="error mt-5"
-        >
-          {{ t('cluster.rke2.cloudProvider.unsupported') }}
-        </Banner>
         <h3>
           {{ t('cluster.rke2.cloudProvider.header') }}
         </h3>
