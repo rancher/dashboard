ed31439ac1f57eb37857de8d06527e7072129250
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2024-02-15 09:09:46 +0000

    add option of 'none' for Container Network select in Basics tab of RKE2 provisioning (#10385)
    * add option of 'none' for Container Network select in Basics tab of RKE2 provisioning + add e2e test to check that option is available and can be selected
    
    * add correct logic to include none as a CNI option
    
    * address pr comments
    
    * fix small bug with missing optional chaining
    
    * add banner to Basics component when CNI option selected is 'none' to warn user about the need of additional configuration
    
    * update e2e to test banner
    
    * fix issue
    
    ---------
    
    Co-authored-by: Alexandre Alves <aalves@Alexandres-MacBook-Pro.local>

--- shell/edit/provisioning.cattle.io.cluster/tabs/Basics.vue
+++ shell/edit/provisioning.cattle.io.cluster/tabs/Basics.vue
@@ -417,6 +417,13 @@ export default {
     >
       <span v-clean-html="t('cluster.banner.cloudProviderAddConfig', {}, true)" />
     </Banner>
+    <Banner
+      v-if="serverConfig.cni === 'none'"
+      color="warning"
+      data-testid="clusterBasics__noneOptionSelectedForCni"
+    >
+      <span v-clean-html="t('cluster.rke2.cni.cniNoneBanner', {}, true)" />
+    </Banner>
     <div class="row mb-10">
       <div class="col span-6">
         <LabeledSelect
