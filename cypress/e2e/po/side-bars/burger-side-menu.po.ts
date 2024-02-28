af78f3729b34c33436f9bc3a173c707661e95bb5
Alexandre Alves <aalves@Alexandres-MacBook-Pro.local>
2024-02-28 11:21:14 +0000

    update home link e2e getter with correct structure

--- cypress/e2e/po/side-bars/burger-side-menu.po.ts
+++ cypress/e2e/po/side-bars/burger-side-menu.po.ts
@@ -156,7 +156,7 @@ export default class BurgerMenuPo extends ComponentPo {
    * @returns {Cypress.Chainable}
    */
   home(): Cypress.Chainable {
-    return this.self().find('.body > div > a').first();
+    return this.self().find('.body > div > div > a').first();
   }
 
   /**
