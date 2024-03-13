6bef3726d1a7515268a6844c3f30b420926517fb
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2024-03-13 14:21:09 -0700

    remove legacy items except for project config-maps and secrets

--- shell/config/product/legacy.js
+++ shell/config/product/legacy.js
@@ -17,36 +17,6 @@ export function init(store) {
   });
 
   virtualType({
-    labelKey:   'legacy.alerts',
-    name:       'v1-alerts',
-    group:      'Root',
-    namespaced: true,
-    weight:     111,
-    route:      { name: 'c-cluster-legacy-pages-page', params: { page: 'alerts' } },
-    exact:      true
-  });
-
-  virtualType({
-    labelKey:   'legacy.catalogs',
-    name:       'v1-catalogs',
-    group:      'Root',
-    namespaced: true,
-    weight:     111,
-    route:      { name: 'c-cluster-legacy-pages-page', params: { page: 'catalogs' } },
-    exact:      true
-  });
-
-  virtualType({
-    labelKey:   'legacy.notifiers',
-    name:       'v1-notifiers',
-    group:      'Root',
-    namespaced: true,
-    weight:     111,
-    route:      { name: 'c-cluster-legacy-pages-page', params: { page: 'notifiers' } },
-    exact:      true
-  });
-
-  virtualType({
     ifHave:     IF_HAVE.PROJECT,
     labelKey:   'legacy.project.label',
     namespaced: true,
@@ -68,58 +38,9 @@ export function init(store) {
     overview:   false,
   });
 
-  basicType([
-    'v1-alerts',
-    'v1-catalogs',
-    'v1-notifiers',
-    'v1-project-overview'
-  ]);
+  basicType(['v1-project-overview']);
 
   // Project Pages
-
-  virtualType({
-    ifHave:     IF_HAVE.PROJECT,
-    labelKey:   'legacy.alerts',
-    namespaced: true,
-    name:       'project-alerts',
-    weight:     105,
-    route:      { name: 'c-cluster-legacy-project-page', params: { page: 'alerts' } },
-    exact:      true,
-    overview:   false,
-  });
-
-  virtualType({
-    ifHave:     IF_HAVE.PROJECT,
-    labelKey:   'legacy.apps',
-    namespaced: true,
-    name:       'project-apps',
-    weight:     110,
-    route:      { name: 'c-cluster-legacy-project-page', params: { page: 'apps' } },
-    exact:      true,
-    overview:   false,
-  });
-
-  virtualType({
-    ifHave:     IF_HAVE.PROJECT,
-    labelKey:   'legacy.catalogs',
-    namespaced: true,
-    name:       'project-catalogs',
-    weight:     105,
-    route:      { name: 'c-cluster-legacy-project-page', params: { page: 'catalogs' } },
-    exact:      true,
-    overview:   false,
-  });
-
-  virtualType({
-    ifHave:     IF_HAVE.PROJECT,
-    labelKey:   'legacy.monitoring',
-    namespaced: true,
-    name:       'project-monitoring',
-    weight:     105,
-    route:      { name: 'c-cluster-legacy-project-page', params: { page: 'monitoring' } },
-    exact:      true,
-    overview:   false,
-  });
 
   virtualType({
     ifHave:     IF_HAVE.PROJECT,
@@ -144,11 +65,7 @@ export function init(store) {
   });
 
   basicType([
-    'project-apps',
-    'project-alerts',
-    'project-catalogs',
     'project-config-maps',
-    'project-monitoring',
     'project-secrets',
   ], 'Project');
 }
