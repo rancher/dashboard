075b8ba9bc354a0c4d9ae9303c97d9ff8638e7dd
Richard <richard@localhost.localdomain>
2024-03-21 16:58:05 +0000

    Add unit tests

--- shell/plugins/dashboard-store/__tests__/actions.spec.ts
+++ shell/plugins/dashboard-store/__tests__/actions.spec.ts
@@ -1,127 +1,127 @@
 import _actions from '@shell/plugins/dashboard-store/actions';
 
-const { findAll } = _actions;
+const { findAll, findMatching, find } = _actions;
 
 describe('dashboard-store: actions', () => {
-  const setupContext = () => {
-    const commit = jest.fn();
-    const dispatch = jest.fn((...args) => {
-      switch (args[0]) {
-      case 'request':
-        return { data: ['requestData'] };
-      }
-    });
-    const state = { config: { namespace: 'unitTest' } };
-    const getters = {
-      normalizeType:    jest.fn(() => 'getters.normalizeType'),
-      typeRegistered:   jest.fn(() => false),
-      haveAll:          jest.fn(() => false),
-      haveAllNamespace: jest.fn(() => false),
-      all:              jest.fn(() => 'getters.all'),
-      urlFor:           jest.fn(() => 'getters.urlFor'), // we're not testing the urlFor getter so we don't need to do anything with opt here
-    };
-    const rootGetters = {
-      'type-map/optionsFor': jest.fn(),
-      'auth/fromHeader':     'foo'
+  describe('findAll', () => {
+    // Note - there are TS errors alll over this describe and should not have merged with them in.
+    const setupContext = () => {
+      const commit = jest.fn();
+      const dispatch = jest.fn((...args) => {
+        switch (args[0]) {
+        case 'request':
+          return { data: ['requestData'] };
+        }
+      });
+      const state = { config: { namespace: 'unitTest' } };
+      const getters = {
+        normalizeType:    jest.fn(() => 'getters.normalizeType'),
+        typeRegistered:   jest.fn(() => false),
+        haveAll:          jest.fn(() => false),
+        haveAllNamespace: jest.fn(() => false),
+        all:              jest.fn(() => 'getters.all'),
+        urlFor:           jest.fn(() => 'getters.urlFor'), // we're not testing the urlFor getter so we don't need to do anything with opt here
+      };
+      const rootGetters = {
+        'type-map/optionsFor': jest.fn(),
+        'auth/fromHeader':     'foo'
+      };
+
+      // we're not testing function output based off of state or getter inputs here since they are dependencies and should be tested independently
+      return {
+        state,
+        getters,
+        rootGetters,
+        commit,
+        dispatch
+      };
     };
 
-    // we're not testing function output based off of state or getter inputs here since they are dependencies and should be tested independently
-    return {
-      state,
-      getters,
-      rootGetters,
-      commit,
-      dispatch
-    };
-  };
-
-  const standardAssertions = {
-    returnsPromise:
-      {
+    const standardAssertions = {
+      returnsPromise: {
         assertionLabel: 'returns a promise',
         valueGetter:    ({ findAllPromise }) => typeof findAllPromise.then,
         valueExpected:  'function'
       },
-    callsAll: {
-      assertionLabel: 'calls the "all" getter with the normalizedType',
-      valueGetter:    ({ getters }) => getters.all.mock.calls[0][0],
-      valueExpected:  'getters.normalizeType'
-    },
-    returnsFromAll: {
-      assertionLabel: 'returns the value expected from the "all" getter',
-      valueGetter:    ({ findAllReturnValue }) => findAllReturnValue,
-      valueExpected:  'getters.all'
-    },
-    firstDispatchAction: {
-      assertionLabel: 'first dispatch should be the "request" action',
-      valueGetter:    ({ dispatch }) => dispatch.mock.calls[0][0],
-      valueExpected:  'request'
-    },
-    firstDispatchParams: {
-      assertionLabel: 'first dispatch parameters should be provided a normalized type and a url, streaming, and "metadata.managedFields" excluded under opt',
-      valueGetter:    ({ dispatch }) => dispatch.mock.calls[0][1],
-      valueExpected:  {
-        type: 'getters.normalizeType',
-        opt:  {
-          url:    'getters.urlFor',
-          stream: true
-        }
+      callsAll: {
+        assertionLabel: 'calls the "all" getter with the normalizedType',
+        valueGetter:    ({ getters }) => getters.all.mock.calls[0][0],
+        valueExpected:  'getters.normalizeType'
       },
-      assertionMethod: 'toMatchObject'
-    },
-    secondDispatchAction: {
-      assertionLabel: 'second dispatch should be the "watch" action',
-      valueGetter:    ({ dispatch }) => dispatch.mock.calls[1][0],
-      valueExpected:  'watch'
-    },
-    secondDispatchParams: {
-      assertionLabel:  'second dispatch parameters should have a normalized type and force set to false',
-      valueGetter:     ({ dispatch }) => dispatch.mock.calls[1][1],
-      valueExpected:   { type: 'getters.normalizeType', force: false },
-      assertionMethod: 'toMatchObject'
-    },
-    countDispatches: {
-      assertionLabel:  'should only make two dispatches',
-      valueGetter:     ({ dispatch }) => dispatch.mock.calls,
-      valueExpected:   2,
-      assertionMethod: 'toHaveLength'
-    },
-    firstCommitMutation: {
-      assertionLabel: 'first commit should be the "registerType" mutation',
-      valueGetter:    ({ commit }) => commit.mock.calls[0][0],
-      valueExpected:  'registerType'
-    },
-    firstCommitParams: {
-      assertionLabel: 'first commit parameter should be a normalized type',
-      valueGetter:    ({ commit }) => commit.mock.calls[0][1],
-      valueExpected:  'getters.normalizeType'
-    },
-    secondCommitMutation: {
-      assertionLabel: 'second commit should be the "loadAll" mutation',
-      valueGetter:    ({ commit }) => commit.mock.calls[1][0],
-      valueExpected:  'loadAll'
-    },
-    secondCommitParams: {
-      assertionLabel: 'second commit parameters should have a normalized type, ctx.state.config.namespace, data returned by request, and skipHaveAll set to false',
-      valueGetter:    ({ commit }) => commit.mock.calls[1][1],
-      valueExpected:  {
-        type:        'getters.normalizeType',
-        ctx:         { state: { config: { namespace: 'unitTest' } } },
-        data:        ['requestData'],
-        skipHaveAll: false,
+      returnsFromAll: {
+        assertionLabel: 'returns the value expected from the "all" getter',
+        valueGetter:    ({ findAllReturnValue }) => findAllReturnValue,
+        valueExpected:  'getters.all'
       },
-      assertionMethod: 'toMatchObject'
-    },
-    countCommits: {
-      assertionLabel:  'should only make two commits',
-      valueGetter:     ({ commit }) => commit.mock.calls,
-      valueExpected:   2,
-      assertionMethod: 'toHaveLength'
-    }
+      firstDispatchAction: {
+        assertionLabel: 'first dispatch should be the "request" action',
+        valueGetter:    ({ dispatch }) => dispatch.mock.calls[0][0],
+        valueExpected:  'request'
+      },
+      firstDispatchParams: {
+        assertionLabel: 'first dispatch parameters should be provided a normalized type and a url, streaming, and "metadata.managedFields" excluded under opt',
+        valueGetter:    ({ dispatch }) => dispatch.mock.calls[0][1],
+        valueExpected:  {
+          type: 'getters.normalizeType',
+          opt:  {
+            url:    'getters.urlFor',
+            stream: true
+          }
+        },
+        assertionMethod: 'toMatchObject'
+      },
+      secondDispatchAction: {
+        assertionLabel: 'second dispatch should be the "watch" action',
+        valueGetter:    ({ dispatch }) => dispatch.mock.calls[1][0],
+        valueExpected:  'watch'
+      },
+      secondDispatchParams: {
+        assertionLabel:  'second dispatch parameters should have a normalized type and force set to false',
+        valueGetter:     ({ dispatch }) => dispatch.mock.calls[1][1],
+        valueExpected:   { type: 'getters.normalizeType', force: false },
+        assertionMethod: 'toMatchObject'
+      },
+      countDispatches: {
+        assertionLabel:  'should only make two dispatches',
+        valueGetter:     ({ dispatch }) => dispatch.mock.calls,
+        valueExpected:   2,
+        assertionMethod: 'toHaveLength'
+      },
+      firstCommitMutation: {
+        assertionLabel: 'first commit should be the "registerType" mutation',
+        valueGetter:    ({ commit }) => commit.mock.calls[0][0],
+        valueExpected:  'registerType'
+      },
+      firstCommitParams: {
+        assertionLabel: 'first commit parameter should be a normalized type',
+        valueGetter:    ({ commit }) => commit.mock.calls[0][1],
+        valueExpected:  'getters.normalizeType'
+      },
+      secondCommitMutation: {
+        assertionLabel: 'second commit should be the "loadAll" mutation',
+        valueGetter:    ({ commit }) => commit.mock.calls[1][0],
+        valueExpected:  'loadAll'
+      },
+      secondCommitParams: {
+        assertionLabel: 'second commit parameters should have a normalized type, ctx.state.config.namespace, data returned by request, and skipHaveAll set to false',
+        valueGetter:    ({ commit }) => commit.mock.calls[1][1],
+        valueExpected:  {
+          type:        'getters.normalizeType',
+          ctx:         { state: { config: { namespace: 'unitTest' } } },
+          data:        ['requestData'],
+          skipHaveAll: false,
+        },
+        assertionMethod: 'toMatchObject'
+      },
+      countCommits: {
+        assertionLabel:  'should only make two commits',
+        valueGetter:     ({ commit }) => commit.mock.calls,
+        valueExpected:   2,
+        assertionMethod: 'toHaveLength'
+      }
 
-  };
+    };
 
-  describe('dashboard-store > actions > findAll', () => {
     describe('called without a cache for the type in the second param', () => {
       const {
         dispatch, commit, getters, rootGetters, state
@@ -160,6 +160,115 @@ describe('dashboard-store: actions', () => {
           }))[assertionMethod](valueExpected);
         }
       );
+    });
+  });
+
+  describe('findMatching', () => {
+    const setupContext = () => {
+      const commit = jest.fn();
+      const dispatch = jest.fn(() => 'dispatch');
+
+      const state = { config: { namespace: 'unitTest' } };
+      const getters = {
+        normalizeType:  jest.fn((type) => type),
+        typeRegistered: jest.fn(() => true),
+        haveSelector:   jest.fn(() => false),
+        matching:       jest.fn(() => 'getters.all'),
+        urlFor:         jest.fn(() => 'getters.urlFor'),
+        urlOptions:     jest.fn(() => 'getters.urlOptions')
+      };
+      const rootGetters = { 'type-map/optionsFor': jest.fn() };
+
+      // we're not testing function output based off of state or getter inputs here since they are dependencies and should be tested independently
+      return {
+        state,
+        getters,
+        rootGetters,
+        commit,
+        dispatch
+      };
+    };
+    const genericType = 'services';
+    const genericSelector = 'a=b';
+    const genericOpt = {};
+
+    const assertionChain = [{
+      assertionLabel: 'Basic Selector',
+      input:          {
+        type:      genericType,
+        selector:  genericSelector,
+        opt:       { ...genericOpt },
+        namespace: undefined
+      },
+      output: {
+        getters: {
+          urlFor: [
+            genericType,
+            null,
+            {
+              ...genericOpt,
+              depaginate:    undefined,
+              labelSelector: genericSelector,
+              url:           'getters.urlFor',
+            }
+          ]
+        },
+        actions: {
+          request: {
+            opt:  {},
+            type: genericType
+          }
+        }
+      }
+    }];
+
+    const {
+      dispatch,
+      commit,
+      getters,
+      rootGetters,
+      state
+    } = setupContext();
+
+    it.each(assertionChain)(
+      '$assertionLabel',
+      async({ input, output }) => {
+        await findMatching(
+          {
+            dispatch,
+            getters,
+            rootGetters,
+            state,
+            commit,
+          },
+          input
+        );
+        expect(getters.urlFor).toHaveBeenCalledWith(...output.getters.urlFor);
+      }
+    );
+  });
+
+  describe('find', () => {
+    it('rewatch for resources that are already in the store', () => {
+      const ctx = {
+        state:    { config: { namespace: 'test_store' } },
+        getters:  { byId: jest.fn().mockReturnValueOnce({ metadata: { resourceVersion: 1 } }) },
+        dispatch: jest.fn()
+      };
+
+      const type = 'test_type';
+      const id = 'test_id';
+      const opt = {};
+
+      find(ctx, {
+        type, id, opt
+      });
+
+      expect(ctx.getters.byId).toHaveBeenCalledWith(type, id);
+      expect(ctx.dispatch).toHaveBeenCalledWith('watch', {
+        id, namespace: undefined, revision: 1, type
+      });
+      expect(ctx.dispatch).toHaveBeenCalledTimes(1);
     });
   });
 });
