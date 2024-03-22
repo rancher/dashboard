87709e755ebd9bc170c730e0de1a384f87fb1d59
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2024-03-22 13:16:45 -0700

    add permission warning to all auth providers

---
+++ shell/edit/auth/AuthProviderWarningBanners.vue
@@ -0,0 +1,33 @@
+<script>
+import { Banner } from '@components/Banner';
+
+export default {
+  name: 'AuthProviderWarningBanners',
+
+  components: { Banner },
+
+  props: {
+    tArgs: {
+      type:     Object,
+      required: true,
+      default:  () => { },
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <Banner
+      :label="t('authConfig.stateBanner.disabled', tArgs)"
+      color="warning"
+      data-testid="auth-provider-disabled-warning-banner"
+    />
+    <Banner
+      color="warning"
+      data-testid="auth-provider-admin-permissions-warning-banner"
+    >
+      <span v-clean-html="t('authConfig.associatedWarning', tArgs, true)" />
+    </Banner>
+  </div>
+</template>
