1924094d4c0f7acff4d737cdf53242d8f7d87c79
Phillip Rak <rak.phillip@gmail.com>
2024-03-25 13:44:40 -0700

    Add Last Login to users table
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/config/table-headers.js
+++ shell/config/table-headers.js
@@ -398,6 +398,14 @@ export const USER_PROVIDER = {
   sort:        'providerDisplay',
 };
 
+export const USER_LAST_LOGIN = {
+  name:      'user-last-login',
+  label:     'Last Login',
+  value:     'userLastLogin',
+  formatter: 'LiveDate',
+  sort:      'user-last-login',
+};
+
 export const USER_ID = {
   name:          'user-id',
   labelKey:      'tableHeaders.userId',
