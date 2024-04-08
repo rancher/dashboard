e30e8ed7ac9fe548f1076607c21fa968f1218aba
Richard Cox <richard.cox@suse.com>
2024-04-08 11:22:11 +0100

    Fix missing name column in non-server-side paginated node list

--- cypress/e2e/tests/pages/explorer/node-list.spec.ts
+++ cypress/e2e/tests/pages/explorer/node-list.spec.ts
@@ -15,7 +15,7 @@ describe('Nodes list', { tags: ['@explorer', '@adminUser'], testIsolation: 'off'
 
   after(() => {
     // Ensure we delete the dummy node
-    cy.deleteRancherResource('v1', 'nodes', 'bigip1');
+    cy.deleteRancherResource('v1', 'nodes', dummyNode.metadata.name);
   });
 
   it('should show the nodes list page', () => {
@@ -48,8 +48,7 @@ describe('Nodes list', { tags: ['@explorer', '@adminUser'], testIsolation: 'off'
       // Check the node names
       nodeList.sortableTable().rowNames().should((names: any) => {
         expect(names).to.have.length(count);
-        expect(names).to.contain('bigip1');
-      });
+        expect(names).to.contain(dummyNode.metadata.name);
     });
 
     // Simple test to assert we haven't broken Node detail page
