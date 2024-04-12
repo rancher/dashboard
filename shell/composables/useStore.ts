f2bf53015a2b411c17eecc0c1086a51ee1535ae7
Phillip Rak <rak.phillip@gmail.com>
2024-04-12 12:52:32 -0700

    Document `useStore()`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/composables/useStore.ts
+++ shell/composables/useStore.ts
@@ -1,5 +1,11 @@
 import { getCurrentInstance } from 'vue';
 
+/**
+ * useStore allows for accessing Vuex stores from within a setup function. This is a temporary measure for working with
+ * Vuex in Vue2.
+ *
+ * TODO: #9541 Remove for Vue 3 migration
+ */
 export const useStore = ():unknown => {
   const vm = getCurrentInstance();
 
