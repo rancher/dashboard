26b90c74edc868437e6799b6d7648a6edea61f47
Neil MacDougall <nmacdougall@suse.com>
2024-04-19 15:06:57 +0100

    Fix lint issues

--- shell/store/type-map.js
+++ shell/store/type-map.js
@@ -991,7 +991,7 @@ export const getters = {
             let satisfiesIfHave = true;
 
             // Support an array of required types that the user must have access to
-            for(let i = 0; i < ifHaveTypeArray.length; i++) {
+            for (let i = 0; i < ifHaveTypeArray.length; i++) {
               const ifHaveType = ifHaveTypeArray[i];
               const targetedSchemas = typeof ifHaveType === 'string' ? schemas : rootGetters[`${ ifHaveType.store }/all`](SCHEMA);
               const type = typeof ifHaveType === 'string' ? ifHaveType : ifHaveType?.type;
@@ -1007,7 +1007,7 @@ export const getters = {
                 satisfiesIfHave = false;
                 break;
               }
-            };
+            }
 
             if (!satisfiesIfHave) {
               continue;
