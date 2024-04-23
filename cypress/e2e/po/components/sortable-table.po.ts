dfb6dac7f1639e08fc744eeaad264f53761bb319
Neil MacDougall <nmacdougall@suse.com>
2024-04-23 16:12:09 +0100

    Use common type for options

--- cypress/e2e/po/components/sortable-table.po.ts
+++ cypress/e2e/po/components/sortable-table.po.ts
@@ -1,4 +1,4 @@
-import ComponentPo from '@/cypress/e2e/po/components/component.po';
+import ComponentPo, { GetOptions } from '@/cypress/e2e/po/components/component.po';
 import ActionMenuPo from '@/cypress/e2e/po/components/action-menu.po';
 import CheckboxInputPo from '@/cypress/e2e/po/components/checkbox-input.po';
 import ListRowPo from '@/cypress/e2e/po/components/list-row.po';
@@ -84,7 +84,7 @@ export default class SortableTablePo extends ComponentPo {
     return this.self().find('tbody tr:not(.sub-row)', options);
   }
 
-  rowElementWithName(name: string, options?: any) {
+  rowElementWithName(name: string, options?: GetOptions) {
     return this.self().contains('tbody tr', new RegExp(` ${ name } `), options);
   }
 
