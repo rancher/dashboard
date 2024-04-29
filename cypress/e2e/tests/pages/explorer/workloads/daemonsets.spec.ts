903318b6bd6f5f9a60c948e87a9dde051accede3
Alexandre Alves <alexandre.alves@suse.com>
2024-04-29 14:07:19 +0100

    fix e2e issue with nodes req when entering daemonsets list view

--- cypress/e2e/tests/pages/explorer/workloads/daemonsets.spec.ts
+++ cypress/e2e/tests/pages/explorer/workloads/daemonsets.spec.ts
@@ -8,12 +8,20 @@ describe('Cluster Explorer', { tags: ['@explorer', '@adminUser'] }, () => {
     generateFakeClusterDataAndIntercepts(fakeProvClusterId, fakeMgmtClusterId);
 
     // to test payload of https://github.com/rancher/dashboard/issues/9874
-    cy.intercept('PUT', 'k8s/clusters/some-fake-mgmt-id/v1/apps.daemonsets/calico-system/calico-node', (req: any) => {
+    cy.intercept('PUT', `k8s/clusters/${ fakeMgmtClusterId }/v1/apps.daemonsets/calico-system/calico-node`, (req: any) => {
       req.reply({
         statusCode: 200,
-        body:       { value: {} }
+        body:       {}
       });
     }).as('daemonsetEdit');
+
+    // to prevent error when rendering the daemonsets list after save (otherwise test will fail)
+    cy.intercept('GET', `k8s/clusters/${ fakeMgmtClusterId }/v1/nodes`, (req: any) => {
+      req.reply({
+        statusCode: 200,
+        body:       { data: [] }
+      });
+    }).as('nodesReq');
 
     cy.login();
   });
