5f32e8d99600a09de6613a04129e0247ad516385
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2024-05-03 13:12:10 -0700

    fixed comments

--- shell/plugins/global-formatters.js
+++ shell/plugins/global-formatters.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
-import Vue from 'vue';const components = require.context('@shell/components/formatter', false, /[A-Z]\w+\.(vue)$/);
+import Vue from 'vue';
+
+const components = require.context('@shell/components/formatter', false, /[A-Z]\w+\.(vue)$/);
 
 const globalFormatters = {
   install: (Vue) => {
@@ -19,6 +21,11 @@ const globalFormatters = {
 
 export default globalFormatters;
 
-console.warn('The implicit addition of global formatters has been deprecated in Rancher Shell and will be removed in a future version. Make sure to invoke `Vue.use(globalFormatters)` to maintain compatibility.');
+// This is being done for backwards compatibility with our extensions that have written tests and didn't properly make use of Vue.use() when importing and mocking translations
+const isThisFileBeingExecutedInATest = process.env.NODE_ENV === 'test';
 
-Vue.use(globalFormatters);
+if (isThisFileBeingExecutedInATest) {
+  console.warn('The implicit addition of global formatters has been deprecated in Rancher Shell and will be removed in a future version. Make sure to invoke `Vue.use(globalFormatters)` to maintain compatibility.');
+
+  Vue.use(globalFormatters);
+}
