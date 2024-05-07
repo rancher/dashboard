b416b693a994e0b7aa85c8d01905d62d7772236d
Phillip Rak <rak.phillip@gmail.com>
2024-05-07 09:22:20 -0700

    Resolve issues with failing e2e tests
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- cypress/e2e/po/components/list-row.po.ts
+++ cypress/e2e/po/components/list-row.po.ts
@@ -2,6 +2,6 @@ import ComponentPo from '@/cypress/e2e/po/components/component.po';
 
 export default class ListRowPo extends ComponentPo {
   column(index: number) {
-    return this.self().find('td').eq(index);
+    return this.self().find('td').eq(index).scrollIntoView();
   }
 }
