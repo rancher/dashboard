e8347b0690577cee032f8c6ae6b9b5a0d439c71d
Neil MacDougall <nmacdougall@suse.com>
2024-05-20 13:19:38 +0100

    Fix incorrect color for text-warning

--- cypress/e2e/po/prompts/promptRemove.po.ts
+++ cypress/e2e/po/prompts/promptRemove.po.ts
@@ -17,4 +17,9 @@ export default class PromptRemove extends ComponentPo {
   remove() {
     return this.self().getId('prompt-remove-confirm-button').click();
   }
+
+  // Get the warning message
+  warning() {
+    return this.self().get('[warning] .text-warning');
+  }
 }
