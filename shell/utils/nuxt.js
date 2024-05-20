65b5a417c574c198a22a587b7049778dfcaf1ba0
Cody Jackson <cody.jackson@suse.com>
2024-05-20 13:55:07 -0700

    Removing a few more pieces of dead code that I noticed

--- shell/utils/nuxt.js
+++ shell/utils/nuxt.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { joinURL, normalizeURL, withQuery, withoutTrailingSlash } from 'ufo';
+import { normalizeURL, withQuery, withoutTrailingSlash } from 'ufo';
 
 export function createGetCounter(counterObject, defaultKey = '') {
   return function getCounter(id = defaultKey) {
@@ -330,8 +330,6 @@ export function addLifecycleHook(vm, hook, fn) {
     vm.$options[hook].push(fn);
   }
 }
-
-export const urlJoin = joinURL;
 
 export const stripTrailingSlash = withoutTrailingSlash;
 
