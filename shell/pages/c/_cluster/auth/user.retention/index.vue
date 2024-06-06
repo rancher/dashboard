04603c498f56540981e19ec819ff5edfbbac9d86
Phillip Rak <rak.phillip@gmail.com>
2024-06-06 14:13:48 -0700

    Replace cru-resource with footer
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/pages/c/_cluster/auth/user.retention/index.vue
+++ shell/pages/c/_cluster/auth/user.retention/index.vue
@@ -2,7 +2,7 @@
 import { defineComponent, ref, reactive, onMounted } from 'vue';
 
 import TabTitle from '@shell/components/TabTitle';
-import CruResource from '@shell/components/CruResource.vue';
+import Footer from '@shell/components/form/Footer';
 import { useStore } from '@shell/composables/useStore';
 import { MANAGEMENT } from '@shell/config/types';
 import { SETTING } from '@shell/config/settings';
@@ -17,7 +17,7 @@ export default defineComponent({
     Checkbox,
     LabeledInput,
     ToggleSwitch,
-    CruResource,
+    Footer,
   },
   setup() {
     const store = useStore();
@@ -113,78 +113,94 @@ export default defineComponent({
       </div>
     </header>
     <h2>User retention</h2>
-    <cru-resource
-      mode="EDIT"
-      :resource="{ }"
-      :can-yaml="false"
-      @finish="save"
+    <div
+      v-if="!loading"
+      class="form-user-retention"
     >
-      <div
-        v-if="!loading"
-        class="form-user-retention"
+      <div class="input-fieldset">
+        <checkbox
+          v-model="disableAfterPeriod"
+          label="Disable user accounts after an inactivity  period (days since last login)"
+        />
+        <labeled-input
+          v-model="userRetentionSettings[SETTING.DISABLE_INACTIVE_USER_AFTER]"
+          label="Inactivity period (days)"
+          :disabled="!disableAfterPeriod"
+        />
+      </div>
+      <div class="input-fieldset">
+        <checkbox
+          v-model="deleteAfterPeriod"
+          label="Delete user accounts after an inactivity  period (days since last login)"
+        />
+        <labeled-input
+          v-model="userRetentionSettings[SETTING.DELETE_INACTIVE_USER_AFTER]"
+          label="Inactivity period (days)"
+          :disabled="!deleteAfterPeriod"
+        />
+      </div>
+      <template
+        v-if="disableAfterPeriod || deleteAfterPeriod"
       >
         <div class="input-fieldset">
-          <checkbox
-            v-model="disableAfterPeriod"
-            label="Disable user accounts after an inactivity  period (days since last login)"
-          />
           <labeled-input
-            v-model="userRetentionSettings[SETTING.DISABLE_INACTIVE_USER_AFTER]"
-            label="Inactivity period (days)"
-            :disabled="!disableAfterPeriod"
+            v-model="userRetentionSettings[SETTING.USER_RETENTION_CRON]"
+            required
+            type="cron"
+            label="User retention process schedule"
+            sub-label="The user retention process runs as a cron job (required)"
           />
         </div>
-        <div class="input-fieldset">
-          <checkbox
-            v-model="deleteAfterPeriod"
-            label="Delete user accounts after an inactivity  period (days since last login)"
+        <div class="input-fieldset condensed">
+          <toggle-switch
+            v-model="userRetentionSettings[SETTING.USER_RETENTION_DRY_RUN]"
+            :onValue="'true'"
+            :offValue="'false'"
+            on-label="Run the user retention process in DRY mode (no changes will be applied)"
           />
+          <span class="input-detail">You can check the logs to see which accounts would be affected</span>
+        </div>
+        <div class="input-fieldset condensed">
           <labeled-input
-            v-model="userRetentionSettings[SETTING.DELETE_INACTIVE_USER_AFTER]"
-            label="Inactivity period (days)"
-            :disabled="!deleteAfterPeriod"
+            v-model="userRetentionSettings[SETTING.USER_LAST_LOGIN_DEFAULT]"
+            label="Default last login (ms)"
+            sub-label="Accounts without a registered last login timestamp will get this as a default"
+            placeholder="Unix timestamp"
           />
         </div>
-        <template
-          v-if="disableAfterPeriod || deleteAfterPeriod"
-        >
-          <div class="input-fieldset">
-            <labeled-input
-              v-model="userRetentionSettings[SETTING.USER_RETENTION_CRON]"
-              required
-              type="cron"
-              label="User retention process schedule"
-              sub-label="The user retention process runs as a cron job (required)"
-            />
-          </div>
-          <div class="input-fieldset condensed">
-            <toggle-switch
-              v-model="userRetentionSettings[SETTING.USER_RETENTION_DRY_RUN]"
-              :onValue="'true'"
-              :offValue="'false'"
-              on-label="Run the user retention process in DRY mode (no changes will be applied)"
-            />
-            <span class="input-detail">You can check the logs to see which accounts would be affected</span>
-          </div>
-          <div class="input-fieldset condensed">
-            <labeled-input
-              v-model="userRetentionSettings[SETTING.USER_LAST_LOGIN_DEFAULT]"
-              label="Default last login (ms)"
-              sub-label="Accounts without a registered last login timestamp will get this as a default"
-              placeholder="Unix timestamp"
-            />
-          </div>
-        </template>
-      </div>
-    </cru-resource>
+      </template>
+    </div>
+    <Footer
+      class="footer-user-retention"
+      mode="edit"
+      @save="save"
+    />
   </div>
 </template>
 
 <style lang="scss" scoped>
   .form-user-retention {
     display: flex;
+    flex: 1;
     flex-direction: column;
     gap: 1.5rem;
+  }
+
+  .footer-user-retention {
+    border-top: var(--header-border-size) solid var(--header-border);
+    right: 0;
+    position: sticky;
+    bottom: 0;
+    background-color: var(--header-bg);
+    margin-left: -20px;
+    margin-right: -20px;
+    margin-bottom: -20px;
+    margin-top: 20px;
+    padding: 10px 20px;
+
+    ::v-deep .spacer-small {
+      padding: 0;
+    }
   }
 
   .input-fieldset {
