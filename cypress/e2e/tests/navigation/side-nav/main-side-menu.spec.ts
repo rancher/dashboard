7deba3c8bce02a037de06735b6756b3a29a906f7
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2024-06-07 14:14:47 -0700

    update version in e2e test

--- cypress/e2e/tests/navigation/side-nav/main-side-menu.spec.ts
+++ cypress/e2e/tests/navigation/side-nav/main-side-menu.spec.ts
@@ -66,7 +66,7 @@ describe('Side Menu: main', () => {
         })
         .click();
 
-      cy.url().should('include', 'https://ranchermanager.docs.rancher.com/v2.8/how-to-guides/new-user-guides/launch-kubernetes-with-rancher/rke1-vs-rke2-differences#cluster-api');
+      cy.url().should('include', 'https://ranchermanager.docs.rancher.com/v2.9/how-to-guides/new-user-guides/launch-kubernetes-with-rancher/rke1-vs-rke2-differences#cluster-api');
     });
 
     it('Local cluster should show a name and description on the side menu and display a tooltip when hovering it show the full name and description', { tags: ['@navigation', '@adminUser'] }, () => {
