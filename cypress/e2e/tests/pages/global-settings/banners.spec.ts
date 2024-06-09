7d0bff24f29d79d3bec550a5956e458d58b272fb
Neil MacDougall <nmacdougall@suse.com>
2024-06-09 16:44:37 +0100

    Fix lint issues with trailing spaces

--- cypress/e2e/tests/pages/global-settings/banners.spec.ts
+++ cypress/e2e/tests/pages/global-settings/banners.spec.ts
@@ -33,7 +33,6 @@ const settings = {
     new:      '#ddd603',
     newRGB:   'rgb(221, 214, 3)'
   }
-
 };
 
 describe('Banners', { testIsolation: 'off' }, () => {
@@ -307,138 +306,138 @@ describe('Banners', { testIsolation: 'off' }, () => {
     cy.getRancherResource('v1', 'management.cattle.io.settings').then((data: any) => {
       const banners = data.body.data.find((setting) => setting.id === 'ui-banners');
       const value = JSON.parse(banners.value);
-  
+
       fn(value);
-  
+
       banners.value = JSON.stringify(value);
-  
+
       cy.setRancherResource('v1', 'management.cattle.io.settings', 'ui-banners', banners);
     });
   }
-  
+
   // Common tests for Header and Footer banners
   function bannerTests(bannerName: string) {
     it('Should not have banner', { tags: ['@globalSettings', '@adminUser', '@standardUser'] }, () => {
       const banner = new FixedBannerPo(`#banner-${ bannerName.toLowerCase() }`);
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
     });
-  
+
     it('Should use banner from ui-banners setting', { tags: ['@globalSettings', '@adminUser', '@standardUser'] }, () => {
       const banner = new FixedBannerPo(`#banner-${ bannerName.toLowerCase() }`);
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
-  
+
       // Update the ui-banners setting to enable the banner
       updateBannersSetting((value) => {
         value[`show${ bannerName }`] = 'true';
         value[`banner${ bannerName }`].text = 'TEST Banner (ui-banners)';
         value[`banner${ bannerName }`].background = '#00ff00';
       });
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkExists();
-  
+
       banner.text().should('eq', 'TEST Banner (ui-banners)');
       banner.backgroundColor().should('eq', 'rgb(0, 255, 0)');
-  
+
       // Turn off the banner
       updateBannersSetting((value) => {
         value[`show${ bannerName }`] = 'false';
       });
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
     });
-  
+
     it('Should use banner from individual setting', { tags: ['@globalSettings', '@adminUser', '@standardUser'] }, () => {
       const banner = new FixedBannerPo(`#banner-${ bannerName.toLowerCase() }`);
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
-  
+
       // Set the banner via the individual setting
       const bannerSetting = makeSetting(`ui-banner-${ bannerName.toLowerCase() }`, {
         text:       'Test Banner (individual setting)',
         background: '#ff0000'
       });
-  
+
       cy.createRancherResource('v1', 'management.cattle.io.settings', JSON.stringify(bannerSetting));
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkExists();
-  
+
       banner.text().should('eq', 'Test Banner (individual setting)');
       banner.backgroundColor().should('eq', 'rgb(255, 0, 0)');
-  
+
       cy.deleteRancherResource('k8s', 'clusters/local/apis/management.cattle.io/v3/settings', `ui-banner-${ bannerName.toLowerCase() }`);
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
     });
-  
+
     it('Should prefer setting from individual setting', { tags: ['@globalSettings', '@adminUser', '@standardUser'] }, () => {
       const banner = new FixedBannerPo(`#banner-${ bannerName.toLowerCase() }`);
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
-  
+
       // Update the ui-banners setting to enable the banner
       updateBannersSetting((value) => {
         value[`show${ bannerName }`] = 'true';
         value[`banner${ bannerName }`].text = 'TEST Banner (ui-banners)';
         value[`banner${ bannerName }`].background = '#00ff00';
       });
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkExists();
-  
+
       banner.text().should('eq', 'TEST Banner (ui-banners)');
       banner.backgroundColor().should('eq', 'rgb(0, 255, 0)');
-  
+
       // Set the banner via the individual setting
       const bannerSetting = makeSetting(`ui-banner-${ bannerName.toLowerCase() }`, {
         text:       'Test Banner (individual setting)',
         background: '#ff0000'
       });
-  
+
       cy.createRancherResource('v1', 'management.cattle.io.settings', JSON.stringify(bannerSetting));
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkExists();
-  
+
       banner.text().should('eq', 'HELLO TEST');
       banner.backgroundColor().should('eq', 'rgb(255, 0, 0)');
-  
+
       // Turn off the banner via the banners setting
       updateBannersSetting((value) => {
         value[`show${ bannerName }`] = 'false';
       });
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       // Banner should still exist from the individual setting
       banner.checkExists();
-  
+
       banner.text().should('eq', 'Test Banner (individual setting)');
       banner.backgroundColor().should('eq', 'rgb(255, 0, 0)');
-  
+
       cy.deleteRancherResource('k8s', 'clusters/local/apis/management.cattle.io/v3/settings', `ui-banner-${ bannerName.toLowerCase() }`);
-  
+
       HomePagePo.goToAndWaitForGet();
-  
+
       banner.checkNotExists();
     });
   }
@@ -452,5 +451,5 @@ describe('Banners', { testIsolation: 'off' }, () => {
   });
 
   // describe('Login Banner', () => {
-  // });  
+  // });
 });
