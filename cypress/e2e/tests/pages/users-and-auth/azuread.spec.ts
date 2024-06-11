121a45ed8c1d3bb95b5350fa0c712888b49345a3
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2024-06-11 15:21:17 -0700

    add group membership filter to azuread

--- cypress/e2e/tests/pages/users-and-auth/azuread.spec.ts
+++ cypress/e2e/tests/pages/users-and-auth/azuread.spec.ts
@@ -8,6 +8,8 @@ const azureadPo = new AzureadPo('local');
 const tenantId = '564b6f53-ebf4-43c3-8077-44c56a44990a';
 const applicationId = '18cca356-170e-4bd9-a4a4-2e349855f96b';
 const appSecret = 'test';
+const defaultGroupMembershipFilter = '';
+const groupMembershipFilter = 'test';
 const defaultEndpoint = 'https://login.microsoftonline.com/';
 const defaultAuthEndpoint = 'https://login.microsoftonline.com/564b6f53-ebf4-43c3-8077-44c56a44990a/oauth2/v2.0/authorize';
 const defaultTokenEndpoint = 'https://login.microsoftonline.com/564b6f53-ebf4-43c3-8077-44c56a44990a/oauth2/v2.0/token';
@@ -39,6 +41,7 @@ describe('AzureAD', { tags: ['@adminUser', '@usersAndAuths'] }, () => {
       expect(req.body.tenantId).to.equal(tenantId);
       expect(req.body.applicationId).to.equal(applicationId);
       expect(req.body.applicationSecret).to.equal(appSecret);
+      expect(req.body.groupMembershipFilter).to.equal(defaultGroupMembershipFilter);
       expect(req.body.endpoint).to.equal(defaultEndpoint);
       expect(req.body.authEndpoint).to.equal(defaultAuthEndpoint);
       expect(req.body.tokenEndpoint).to.equal(defaultTokenEndpoint);
@@ -63,6 +66,7 @@ describe('AzureAD', { tags: ['@adminUser', '@usersAndAuths'] }, () => {
       expect(req.body.tenantId).to.equal(tenantId);
       expect(req.body.applicationId).to.equal(applicationId);
       expect(req.body.applicationSecret).to.equal(appSecret);
+      expect(req.body.groupMembershipFilter).to.equal(groupMembershipFilter);
       expect(req.body.endpoint).to.equal(endpoint);
       expect(req.body.authEndpoint).to.equal(authEndpoint);
       expect(req.body.tokenEndpoint).to.equal(tokenEndpoint);
@@ -76,6 +80,10 @@ describe('AzureAD', { tags: ['@adminUser', '@usersAndAuths'] }, () => {
     azureadPo.enterTenantId(tenantId);
     azureadPo.enterApplicationId(applicationId);
     azureadPo.enterApplicationSecret(appSecret);
+
+    // enable group membership filter and add a text as filter
+    azureadPo.groupMembershipFilterCheckbox().set();
+    azureadPo.enterGroupMembershipFilter(groupMembershipFilter);
 
     azureadPo.selectEndpointsOption(2);
 
