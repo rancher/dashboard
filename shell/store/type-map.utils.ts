037dd1cf1989f5d6c2618b2bfcc89b5ab11694fa
Richard Cox <richard.cox@suse.com>
2024-06-14 17:49:57 +0100

    Correctly move `age` column to last position

--- shell/store/type-map.utils.ts
+++ shell/store/type-map.utils.ts
@@ -105,7 +105,7 @@ export function createHeaders(
   }
 
   // Age always goes last
-  const ageColumnActual = out.find((o) => o.name === 'Age');
+  const ageColumnActual = out.find((o) => o.name.toLocaleLowerCase() === 'age');
 
   if ( ageColumnActual ) {
     removeObject(out, ageColumnActual);
