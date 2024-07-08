d88c454496cbe5993ceb6c3122dbfb23f36f397a
Richard <richard@linux.fritz.box>
2024-07-08 10:29:26 +0100

    Handle harvester loading in a route guard to better handle the unauthenticated method. This is done by providing a 404 catch all page which is authenticated.

--- shell/config/router/navigation-guards/index.js
+++ shell/config/router/navigation-guards/index.js
@@ -1,6 +1,7 @@
 import { install as installLoadInitialSettings } from '@shell/config/router/navigation-guards/load-initial-settings';
 import { install as installAttemptFirstLogin } from '@shell/config/router/navigation-guards/attempt-first-login';
 import { install as installAuthentication } from '@shell/config/router/navigation-guards/authentication';
+import { install as installHarvester } from '@shell/config/router/navigation-guards/harvester';
 import { install as installI18N } from '@shell/config/router/navigation-guards/i18n';
 
 /**
@@ -10,7 +11,7 @@ export function installNavigationGuards(router, context) {
   // NOTE: the order of the installation matters.
   // Be intentional when adding, removing or modifying the guards that are installed.
 
-  const navigationGuardInstallers = [installLoadInitialSettings, installAttemptFirstLogin, installAuthentication, installI18N];
+  const navigationGuardInstallers = [installLoadInitialSettings, installAttemptFirstLogin, installAuthentication, installHarvester, installI18N];
 
   navigationGuardInstallers.forEach((installer) => installer(router, context));
 }
