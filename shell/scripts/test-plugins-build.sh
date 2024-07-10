abebce88f02a250ff2af8fe07ad4eac61de3cf19
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2024-07-10 16:21:19 +0500

    experimenting

--- shell/scripts/test-plugins-build.sh
+++ shell/scripts/test-plugins-build.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR="$( cd $SCRIPT_DIR && cd ../.. & pwd)"
 SHELL_DIR=$BASE_DIR/shell/
 SHELL_VERSION="99.99.99"
-DEFAULT_YARN_REGISTRY="https://registry.npmjs.org"
+DEFAULT_YARN_REGISTRY="https://registry.yarnpkg.com/"
 VERDACCIO_YARN_REGISTRY="http://localhost:4873"
 
 echo ${SCRIPT_DIR}
@@ -209,10 +209,8 @@ function clone_repo_test_extension_build() {
   popd
 
   # delete folder
-  if [ $TEST_PERSIST_BUILD != "true" ]; then
-    echo "Removing folder ${BASE_DIR}/$REPO_NAME"
-    rm -rf ${BASE_DIR}/$REPO_NAME
-  fi
+  echo "Removing folder ${BASE_DIR}/$REPO_NAME"
+  rm -rf ${BASE_DIR}/$REPO_NAME
   yarn config set registry ${DEFAULT_YARN_REGISTRY}
 }
 
