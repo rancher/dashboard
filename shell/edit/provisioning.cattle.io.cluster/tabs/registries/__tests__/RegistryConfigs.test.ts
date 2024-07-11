d578f46a43bd24e51886308aaf7fed8524103c0d
Francesco Torchia <francesco.torchia@suse.com>
2024-07-11 18:06:30 +0200

    Encode CA bundle key in base64 format
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ shell/edit/provisioning.cattle.io.cluster/tabs/registries/__tests__/RegistryConfigs.test.ts
@@ -0,0 +1,61 @@
+import { mount, Wrapper } from '@vue/test-utils';
+import { clone } from '@shell/utils/object';
+import { _EDIT } from '@shell/config/query-params';
+import { PROV_CLUSTER } from '@shell/edit/provisioning.cattle.io.cluster/__tests__/utils/cluster';
+import RegistryConfigs from '@shell/edit/provisioning.cattle.io.cluster/tabs/registries/RegistryConfigs.vue';
+
+describe('component: RegistryConfigs', () => {
+  let wrapper: Wrapper<InstanceType<typeof RegistryConfigs> & { [key: string]: any }>;
+
+  const mountOptions = {
+    propsData: {
+      value:                     {},
+      mode:                      _EDIT,
+      clusterRegisterBeforeHook: () => {}
+    },
+    stubs: {
+      SelectOrCreateAuthSecret: true,
+      SecretSelector:           true,
+    },
+    mocks: { $store: { getters: { 'i18n/t': jest.fn() } } }
+  };
+
+  describe('key CA Cert Bundle', () => {
+    it('should display default key', () => {
+      const value = clone(PROV_CLUSTER);
+
+      value.spec.rkeConfig.registries.configs = { foo: { caBundle: 'Zm9vYmFy' } };
+
+      mountOptions.propsData.value = value;
+
+      wrapper = mount(
+        RegistryConfigs,
+        mountOptions
+      );
+
+      const registry = wrapper.find('[data-testid^="registry-caBundle"]').element as HTMLTextAreaElement;
+
+      expect(registry.value).toBe('foobar');
+    });
+
+    it('should update key in base64 format', async() => {
+      const value = clone(PROV_CLUSTER);
+
+      value.spec.rkeConfig.registries.configs = { foo: { caBundle: 'Zm9vYmFy' } };
+
+      mountOptions.propsData.value = value;
+
+      wrapper = mount(
+        RegistryConfigs,
+        mountOptions
+      );
+
+      const registry = wrapper.find('[data-testid^="registry-caBundle"]');
+
+      await registry.setValue('ssh key');
+      wrapper.vm.update();
+
+      expect(wrapper.emitted('updateConfigs')![0][0]['foo']['caBundle']).toBe('c3NoIGtleQ==');
+    });
+  });
+});
