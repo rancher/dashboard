338f821b99f7ac23a78d3e6feec8472ec398d495
Francesco Torchia <francesco.torchia@suse.com>
2024-07-16 10:08:53 +0200

    Remove duplicate codecov download
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- .github/workflows/scripts/install-codecov.sh
+++ .github/workflows/scripts/install-codecov.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl -Os https://cli.codecov.io/latest/linux/codecov
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
 curl -Os https://cli.codecov.io/latest/linux/codecov
 curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM
