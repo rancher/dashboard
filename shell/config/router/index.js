6845c7f3beacb08fa228403b50b017129a7a6163
Phillip Rak <rak.phillip@gmail.com>
2024-07-23 10:41:09 -0700

    Resolving router-related warnings
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/config/router/index.js
+++ shell/config/router/index.js
@@ -1,22 +1,21 @@
-import { createApp } from 'vue';
-import { createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Routes from '@shell/config/router/routes';
 import { installNavigationGuards } from '@shell/config/router/navigation-guards';
-const vueApp = createApp({});
 
-vueApp.use(Router);
-
-export const routerOptions = {
-  mode:     'history',
+export const routerOptions = createRouter({
+  history:  createWebHistory(process.env.routerBase || '/'),
   // Note: router base comes from the ROUTER_BASE env var
   base:     process.env.routerBase || '/',
   routes:   Routes,
-  fallback: false
-};
+  fallback: false,
+});
 
 export function extendRouter(config, context) {
   const base = (config._app && config._app.basePath) || routerOptions.base;
-  const router = new Router({ ...routerOptions, base });
+  const router = createRouter({
+    ...routerOptions,
+    base,
+  });
 
   installNavigationGuards(router, context);
 
