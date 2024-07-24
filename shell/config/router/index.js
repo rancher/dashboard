e0d584a69f6cd888d14af696429f9a18e2a19ad4
Phillip Rak <rak.phillip@gmail.com>
2024-07-24 10:01:02 -0700

    Fix problem with extenRouter
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/config/router/index.js
+++ shell/config/router/index.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Routes from '@shell/config/router/routes';
 import { installNavigationGuards } from '@shell/config/router/navigation-guards';
 
-export const routerOptions = createRouter({
+export const routerOptions = {
   history:  createWebHistory(process.env.routerBase || '/'),
   // Note: router base comes from the ROUTER_BASE env var
   base:     process.env.routerBase || '/',
   routes:   Routes,
   fallback: false,
-});
+};
 
 export function extendRouter(config, context) {
   const base = (config._app && config._app.basePath) || routerOptions.base;
