8b64b8f3bb5374b33d78f64c7a927e8293cfd070
Phillip Rak <rak.phillip@gmail.com>
2024-07-26 10:21:00 -0700

    Fix router redirect to home after login
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/pages/auth/login.vue
+++ shell/pages/auth/login.vue
@@ -289,7 +289,7 @@ export default {
           this.$store.dispatch('auth/setInitialPass', this.password);
           this.$router.push({ name: 'auth-setup' });
         } else {
-          this.$router.replace('/');
+          this.$router.push({ name: 'index' });
         }
       } catch (err) {
         this.err = err;
