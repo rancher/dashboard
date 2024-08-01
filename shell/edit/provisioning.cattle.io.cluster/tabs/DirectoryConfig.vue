870c9d7269ea9643ca2602b829773e8e3524496a
Alexandre Alves <97888974+aalves08@users.noreply.github.com>
2024-08-01 12:37:17 +0100

    remove checkbox from dataDirectories cluster config (#11573)

--- shell/edit/provisioning.cattle.io.cluster/tabs/DirectoryConfig.vue
+++ shell/edit/provisioning.cattle.io.cluster/tabs/DirectoryConfig.vue
@@ -1,16 +1,12 @@
 
 <script>
-import { Checkbox } from '@components/Form/Checkbox';
 import { LabeledInput } from '@components/Form/LabeledInput';
 import { _CREATE } from '@shell/config/query-params';
 
 export default {
   name:       'DirectoryConfig',
-  components: {
-    Checkbox,
-    LabeledInput,
-  },
-  props: {
+  components: { LabeledInput },
+  props:      {
     mode: {
       type:     String,
       required: true,
@@ -21,50 +17,11 @@ export default {
       required: true,
     },
   },
-  data() {
-    let atLeastOneDirWithAnIdentifier = false;
-    let allDirsWithSameIdentifier = false;
-
-    if (this.value?.systemAgent?.length || this.value?.provisioning?.length || this.value?.k8sDistro?.length) {
-      atLeastOneDirWithAnIdentifier = true;
-      if (this.value?.systemAgent === this.value?.provisioning && this.value?.provisioning === this.value?.k8sDistro &&
-      this.value?.systemAgent === this.value?.k8sDistro) {
-        allDirsWithSameIdentifier = true;
-      }
-    }
-
-    return {
-      isSettingCommonConfig: !(atLeastOneDirWithAnIdentifier && !allDirsWithSameIdentifier),
-      commonConfig:          allDirsWithSameIdentifier ? this.value?.systemAgent : '',
-    };
-  },
-  watch: {
-    commonConfig(neu) {
-      if (neu && neu.length && this.isSettingCommonConfig) {
-        this.value.systemAgent = neu;
-        this.value.provisioning = neu;
-        this.value.k8sDistro = neu;
-      }
-    }
-  },
   computed: {
     disableEditInput() {
       return this.mode !== _CREATE;
     }
-  },
-  methods: {
-    handleCommonConfig(val) {
-      this.isSettingCommonConfig = val;
-
-      if (val) {
-        this.value.systemAgent = '';
-        this.value.provisioning = '';
-        this.value.k8sDistro = '';
-      } else {
-        this.commonConfig = '';
-      }
-    }
-  },
+  }
 };
 </script>
 
@@ -74,54 +31,33 @@ export default {
       <h3 class="mb-20">
         {{ t('cluster.directoryConfig.title') }}
       </h3>
-      <Checkbox
-        class="mb-10"
-        :value="isSettingCommonConfig"
-        :mode="mode"
-        :label="t('cluster.directoryConfig.checkboxText')"
-        :disabled="disableEditInput"
-        data-testid="rke2-directory-config-individual-config-checkbox"
-        @input="handleCommonConfig"
-      />
       <LabeledInput
-        v-if="isSettingCommonConfig"
-        v-model="commonConfig"
+        v-model="value.systemAgent"
         class="mb-20"
         :mode="mode"
-        :label="t('cluster.directoryConfig.common.label')"
-        :tooltip="t('cluster.directoryConfig.common.tooltip')"
+        :label="t('cluster.directoryConfig.systemAgent.label')"
+        :tooltip="t('cluster.directoryConfig.systemAgent.tooltip')"
         :disabled="disableEditInput"
-        data-testid="rke2-directory-config-common-data-dir"
+        data-testid="rke2-directory-config-systemAgent-data-dir"
       />
-      <div v-if="!isSettingCommonConfig">
-        <LabeledInput
-          v-model="value.systemAgent"
-          class="mb-20"
-          :mode="mode"
-          :label="t('cluster.directoryConfig.systemAgent.label')"
-          :tooltip="t('cluster.directoryConfig.systemAgent.tooltip')"
-          :disabled="disableEditInput"
-          data-testid="rke2-directory-config-systemAgent-data-dir"
-        />
-        <LabeledInput
-          v-model="value.provisioning"
-          class="mb-20"
-          :mode="mode"
-          :label="t('cluster.directoryConfig.provisioning.label')"
-          :tooltip="t('cluster.directoryConfig.provisioning.tooltip')"
-          :disabled="disableEditInput"
-          data-testid="rke2-directory-config-provisioning-data-dir"
-        />
-        <LabeledInput
-          v-model="value.k8sDistro"
-          class="mb-20"
-          :mode="mode"
-          :label="t('cluster.directoryConfig.k8sDistro.label')"
-          :tooltip="t('cluster.directoryConfig.k8sDistro.tooltip')"
-          :disabled="disableEditInput"
-          data-testid="rke2-directory-config-k8sDistro-data-dir"
-        />
-      </div>
+      <LabeledInput
+        v-model="value.provisioning"
+        class="mb-20"
+        :mode="mode"
+        :label="t('cluster.directoryConfig.provisioning.label')"
+        :tooltip="t('cluster.directoryConfig.provisioning.tooltip')"
+        :disabled="disableEditInput"
+        data-testid="rke2-directory-config-provisioning-data-dir"
+      />
+      <LabeledInput
+        v-model="value.k8sDistro"
+        class="mb-20"
+        :mode="mode"
+        :label="t('cluster.directoryConfig.k8sDistro.label')"
+        :tooltip="t('cluster.directoryConfig.k8sDistro.tooltip')"
+        :disabled="disableEditInput"
+        data-testid="rke2-directory-config-k8sDistro-data-dir"
+      />
       <div class="mb-40" />
     </div>
   </div>
