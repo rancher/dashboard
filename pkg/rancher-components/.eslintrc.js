eb13e75011028107bcbbfec5c29e28438d8cf61f
cnotv <giuseppe.leo@suse.com>
2024-08-15 15:32:21 +0200

    Linter: Fix rancher/components types after refactoring

--- pkg/rancher-components/.eslintrc.js
+++ pkg/rancher-components/.eslintrc.js
@@ -1,57 +1,49 @@
 module.exports = {
-  "root": true,
-  "env": {
-    "node": true
-  },
-  "extends": [
-    "../../.eslintrc.default.js"
+  root:    true,
+  env:     { node: true },
+  extends: [
+    '../../.eslintrc.default.js'
   ],
-  "parserOptions": {
-    "ecmaVersion": 2020
-  },
-  "rules": {
-    "vue/no-mutating-props": "warn",
-    "@typescript-eslint/no-empty-function": [
-      "error",
+  parserOptions: { ecmaVersion: 2020 },
+  rules:         {
+    'vue/no-mutating-props':                'warn',
+    '@typescript-eslint/no-empty-function': [
+      'error',
       {
-        "allow": [
-          "arrowFunctions"
+        allow: [
+          'arrowFunctions'
         ]
       }
     ],
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "vue/multi-word-component-names": "off",
-    "@typescript-eslint/ban-ts-comment": [
-      "error",
-      {
-        "ts-nocheck": false
-      }
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'vue/multi-word-component-names':           'off',
+    '@typescript-eslint/ban-ts-comment':        [
+      'error',
+      { 'ts-nocheck': false }
     ],
-    "vue/one-component-per-file": "off",
-    "vue/no-deprecated-slot-attribute": "off",
-    "vue/require-explicit-emits": "off",
-    "vue/v-on-event-hyphenation": "off"
+    'vue/one-component-per-file':       'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/require-explicit-emits':       'off',
+    'vue/v-on-event-hyphenation':       'off'
   },
-  "settings": {
-    "import/ignore": [
-      "vue"
+  settings: {
+    'import/ignore': [
+      'vue'
     ]
   },
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/__mocks__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-        "**/*.spec.{j,t}s?(x)"
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/__mocks__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/*.spec.{j,t}s?(x)'
       ],
-      "env": {
-        "jest": true
-      }
+      env: { jest: true }
     }
   ],
-  "ignorePatterns": [
-    "src/shim-tsx.d.ts",
-    "src/shim-vue.d.ts"
+  ignorePatterns: [
+    'src/shim-tsx.d.ts',
+    'src/shim-vue.d.ts'
   ]
-}
+};
