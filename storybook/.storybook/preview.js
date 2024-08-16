2439c274ad6f498f4744a16c2c99b180a2174ac8
cnotv <giuseppe.leo@suse.com>
2024-08-16 14:32:46 +0200

    Bump Storybook dependencies
    Storybook: Restore lock file
    
    Storybook: Add CLI dependency
    
    Storybook: run suggested migration script
    
    Storybook: Add framework dependency to the config
    
    Storybook: Remove chromebook
    
    Storybook: migrate to 8; correct framework dependency
    
    Storybook: Migrate configuration to Vue3 syntax
    
    Storybook: Fix mdx linting issues

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import  { setup }  from '@storybook/vue3';
+import { createStore } from 'vuex'
 import vSelect from 'vue-select';
 import FloatingVue from 'floating-vue';
 import { themes } from '@storybook/theming';
@@ -10,9 +10,7 @@ import withEvents from 'storybook-auto-events';
 const i18nStrings = require('../../shell/assets/translations/en-us.yaml');
 import cleanTooltipDirective  from '@shell/directives/clean-tooltip';
 import ShortKey from 'vue-shortkey';
-import trimWhitespaceDirective from '@shell/directives/trim-whitespace';
 import cleanHtmlDirective from '@shell/directives/clean-html';
-
 
 // Store modules
 import growl from './store/growl';
@@ -22,100 +20,91 @@ import table from './store/table';
 // i18n
 import i18n from '../../shell/plugins/i18n';
 
-// Register custom i18n plugin
-Vue.use(i18n);
+setup((vueApp) => {
+  vueApp.use(i18n, { store: { dispatch() {} } });
+  vueApp.use(FloatingVue);
+  vueApp.directive('clean-html', cleanHtmlDirective);
+  vueApp.directive('clean-tooltip', cleanTooltipDirective);
+  vueApp.component('v-select', vSelect);
+  vueApp.use(ShortKey, { prevent: ['input', 'textarea', 'select'] });
+  vueApp.component('router-link', {
+    props:   ['to'],
+    template: '<a>link</a>',
+  })
 
-Vue.use(Vuex);
-Vue.use(ShortKey, { prevent: ['input', 'textarea', 'select'] });
-Vue.component('v-select', vSelect);
-Vue.use(FloatingVue);
+  vueApp.use(storePlugin);
 
-Vue.component('router-link', {
-  props:   ['to'],
-  template: '<a>link</a>',
+  window.__codeMirrorLoader = () => import(/* webpackChunkName: "codemirror" */ '@shell/plugins/codemirror');
 })
 
-Vue.directive('clean-tooltip', cleanTooltipDirective);
-Vue.directive('trim-whitespace', trimWhitespaceDirective);
-Vue.directive('clean-html', cleanHtmlDirective);
-
-window.__codeMirrorLoader = () => import(/* webpackChunkName: "codemirror" */ '@shell/plugins/codemirror');
-
-// Defines namespaced modules for the Store.
-export const store = new Vuex.Store({
-  getters: {
-    'i18n/exists': key => store.getters['i18n/t'],
-    'i18n/t': state => (key, args) => {
-      const msg = get(i18nStrings, key) || key;
-
-      if (msg?.includes('{')) {
-        const formatter = new IntlMessageFormat(msg, state.selected);
-        return formatter.format(args);
-      }
-
-      return msg;
-    },
-  },
-  modules: {
-    growl,
-    codeMirror,
-    table
-  }
-});
-
-
 const storePlugin = {
-  store,
-  install (Vue, options) {
-    Vue.prototype.$store = store
+  store: createStore({
+    getters: {
+      'i18n/exists': key => store.getters['i18n/t'],
+      'i18n/t': state => (key, args) => {
+        const msg = get(i18nStrings, key) || key;
+  
+        if (msg?.includes('{')) {
+          const formatter = new IntlMessageFormat(msg, state.selected);
+          return formatter.format(args);
+        }
+  
+        return msg;
+      },
+    },
+    modules: {
+      growl,
+      codeMirror,
+      table
+    }
+  }),
+  install (vueApp, options) {
+    vueApp.prototype.$store = store
   }
 };
 
-Vue.use(storePlugin);
-
-export const parameters = {
-  options: {
-    storySort: {
-      order: ['Foundation', 'Form', 'Components', 'Examples'],
+const preview = {
+  parameters: {
+    options: {
+      storySort: {
+        order: ['Foundation', 'Form', 'Components', 'Examples'],
+      },
     },
-  },
-  previewTabs: {
-    canvas: { hidden: false },
-  },
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  layout: 'centered',
-  // viewMode: 'docs',
-  // Auto set controls based on the property name
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    previewTabs: {
+      canvas: { hidden: false },
     },
-  },
-  darkMode: {
-    dark: {
-      ...themes.dark,
-      brandTitle: 'Rancher Storybook',
-      brandImage: 'dark/rancher-logo.svg'
+    layout: 'centered',
+    // viewMode: 'docs',
+    // Auto set controls based on the property name
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
-    light: {
-      ...themes.normal,
-      brandTitle: 'Rancher Storybook',
-      brandImage: 'rancher-logo.svg'
-    },
-    darkClass: 'theme-dark',
-    lightClass: 'theme-light',
-    stylePreview: true
+    darkMode: {
+      dark: {
+        ...themes.dark,
+        brandTitle: 'Rancher Storybook',
+        brandImage: 'dark/rancher-logo.svg'
+      },
+      light: {
+        ...themes.normal,
+        brandTitle: 'Rancher Storybook',
+        brandImage: 'rancher-logo.svg'
+      },
+      darkClass: 'theme-dark',
+      lightClass: 'theme-light',
+      stylePreview: true
+    }
   }
 }
 
-export const decorators = [
-  withEvents
-];
 
 // Add keyboard shortcut to toggle between dark and light modes
 window.onload = () => {
   installShortcut();
 }
 
-export default store
+export const decorators = [ withEvents ];
+export default preview;
