103610565445c17b88a478840d92560852788f0a
Cody Jackson <cody.jackson@suse.com>
2024-08-21 06:22:11 -0700

    Adding a custom data-test id to the add button of arraylist
    I did this so I could change how that button is referenced in tests. I don't think we ever should've used the add-label attribute.

--- shell/components/form/ArrayList.vue
+++ shell/components/form/ArrayList.vue
@@ -349,7 +349,7 @@ export default {
           type="button"
           class="btn role-tertiary add"
           :disabled="loading || disableAdd"
-          data-testid="array-list-button"
+          :data-testid="`array-list-button${addLabel && _addLabel ? ('-'+_addLabel).replace(' ', '-').toLowerCase() : ''}`"
           @click="add()"
         >
           <i
