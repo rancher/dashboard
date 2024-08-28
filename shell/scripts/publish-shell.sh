e431c789b02c0d38837107cd73b3074ccd088cc7
Alexandre Alves <alexandre.alves@suse.com>
2024-08-28 12:33:54 +0100

    fix publish shell

--- shell/scripts/publish-shell.sh
+++ shell/scripts/publish-shell.sh
@@ -1,222 +1,102 @@
 #!/usr/bin/env bash
 
-set -e
-
-echo "Checking plugin build"
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BASE_DIR="$( cd $SCRIPT_DIR && cd ../.. & pwd)"
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+BASE_DIR="$(
+  cd $SCRIPT_DIR && cd ../.. &
+  pwd
+)"
 SHELL_DIR=$BASE_DIR/shell/
-SHELL_VERSION="99.99.99"
-DEFAULT_YARN_REGISTRY="https://registry.yarnpkg.com/"
-VERDACCIO_YARN_REGISTRY="http://localhost:4873"
+CREATORS_DIR=$BASE_DIR/shell/creators/extension
+PUBLISH_ARGS="--no-git-tag-version --access public $PUBLISH_ARGS"
+FORCE_PUBLISH_TO_NPM="false"
+DEFAULT_YARN_REGISTRY="https://registry.npmjs.org"
 
-echo ${SCRIPT_DIR}
-
-SKIP_SETUP="false"
-SKIP_STANDALONE="false"
-
-if [ "$1" == "-s" ]; then
-  SKIP_SETUP="true"
+# if TAG doesn't exist, we can exit as it's needed for any type of publish.
+if [ -z "$TAG" ]; then
+  echo "You need to set the TAG variable first!"
+  exit 1
 fi
 
-if [ $SKIP_SETUP == "false" ]; then
-  set +e
-  which verdaccio > /dev/null
+if [ ! -d "${BASE_DIR}/node_modules" ]; then
+  echo "You need to run 'yarn install' first"
+  exit 1
+fi
+
+echo "Publishing Shell Packages"
+
+if [ "$1" == "--npm" ]; then
+  FORCE_PUBLISH_TO_NPM="true"
+fi
+
+if [ "$FORCE_PUBLISH_TO_NPM" == "true" ]; then
+  export YARN_REGISTRY=$DEFAULT_YARN_REGISTRY
+fi
+
+pushd ${SHELL_DIR} >/dev/null
+
+function publish() {
+  NAME=$1
+  FOLDER=$2
+
+  # if we pass a third arg, that is the version number
+  # that we want to actually publish on NPM
+  # they should match with the package.json version stated 
+  # because of the check in the "Check Tags Version Matching" step in the workflow
+  if [ -n "$3" ]; then
+    PKG_VERSION=$3
+  fi
+
+  echo "Publishing ${NAME} from ${FOLDER}"
+  pushd ${FOLDER} >/dev/null
+
+  # For now, copy the rancher components into the shell and ship them with it
+  if [ "$NAME" == "Shell" ]; then
+    echo "Adding Rancher Components"
+    rm -rf ./rancher-components
+    cp -R ${BASE_DIR}/pkg/rancher-components/src/components ./rancher-components/
+  fi
+
+  # If we need to release shell as well, we tag it as v2
+  PUBLISH_ARGS="--no-git-tag-version --access public --tag v2"
+
+  # Make a note of dependency versions, if required
+  node ${SCRIPT_DIR}/record-deps.js
+
+  yarn publish . --new-version ${PKG_VERSION} ${PUBLISH_ARGS}
   RET=$?
-  set -e
+
+  popd >/dev/null
 
   if [ $RET -ne 0 ]; then
-    echo "Verdaccio not installed"
-
-    npm install -g verdaccio
+    echo "Error publishing package ${NAME}"
+    exit $RET
   fi
-
-  set +e
-  RUNNING=$(pgrep Verdaccio | wc -l | xargs)
-  set -e
-
-  if [ $RUNNING -eq 0 ]; then
-    verdaccio > verdaccio.log &
-    PID=$!
-
-    echo "Verdaccio: $PID"
-
-    sleep 10
-
-    echo "Configuring Verdaccio user"
-
-    # Remove existing admin if already there
-    if [ -f ~/.config/verdaccio/htpasswd ]; then
-      sed -i.bak -e '/^admin:/d' ~/.config/verdaccio/htpasswd
-    fi
-
-    curl -XPUT -H "Content-type: application/json" -d '{ "name": "admin", "password": "admin" }' 'http://localhost:4873/-/user/admin' > login.json
-    TOKEN=$(jq -r .token login.json)
-    rm login.json
-    cat > ~/.npmrc << EOF
-//127.0.0.1:4873/:_authToken="$TOKEN"
-//localhost:4873/:_authToken="$TOKEN"
-EOF
-  else
-    echo "Verdaccio is already running"
-  fi
-fi
-
-if [ -d ~/.local/share/verdaccio/storage/@rancher ]; then 
-  rm -rf ~/.local/share/verdaccio/storage/@rancher/*
-else
-  rm -rf ~/.config/verdaccio/storage/@rancher/*
-fi
-
-export YARN_REGISTRY=$VERDACCIO_YARN_REGISTRY
-export NUXT_TELEMETRY_DISABLED=1
-
-# Remove test package from previous run, if present
-if [ "${TEST_PERSIST_BUILD}" != "true" ]; then
-  echo "Removing folder ${BASE_DIR}/pkg/test-pkg"
-  rm -rf ${BASE_DIR}/pkg/test-pkg
-fi
-
-# We need to patch the version number of the shell, otherwise if we are running
-# with the currently published version, things will fail as those versions
-# are already published and Verdaccio will check, since it is a read-through cache
-sed -i.bak -e "s/\"version\": \"[0-9]*.[0-9]*.[0-9]*\(-alpha\.[0-9]*\|-release[0-9]*.[0-9]*.[0-9]*\|-rc\.[0-9]*\)\{0,1\}\",/\"version\": \"${SHELL_VERSION}\",/g" ${SHELL_DIR}/package.json
-rm ${SHELL_DIR}/package.json.bak
-
-# Same as above for Rancher Components
-# We might have bumped the version number but its not published yet, so this will fail
-sed -i.bak -e "s/\"version\": \"[0-9]*.[0-9]*.[0-9]*\(-alpha\.[0-9]*\|-release[0-9]*.[0-9]*.[0-9]*\|-rc\.[0-9]*\)\{0,1\}\",/\"version\": \"${SHELL_VERSION}\",/g" ${BASE_DIR}/pkg/rancher-components/package.json
-
-# Publish shell pkg (tag is needed as publish-shell is optimized to work with release-shell-pkg workflow)
-echo "Publishing Shell package to local registry"
-yarn install
-export TAG="shell-pkg-v${SHELL_VERSION}"
-${SHELL_DIR}/scripts/publish-shell.sh
-
-# Publish creators pkg (tag is needed as publish-shell is optimized to work with release-shell-pkg workflow)
-echo "Publishing Creators package to local registry"
-export TAG="creators-pkg-v${SHELL_VERSION}"
-${SHELL_DIR}/scripts/publish-shell.sh
-
-# Publish rancher components
-yarn build:lib
-yarn publish:lib
-
-# We pipe into cat for cleaner logging - we need to set pipefail
-# to ensure the build fails in these cases
-set -o pipefail
-
-if [ "${SKIP_STANDALONE}" == "false" ]; then
-  DIR=$(mktemp -d)
-  pushd $DIR > /dev/null
-
-  echo "Using temporary directory ${DIR}"
-
-  echo "Verifying extension creator"
-
-  FORCE_COLOR=true yarn create @rancher/extension test-pkg --app-name test-app | cat
-
-  pushd test-app > /dev/null
-
-  yarn install
-  FORCE_COLOR=true yarn build | cat
-
-  # Add test list component to the test package
-  # Validates rancher-components imports
-  mkdir -p pkg/test-pkg/list
-  cp ${SHELL_DIR}/list/catalog.cattle.io.clusterrepo.vue pkg/test-pkg/list
-
-  FORCE_COLOR=true yarn build-pkg test-pkg | cat
-
-  echo "Cleaning temporary dir"
-  popd > /dev/null
-
-  if [ "${TEST_PERSIST_BUILD}" != "true" ]; then
-    echo "Removing folder ${DIR}"
-    rm -rf ${DIR}
-  fi
-fi
-
-pushd $BASE_DIR
-
-# Now try a plugin within the dashboard codebase
-echo "Validating in-tree package"
-
-yarn install
-
-if [ "${TEST_PERSIST_BUILD}" != "true" ]; then
-  echo "Removing folder ./pkg/test-pkg"
-  rm -rf ./pkg/test-pkg
-fi
-
-yarn create @rancher/extension test-pkg -i
-cp ${SHELL_DIR}/list/catalog.cattle.io.clusterrepo.vue ./pkg/test-pkg/list
-FORCE_COLOR=true yarn build-pkg test-pkg | cat
-
-if [ "${TEST_PERSIST_BUILD}" != "true" ]; then
-  echo "Removing folder ./pkg/test-pkg"
-  rm -rf ./pkg/test-pkg
-fi
-
-# function to clone repos and install dependencies (including the newly published shell version)
-function clone_repo_test_extension_build() {
-  REPO_NAME=$1
-  PKG_NAME=$2
-
-  echo -e "\nSetting up $REPO_NAME repository locally\n"
-
-  # set registry to default (to install all of the other dependencies)
-  yarn config set registry ${DEFAULT_YARN_REGISTRY}
-
-  if [ "${TEST_PERSIST_BUILD}" != "true" ]; then
-    echo "Removing folder ${BASE_DIR}/$REPO_NAME"
-    rm -rf ${BASE_DIR}/$REPO_NAME
-  fi
-
-  # cloning repo
-  git clone https://github.com/rancher/$REPO_NAME.git
-  pushd ${BASE_DIR}/$REPO_NAME
-
-  echo -e "\nInstalling dependencies for $REPO_NAME\n"
-  yarn install
-
-  # set registry to local verdaccio (to install new shell)
-  yarn config set registry ${VERDACCIO_YARN_REGISTRY}
-
-  # update package.json to use a specific version of shell
-  sed -i.bak -e "s/\"\@rancher\/shell\": \"[0-9]*.[0-9]*.[0-9]*\",/\"\@rancher\/shell\": \"${SHELL_VERSION}\",/g" package.json
-  rm package.json.bak
-
-  # we need to remove yarn.lock, otherwise it would install a version that we don't want
-  rm yarn.lock
-
-  echo -e "\nInstalling newly built shell version\n"
-
-  # installing new version of shell
-  yarn add @rancher/shell@${SHELL_VERSION}
-
-  # test build-pkg
-  FORCE_COLOR=true yarn build-pkg $PKG_NAME | cat
-
-  # # kubewarden has some unit tests and they should be quick to run... Let's check them as well
-  # if [ "${REPO_NAME}" == "kubewarden-ui" ]; then
-  #   yarn test:ci
-  # fi
-
-  # return back to the base path
-  popd
-
-  # delete folder
-  echo "Removing folder ${BASE_DIR}/$REPO_NAME"
-  rm -rf ${BASE_DIR}/$REPO_NAME
-  yarn config set registry ${DEFAULT_YARN_REGISTRY}
 }
 
-# Here we just add the extension that we want to include as a check (all our official extensions should be included here)
-# Don't forget to add the unit tests exception to clone_repo_test_extension_build function if a new extension has those
-clone_repo_test_extension_build "kubewarden-ui" "kubewarden"
-clone_repo_test_extension_build "elemental-ui" "elemental"
-clone_repo_test_extension_build "capi-ui-extension" "capi"
+# Generate the type definitions for the shell
+${SCRIPT_DIR}/typegen.sh
 
-echo "All done"
+echo "TAG ${TAG}"
+  
+  # let's get the package name and version from the tag
+PKG_NAME=$(sed 's/-pkg-v.*//' <<< "$TAG")
+PKG_V=$(sed 's/.*-pkg-v//'<<< "$TAG")
+
+echo "PKG_NAME ${PKG_NAME}"
+echo "PKG_V ${PKG_V}"
+
+# version comparison checks
+case $PKG_NAME in
+  "shell")
+    echo "Publishing only Shell pkg via tagged release"
+    publish "Shell" ${SHELL_DIR} ${PKG_V}
+    ;;
+  "creators")
+    echo "Publishing only Creators pkg via tagged release"
+    publish "Extension creator" ${CREATORS_DIR} ${PKG_V}
+    ;;
+  *)
+    echo "something went wrong with the tagging name => TAG: ${TAG} , PKG_NAME: ${PKG_NAME}. Admissable names are 'shell' and 'creator'"
+    exit 1
+    ;;
+esac
