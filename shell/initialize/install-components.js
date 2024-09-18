e026b0c2cb61dcb34b5f7915a6bd013bd6a760f8
Phillip Rak <rak.phillip@gmail.com>
2024-09-18 13:14:56 -0700

    Remove deprecated `NuxtChild`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/initialize/install-components.js
+++ shell/initialize/install-components.js
@@ -1,17 +1,11 @@
 
-import NuxtChild from '@shell/components/nuxt/nuxt-child.js';
 import vSelect from 'vue-select';
 
 /**
  * Add components to the Vue instance
  * @param {*} vueApp Vue instance
- * TODO: #11070 - Remove Nuxt residuals
  */
 export const installComponents = (vueApp) => {
-  // Component: <NuxtChild>
-  vueApp.component(NuxtChild.name, NuxtChild);
-  vueApp.component('NChild', NuxtChild);
-
   // Vendor components
   vueApp.component('v-select', vSelect);
 };
