3c07dcf1ceff3f1b9cc80c0d6a4131196ad58063
Izaac Zavaleta <izaac.zavaleta@suse.com>
2024-09-27 09:45:05 -0700

    Adjust cypress execution to generate html report on Jenkins

--- cypress/jenkins/scpget.sh
+++ cypress/jenkins/scpget.sh
@@ -7,5 +7,13 @@ export PATH=$PATH:"${WORKSPACE}/bin"
 PRIV_KEY="${WORKSPACE}/.ssh/jenkins_ecdsa"
 NODE_EXTERNAL_IP="$(corral vars ci first_node_ip)"
 
+
+REPORT_DIR="."
+
+if [[ $# -gt 1 ]]; then
+  REPORT_DIR="$2"
+  mkdir -p "$2"
+fi
+
 scp -r -i ${PRIV_KEY} -o StrictHostKeyChecking=no \
-  -o UserKnownHostsFile=/dev/null "${AWS_SSH_USER}@${NODE_EXTERNAL_IP}:$1" .
+  -o UserKnownHostsFile=/dev/null "root@${NODE_EXTERNAL_IP}:$1" "${REPORT_DIR}"
