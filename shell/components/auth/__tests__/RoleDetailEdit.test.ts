424c389378165a772c77e1027d4beb79f6f34f12
cnotv <giuseppe.leo@suse.com>
2024-10-17 17:49:35 +0200

    Add tests to ensure verbs are not manipulated

--- shell/components/auth/__tests__/RoleDetailEdit.test.ts
+++ shell/components/auth/__tests__/RoleDetailEdit.test.ts
@@ -2,22 +2,21 @@ import { mount } from '@vue/test-utils';
 import RoleDetailEdit from '@shell/components/auth/RoleDetailEdit.vue';
 import { SUBTYPE_MAPPING } from '@shell/models/management.cattle.io.roletemplate';
 
-const role = {
-  apiVersion:            'management.cattle.io/v3',
-  kind:                  'GlobalRole',
-  metadata:              { name: 'global-role-with-inherited' },
-  inheritedClusterRoles: ['cluster-admin'],
-  rules:
-[{
-  verbs:     ['get', 'list'],
-  resources: ['pods'],
-  apiGroups: ['']
-}],
-  subtype: SUBTYPE_MAPPING.GLOBAL.id
-};
 
 describe('component: RoleDetailEdit', () => {
   it('does not have validation errors when the role has no displayName', () => {
+    const role = {
+      apiVersion:            'management.cattle.io/v3',
+      kind:                  'GlobalRole',
+      metadata:              { name: 'global-role-with-inherited' },
+      inheritedClusterRoles: ['cluster-admin'],
+      rules: [{
+        verbs:     ['get', 'list'],
+        resources: ['pods'],
+        apiGroups: ['']
+      }],
+      subtype: SUBTYPE_MAPPING.GLOBAL.id
+    };
     const wrapper = mount(RoleDetailEdit, {
       props: { value: role },
 
@@ -28,19 +27,58 @@ describe('component: RoleDetailEdit', () => {
           $store:      {
             dispatch: jest.fn(),
             getters:  {
-              currentStore: () => 'store', 'i18n/t': jest.fn(), 'store/schemaFor': jest.fn(), 'store/customisation/': jest.fn()
+              currentStore: () => 'store',
+              'i18n/t': jest.fn(),
+              'store/schemaFor': jest.fn(),
+              'store/customisation/': jest.fn()
             }
           }
         },
 
         stubs: {
           CruResource: { template: '<div><slot></slot></div>' },
-          // NameNsDescription: true,
           Tab:         { template: '<div><slot></slot></div>' },
         },
       },
     });
 
     expect((wrapper.vm as any).fvFormIsValid).toBe(true);
+  });
+
+  it.each([
+    [['*']],
+    [['create', 'delete', 'get', 'list', 'patch', 'update', 'watch']],
+  ])('should display the verbs %p', (verbs: string[]) => {
+    const wrapper = mount(RoleDetailEdit, {
+      props: {
+        value: {
+          rules: [{verbs}],
+          subtype: 'GLOBAL'
+        },
+      },
+
+      global: {
+        mocks: {
+          $fetchState: { pending: false },
+          $route:      { name: 'anything' },
+          $store:      {
+            dispatch: jest.fn(),
+            getters:  {
+              currentStore: () => 'store',
+              'i18n/t': jest.fn(),
+              'store/schemaFor': jest.fn(),
+              'store/customisation/': jest.fn()
+            }
+          }
+        },
+
+        stubs: {
+          CruResource: { template: '<div><slot></slot></div>' },
+          Tab:         { template: '<div><slot></slot></div>' },
+        },
+      },
+    });
+
+    expect(wrapper.vm.value.rules[0].verbs).toStrictEqual(verbs);
   });
 });
