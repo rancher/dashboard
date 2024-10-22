099cd87cf6914398314763ed090c6369e6db06ee
cnotv <giuseppe.leo@suse.com>
2024-10-22 19:01:16 +0200

    Convert first component to SFC/MDX new model

---
+++ styleguide/src/stories/Form/Input.stories.ts
@@ -0,0 +1,94 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import LabeledInput from '@/pkg/rancher-components/src/components/Form/LabeledInput/LabeledInput.vue';
+
+const meta: Meta<typeof LabeledInput> = {
+  component: LabeledInput,
+};
+ 
+export default meta;
+type Story = StoryObj<typeof LabeledInput>;
+
+export const Default: Story = {
+  render: (args: any) => ({
+    components: { LabeledInput },
+    setup() {
+      return { args };
+    },
+    template: '<LabeledInput v-bind="args" />',
+  }),
+  args: {
+    label: 'Name',
+    type: 'text',
+    value: 'Simon',
+  },
+};
+
+export const Info: Story = {
+  render: (args: any) => ({
+    components: { LabeledInput },
+    setup() {
+      return { args };
+    },
+    template: '<LabeledInput v-bind="args" />',
+  }),
+  args: {
+    label: 'Name',
+    type: 'text',
+    value: 'Simon',
+    status: 'info',
+    tooltipKey: 'Info message'
+  },
+};
+
+export const Success: Story = {
+  render: (args: any) => ({
+    components: { LabeledInput },
+    setup() {
+      return { args };
+    },
+    template: '<LabeledInput v-bind="args" />',
+  }),
+  args: {
+    label: 'Name',
+    type: 'text',
+    value: 'Simon',
+    status: 'success',
+    tooltipKey: 'Success message'
+  },
+};
+
+export const Warning: Story = {
+  render: (args: any) => ({
+    components: { LabeledInput },
+    setup() {
+      return { args };
+    },
+    template: '<LabeledInput v-bind="args" />',
+  }),
+  args: {
+    label: 'Name',
+    type: 'text',
+    value: 'Simon',
+    status: 'warning',
+    tooltipKey: 'Warning message'
+  },
+};
+
+
+export const Error: Story = {
+  render: (args: any) => ({
+    components: { LabeledInput },
+    setup() {
+      return { args };
+    },
+    template: '<LabeledInput v-bind="args" />',
+  }),
+  args: {
+    label: 'Name',
+    type: 'text',
+    value: 'Simon',
+    status: 'error',
+    tooltipKey: 'Error message'
+  },
+};
+
