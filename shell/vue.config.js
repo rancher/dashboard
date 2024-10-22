9815e3c07632e46a846168110c6a63b1c4485201
cnotv <giuseppe.leo@suse.com>
2024-10-22 13:46:55 +0200

    Correct issue in importing a node style within the SCSS instead of vue config

--- shell/vue.config.js
+++ shell/vue.config.js
@@ -516,6 +516,7 @@ module.exports = function(dir, _appConfig) {
             @import "~shell/assets/styles/base/_variables.scss";
             @import "~shell/assets/styles/base/_functions.scss";
             @import "~shell/assets/styles/base/_mixins.scss";
+            @import 'node_modules/xterm/css/xterm.css';
           `
         }
       }
