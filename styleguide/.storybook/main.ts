412f7e29960000f80a71902c9d986bb89cb24f5d
cnotv <giuseppe.leo@suse.com>
2024-10-22 14:02:55 +0200

    Add existing configuration for Webpack

--- styleguide/.storybook/main.ts
+++ styleguide/.storybook/main.ts
@@ -1,6 +1,10 @@
 import type { StorybookConfig } from "@storybook/vue3-webpack5";
 import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+import fs from "fs";
 import path, { join, dirname } from "path";
+import webpack from "webpack";
+
+const baseFolder = path.resolve(__dirname, '..', '..');
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -9,6 +13,70 @@ import path, { join, dirname } from "path";
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
+const getSassLoader = () => ({
+    loader: 'sass-loader',
+    options: {
+      additionalData: `@use "sass:math"; @import '~shell/assets/styles/app.scss';`,
+      sassOptions: {
+        importer: (url, prev, done) => {
+          if (url.indexOf('~/') === 0) {
+            const file = path.resolve(baseFolder, url.substr(2));
+            return fs.exists(file, (ok) => {
+              if (ok) {
+                return done({ file });
+              }
+              return done(null);
+            });
+          } else if (url.indexOf('@/node_modules') === 0) {
+            return done({ file: path.resolve(baseFolder, url.substr(2)) });
+          }
+
+          done(null);
+        }
+      }
+    },
+});
+
+/**
+ * Replace js-modal and xterm imports with absolute paths
+ * @param resource 
+ */
+const replaceModulePath = (resource) => {
+  const modulePathSplit = resource.request.split('!');
+  const modulePath = modulePathSplit.pop();
+  if (modulePath) {
+    const match = modulePath.match(/node_modules\/(.*)/);
+    if (match) {
+      modulePathSplit.push(path.join(baseFolder, match[0]));
+    } else {
+      modulePathSplit.push(modulePath);
+    }
+
+    resource.request = modulePathSplit.join('!');
+  }
+}
+
+/**
+ * Add aliases 
+ * https://storybook.js.org/docs/writing-stories/mocking-data-and-modules/mocking-modules#builder-aliases
+ * @param config 
+ */
+const setAliases = (config: webpack.Configuration) => {
+  if (config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      '~': baseFolder,
+      '@': baseFolder,
+      '@shell': path.join(baseFolder, 'shell'),
+      '@components': path.join(baseFolder, 'pkg', 'rancher-components', 'src', 'components'),
+      '~shell': path.join(baseFolder, 'shell'),
+    };
+  };
+
+  return config;
+}
+
 const config: StorybookConfig = {
   framework: {
     name: getAbsolutePath('@storybook/vue3-webpack5'),
@@ -32,22 +100,18 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true
   },
-  webpackFinal: async (config, { configType }) => {
-    if (config.resolve) {
-      const baseFolder = path.resolve(__dirname, '..', '..');
+  webpackFinal: async (config) => {
+    config = setAliases(config);
+    config.module?.rules?.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', getSassLoader()],
+      include: baseFolder,
+    });
 
-      // Add aliases https://storybook.js.org/docs/writing-stories/mocking-data-and-modules/mocking-modules#builder-aliases
-      config.resolve.alias = {
-        ...config.resolve?.alias,
-        '~': baseFolder,
-        '@': baseFolder,
-        '@shell': path.join(baseFolder, 'shell'),
-        '@components': path.join(baseFolder, 'pkg', 'rancher-components', 'src', 'components'),
-        '~shell': path.join(baseFolder, 'shell'),
-      };
-
+    if (config.plugins) {
       // BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default.
-      config.plugins?.push(new NodePolyfillPlugin() as any);
+      config.plugins.push(new NodePolyfillPlugin() as any);
+      config.plugins.push(new webpack.NormalModuleReplacementPlugin(/js-modal|xterm|diff2html/, replaceModulePath));
     }
  
     return config
