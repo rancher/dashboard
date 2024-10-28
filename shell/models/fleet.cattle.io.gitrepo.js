965833d12d1fa27bdb1f074f038542bdfea77718
Richard Cox <richard.cox@suse.com>
2024-10-28 16:19:56 +0000

    Update following review - avoid 0 showing as `?` - e.g. `0 || ?`

--- shell/models/fleet.cattle.io.gitrepo.js
+++ shell/models/fleet.cattle.io.gitrepo.js
@@ -307,12 +307,11 @@ export default class GitRepo extends SteveModel {
       bundle.namespacedName.startsWith(`${ this.namespace }:${ this.name }`));
   }
 
+  /**
+   * Bundles with state of active
+   */
   get bundlesReady() {
-    if (this.bundles && this.bundles.length) {
-      return this.bundles.filter((bundle) => bundle.state === 'active');
-    }
-
-    return 0;
+    return this.bundles?.filter((bundle) => bundle.state === 'active');
   }
 
   get bundleDeployments() {
