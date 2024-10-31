044900539274116bde3458553549fadd4a1daabb
Alejandro Ruiz <alejandro.ruiz@suse.com>
2024-10-31 13:52:35 +0100

    Avoid using spec for getting resources in Bundle details page

--- shell/detail/fleet.cattle.io.bundle.vue
+++ shell/detail/fleet.cattle.io.bundle.vue
@@ -1,5 +1,4 @@
 <script>
-import { FLEET } from '@shell/config/types';
 import FleetBundleResources from '@shell/components/fleet/FleetBundleResources.vue';
 import SortableTable from '@shell/components/SortableTable';
 
@@ -17,61 +16,13 @@ export default {
     }
   },
 
-  data() {
-    return { repo: null };
-  },
-
-  async fetch() {
-    const { namespace, labels } = this.value.metadata;
-    const repoName = `${ namespace }/${ labels['fleet.cattle.io/repo-name'] }`;
-
-    if (this.hasRepoLabel) {
-      this.repo = await this.$store.dispatch('management/find', { type: FLEET.GIT_REPO, id: repoName });
-    }
-  },
-
   computed: {
-    hasRepoLabel() {
-      return !!(this.value?.metadata?.labels && this.value?.metadata?.labels['fleet.cattle.io/repo-name']);
-    },
     bundleResources() {
-      if (this.hasRepoLabel) {
-        const bundleResourceIds = this.bundleResourceIds;
-
-        return this.repo?.status?.resources?.filter((resource) => {
-          return bundleResourceIds.includes(resource.name);
-        });
-      } else if (this.value?.spec?.resources?.length) {
-        return this.value?.spec?.resources.map((item) => {
-          return {
-            content: item.content,
-            name:    item.name.includes('.') ? item.name.split('.')[0] : item.name
-          };
-        });
-      }
-
-      return [];
-    },
-    resourceHeaders() {
-      return [
-        {
-          name:     'name',
-          value:    'name',
-          sort:     ['name'],
-          labelKey: 'tableHeaders.name',
-        },
-      ];
+      return this.value?.status?.resourceKey || [];
     },
     resourceCount() {
-      return (this.bundleResources && this.bundleResources.length) || this.value?.spec?.resources?.length;
+      return this.value.status?.summary?.desiredReady || 0;
     },
-    bundleResourceIds() {
-      if (this.value.status?.resourceKey) {
-        return this.value?.status?.resourceKey.map((item) => item.name);
-      }
-
-      return [];
-    }
   }
 };
 
@@ -84,18 +35,7 @@ export default {
       <span>{{ resourceCount }}</span>
     </div>
     <FleetBundleResources
-      v-if="hasRepoLabel"
       :value="bundleResources"
-    />
-    <SortableTable
-      v-else
-      :rows="bundleResources"
-      :headers="resourceHeaders"
-      :table-actions="false"
-      :row-actions="false"
-      key-field="tableKey"
-      default-sort-by="state"
-      :paged="true"
     />
   </div>
 </template>
