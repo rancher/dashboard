cea45fa3d7663b270f367d8f3d3a1ba6d26e17a5
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2024-10-31 07:22:24 -0700

    fix aftereach hook in explorer apps test

--- cypress/e2e/tests/pages/explorer/apps/repositories.spec.ts
+++ cypress/e2e/tests/pages/explorer/apps/repositories.spec.ts
@@ -56,7 +56,7 @@ describe('Apps', () => {
 
         // Ensure this runs after an attempt, rather than all attemps (`after` only runs once after all cypress retries)
         afterEach(() => {
-          reposToDelete.forEach((r) => cy.deleteRancherResource('v1', 'catalog.cattle.io.clusterrepos', r));
+          reposToDelete.forEach((r) => cy.deleteRancherResource('v1', 'catalog.cattle.io.clusterrepos', r, false));
         });
       });
 
