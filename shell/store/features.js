1d759c363c4c378f239d1c8e3fb1a9f6bc0a4790
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2024-11-07 10:22:04 -0800

    refactor getting feature flag + check provider before running the logic

--- shell/store/features.js
+++ shell/store/features.js
@@ -35,6 +35,7 @@ export const HARVESTER_CONTAINER = create('harvester-baremetal-container-workloa
 export const FLEET_WORKSPACE_BACK = create('provisioningv2-fleet-workspace-back-population', false);
 export const STEVE_CACHE = create('ui-sql-cache', false);
 export const UIEXTENSION = create('uiextension', true);
+export const PROVISIONING_PRE_BOOTSTRAP = create('provisioningprebootstrap', false);
 
 // Not currently used.. no point defining ones we don't use
 // export const EMBEDDED_CLUSTER_API = create('embedded-cluster-api', true);
