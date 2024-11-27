87dfdb7d251c94b45f92b0f32c3f4992c61be5d2
Alexandre Alves <alexandre.alves@suse.com>
2024-11-27 11:57:13 +0000

    fix e2e test following removal of restricted admin role

--- cypress/e2e/tests/pages/users-and-auth/users.spec.ts
+++ cypress/e2e/tests/pages/users-and-auth/users.spec.ts
@@ -41,21 +41,6 @@ describe('Users', { tags: ['@usersAndAuths', '@adminUser'] }, () => {
     userCreate.saveAndWaitForRequests('POST', '/v3/globalrolebindings');
   });
 
-  it('can create Restricted Admin', () => {
-    const restrictedAdminUsername = `${ runPrefix }-restrictedAdmin-user`;
-    const restrictedAdminPassword = 'restrictedAdmin-password';
-
-    usersPo.goTo();
-    usersPo.list().create();
-
-    userCreate.waitForPage();
-    userCreate.username().set(restrictedAdminUsername);
-    userCreate.newPass().set(restrictedAdminPassword);
-    userCreate.confirmNewPass().set(restrictedAdminPassword);
-    userCreate.selectCheckbox('Restricted Administrator').set();
-    userCreate.saveAndWaitForRequests('POST', '/v3/globalrolebindings');
-  });
-
   it('can create User-Base', () => {
     const userBasePassword = 'userBase-password';
 
@@ -122,9 +107,8 @@ describe('Users', { tags: ['@usersAndAuths', '@adminUser'] }, () => {
     const mgmtUserEditPo = new MgmtUserEditPo();
 
     mgmtUserEditPo.globalRoleBindings().globalOptions().then((list) => {
-      expect(list.length).to.eq(4);
+      expect(list.length).to.eq(3);
       expect(list[0]).to.eq('Administrator');
-      expect(list[1]).to.eq('Restricted Administrator');
       expect(list[2]).to.eq('Standard User');
       expect(list[3]).to.eq('User-Base');
     });
