c13d76251f3cfb4037667b2e07dc0b68759d20c5
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2024-12-01 11:21:26 -0800

    update e2e tests

--- cypress/e2e/tests/pages/global-settings/index.spec.ts
+++ cypress/e2e/tests/pages/global-settings/index.spec.ts
@@ -1,6 +1,6 @@
 import PagePo from '@/cypress/e2e/po/pages/page.po';
 
-describe('Global Settings Index', { testIsolation: 'off', tags: ['@explorer', '@adminUser'] }, () => {
+describe('Global Settings Index', { testIsolation: 'off', tags: ['@globalSettings', '@adminUser'] }, () => {
   before(() => {
     cy.login();
   });
