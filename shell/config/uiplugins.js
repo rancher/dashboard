ee7e883c52b630b643a6f52b3714b7542e324963
Masashi Honma <masashi.honma@gmail.com>
2024-12-02 14:05:26 +0900

    Bump UI_PLUGIN_API_VERSION
    Resolves #12724.
    
    According to Rancher Extension Support Matrix
    (https://extensions.rancher.io/extensions/next/support-matrix) we should use
    1.2.3 for Rancher 2.8.
    
    Indeed when I used trailing annotation, install button of the app was not shown.
    
    "catalog.cattle.io/ui-extensions-version": ">= 1.2.3 < 2.0.0"
    
    Signed-off-by: Masashi Honma <masashi.honma@gmail.com>

--- shell/config/uiplugins.js
+++ shell/config/uiplugins.js
@@ -1,7 +1,7 @@
 import semver from 'semver';
 
 // Version of the plugin API supported
-export const UI_PLUGIN_API_VERSION = '1.2.0';
+export const UI_PLUGIN_API_VERSION = '1.2.3';
 export const UI_PLUGIN_HOST_APP = 'rancher-manager';
 
 export const UI_PLUGIN_BASE_URL = '/api/v1/namespaces/cattle-ui-plugin-system/services/http:ui-plugin-operator:80/proxy';
