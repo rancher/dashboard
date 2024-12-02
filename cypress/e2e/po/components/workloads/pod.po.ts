37e39454f555bf361bcff901f76806b39c76c549
Izaac Zavaleta <izaac.zavaleta@suse.com>
2024-12-01 22:26:56 -0700

    Check default limit values in UI while creating a Pod with the Form

--- cypress/e2e/po/components/workloads/pod.po.ts
+++ cypress/e2e/po/components/workloads/pod.po.ts
@@ -2,10 +2,16 @@ import CreateEditViewPo from '@/cypress/e2e/po/components/create-edit-view.po';
 import Kubectl from '@/cypress/e2e/po/components/kubectl.po';
 import Shell from '@/cypress/e2e/po/components/shell.po';
 import NameNsDescription from '@/cypress/e2e/po/components/name-ns-description.po';
+import { PodContainer } from '@/cypress/e2e/po/components/workloads/pods/pod-container.po';
+import ResourceDetailPo from '~/cypress/e2e/po/edit/resource-detail.po';
 
 export default class PodPo extends CreateEditViewPo {
   constructor(selector = '.dashboard-root') {
     super(selector);
+  }
+
+  resourceTitle() {
+    return this.self().find('masthead-resource-title');
   }
 
   nameNsDescription() {
@@ -25,5 +31,13 @@ export default class PodPo extends CreateEditViewPo {
     const shell = new Shell();
 
     shell.openTerminal();
+  }
+
+  containerButton() {
+    return new PodContainer();
+  }
+
+  saveCreateForm(): ResourceDetailPo {
+    return new ResourceDetailPo(this.self());
   }
 }
