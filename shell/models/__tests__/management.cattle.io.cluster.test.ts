e966f719f1240bbf3bf96225db5bc549f6af25fa
Nancy <42977925+mantis-toboggan-md@users.noreply.github.com>
2024-12-03 11:56:35 -0800

    [2.10.1] Fix editing rke2/k3s clusters in 'pending' state (#12736)
    * fix cluster isImported check to account for clusters in the process of provisioning
    
    * remove tests

--- shell/models/__tests__/management.cattle.io.cluster.test.ts
+++ shell/models/__tests__/management.cattle.io.cluster.test.ts
@@ -4,28 +4,6 @@ jest.mock('@shell/utils/clipboard', () => {
   return { copyTextToClipboard: jest.fn(() => Promise.resolve({})) };
 });
 
-const importedRKE2ClusterInfo = { status: { driver: 'rke2', provider: 'rke2' } };
-
-const provisionedRKE2ClusterInfo = { status: { driver: 'rke2', provider: 'imported' } };
-
-const importedK3sClusterInfo = { status: { driver: 'k3s', provider: 'k3s' } };
-
-const provisionedK3sClusterInfo = { status: { driver: 'k3s', provider: 'imported' } };
-
-const importedAksClusterInfo = { spec: { aksConfig: { imported: true } }, status: { provider: 'aks', driver: 'AKS' } };
-
-const provisionedAksClusterInfo = { spec: { aksConfig: { imported: false } }, status: { provider: 'aks', driver: 'AKS' } };
-
-const importedRKE1ClusterInfo = { status: { provider: 'rke', driver: 'imported' } };
-
-const provisionedRKE1ClusterInfo = { status: { provider: 'rke', driver: 'rancherKubernetesEngine' } };
-
-const localRKE1ClusterInfo = { status: { provider: 'rke', driver: 'imported' } };
-
-const localRKE2ClusterInfo = { status: { provider: 'rke2', driver: 'rke2' } };
-
-const localEKSClusterInfo = { status: { provider: 'eks', driver: 'imported' } };
-
 describe('class MgmtCluster', () => {
   describe('provisioner', () => {
     const testCases = [
@@ -41,25 +19,5 @@ describe('class MgmtCluster', () => {
       expect(cluster.provisioner).toBe(expected);
     }
     );
-  });
-
-  describe('isImported', () => {
-    it.each([
-      [importedRKE2ClusterInfo, true],
-      [provisionedRKE2ClusterInfo, false],
-      [importedK3sClusterInfo, true],
-      [provisionedK3sClusterInfo, false],
-      [importedAksClusterInfo, true],
-      [provisionedAksClusterInfo, false],
-      [importedRKE1ClusterInfo, true],
-      [provisionedRKE1ClusterInfo, false],
-      [localRKE1ClusterInfo, true],
-      [localRKE2ClusterInfo, true],
-      [localEKSClusterInfo, true]
-    ])('should return isImported based on props data', (clusterData, expected) => {
-      const cluster = new MgmtCluster(clusterData);
-
-      expect(cluster.isImported).toBe(expected);
-    });
   });
 });
