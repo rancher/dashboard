947a0788c3cd464000edb6ff8985af95aab8a08b
cnotv <giuseppe.leo@suse.com>
2024-12-18 18:25:26 +0100

    Rename automated test for convenience

--- cypress/visual/automated.visual.ts
+++ cypress/visual/automated.visual.ts
@@ -7,12 +7,16 @@
 
 import storybook from "@/storybook/storybook-static/index.json";
 
+const exceptions = ['IconIsDefault']; // Stories to be excluded
+
 // Retrieve all stories from the storybook build json, excluding docs and examples
 const stories = Object.values(storybook.entries).filter(
-  (entry) => entry.type === "story" && !entry.title.includes('Example'),
+  (entry) => entry.type === "story"
+    && !entry.title.includes('Example')
+    && exceptions.some(exception => !entry.title.includes(exception))
 );
 
-const filteredStories = stories.filter((story) => story.id.includes('stories-form-checkbox--default'));
+const filteredStories = stories;
 filteredStories.forEach((story) => {
   describe(`${story.title} ${story.name}`, () => {
     it('should render correctly and have no regressions', () => {
@@ -20,7 +24,7 @@ filteredStories.forEach((story) => {
       console.log(URL);
       // mount(<Checkbox />);
       cy.visit(URL)
-      cy.get('#storybook-root').should('not.be.empty').toMatchImageSnapshot();
+      cy.get('#storybook-root').should('not.be.empty').matchImageSnapshot();
     });
   });
 });
