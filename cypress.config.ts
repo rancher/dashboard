0c058680d4ee09bc92366d10dc6a512f3b08842e
Neil MacDougall <nmacdougall@suse.com>
2024-12-18 08:54:14 +0000

    Fix lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -102,6 +102,7 @@ export default defineConfig({
     azureClientSecret:   process.env.AZURE_CLIENT_SECRET,
     customNodeIp:        process.env.CUSTOM_NODE_IP,
     customNodeKey:       process.env.CUSTOM_NODE_KEY,
+    accessibility:       !!process.env.TEST_A11Y, // Are we running accessibility tests?
     a11yFolder:          path.join('.', 'cypress', 'accessibility'),
     gkeServiceAccount:   process.env.GKE_SERVICE_ACCOUNT,
   },
