f683558a2df2280d4fc0f925fd2ae1b9dd585842
cnotv <giuseppe.leo@suse.com>
2024-12-18 12:02:21 +0100

    Create loop for Storybook stories to be tested, allowing filtering

--- cypress/visual/Checkbox.visual.ts
+++ cypress/visual/Checkbox.visual.ts
@@ -2,16 +2,25 @@
 // import {composeStories} from '@storybook/testing-vue';
 // import {mount} from '@cypress/vue';
 // import * as stories from '../../storybook/src/stories/form/Checkbox.mdx';
-
 // compile the "Simple" story with the library
 // const { Checkbox } = composeStories(stories);
 
-describe('Checkbox component', () => {
-  it('should render correctly', () => {
-    // mount(<Checkbox />);
-    cy.visit('/iframe.html?id=stories-form-checkbox--default')
-    const container = cy.get('#storybook-root')
-    container.should('not.be.empty');
-    cy.get('#storybook-root').toMatchImageSnapshot();
+import storybook from "@/storybook/storybook-static/index.json";
+
+// Retrieve all stories from the storybook build json, excluding docs and examples
+const stories = Object.values(storybook.entries).filter(
+  (entry) => entry.type === "story" && !entry.title.includes('Example'),
+);
+
+const filteredStories = stories.filter((story) => story.id.includes('stories-form-checkbox--default'));
+filteredStories.forEach((story) => {
+  describe(`${story.title} ${story.name}`, () => {
+    it('should render correctly and have no regressions', () => {
+      const URL = `/iframe.html?id=${story.id}`
+      console.log(URL);
+      // mount(<Checkbox />);
+      cy.visit(URL)
+      cy.get('#storybook-root').should('not.be.empty').toMatchImageSnapshot();
+    });
   });
 });
