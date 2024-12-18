15c049e55eb4b07efc6d6a2791d316ddc80e8144
Neil MacDougall <nmacdougall@suse.com>
2024-12-18 09:30:25 +0000

    Tweak tests

--- cypress/e2e/tests/accessibility/login.spec.ts
+++ cypress/e2e/tests/accessibility/login.spec.ts
@@ -7,22 +7,19 @@ describe('Login page a11y testing', { tags: ['@adminUser', '@accessibility'] }, 
     loginPage.goTo();
     loginPage.waitForPage();
     cy.injectAxe();
-
-    cy.checkPageAccessibility();
-
-    loginPage.username().set('test');
-    loginPage.submit();
+    loginPage.username().set('test user');
 
     cy.checkPageAccessibility();
   });
 
-  // it('locale selector', () => {
-  //   loginPage.goTo();
-  //   loginPage.waitForPage();
+  it('locale selector', () => {
+    const loginPage = new LoginPagePo();
 
-  //   cy.injectAxe();
-  //   cy.get('[data-testid="locale-selector"]').click();
-  //   cy.checkPageAccessibility();
-  //   cy.checkElementAccessibility('#username', 'Username field checks');
-  // });
+    loginPage.goTo();
+    loginPage.waitForPage();
+    cy.injectAxe();
+    cy.get('[data-testid="locale-selector"]').click();
+    cy.checkPageAccessibility();
+    cy.checkElementAccessibility('#username', 'Username field checks');
+  });
 });
