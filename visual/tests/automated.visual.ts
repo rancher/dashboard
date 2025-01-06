38ac05fa38e4cc703f3bbbe1c44aec90a0fa22fd
cnotv <giuseppe.leo@suse.com>
2025-01-06 18:07:09 +0100

    Remove unnecessary code from the test

--- visual/tests/automated.visual.ts
+++ visual/tests/automated.visual.ts
@@ -1,10 +1,3 @@
-// import { expect } from '@storybook/test';
-// import {composeStories} from '@storybook/testing-vue';
-// import {mount} from '@cypress/vue';
-// import * as stories from '../../storybook/src/stories/form/Checkbox.mdx';
-// compile the "Simple" story with the library
-// const { Checkbox } = composeStories(stories);
-
 import storybook from "@/storybook/storybook-static/index.json";
 
 const exceptions = ['IconIsDefault', 'Table']; // Stories to be excluded
@@ -21,8 +14,6 @@ filteredStories.forEach((story) => {
   describe(`${story.title} ${story.name}`, () => {
     it('should render correctly and have no regressions', () => {
       const URL = `/iframe.html?id=${story.id}`
-      console.log(URL);
-      // mount(<Checkbox />);
       cy.visit(URL)
       cy.get('#storybook-root').should('not.be.empty').matchImageSnapshot();
     });
