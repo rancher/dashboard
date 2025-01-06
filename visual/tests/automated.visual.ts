b8c485bae1452076edab87b8972fbe5f3cb1bd8e
cnotv <giuseppe.leo@suse.com>
2025-01-06 17:06:31 +0100

    Exclude Tables due rendering issue; Correct logic not excluding all the cases

--- visual/tests/automated.visual.ts
+++ visual/tests/automated.visual.ts
@@ -7,13 +7,13 @@
 
 import storybook from "@/storybook/storybook-static/index.json";
 
-const exceptions = ['IconIsDefault']; // Stories to be excluded
+const exceptions = ['IconIsDefault', 'Table']; // Stories to be excluded
 
 // Retrieve all stories from the storybook build json, excluding docs and examples
 const stories = Object.values(storybook.entries).filter(
   (entry) => entry.type === "story"
     && !entry.title.includes('Example')
-    && exceptions.some(exception => !entry.title.includes(exception))
+    && exceptions.every(exception => !entry.title.includes(exception))
 );
 
 const filteredStories = stories;
