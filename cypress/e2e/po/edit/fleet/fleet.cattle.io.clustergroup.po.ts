52a56e7460401bda294dd36a8f549dbd8ec69993
Yonas Berhe <yberhe@Yonass-MacBook-Pro.local>
2025-01-08 11:00:48 -0800

    automation: fleet cluster groups tests

--- cypress/e2e/po/edit/fleet/fleet.cattle.io.clustergroup.po.ts
+++ cypress/e2e/po/edit/fleet/fleet.cattle.io.clustergroup.po.ts
@@ -1,27 +1,35 @@
 import PagePo from '@/cypress/e2e/po/pages/page.po';
 import AsyncButtonPo from '@/cypress/e2e/po/components/async-button.po';
-import CodeMirrorPo from '@/cypress/e2e/po/components/code-mirror.po';
-
+import ResourceDetailPo from '@/cypress/e2e/po/edit/resource-detail.po';
+import NameNsDescription from '@/cypress/e2e/po/components/name-ns-description.po';
 export default class FleetClusterGroupsCreateEditPo extends PagePo {
-  private static createPath(clusterId: string, id?: string ) {
-    const root = `/c/${ clusterId }/explorer/storage.k8s.io.storageclass/create`;
+  private static createPath(clusterId: string, workspace?: string, id?: string ) {
+    const root = `/c/${ clusterId }/fleet/fleet.cattle.io.clustergroup`;
 
-    return id ? `${ root }/${ id }` : `${ root }/create`;
+    return id ? `${ root }/${ workspace }/${ id }` : `${ root }/create`;
   }
 
   static goTo(path: string): Cypress.Chainable<Cypress.AUTWindow> {
     throw new Error('invalid');
   }
 
-  constructor(clusterId = '_', id?: string) {
-    super(FleetClusterGroupsCreateEditPo.createPath(clusterId, id));
+  constructor(clusterId = '_', workspace?: string, id?: string) {
+    super(FleetClusterGroupsCreateEditPo.createPath(clusterId, workspace, id));
   }
 
-  editAsYaml() {
-    return new AsyncButtonPo('[data-testid="form-yaml"]', this.self());
+  title() {
+    return this.self().get('.title .primaryheader  h1');
   }
 
-  yamlEditor(): CodeMirrorPo {
-    return CodeMirrorPo.bySelector(this.self(), '[data-testid="yaml-editor-code-mirror"]');
+  nameNsDescription() {
+    return new NameNsDescription(this.self());
+  }
+
+  saveCreateForm(): ResourceDetailPo {
+    return new ResourceDetailPo(this.self());
+  }
+
+  saveButton() {
+    return new AsyncButtonPo('[data-testid="form-save"]', this.self());
   }
 }
--- cypress/e2e/tests/pages/fleet/cluster-groups.spec.ts
+++ cypress/e2e/tests/pages/fleet/cluster-groups.spec.ts
@@ -1,63 +1,147 @@
 import { FleetClusterGroupsListPagePo } from '@/cypress/e2e/po/pages/fleet/fleet.cattle.io.clustergroup.po';
 import FleetClusterGroupDetailsPo from '@/cypress/e2e/po/detail/fleet/fleet.cattle.io.clustergroup.po';
 import { HeaderPo } from '@/cypress/e2e/po/components/header.po';
+import PromptRemove from '@/cypress/e2e/po/prompts/promptRemove.po';
 
 describe('Cluster Groups', { testIsolation: 'off', tags: ['@fleet', '@adminUser'] }, () => {
   const fleetClusterGroups = new FleetClusterGroupsListPagePo();
   const headerPo = new HeaderPo();
+  const localWorkspace = 'fleet-local';
+  let clusterGroupName;
+  let removeClusterGroups = false;
+  const clusterGroupsToDelete = [];
 
-  describe('List', { tags: ['@vai', '@adminUser'] }, () => {
-    before(() => {
-      cy.login();
-    });
-
-    it('check table headers are available in list and details view', () => {
-      const groupName = 'default';
-      const workspace = 'fleet-local';
-
-      FleetClusterGroupsListPagePo.navTo();
-      fleetClusterGroups.waitForPage();
-      headerPo.selectWorkspace(workspace);
-      fleetClusterGroups.clusterGroupsList().rowWithName(groupName).checkVisible();
-
-      // check table headers
-      const expectedHeaders = ['State', 'Name', 'Clusters Ready', 'Resources', 'Age'];
-
-      fleetClusterGroups.clusterGroupsList().resourceTable().sortableTable().tableHeaderRow()
-        .within('.table-header-container .content')
-        .each((el, i) => {
-          expect(el.text().trim()).to.eq(expectedHeaders[i]);
-        });
-
-      // go to fleet cluster details
-      fleetClusterGroups.goToDetailsPage(groupName);
-
-      const fleetClusterGroupDetailsPage = new FleetClusterGroupDetailsPo(workspace, groupName);
-
-      fleetClusterGroupDetailsPage.waitForPage(null, 'clusters');
-
-      // check table headers
-      const expectedHeadersDetailsView = ['State', 'Name', 'Bundles Ready', 'Repos Ready', 'Resources', 'Last Seen', 'Age'];
-
-      fleetClusterGroupDetailsPage.clusterList().resourceTable().sortableTable()
-        .tableHeaderRow()
-        .within('.table-header-container .content')
-        .each((el, i) => {
-          expect(el.text().trim()).to.eq(expectedHeadersDetailsView[i]);
-        });
+  before(() => {
+    cy.login();
+    cy.createE2EResourceName('cluster-group').then((name) => {
+      clusterGroupName = name;
     });
   });
-  describe('Edit', { tags: ['@vai', '@adminUser'] }, () => {
-    before(() => {
-      cy.login();
-    });
 
-    it('can open "Edit as YAML"', () => {
-      FleetClusterGroupsListPagePo.navTo();
-      fleetClusterGroups.waitForPage();
-      fleetClusterGroups.clickCreate();
-      fleetClusterGroups.createFleetClusterGroupsForm().editAsYaml().click();
-      fleetClusterGroups.createFleetClusterGroupsForm().yamlEditor().checkExists();
-    });
+  it('can create cluster group', () => {
+    FleetClusterGroupsListPagePo.navTo();
+    fleetClusterGroups.waitForPage();
+    headerPo.selectWorkspace(localWorkspace);
+    fleetClusterGroups.clickCreate();
+    fleetClusterGroups.createFleetClusterGroupsForm().waitForPage();
+
+    fleetClusterGroups.createFleetClusterGroupsForm().nameNsDescription().name().set(clusterGroupName);
+    fleetClusterGroups.createFleetClusterGroupsForm().saveCreateForm().cruResource().saveOrCreate()
+      .click()
+      .then(() => {
+        removeClusterGroups = true;
+        clusterGroupsToDelete.push(`${ localWorkspace }/${ clusterGroupName }`);
+      });
+
+    fleetClusterGroups.waitForPage();
+    fleetClusterGroups.clusterGroupsList().details(clusterGroupName, 1).should('be.visible');
+  });
+
+  it('can edit a cluster group', () => {
+    FleetClusterGroupsListPagePo.navTo();
+    fleetClusterGroups.waitForPage();
+    headerPo.selectWorkspace(localWorkspace);
+    fleetClusterGroups.clusterGroupsList().actionMenu(clusterGroupName).getMenuItem('Edit Config').click();
+    fleetClusterGroups.createFleetClusterGroupsForm(localWorkspace, clusterGroupName).waitForPage('mode=edit');
+    fleetClusterGroups.createFleetClusterGroupsForm().nameNsDescription().description().set(`${ clusterGroupName }-fleet-desc`);
+    fleetClusterGroups.createFleetClusterGroupsForm().saveCreateForm().cruResource().saveAndWaitForRequests('PUT', `v1/fleet.cattle.io.clustergroups/${ localWorkspace }/${ clusterGroupName }`)
+      .then(({ response }) => {
+        expect(response?.statusCode).to.eq(200);
+        expect(response?.body.metadata).to.have.property('name', clusterGroupName);
+        expect(response?.body.metadata.annotations).to.have.property('field.cattle.io/description', `${ clusterGroupName }-fleet-desc`);
+      });
+    fleetClusterGroups.waitForPage();
+  });
+
+  it('can clone a cluster group', () => {
+    FleetClusterGroupsListPagePo.navTo();
+    fleetClusterGroups.waitForPage();
+    headerPo.selectWorkspace(localWorkspace);
+    fleetClusterGroups.clusterGroupsList().actionMenu(clusterGroupName).getMenuItem('Clone').click();
+    fleetClusterGroups.createFleetClusterGroupsForm(localWorkspace, clusterGroupName).waitForPage('mode=clone');
+    fleetClusterGroups.createFleetClusterGroupsForm().nameNsDescription().name().set(`clone-${ clusterGroupName }`);
+    fleetClusterGroups.createFleetClusterGroupsForm().nameNsDescription().description().set(`${ clusterGroupName }-fleet-desc`);
+    fleetClusterGroups.createFleetClusterGroupsForm().saveCreateForm().cruResource().saveAndWaitForRequests('POST', 'v1/fleet.cattle.io.clustergroups')
+      .then(({ response }) => {
+        expect(response?.statusCode).to.eq(201);
+        removeClusterGroups = true;
+        clusterGroupsToDelete.push(`${ localWorkspace }/clone-${ clusterGroupName }`);
+        expect(response?.body.metadata).to.have.property('name', `clone-${ clusterGroupName }`);
+        expect(response?.body.metadata.annotations).to.have.property('field.cattle.io/description', `${ clusterGroupName }-fleet-desc`);
+      });
+    fleetClusterGroups.waitForPage();
+    fleetClusterGroups.clusterGroupsList().details(`clone-${ clusterGroupName }`, 1).should('be.visible');
+  });
+
+  it('can delete cluster group', () => {
+    FleetClusterGroupsListPagePo.navTo();
+    fleetClusterGroups.waitForPage();
+    headerPo.selectWorkspace(localWorkspace);
+    fleetClusterGroups.clusterGroupsList().actionMenu(clusterGroupName).getMenuItem('Delete').click();
+    fleetClusterGroups.clusterGroupsList().resourceTable().sortableTable().rowNames('.col-link-detail')
+      .then((rows: any) => {
+        const promptRemove = new PromptRemove();
+
+        cy.intercept('DELETE', `v1/fleet.cattle.io.clustergroups/${ localWorkspace }/clone-${ clusterGroupName }`).as('deleteClusterGroup');
+
+        promptRemove.remove();
+        cy.wait('@deleteClusterGroup');
+        fleetClusterGroups.waitForPage();
+        fleetClusterGroups.clusterGroupsList().resourceTable().sortableTable().checkRowCount(false, rows.length - 1);
+        fleetClusterGroups.clusterGroupsList().resourceTable().sortableTable().rowNames('.col-link-detail')
+          .should('not.contain', `clone-${ clusterGroupName }`);
+      });
+  });
+
+  // testing https://github.com/rancher/dashboard/issues/11687
+  it('can open "Edit as YAML"', () => {
+    FleetClusterGroupsListPagePo.navTo();
+    fleetClusterGroups.waitForPage();
+    fleetClusterGroups.clickCreate();
+    fleetClusterGroups.createFleetClusterGroupsForm().saveCreateForm().createEditView().editAsYaml();
+    fleetClusterGroups.createFleetClusterGroupsForm().saveCreateForm().resourceYaml().codeMirror()
+      .checkExists();
+  });
+
+  it('check table headers are available in list and details view', { tags: ['@vai', '@adminUser'] }, () => {
+    const groupName = 'default';
+
+    FleetClusterGroupsListPagePo.navTo();
+    fleetClusterGroups.waitForPage();
+    headerPo.selectWorkspace(localWorkspace);
+    fleetClusterGroups.clusterGroupsList().rowWithName(groupName).checkVisible();
+
+    // check table headers
+    const expectedHeaders = ['State', 'Name', 'Clusters Ready', 'Resources', 'Age'];
+
+    fleetClusterGroups.clusterGroupsList().resourceTable().sortableTable().tableHeaderRow()
+      .within('.table-header-container .content')
+      .each((el, i) => {
+        expect(el.text().trim()).to.eq(expectedHeaders[i]);
+      });
+
+    // go to fleet cluster details
+    fleetClusterGroups.goToDetailsPage(groupName);
+
+    const fleetClusterGroupDetailsPage = new FleetClusterGroupDetailsPo(localWorkspace, groupName);
+
+    fleetClusterGroupDetailsPage.waitForPage(null, 'clusters');
+
+    // check table headers
+    const expectedHeadersDetailsView = ['State', 'Name', 'Bundles Ready', 'Repos Ready', 'Resources', 'Last Seen', 'Age'];
+
+    fleetClusterGroupDetailsPage.clusterList().resourceTable().sortableTable()
+      .tableHeaderRow()
+      .within('.table-header-container .content')
+      .each((el, i) => {
+        expect(el.text().trim()).to.eq(expectedHeadersDetailsView[i]);
+      });
+  });
+
+  after(() => {
+    if (removeClusterGroups) {
+      // delete gitrepo
+      clusterGroupsToDelete.forEach((r) => cy.deleteRancherResource('v1', 'fleet.cattle.io.clustergroups', r, false));
+    }
   });
 });
