d17abad788db5bb2cdcdc813fb8a51fd31ccff26
Francesco Torchia <francesco.torchia@suse.com>
2025-01-09 18:23:35 +0100

    Handle correctDrift:enabled undefined value
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- shell/edit/fleet.cattle.io.gitrepo.vue
+++ shell/edit/fleet.cattle.io.gitrepo.vue
@@ -86,9 +86,7 @@ export default {
 
     this.tlsMode = tls;
 
-    if (this.value.spec.correctDrift === undefined) {
-      this.value.spec['correctDrift'] = { enabled: false };
-    }
+    this.correctDriftEnabled = this.value.spec?.correctDrift?.enabled || false;
 
     this.updateTargets();
   },
@@ -143,6 +141,7 @@ export default {
       privateKey:              null,
       tlsMode:                 null,
       caBundle:                null,
+      correctDriftEnabled:     false,
       targetAdvancedErrors:    null,
       matchingClusters:        null,
       ref,
@@ -496,6 +495,12 @@ export default {
         }
       }
     },
+
+    onSave() {
+      this.value.spec['correctDrift'] = { enabled: this.correctDriftEnabled };
+
+      this.save();
+    }
   }
 };
 </script>
@@ -516,7 +521,7 @@ export default {
     class="wizard"
     @cancel="done"
     @error="e=>errors = e"
-    @finish="save"
+    @finish="onSave"
   >
     <template #noticeBanner>
       <Banner
@@ -651,7 +656,7 @@ export default {
       <h2 v-t="'fleet.gitRepo.resources.label'" />
       <div class="resource-handling">
         <Checkbox
-          v-model:value="value.spec.correctDrift.enabled"
+          v-model:value="correctDriftEnabled"
           :tooltip="t('fleet.gitRepo.resources.correctDriftBanner')"
           data-testid="GitRepo-correctDrift-checkbox"
           class="check"
