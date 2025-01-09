11edbc17874c1507fc8903fbfe9d0b4d22732f5f
Phillip Rak <rak.phillip@gmail.com>
2025-01-09 13:53:35 -0700

    Move RcDropdown to Rancher Components
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/src/index.ts
+++ pkg/rancher-components/src/index.ts
@@ -2,4 +2,11 @@ export { Card } from './components/Card';
 export { BadgeState } from './components/BadgeState';
 export { Banner } from './components/Banner';
 export { StringList } from './components/StringList';
+export {
+  RcDropdown,
+  RcDropdownCollection,
+  RcDropdownItem,
+  RcDropdownSeparator,
+  RcDropdownTrigger
+} from './components/RcDropdown';
 export * from './components/Form';
