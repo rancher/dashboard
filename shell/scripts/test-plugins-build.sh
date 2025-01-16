2bb17c89237ba92e0d23a08126b8d1e1395e5087
Phillip Rak <rak.phillip@gmail.com>
2025-01-16 14:25:26 -0700

    Remove neuvector from `test-plugins-build.sh`
    Neuvector builds currently fail with the error
    
    ```
      ERROR  Failed to compile with 1 error5:49:49 PM
    
     error  in utils/common.ts:85:23
    
    TS2694: Namespace 'dayjs' has no exported member 'ManipulateType'.
        83 |   base: string,
        84 |   interval: number,
      > 85 |   intervalUnit: dayjs.ManipulateType,
           |                       ^^^^^^^^^^^^^^
        86 |   pattern = "YYYYMMDDHHmmss"
        87 | ) {
        88 |   // base format: "YYYYMMDDHHmmss"
    ```
    
    It appears that neuvector might require some updates that are unrelated to bumping Vue to 3.5.x. We will disable this test until neuvector can be patched.
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/scripts/test-plugins-build.sh
+++ shell/scripts/test-plugins-build.sh
@@ -236,7 +236,8 @@ function clone_repo_test_extension_build() {
 # Don't forget to add the unit tests exception to clone_repo_test_extension_build function if a new extension has those
 clone_repo_test_extension_build "rancher" "kubewarden-ui" "kubewarden"
 clone_repo_test_extension_build "rancher" "elemental-ui" "elemental"
-clone_repo_test_extension_build "neuvector" "manager-ext" "neuvector-ui-ext"
+# TODO #13141: Enable neuvector tests after issues have been resolved
+# clone_repo_test_extension_build "neuvector" "manager-ext" "neuvector-ui-ext"
 clone_repo_test_extension_build "rancher" "capi-ui-extension" "capi"
 clone_repo_test_extension_build "StackVista" "rancher-extension-stackstate" "observability"
 clone_repo_test_extension_build "harvester" "harvester-ui-extension" "harvester"
