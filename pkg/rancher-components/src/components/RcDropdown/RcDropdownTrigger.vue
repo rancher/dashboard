5e8698318cb63e0187cf55d2bbad76b55d87a19e
Phillip Rak <rak.phillip@gmail.com>
2025-01-24 10:00:44 -0700

    Improve type inferrence via `InstanceType` and `UseTemplateRef`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/src/components/RcDropdown/RcDropdownTrigger.vue
+++ pkg/rancher-components/src/components/RcDropdown/RcDropdownTrigger.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { inject, onMounted, ref, Ref } from 'vue';
-import { RcButton, RcButtonType } from '@components/RcButton';
+import { inject, onMounted, useTemplateRef } from 'vue';
+import { RcButton } from '@components/RcButton';
 import { DropdownContext, defaultContext } from './RcDropdown'
 
 const {
@@ -11,7 +11,7 @@ const {
   isMenuOpen,
 } = inject<DropdownContext>('dropdownContext') || defaultContext;
 
-const dropdownTrigger = ref<RcButtonType | null>(null);
+const dropdownTrigger = useTemplateRef('dropdownTrigger');
 
 onMounted(() => {
   registerTrigger(dropdownTrigger.value);
