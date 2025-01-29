03c2be9ce88360bb256e0addb65f15458c51c136
Isabela Guimaraes <isabela.guimaraes@suse.com>
2025-01-28 16:37:30 -0800

    removed standardUser tag from the e2e test
    Signed-off-by: Isabela Guimaraes <isabela.guimaraes@suse.com>

--- cypress/e2e/tests/pages/manager/gke-cluster-provisioning.spec.ts
+++ cypress/e2e/tests/pages/manager/gke-cluster-provisioning.spec.ts
@@ -9,7 +9,7 @@ import { DEFAULT_GCP_ZONE } from '@/pkg/gke/util/gcp';
  ******/
 
 // will only run this in jenkins pipeline where cloud credentials are stored
-describe('Deploy GKE cluster with default settings', { tags: ['@manager', '@adminUser', '@standardUser', '@jenkins'] }, () => {
+describe('Deploy GKE cluster with default settings', { tags: ['@manager', '@adminUser', '@jenkins'] }, () => {
   const clusterList = new ClusterManagerListPagePo();
   const loadingPo = new LoadingPo('.loading-indicator');
 
