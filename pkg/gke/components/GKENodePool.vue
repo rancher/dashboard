ba43642cbfc6724c24582dc9676bafecb00acb67
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2025-02-06 12:44:07 -0800

    add unit test

--- pkg/gke/components/GKENodePool.vue
+++ pkg/gke/components/GKENodePool.vue
@@ -322,6 +322,7 @@ export default defineComponent({
           :value="maxPodsConstraint"
           label-key="gke.maxPodsConstraint.label"
           :disabled="!isNew"
+          data-testid="gke-max-pod-constraint-input"
           @update:value="$emit('update:maxPodsConstraint', $event)"
         />
       </div>
