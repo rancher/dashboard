e034a4bd5c606d597907c7601856f89cb8731fa9
Francesco Torchia <francesco.torchia@suse.com>
2025-02-06 12:38:51 +0100

    Fleet: Add created by label in GitRepo details page
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- shell/detail/fleet.cattle.io.gitrepo.vue
+++ shell/detail/fleet.cattle.io.gitrepo.vue
@@ -5,7 +5,7 @@ import FleetSummary from '@shell/components/fleet/FleetSummary';
 import { Banner } from '@components/Banner';
 import FleetResources from '@shell/components/fleet/FleetResources';
 import Tab from '@shell/components/Tabbed/Tab';
-import { FLEET } from '@shell/config/types';
+import { FLEET, MANAGEMENT } from '@shell/config/types';
 import { isHarvesterCluster } from '@shell/utils/cluster';
 import FleetBundles from '@shell/components/fleet/FleetBundles.vue';
 import { checkSchemasForFindAllHash } from '@shell/utils/auth';
@@ -90,6 +90,13 @@ export default {
         type:        FLEET.CLUSTER_GROUP
       }
     }, this.$store);
+
+    if (this.value.authorId && this.$store.getters['management/schemaFor'](MANAGEMENT.USER)) {
+      this.$store.dispatch(`management/find`, {
+        type: MANAGEMENT.USER,
+        id:   this.value.authorId,
+      }, { root: true });
+    }
 
     this.allBundles = allDispatches.allBundles || [];
     this.allFleetClusters = allDispatches.allFleetClusters || [];
