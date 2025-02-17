8bdd03cd50a812342874b0cd622deab8d31aea74
cnotv <giuseppe.leo@suse.com>
2025-02-17 21:34:16 +0100

    Correct unit tests

--- shell/store/__tests__/auth.test.ts
+++ shell/store/__tests__/auth.test.ts
@@ -1,26 +1,21 @@
 import { actions } from '@shell/store/auth';
+import { createStore } from 'vuex';
 
 describe('action: redirectTo', () => {
   it('should include query parameters from redirect', async() => {
-    jest
-      .spyOn(window, 'window', 'get')
-      .mockReturnValue({ location: { href: '' } } as any);
-    const store = {
-      state:    {},
-      commit:   jest.fn(),
-      dispatch: jest.fn(),
-    };
+    jest.spyOn(window, 'window', 'get');
+    const store = { dispatch: jest.fn() };
     const clientId = '123';
     const uri = 'whatever';
     const extras = '&response_type=code&response_mode=query&scope=&state=undefined';
     const expectation = `:///?client_id=${ clientId }&redirect_uri=${ uri }${ extras }`;
-    const opt = {
+    const options = {
       provider:    'azuread',
       redirect:    false,
       redirectUrl: `?client_id=${ clientId }&redirect_uri=${ uri }`,
     };
 
-    const url = await actions.redirectTo(store, opt);
+    const url = await actions.redirectTo(store as any, options);
 
     expect(url).toStrictEqual(expectation);
   });
@@ -28,48 +23,65 @@ describe('action: redirectTo', () => {
   it.each([
     ['genericoidc', '://whatever/?redirect_uri=whatever&scope=openid%20profile%20email&state=undefined'],
   ])('given provider %p should return URL %p', async(provider, expectation) => {
-    jest
-      .spyOn(window, 'window', 'get')
-      .mockReturnValue({ location: { href: '' } } as any);
-    const store = {
-      state:    {},
-      commit:   jest.fn(),
-      dispatch: jest.fn(),
-    };
+    jest.spyOn(window, 'window', 'get');
+    const store = { dispatch: jest.fn() };
     const uri = 'whatever'; // This field is added anyway, so we set a random value
-    const opt = {
+    const options = {
       provider,
       redirect:    false,
       redirectUrl: `whatever?redirect_uri=${ uri }`,
     };
 
-    const url = await actions.redirectTo(store, opt);
+    const url = await actions.redirectTo(store as any, options);
 
     expect(url).toStrictEqual(expectation);
   });
+
+  it('should keep scope from options', async() => {
+    const scope = 'whatever';
+    const options = {
+      provider:    'whatever',
+      redirectUrl: 'whatever',
+      scope,
+      test:        true,
+      redirect:    false
+    };
+    const store = { dispatch: jest.fn() };
+
+    jest.spyOn(window, 'window', 'get');
+    const url = await actions.redirectTo(store as any, options);
+
+    expect(url).toContain(scope);
+  });
 });
 
+jest.mock('@shell/utils/auth');
+
 describe('action: test', () => {
-  it('should call redirect with all the options', async() => {
-    const provider = 'whatever';
-    const redirectUrl = 'whatever';
-    const body = { scope: ['whatever'] };
-    const options = {
-      provider,
-      redirectUrl,
-      scope:    body.scope,
-      test:     true,
-      redirect: false
-    };
-    const dispatchSpy = jest.fn().mockResolvedValue({ doAction: () => ({ redirectUrl }) });
-    const store = {
-      state:    {},
-      commit:   jest.fn(),
-      dispatch: dispatchSpy,
-    };
+  describe('given providers with an action (github, google, azuread, oidc)', () => {
+    it('should call redirectTo with all the options', async() => {
+      const dispatchSpy = jest.fn().mockReturnValue('whatever');
+      const store = createStore({
+        actions: {
+          getAuthConfig: () => ({ doAction: () => 'whatever' }),
+          redirectTo:    dispatchSpy,
+        }
+      });
+      const provider = 'whatever';
+      // const redirectUrl = 'whatever';
+      const redirectUrl = undefined;
+      const body = { scope: ['whatever'] };
+      const options = {
+        provider,
+        redirectUrl,
+        scope:    body.scope,
+        test:     true,
+        redirect: false
+      };
 
-    await actions.test(store, { provider, body });
+      await actions.test(store, { provider, body });
 
-    expect(dispatchSpy).toHaveBeenCalledWith('dispatch', options);
+      expect(dispatchSpy.mock.calls[0][1]).toStrictEqual(options);
+    });
   });
 });
