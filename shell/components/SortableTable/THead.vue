42a54d4e1e6eb2f5186ecd4c4fbf6db1e88c0e5a
Alexandre Alves <alexandre.alves@suse.com>
2025-02-19 15:13:36 +0000

    make screen readers ignore the icons to get sorting indication as its present in th element

--- shell/components/SortableTable/THead.vue
+++ shell/components/SortableTable/THead.vue
@@ -270,6 +270,7 @@ export default {
           <div
             v-if="col.sort"
             class="sort"
+            aria-hidden="true"
           >
             <i
               v-show="hasAdvancedFiltering && !col.isFilter"
