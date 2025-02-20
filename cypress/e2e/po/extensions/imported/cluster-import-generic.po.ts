32ce9d4ede29f0b6e03a8d9e993de27061b60f71
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-02-19 17:16:49 -0800

    fixed some errors

--- cypress/e2e/po/extensions/imported/cluster-import-generic.po.ts
+++ cypress/e2e/po/extensions/imported/cluster-import-generic.po.ts
@@ -1,7 +1,7 @@
 import ClusterManagerImportPagePo from '@/cypress/e2e/po/edit/provisioning.cattle.io.cluster/import/cluster-import.po';
 
 /**
- * Edit page for imported cluster
+ * Import page for generic cluster
  */
 export default class ClusterManagerImportGenericPagePo extends ClusterManagerImportPagePo {
 
