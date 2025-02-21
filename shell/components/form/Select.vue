e5bcd4d115958c5f9af54bb4333496ac564c6875
Alexandre Alves <alexandre.alves@suse.com>
2025-02-21 11:58:07 +0000

    prevent scroll when hitting arrow down key on selects

--- shell/components/form/Select.vue
+++ shell/components/form/Select.vue
@@ -256,7 +256,8 @@ export default {
     }"
     :tabindex="disabled || isView ? -1 : 0"
     @click="focusSearch"
-    @keydown.enter.down="focusSearch"
+    @keydown.enter="focusSearch"
+    @keydown.down.prevent="focusSearch"
     @keydown.space.prevent="focusSearch"
   >
     <v-select
