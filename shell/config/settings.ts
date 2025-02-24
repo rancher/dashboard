b21fb1b3873143942c24d91cf0a7c7fb445aec54
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-02-24 13:14:13 -0800

    tmp

--- shell/config/settings.ts
+++ shell/config/settings.ts
@@ -41,74 +41,76 @@ export const SETTING = {
   CA_CERTS: 'cacerts',
 
   // Allow the local cluster to be hidden
-  HIDE_LOCAL_CLUSTER:                   'hide-local-cluster',
-  AUTH_TOKEN_MAX_TTL_MINUTES:           'auth-token-max-ttl-minutes',
-  KUBECONFIG_GENERATE_TOKEN:            'kubeconfig-generate-token',
-  KUBECONFIG_DEFAULT_TOKEN_TTL_MINUTES: 'kubeconfig-default-token-ttl-minutes',
-  ENGINE_URL:                           'engine-install-url',
-  ENGINE_ISO_URL:                       'engine-iso-url',
-  FIRST_LOGIN:                          'first-login',
-  INGRESS_IP_DOMAIN:                    'ingress-ip-domain',
-  SERVER_URL:                           'server-url',
-  RKE_METADATA_CONFIG:                  'rke-metadata-config',
-  EULA_AGREED:                          'eula-agreed',
-  AUTH_USER_INFO_MAX_AGE_SECONDS:       'auth-user-info-max-age-seconds',
-  AUTH_USER_SESSION_TTL_MINUTES:        'auth-user-session-ttl-minutes',
-  AUTH_USER_INFO_RESYNC_CRON:           'auth-user-info-resync-cron',
-  AUTH_LOCAL_VALIDATE_DESC:             'auth-password-requirements-description',
-  PASSWORD_MIN_LENGTH:                  'password-min-length', // CATTLE_PASSWORD_MIN_LENGTH
-  CLUSTER_TEMPLATE_ENFORCEMENT:         'cluster-template-enforcement',
-  UI_INDEX:                             'ui-index',
-  UI_DASHBOARD_INDEX:                   'ui-dashboard-index',
-  UI_DASHBOARD_HARVESTER_LEGACY_PLUGIN: 'ui-dashboard-harvester-legacy-plugin',
-  UI_OFFLINE_PREFERRED:                 'ui-offline-preferred',
-  SYSTEM_DEFAULT_REGISTRY:              'system-default-registry',
-  UI_ISSUES:                            'ui-issues',
-  PL:                                   'ui-pl',
-  PL_RANCHER_VALUE:                     'rancher',
-  SUPPORTED:                            'has-support',
-  BANNERS:                              'ui-banners',
-  ISSUES:                               'ui-issues',
-  BRAND:                                'ui-brand',
-  LOGO_LIGHT:                           'ui-logo-light',
-  LOGO_DARK:                            'ui-logo-dark',
-  BANNER_LIGHT:                         'ui-banner-light',
-  BANNER_DARK:                          'ui-banner-dark',
-  LOGIN_BACKGROUND_LIGHT:               'ui-login-background-light',
-  LOGIN_BACKGROUND_DARK:                'ui-login-background-dark',
-  PRIMARY_COLOR:                        'ui-primary-color',
-  LINK_COLOR:                           'ui-link-color',
-  COMMUNITY_LINKS:                      'ui-community-links',
-  FAVICON:                              'ui-favicon',
-  UI_PERFORMANCE:                       'ui-performance',
-  UI_CUSTOM_LINKS:                      'ui-custom-links',
-  UI_SUPPORTED_K8S_VERSIONS:            'ui-k8s-supported-versions-range',
+  HIDE_LOCAL_CLUSTER:                            'hide-local-cluster',
+  AUTH_TOKEN_MAX_TTL_MINUTES:                    'auth-token-max-ttl-minutes',
+  KUBECONFIG_GENERATE_TOKEN:                     'kubeconfig-generate-token',
+  KUBECONFIG_DEFAULT_TOKEN_TTL_MINUTES:          'kubeconfig-default-token-ttl-minutes',
+  ENGINE_URL:                                    'engine-install-url',
+  ENGINE_ISO_URL:                                'engine-iso-url',
+  FIRST_LOGIN:                                   'first-login',
+  INGRESS_IP_DOMAIN:                             'ingress-ip-domain',
+  SERVER_URL:                                    'server-url',
+  RKE_METADATA_CONFIG:                           'rke-metadata-config',
+  EULA_AGREED:                                   'eula-agreed',
+  AUTH_USER_INFO_MAX_AGE_SECONDS:                'auth-user-info-max-age-seconds',
+  AUTH_USER_SESSION_TTL_MINUTES:                 'auth-user-session-ttl-minutes',
+  AUTH_USER_INFO_RESYNC_CRON:                    'auth-user-info-resync-cron',
+  AUTH_LOCAL_VALIDATE_DESC:                      'auth-password-requirements-description',
+  PASSWORD_MIN_LENGTH:                           'password-min-length', // CATTLE_PASSWORD_MIN_LENGTH
+  CLUSTER_TEMPLATE_ENFORCEMENT:                  'cluster-template-enforcement',
+  UI_INDEX:                                      'ui-index',
+  UI_DASHBOARD_INDEX:                            'ui-dashboard-index',
+  UI_DASHBOARD_HARVESTER_LEGACY_PLUGIN:          'ui-dashboard-harvester-legacy-plugin',
+  UI_OFFLINE_PREFERRED:                          'ui-offline-preferred',
+  SYSTEM_DEFAULT_REGISTRY:                       'system-default-registry',
+  UI_ISSUES:                                     'ui-issues',
+  PL:                                            'ui-pl',
+  PL_RANCHER_VALUE:                              'rancher',
+  SUPPORTED:                                     'has-support',
+  BANNERS:                                       'ui-banners',
+  ISSUES:                                        'ui-issues',
+  BRAND:                                         'ui-brand',
+  LOGO_LIGHT:                                    'ui-logo-light',
+  LOGO_DARK:                                     'ui-logo-dark',
+  BANNER_LIGHT:                                  'ui-banner-light',
+  BANNER_DARK:                                   'ui-banner-dark',
+  LOGIN_BACKGROUND_LIGHT:                        'ui-login-background-light',
+  LOGIN_BACKGROUND_DARK:                         'ui-login-background-dark',
+  PRIMARY_COLOR:                                 'ui-primary-color',
+  LINK_COLOR:                                    'ui-link-color',
+  COMMUNITY_LINKS:                               'ui-community-links',
+  FAVICON:                                       'ui-favicon',
+  UI_PERFORMANCE:                                'ui-performance',
+  UI_CUSTOM_LINKS:                               'ui-custom-links',
+  UI_SUPPORTED_K8S_VERSIONS:                     'ui-k8s-supported-versions-range',
   /**
    * Allow the backend to force a light/dark theme. Used in non-rancher world and results in the theme used
    * both pre and post log in. If not present defaults to the usual process
    */
-  THEME:                                'ui-theme',
-  SYSTEM_NAMESPACES:                    'system-namespaces',
+  THEME:                                         'ui-theme',
+  SYSTEM_NAMESPACES:                             'system-namespaces',
   /**
    * Cluster Agent configuration
    */
-  CLUSTER_AGENT_DEFAULT_AFFINITY:       'cluster-agent-default-affinity',
-  FLEET_AGENT_DEFAULT_AFFINITY:         'fleet-agent-default-affinity',
+  CLUSTER_AGENT_DEFAULT_AFFINITY:                'cluster-agent-default-affinity',
+  FLEET_AGENT_DEFAULT_AFFINITY:                  'fleet-agent-default-affinity',
   /**
    * manage rancher repositories in extensions (official, partners repos)
   */
-  ADD_EXTENSION_REPOS_BANNER_DISPLAY:   'display-add-extension-repos-banner',
-  AGENT_TLS_MODE:                       'agent-tls-mode',
+  ADD_EXTENSION_REPOS_BANNER_DISPLAY:            'display-add-extension-repos-banner',
+  AGENT_TLS_MODE:                                'agent-tls-mode',
   /**
    * User retention settings
    */
-  USER_RETENTION_CRON:                  'user-retention-cron',
-  USER_RETENTION_DRY_RUN:               'user-retention-dry-run',
-  USER_LAST_LOGIN_DEFAULT:              'user-last-login-default',
-  DISABLE_INACTIVE_USER_AFTER:          'disable-inactive-user-after',
-  DELETE_INACTIVE_USER_AFTER:           'delete-inactive-user-after',
-  K3S_UPGRADER_UNINSTALL_CONCURRENCY:   'k3s-based-upgrader-uninstall-concurrency',
-  IMPORTED_CLUSTER_VERSION_MANAGEMENT:  'imported-cluster-version-management'
+  USER_RETENTION_CRON:                           'user-retention-cron',
+  USER_RETENTION_DRY_RUN:                        'user-retention-dry-run',
+  USER_LAST_LOGIN_DEFAULT:                       'user-last-login-default',
+  DISABLE_INACTIVE_USER_AFTER:                   'disable-inactive-user-after',
+  DELETE_INACTIVE_USER_AFTER:                    'delete-inactive-user-after',
+  K3S_UPGRADER_UNINSTALL_CONCURRENCY:            'k3s-based-upgrader-uninstall-concurrency',
+  IMPORTED_CLUSTER_VERSION_MANAGEMENT:           'imported-cluster-version-management',
+  CLUSTER_AGENT_DEFAULT_PRIORITY_CLASS:          'cluster-agent-default-priority-class',
+  CLUSTER_AGENT_DEFAULT_POD_DISTRIBUTION_BUDGET: 'cluster-agent-default-pod-disruption-budget'
 } as const;
 
 // These are the settings that are allowed to be edited via the UI
@@ -166,7 +168,10 @@ export const ALLOWED_SETTINGS: GlobalSetting = {
     kind:    'integer',
     ruleSet: [{ name: 'minValue', factoryArg: 1 }]
   },
-  [SETTING.IMPORTED_CLUSTER_VERSION_MANAGEMENT]: { kind: 'boolean' }
+  [SETTING.IMPORTED_CLUSTER_VERSION_MANAGEMENT]:           { kind: 'boolean' },
+  [SETTING.CLUSTER_AGENT_DEFAULT_PRIORITY_CLASS]:          { kind: 'json' },
+  [SETTING.CLUSTER_AGENT_DEFAULT_POD_DISTRIBUTION_BUDGET]: { kind: 'json' }
+
 };
 
 export const PROVISIONING_SETTINGS = ['engine-iso-url', 'engine-install-url', 'imported-cluster-version-management'];
