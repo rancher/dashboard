cb4b4431e970feb62528fc1e542ba38c457d88ac
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-02-24 13:14:13 -0800

    tmp

--- shell/list/management.cattle.io.setting.vue
+++ shell/list/management.cattle.io.setting.vue
@@ -5,11 +5,11 @@ import { ALLOWED_SETTINGS } from '@shell/config/settings';
 import { Banner } from '@components/Banner';
 import Loading from '@shell/components/Loading';
 import { VIEW_IN_API } from '@shell/store/prefs';
-import ActionMenu from '@shell/components/ActionMenuShell.vue';
+import Setting from '@shell/components/Setting';
 
 export default {
   components: {
-    Banner, Loading, ActionMenu
+    Banner, Loading, Setting
   },
 
   async fetch() {
@@ -65,10 +65,10 @@ export default {
     return { settings: null };
   },
 
-  computed: {
-    ...mapGetters({ t: 'i18n/t' }),
-    ...mapGetters({ options: 'action-menu/optionsArray' }),
-  },
+//   computed: {
+//     ...mapGetters({ t: 'i18n/t' }),
+//     ...mapGetters({ options: 'action-menu/optionsArray' }),
+//   },
 };
 </script>
 
@@ -86,62 +86,15 @@ export default {
     <div
       v-for="(setting) in settings"
       :key="setting.id"
-      class="advanced-setting mb-20"
-      :data-testid="`advanced-setting__option-${setting.id}`"
     >
-      <div class="header">
-        <div class="title">
-          <h1>
-            {{ setting.id }}
-            <span
-              v-if="setting.fromEnv"
-              class="modified"
-            >{{ t('advancedSettings.setEnv') }}</span>
-            <span
-              v-else-if="setting.customized"
-              class="modified"
-            >{{ t('advancedSettings.modified') }}</span>
-          </h1>
-          <h2>{{ t(`advancedSettings.descriptions.${setting.id}`) }}</h2>
-        </div>
-        <div
-          v-if="setting.hasActions"
-          class="action"
-        >
-          <action-menu
-            :resource="setting.data"
-            :button-aria-label="t('advancedSettings.edit.label')"
-            data-testid="action-button"
-            button-role="tertiary"
-          />
-        </div>
-      </div>
-      <div value>
-        <div v-if="setting.canHide">
-          <button
-            class="btn btn-sm role-primary"
-            role="button"
-            :aria-label="t('advancedSettings.hideShow')"
-            @click="setting.hide = !setting.hide"
-          >
-            {{ setting.hide ? t('advancedSettings.show') : t('advancedSettings.hide') }} {{ setting.id }}
-          </button>
-        </div>
-        <div
-          v-show="!setting.canHide || (setting.canHide && !setting.hide)"
-          class="settings-value"
-        >
-          <pre v-if="setting.kind === 'json'">{{ setting.json }}</pre>
-          <pre v-else-if="setting.kind === 'multiline'">{{ setting.data.value || setting.data.default }}</pre>
-          <pre v-else-if="setting.kind === 'enum'">{{ t(setting.enum) }}</pre>
-          <pre v-else-if="setting.data.value || setting.data.default">{{ setting.data.value || setting.data.default }}</pre>
-          <pre
-            v-else
-            class="text-muted"
-          >&lt;{{ t('advancedSettings.none') }}&gt;</pre>
-        </div>
-      </div>
+      <Setting
+        :value="setting"
+      />
     </div>
+
+    <h2>
+      {{ t('advancedSettings.provisioning.header') }}
+    </h2>
   </div>
 </template>
 
@@ -149,39 +102,18 @@ export default {
 .settings-banner {
   margin-top: 0;
 }
-.advanced-setting {
-  border: 1px solid var(--border);
-  padding: 20px;
-  border-radius: var(--border-radius);
+// .advanced-setting {
+//   border: 1px solid var(--border);
+//   padding: 20px;
+//   border-radius: var(--border-radius);
 
-  h1 {
-    font-size: 14px;
-  }
-  h2 {
-    font-size: 12px;
-    margin-bottom: 0;
-    opacity: 0.8;
-  }
-}
-
-.settings-value pre {
-  margin: 0;
-}
-
-.header {
-  display: flex;
-  margin-bottom: 20px;
-}
-
-.title {
-  flex: 1;
-}
-
-.modified {
-  margin-left: 10px;
-  border: 1px solid var(--primary);
-  border-radius: 5px;
-  padding: 2px 10px;
-  font-size: 12px;
-}
+//   h1 {
+//     font-size: 14px;
+//   }
+//   h2 {
+//     font-size: 12px;
+//     margin-bottom: 0;
+//     opacity: 0.8;
+//   }
+// }
 </style>
