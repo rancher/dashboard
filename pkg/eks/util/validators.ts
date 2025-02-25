799df5950ae463927d2cccd647c4d8ab079ce654
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2025-02-25 13:10:02 -0800

    import eks

--- pkg/eks/util/validators.ts
+++ pkg/eks/util/validators.ts
@@ -1,15 +1,16 @@
 import { set } from '@shell/utils/object';
-import { EKSConfig, EKSNodeGroup } from 'types';
+import { EKSConfig, EKSNodeGroup, NormanCluster } from 'types';
 
 export interface CruEKSContext {
   t: Function,
   config: EKSConfig,
   nodeGroups: EKSNodeGroup[],
+  normanCluster: NormanCluster
 }
 
 const clusterNameRequired = (ctx: CruEKSContext) => {
   return (): string | null => {
-    return !ctx.config.displayName ? ctx.t('validation.required', { key: ctx.t('nameNsDescription.name.label') }) : null;
+    return !ctx.normanCluster.name ? ctx.t('validation.required', { key: ctx.t('nameNsDescription.name.label') }) : null;
   };
 };
 
