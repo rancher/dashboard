6913c700974f958fe19e747e2918b601c43d93b0
Richard Cox <ricox@suse.com>
2025-02-26 14:53:58 +0000

    Fix to track backend changes for exact/partial matches

--- shell/plugins/steve/steve-pagination-utils.ts
+++ shell/plugins/steve/steve-pagination-utils.ts
@@ -433,9 +433,15 @@ class StevePaginationUtils extends NamespaceProjectFilters {
               this.validateField(validateFields, schema, field.field);
 
               const value = encodeURIComponent(field.value);
-              const exactPartial = field.exact ? `'${ value }'` : value;
 
-              return `${ this.convertArrayPath(field.field) }${ field.equals ? '=' : '!=' }${ exactPartial }`;
+              // = exact match
+              // ~ partial match
+              // != not exact match
+              // !~ not partial match
+
+              const operator = `${ field.equals ? '' : '!' }${ field.exact ? '=' : '~' }`;
+
+              return `${ this.convertArrayPath(field.field) }${ operator }${ value }`;
             }
 
             return field.value;
