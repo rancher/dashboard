e11a57ee5092148e3f7ec81f7b2e0f33379aafca
cnotv <giuseppe.leo@suse.com>
2025-02-27 21:25:10 +0100

    Mute all the Vue console warnings

--- jest.setup.js
+++ jest.setup.js
@@ -65,6 +65,10 @@ beforeAll(() => {
   });
 });
 
+jest.mock('@shell/composables/useI18n', () => ({ useI18n: () => (key) => key }));
+// eslint-disable-next-line no-console
+jest.spyOn(console, 'warn').mockImplementation((warning) => warning.includes('[Vue warn]') ? null : console.log(warning));
+
 /**
  * Common initialization for each test, required for resetting states
  */
