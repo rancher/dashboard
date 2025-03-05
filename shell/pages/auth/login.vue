f8386691671796f3aaa4031735967982e549651f
Alexandre Alves <alexandre.alves@suse.com>
2025-03-05 16:49:05 +0000

    working on issues reported in the a11y tests

--- shell/pages/auth/login.vue
+++ shell/pages/auth/login.vue
@@ -311,7 +311,7 @@ export default {
     v-if="$fetchState.pending"
     mode="relative"
   />
-  <main
+  <div
     v-else
     class="main-layout login"
   >
@@ -509,9 +509,10 @@ export default {
         class="col span-6 landscape"
         data-testid="login-landscape__img"
         file-name="login-landscape.svg"
+        :alt="t('login.landscapeAlt')"
       />
     </div>
-  </main>
+  </div>
 </template>
 
 <style lang="scss" scoped>
