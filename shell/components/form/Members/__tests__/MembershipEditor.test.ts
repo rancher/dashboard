41e46da947fb63eb4d8f489798f02a2da3555cd1
cnotv <giuseppe.leo@suse.com>
2025-03-06 17:28:09 +0100

    Add tests

---
+++ shell/components/form/Members/__tests__/MembershipEditor.test.ts
@@ -0,0 +1,31 @@
+import { mount, type VueWrapper } from '@vue/test-utils';
+import MembershipEditor from '@shell/components/form/Members/MembershipEditor.vue';
+
+describe('component: MembershipEditor', () => {
+  let wrapper: VueWrapper<any, any>;
+
+  beforeEach(() => {
+    wrapper = mount(MembershipEditor, {
+      props: {
+        addMemberDialogName: 'addMemberDialogName',
+        parentKey:           'parentKey',
+        mode:                'edit',
+        type:                'no idea',
+      },
+      global: {
+        mocks: {
+          $store:      { getters: { 'rancher/schemaFor': () => ({ type: 'object' }) } },
+          $fetchState: { pending: false },
+        }
+      }
+    });
+  });
+
+  it('should render the component', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should remove the correct member', () => {
+    expect(wrapper.vm.members).toStrictEqual([]);
+  });
+});
