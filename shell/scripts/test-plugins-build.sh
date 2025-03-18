26873da5a28bf3b6d9bf6ae00ec51f769da65604
Alexandre Alves <alexandre.alves@suse.com>
2025-03-18 17:03:56 +0000

    re-enable extension checks

--- shell/scripts/test-plugins-build.sh
+++ shell/scripts/test-plugins-build.sh
@@ -238,9 +238,7 @@ function clone_repo_test_extension_build() {
 # Don't forget to add the unit tests exception to clone_repo_test_extension_build function if a new extension has those
 clone_repo_test_extension_build "rancher" "kubewarden-ui" "kubewarden"
 clone_repo_test_extension_build "rancher" "elemental-ui" "elemental"
-# TODO #13141: Enable neuvector tests after issues have been resolved
 clone_repo_test_extension_build "neuvector" "manager-ext" "neuvector-ui-ext"
-# TODO: #13173: Enable capi, stackstate, and harvester after `entities` resolution has been set
 clone_repo_test_extension_build "rancher" "capi-ui-extension" "capi"
 clone_repo_test_extension_build "StackVista" "rancher-extension-stackstate" "observability"
 clone_repo_test_extension_build "harvester" "harvester-ui-extension" "harvester"
