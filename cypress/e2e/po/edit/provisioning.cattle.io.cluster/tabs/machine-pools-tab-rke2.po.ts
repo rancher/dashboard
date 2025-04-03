a3318d697b5aee11998a2175c79c7188f268b717
momesgin <mo.mesgin@suse.com>
2025-04-03 15:55:35 -0700

    Validation support for RKE2 machine pool configuration (#13789)
    * machinepool validation
    
    * refactor
    
    * finish refactor + e2e test
    
    * remove unnecessary emit item
    
    ---------
    
    Co-authored-by: Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>

--- cypress/e2e/po/edit/provisioning.cattle.io.cluster/tabs/machine-pools-tab-rke2.po.ts
+++ cypress/e2e/po/edit/provisioning.cattle.io.cluster/tabs/machine-pools-tab-rke2.po.ts
@@ -1,6 +1,7 @@
 import ComponentPo from '@/cypress/e2e/po/components/component.po';
 import LabeledSelectPo from '@/cypress/e2e/po/components/labeled-select.po';
 import NameNsDescription from '@/cypress/e2e/po/components/name-ns-description.po';
+import LabeledInputPo from '@/cypress/e2e/po/components/labeled-input.po';
 
 export default class MachinePoolRke2 extends ComponentPo {
   constructor(selector = '.dashboard-root') {
@@ -9,6 +10,14 @@ export default class MachinePoolRke2 extends ComponentPo {
 
   nameNsDescription() {
     return new NameNsDescription(this.self());
+  }
+
+  poolName() {
+    return new LabeledInputPo('[data-testid="machine-pool-name-input"]');
+  }
+
+  poolQuantity() {
+    return new LabeledInputPo('[data-testid="machine-pool-quantity-input"]');
   }
 
   networks(): LabeledSelectPo {
