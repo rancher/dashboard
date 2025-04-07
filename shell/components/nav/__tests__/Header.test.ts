88948db691406fdf1c006a8baa8715fcb13dd88d
cnotv <giuseppe.leo@suse.com>
2025-04-07 11:25:11 +0200

    Define all the mocked required elements

--- shell/components/nav/__tests__/Header.test.ts
+++ shell/components/nav/__tests__/Header.test.ts
@@ -3,9 +3,52 @@ import { mount, VueWrapper } from '@vue/test-utils';
 import Header from '@shell/components/nav/Header.vue';
 
 describe('component: Header', () => {
-  it('should be rendered', () => {
-    const wrapper: VueWrapper<any, any> = mount(Header, {});
+  let wrapper: VueWrapper<any, any>;
 
-    expect(wrapper).toBe(true);
+  beforeEach(() => {
+    wrapper = mount(Header, {
+      global: {
+        directives: { shortkey: jest.fn() },
+        mocks:      {
+          $config: { rancherEnv: 'whatever' },
+          $store:  {
+            getters: {
+              currentCluster: jest.fn(() => ({
+                name: 'test-cluster',
+                type: 'rancher',
+              })),
+              'management/byId':              jest.fn(),
+              'management/schemaFor':         jest.fn(),
+              'rancher/schemaFor':            jest.fn(),
+              'management/paginationEnabled': jest.fn(),
+              'management/all':               jest.fn(),
+              'prefs/get':                    jest.fn(),
+              'rancher/byId':                 jest.fn(),
+              isRancher:                      jest.fn(),
+              isExplorer:                     jest.fn(),
+              isRancherInHarvester:           jest.fn(),
+              isMultiCluster:                 jest.fn(),
+              isSingleProduct:                jest.fn(),
+              currentProduct:                 jest.fn(),
+              rootProduct:                    jest.fn(),
+              pageActions:                    jest.fn(),
+              clusterReady:                   jest.fn(),
+              backToRancherLink:              jest.fn(),
+              backToRancherGlobalLink:        jest.fn(),
+              'i18n/withFallback':            jest.fn(),
+            },
+            dispatch: jest.fn(),
+          }
+        },
+        stubs: {
+          TopLevelMenu:  true,
+          'router-link': true
+        }
+      }
+    });
+  });
+
+  it('should be rendered', () => {
+    expect(wrapper.exists()).toBe(true);
   });
 });
