5ef552de564c1f9e528285381437a6b11c5549bc
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-04-10 15:48:21 -0700

    Hid Basics tab for imported RKE1 cluster (#14078)

--- pkg/imported/components/CruImported.vue
+++ pkg/imported/components/CruImported.vue
@@ -223,7 +223,9 @@ export default defineComponent({
     },
 
     showBasics() {
-      return this.isCreate || !!this.config || !!this.normanCluster.annotations[IMPORTED_CLUSTER_VERSION_MANAGEMENT];
+      const hasFieldsToShow = !!this.config || !!this.normanCluster.annotations[IMPORTED_CLUSTER_VERSION_MANAGEMENT];
+
+      return (!this.isRKE1 && hasFieldsToShow) || this.isCreate;
     },
     enableInstanceDescription() {
       return this.isLocal || this.isCreate;
