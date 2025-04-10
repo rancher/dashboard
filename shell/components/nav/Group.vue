d566f6f4400188ab080c766e252608358e822f1c
edenhernandez-suse <eden.hernandez@suse.com>
2025-04-10 11:16:26 +0200

    Lint fix

--- shell/components/nav/Group.vue
+++ shell/components/nav/Group.vue
@@ -448,7 +448,7 @@ export default {
       color: var(--on-active);
       background: var(--active);
       font-weight: bold;
-      
+
       &:hover {
         background: var(--nav-active--hover);
       }
