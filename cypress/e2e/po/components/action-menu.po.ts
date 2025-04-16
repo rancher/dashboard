c2855670ed09e5591fd323a183ae39c70f71bb5c
Phillip Rak <rak.phillip@gmail.com>
2025-04-15 17:21:29 -0700

    Fix e2e tests
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- cypress/e2e/po/components/action-menu.po.ts
+++ cypress/e2e/po/components/action-menu.po.ts
@@ -2,11 +2,11 @@ import ComponentPo from '@/cypress/e2e/po/components/component.po';
 
 export default class ActionMenuPo extends ComponentPo {
   constructor(arg:any) {
-    super(arg || cy.get('main > div > .menu'));
+    super(arg || cy.get('[dropdown-menu-collection]'));
   }
 
   clickMenuItem(index: number) {
-    return this.self().find('li').eq(index).click();
+    return this.self().find('[dropdown-menu-item]').eq(index).click();
   }
 
   getMenuItem(name: string) {
