05f9919beb9a0336ecf0d5775c5b80d0e62adee3
Phillip Rak <rak.phillip@gmail.com>
2025-04-17 14:20:15 -0700

    Create new `RcDropdownMenuItemCheckbox` component
    `RcDropdownMenuItemCheckbox` specifies the styles and behaviors for a menu items that acts as a checkbox.
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-components/src/components/RcDropdown/index.ts
+++ pkg/rancher-components/src/components/RcDropdown/index.ts
@@ -1,5 +1,6 @@
 export { default as RcDropdown } from './RcDropdown.vue';
 export { default as RcDropdownItem } from './RcDropdownItem.vue';
+export { default as RcDropdownItemCheckbox } from './RcDropdownItemCheckbox.vue';
 export { default as RcDropdownSeparator } from './RcDropdownSeparator.vue';
 export { default as RcDropdownTrigger } from './RcDropdownTrigger.vue';
 export { default as RcDropdownMenu } from './RcDropdownMenu.vue';
