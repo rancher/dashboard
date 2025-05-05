45f2b6d783467926fcf97cc3f0aec184eef9d6d5
Alexandre Alves <alexandre.alves@suse.com>
2025-05-05 11:28:31 +0100

    remove todos

--- shell/dialog/UninstallExtensionDialog.vue
+++ shell/dialog/UninstallExtensionDialog.vue
@@ -35,7 +35,6 @@ export default {
       default:  () => {},
       required: true
     },
-    // TODO: handle console warns if we remove these props (resources, regbackgroundclosing)
     resources: {
       type:    Array,
       default: () => []
