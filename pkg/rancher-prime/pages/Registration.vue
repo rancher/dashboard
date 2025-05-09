b0fe7ac17903a92a1a113aee11e4289c37f40941
cnotv <giuseppe.leo@suse.com>
2025-05-09 18:44:10 +0200

    Update registration page

--- pkg/rancher-prime/pages/Registration.vue
+++ pkg/rancher-prime/pages/Registration.vue
@@ -3,10 +3,11 @@ import { computed } from 'vue';
 import { useStore } from 'vuex';
 import { useI18n } from '@shell/composables/useI18n';
 
-import { Card } from '@components/Card';
+import Tab from '@shell/components/Tabbed/Tab';
+import Tabbed from '@shell/components/Tabbed';
 import LabeledInput from '@components/Form/LabeledInput/LabeledInput.vue';
+import { BadgeState } from '@components/BadgeState';
 import AsyncButton from '@shell/components/AsyncButton';
-import StatusBadge from '@shell/components/StatusBadge';
 import Banner from '@components/Banner/Banner.vue';
 import FileSelector from '@shell/components/form/FileSelector';
 import { usePrimeRegistration } from './registration.composable';
@@ -22,7 +23,7 @@ const {
   deregister,
   errors,
   registrationCode,
-  expirationStatus
+  registrationBanner
 } = usePrimeRegistration();
 
 /**
@@ -35,27 +36,23 @@ const isRegistered = computed(() => registrationStatus.value === 'registered-onl
  */
 const isRegistering = computed(() => registrationStatus.value === 'registering-online' || registrationStatus.value === 'registering-offline');
 
-const isRegisteredOnline = computed(() => registrationStatus.value === 'registered-online');
-const isRegisteredOffline = computed(() => registrationStatus.value === 'registered-offline');
-
 /**
  * Track offline registration progress, to switch between file selector and async button
  */
 const isRegisteringOffline = computed(() => registrationStatus.value === 'registering-offline');
-
-/**
- * Stored expiration date, retrieved from CRD
- */
-const expirationDate = computed(() => 'XX/XX/XXXX');
-
 </script>
 
 <template>
   <div style="padding: 20px;">
     <h1>{{ t('registration.title') }}</h1>
-    <p
-      v-clean-html="t('registration.description', {}, true)"
-    />
+
+    <!-- Status Banner -->
+    <Banner
+      :color="registrationBanner.type"
+      data-testid="registration-banner-status"
+    >
+      {{ t(registrationBanner.message) }}
+    </Banner>
 
     <!-- Errors banner -->
     <Banner
@@ -72,144 +69,135 @@ const expirationDate = computed(() => 'XX/XX/XXXX');
       </p>
     </Banner>
 
-    <!-- Registered -->
-    <div class="row mt-20">
-      <!-- Online registration -->
-      <div class="col span-6">
-        <Card :showActions="false">
-          <template #title>
-            <h2>{{ t('registration.online.title') }}</h2>
-          </template>
+    <Tabbed>
+      <!-- Online -->
+      <Tab
+        name="online"
+        :label="t('registration.online.title')"
+        :weight="2"
+      >
+        <p
+          v-clean-html="t('registration.online.description', {}, true)"
+          class="mt-20"
+        />
+        <LabeledInput
+          v-model:value="registrationCode"
+          class="mt-20"
+          :disabled="isRegistered || isRegistering"
+          label-key="registration.online.input.label"
+          placeholder-key="registration.online.input.placeholder"
+          data-testid="registration-code-input"
+        />
 
-          <template #body>
-            <p
-              v-clean-html="t('registration.online.description', {}, true)"
-              class="mt-20"
-            />
+        <AsyncButton
+          class="mt-20"
+          :waitingLabel="t('registration.online.button-cta.progress')"
+          :action-label="t('registration.online.button-cta.label')"
+          data-testid="registration-online-cta"
+          :disabled="isRegistered || isRegistering || !registrationCode"
+          @click="registerOnline"
+        />
+      </Tab>
 
-            <LabeledInput
-              v-model:value="registrationCode"
-              class="mt-20"
-              :disabled="isRegistered || isRegistering"
-              label-key="registration.online.input.label"
-              placeholder-key="registration.online.input.placeholder"
-              data-testid="registration-code-input"
-            />
+      <!-- Offline -->
+      <Tab
+        name="offline"
+        :label="t('registration.offline.title')"
+        :weight="1"
+      >
+        <!-- Step 1 -->
+        <p
+          v-clean-html="t('registration.offline.stepone', {}, true)"
+          class="mt-20"
+        />
+        <div>
+          <AsyncButton
+            class="mt-20"
+            actionColor="role-secondary"
+            successColor="role-secondary"
+            :waitingLabel="t('registration.offline.button.download.progress')"
+            :action-label="t('registration.offline.button.download.label')"
+            :success-label="t('registration.offline.button.download.label')"
+            data-testid="registration-offline-download"
+            :disabled="isRegistered"
+            @click="downloadOfflineRequest"
+          />
+        </div>
 
-            <!-- Show online Certificate expiration and status -->
-            <div
-              v-if="isRegisteredOnline"
-            >
-              <StatusBadge
-                v-if="expirationStatus"
-                :status="expirationStatus"
-                :label="t(`registration.registered.description.${expirationStatus}`, { expirationDate }, true)"
-                data-testid="expiration-status-online"
-              />
-              <div class="mt-20">
-                <AsyncButton
-                  currentPhase="error"
-                  :waitingLabel="t('registration.registered.button-cta.progress')"
-                  :error-label="t('registration.registered.button-cta.label')"
-                  data-testid="registration-online-deregister-cta"
-                  :disabled="isRegistering"
-                  @click="deregister"
-                />
-              </div>
-            </div>
+        <!-- Step 2 -->
+        <p
+          v-clean-html="t('registration.offline.steptwo', {}, true)"
+          class="mt-20"
+        />
+        <!-- Show async button while submitting file -->
+        <div v-if="isRegisteringOffline">
+          <AsyncButton
+            class="mt-20"
+            :waitingLabel="t('registration.offline.button.register.progress')"
+            :action-label="t('registration.offline.button.register.label')"
+            data-testid="registration-offline-cta"
+            :disabled="isRegistered || isRegistering"
+            :currentPhase="'waiting'"
+          />
+        </div>
 
-            <!-- Show button to register online -->
-            <div v-else>
-              <AsyncButton
-                class="mt-20"
-                :waitingLabel="t('registration.online.button-cta.progress')"
-                :action-label="t('registration.online.button-cta.label')"
-                data-testid="registration-online-cta"
-                :disabled="isRegistered || isRegistering || !registrationCode"
-                @click="registerOnline"
-              />
-            </div>
-          </template>
-        </Card>
-      </div>
+        <!-- Show file selector for offline registration -->
+        <div v-else>
+          <FileSelector
+            class="role-primary mt-20"
+            :label="t('registration.offline.button.register.label')"
+            :disabled="isRegistered || isRegistering"
+            data-testid="registration-offline-cta"
+            @selected="registerOffline"
+          />
+        </div>
+      </Tab>
+    </Tabbed>
 
-      <!-- Offline registration -->
-      <div class="col span-6">
-        <Card
-          :showHighlightBorder="false"
-          :showActions="false"
-        >
-          <template #title>
-            <h2>{{ t('registration.offline.title') }}</h2>
-          </template>
+    <!-- List -->
+    <div class="mt-40">
+      <h3>{{ t('registration.list.title') }}</h3>
 
-          <template #body>
-            <p
-              v-clean-html="t('registration.offline.description', {}, true)"
-              class="mt-20"
-            />
-
-            <!-- Download offline registration request -->
-            <div>
-              <AsyncButton
-                class="mt-20"
-                actionColor="role-secondary"
-                successColor="role-secondary"
-                :waitingLabel="t('registration.offline.button-download.progress')"
-                :action-label="t('registration.offline.button-download.label')"
-                :success-label="t('registration.offline.button-download.label')"
-                data-testid="registration-offline-download"
-                :disabled="isRegistered"
-                @click="downloadOfflineRequest"
-              />
-            </div>
-
-            <!-- Show Offline Certificate expiration and status -->
-            <div
-              v-if="isRegisteredOffline"
-            >
-              <StatusBadge
-                v-if="expirationStatus"
-                :status="expirationStatus"
-                :label="t(`registration.registered.description.${expirationStatus}`, { expirationDate }, true)"
-                data-testid="expiration-status-offline"
-              />
-              <div class="mt-20">
-                <AsyncButton
-                  currentPhase="error"
-                  :waitingLabel="t('registration.registered.button-cta.progress')"
-                  :error-label="t('registration.registered.button-cta.label')"
-                  data-testid="registration-offline-deregister-cta"
-                  :disabled="isRegistering"
-                  @click="deregister"
-                />
-              </div>
-            </div>
-
-            <!-- Show async button while submitting file -->
-            <div v-else-if="isRegisteringOffline">
-              <AsyncButton
-                class="mt-20"
-                :waitingLabel="t('registration.offline.button-cta.progress')"
-                :action-label="t('registration.offline.button-cta.label')"
-                data-testid="registration-offline-cta"
-                :disabled="isRegistered || isRegistering"
-                :currentPhase="'waiting'"
-              />
-            </div>
-
-            <!-- Show file selector for offline registration -->
-            <div v-else>
-              <FileSelector
-                class="role-primary mt-20"
-                :label="t('registration.offline.button-cta.label')"
-                :disabled="isRegistered || isRegistering"
-                data-testid="registration-offline-cta"
-                @selected="registerOffline"
-              />
-            </div>
-          </template>
-        </Card>
+      <div class="mt-40 grid-5">
+        <div class="color-disabled-text">
+          {{ t('registration.list.table.header.status') }}
+        </div>
+        <div class="color-disabled-text">
+          {{ t('registration.list.table.header.product') }}
+        </div>
+        <div class="color-disabled-text">
+          {{ t('registration.list.table.header.expiration') }}
+        </div>
+        <div class="color-disabled-text">
+          {{ t('registration.list.table.header.code') }}
+        </div>
+        <div />
+        <div>
+          <BadgeState
+            :label="registration.message"
+            :color="registration.color"
+          />
+        </div>
+        <div>
+          {{ registration.product }}
+        </div>
+        <div>
+          {{ registration.expiration }}
+        </div>
+        <div>
+          {{ registration.code }}
+        </div>
+        <div>
+          <AsyncButton
+            currentPhase="error"
+            :waitingLabel="t('registration.list.table.button.progress')"
+            :error-label="t('registration.list.table.button.label')"
+            :action-label="t('registration.list.table.button.label')"
+            data-testid="registration-offline-deregister-cta"
+            :disabled="isRegistering || !isRegistered"
+            @click="deregister"
+          />
+        </div>
       </div>
     </div>
   </div>
