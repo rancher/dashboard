7bbb2f9e8bc17b704b23f6db87eb09c01e232564
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-05-09 17:18:41 -0700

    updated to match 2.11 PR

--- cypress/e2e/tests/pages/virtualization-mgmt/harvester.spec.ts
+++ cypress/e2e/tests/pages/virtualization-mgmt/harvester.spec.ts
@@ -21,11 +21,18 @@ describe('Harvester', { tags: ['@virtualizationMgmt', '@adminUser'] }, () => {
     });
   });
 
+  /**
+   * Assumes that Harvester Extension is NOT installed
+   *
+   * Harvester Extension will also be removed after all tests run
+   *
+   * (pattern needs fixing)
+   */
   it('can auto install harvester and begin process of importing a harvester cluster', () => {
     cy.intercept('POST', CLUSTER_REPOS_BASE_URL).as('createHarvesterChart');
     cy.intercept('PUT', `${ CLUSTER_REPOS_BASE_URL }/${ harvesterGitRepoName }`).as('updateHarvesterChart');
     cy.intercept('POST', `${ CLUSTER_REPOS_BASE_URL }/${ harvesterGitRepoName }?action=install`).as('installHarvesterExtension');
-    cy.intercept('POST', '/v1/provisioning.cattle.io.clusters').as('createHarvesterCluster');
+    cy.intercept('POST', '/v3/clusters').as('createHarvesterCluster');
 
     // verify install button and message displays
     harvesterPo.goTo();
@@ -66,22 +73,24 @@ describe('Harvester', { tags: ['@virtualizationMgmt', '@adminUser'] }, () => {
     harvesterPo.createHarvesterClusterForm().resourceDetail().createEditView().create();
     cy.wait('@createHarvesterCluster').then(({ response }) => {
       expect(response?.statusCode).to.eq(201);
+
+      const harvesterClusterId = response.body.id;
+      const harvesterDetails = new HarvesterClusterDetailsPo(undefined, undefined, harvesterClusterId);
+
+      harvesterDetails.waitForPage(null, 'registration');
+      harvesterDetails.title().should('contain', harvesterClusterName);
+
+      // navigate to harvester list page and verify the logo and tagline do not display after cluster created
+      HarvesterClusterPagePo.navTo();
+      harvesterPo.waitForPage();
+      harvesterPo.list().resourceTable().sortableTable().rowWithName(harvesterClusterName)
+        .checkVisible();
+      harvesterPo.harvesterLogo().should('not.exist');
+      harvesterPo.harvesterTagline().should('not.exist');
+
+      // delete cluster
+      cy.deleteRancherResource('v1', 'provisioning.cattle.io.clusters', `fleet-default/${ harvesterClusterId }`);
     });
-    const harvesterDetails = new HarvesterClusterDetailsPo(undefined, undefined, harvesterClusterName);
-
-    harvesterDetails.waitForPage(null, 'registration');
-    harvesterDetails.title().should('contain', harvesterClusterName);
-
-    // navigate to harvester list page and verify the logo and tagline do not display after cluster created
-    HarvesterClusterPagePo.navTo();
-    harvesterPo.waitForPage();
-    harvesterPo.list().resourceTable().sortableTable().rowWithName(harvesterClusterName)
-      .checkVisible();
-    harvesterPo.harvesterLogo().should('not.exist');
-    harvesterPo.harvesterTagline().should('not.exist');
-
-    // delete cluster
-    cy.deleteRancherResource('v1', 'provisioning.cattle.io.clusters', `fleet-default/${ harvesterClusterName }`);
   });
 
   it('missing repo message should display when repo does NOT exist', () => {
@@ -210,7 +219,7 @@ describe('Harvester', { tags: ['@virtualizationMgmt', '@adminUser'] }, () => {
       harvesterPo.updateOrInstallButton().click();
 
       // wait for update version update
-      cy.wait('@upgradeHarvesterExtension', MEDIUM_TIMEOUT_OPT).then(({ request, response }) => {
+      cy.wait('@upgradeHarvesterExtension', LONG_TIMEOUT_OPT).then(({ request, response }) => {
         expect(response?.statusCode).to.eq(201);
         expect(request?.body?.charts[0].version).to.eq(versions[0]);
       });
--- shell/utils/uiplugins.ts
+++ shell/utils/uiplugins.ts
@@ -44,9 +44,11 @@ export async function getLatestExtensionVersion(
  *
  * @param store Vue store
  * @param name Name of the extension
+ * @param maxRetries Number of times to check for availability
+ * @param retryWait Gap (in ms) between availability checks
  * @returns the extension object when available, null if timed out waiting for it to be available
  */
-export async function waitForUIExtension(store: any, name: string): Promise<any> {
+export async function waitForUIExtension(store: any, name: string, maxRetries = MAX_RETRIES, retryWait = RETRY_WAIT): Promise<any> {
   let tries = 0;
 
   while (true) {
@@ -69,11 +71,11 @@ export async function waitForUIExtension(store: any, name: string): Promise<any>
 
     tries++;
 
-    if (tries > MAX_RETRIES) {
+    if (tries > maxRetries) {
       return null;
     }
 
-    await new Promise((resolve) => setTimeout(resolve, RETRY_WAIT));
+    await new Promise((resolve) => setTimeout(resolve, retryWait));
   }
 }
 
@@ -82,9 +84,11 @@ export async function waitForUIExtension(store: any, name: string): Promise<any>
  *
  * @param store Vue store
  * @param extension Extension object
+ * @param maxRetries Number of times to check for availability
+ * @param retryWait Gap (in MS) between availability checks
  * @returns true when available, false if timed out waiting for it to be available
  */
-export async function waitForUIPackage(store: any, extension: any): Promise<boolean> {
+export async function waitForUIPackage(store: any, extension: any, maxRetries = MAX_RETRIES, retryWait = RETRY_WAIT): Promise<boolean> {
   let tries = 0;
 
   const { name, version } = extension;
@@ -104,11 +108,11 @@ export async function waitForUIPackage(store: any, extension: any): Promise<bool
 
     tries++;
 
-    if (tries > MAX_RETRIES) {
+    if (tries > maxRetries) {
       return false;
     }
 
-    await new Promise((resolve) => setTimeout(resolve, RETRY_WAIT));
+    await new Promise((resolve) => setTimeout(resolve, retryWait));
   }
 }
 
