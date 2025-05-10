ea54d2b97a116d2c6b5c5b589fa11dece003d28e
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-05-09 17:18:41 -0700

    updated to match 2.11 PR

--- pkg/harvester-manager/edit/harvesterhci.io.management.cluster.vue
+++ pkg/harvester-manager/edit/harvesterhci.io.management.cluster.vue
@@ -1,21 +1,21 @@
 <script>
 import CreateEditView from '@shell/mixins/create-edit-view';
+
 import CruResource from '@shell/components/CruResource';
 import NameNsDescription from '@shell/components/form/NameNsDescription';
 import Tab from '@shell/components/Tabbed/Tab';
 import Tabbed from '@shell/components/Tabbed';
-import { HCI, SCHEMA, VIRTUAL_HARVESTER_PROVIDER, NORMAN } from '@shell/config/types';
-import ClusterMembershipEditor from '@shell/components/form/Members/ClusterMembershipEditor';
+import { HCI, VIRTUAL_HARVESTER_PROVIDER, NORMAN } from '@shell/config/types';
+import ClusterMembershipEditor, { canViewClusterMembershipEditor } from '@shell/components/form/Members/ClusterMembershipEditor';
 import { Banner } from '@components/Banner';
-import Labels from '@shell/edit/provisioning.cattle.io.cluster/Labels';
-import AgentEnv from '@shell/edit/provisioning.cattle.io.cluster/AgentEnv';
-import { set, get, clone } from '@shell/utils/object';
+import Labels from '@shell/components/form/Labels.vue';
+import KeyValue from '@shell/components/form/KeyValue';
+import Loading from '@shell/components/Loading';
+import { set, get } from '@shell/utils/object';
 import { CAPI as CAPI_LABEL } from '@shell/config/labels-annotations';
 import cloneDeep from 'lodash/cloneDeep';
+import { _VIEW } from '@shell/config/query-params';
 
-import { createYaml } from '@shell/utils/create-yaml';
-
-const REAL_TYPE = NORMAN.CLUSTER;
 const defaultCluster = {
   agentEnvVars: [],
   labels:       {},
@@ -33,7 +33,8 @@ export default {
     Tab,
     Tabbed,
     Labels,
-    AgentEnv
+    KeyValue,
+    Loading
   },
 
   mixins: [CreateEditView],
@@ -57,34 +58,27 @@ export default {
     if (this.value.id) {
       const liveNormanCluster = await this.value.findNormanCluster();
 
-      this.normanCluster = await store.dispatch(`rancher/clone`, { resource: liveNormanCluster });
+      this.normanCluster = await store.dispatch('rancher/clone', { resource: liveNormanCluster });
+      if ( this.normanCluster && !this.normanCluster?.agentEnvVars) {
+        this.normanCluster.agentEnvVars = [];
+      }
     } else {
       this.normanCluster = await store.dispatch('rancher/create', { type: NORMAN.CLUSTER, ...cloneDeep(defaultCluster) }, { root: true });
     }
   },
 
   data() {
-    return { membershipUpdate: {}, normanCluster: { name: '' } };
+    return {
+      membershipUpdate: {}, normanCluster: { name: '' }, VIEW: _VIEW
+    };
   },
   created() {
     this.registerAfterHook(this.saveRoleBindings, 'save-role-bindings');
   },
 
   computed: {
-    generateYaml() {
-      return () => {
-        const resource = this.normanCluster;
-
-        const inStore = this.$store.getters['currentStore'](resource);
-        const schemas = this.$store.getters[`${ inStore }/all`](SCHEMA);
-        const clonedResource = clone(resource);
-
-        delete clonedResource.isSpoofed;
-
-        const out = createYaml(schemas, REAL_TYPE, clonedResource);
-
-        return out;
-      };
+    canManageMembers() {
+      return canViewClusterMembershipEditor(this.$store);
     },
   },
 
@@ -94,8 +88,8 @@ export default {
         name:   'c-cluster-product-resource-namespace-id',
         params: {
           resource:  HCI.CLUSTER,
-          namespace: this.value.metadata.namespace,
-          id:        this.normanCluster.id,
+          namespace: this.normanCluster.provisioningClusterNs,
+          id:        this.normanCluster.provisioningClusterName,
         },
       });
     },
@@ -106,19 +100,22 @@ export default {
         [CAPI_LABEL.PROVIDER]: VIRTUAL_HARVESTER_PROVIDER,
       });
 
-      set(this.normanCluster, 'type', REAL_TYPE);
-      set(this.value, 'type', REAL_TYPE);
+      set(this.normanCluster, 'type', NORMAN.CLUSTER);
+      set(this.value, 'type', NORMAN.CLUSTER);
 
       await this.save(...arguments);
     },
+
     onMembershipUpdate(update) {
       this.membershipUpdate = update;
     },
+
     async saveRoleBindings() {
       if (this.membershipUpdate.save) {
         await this.membershipUpdate.save(this.normanCluster.id);
       }
     },
+
     async actuallySave() {
       if (this.isEdit) {
         return await this.normanCluster.save();
@@ -139,57 +136,82 @@ export default {
     :errors="errors"
     :validation-passed="true"
     :done-route="doneRoute"
-    :generate-yaml="generateYaml"
+    :can-yaml="false"
     @finish="saveOverride"
     @error="e=>errors = e"
   >
-    <Banner
-      v-if="isCreate"
-      color="warning"
-    >
-      {{ t('harvesterManager.cluster.supportMessage') }}
-    </Banner>
-    <div class="mt-20">
-      <NameNsDescription
-        v-if="!isView"
-        v-model:value="normanCluster"
-        :mode="mode"
-        :namespaced="false"
-        nameKey="name"
-        descriptionKey="description"
-        name-label="cluster.name.label"
-        name-placeholder="cluster.name.placeholder"
-        description-label="cluster.description.label"
-        description-placeholder="cluster.description.placeholder"
-      />
-    </div>
-
-    <Tabbed :side-tabs="true">
-      <Tab
-        name="memberRoles"
-        label-key="cluster.tabs.memberRoles"
-        :weight="3"
+    <Loading
+      v-if="$fetchState.pending"
+      mode="relative"
+    />
+    <div v-else>
+      <Banner
+        v-if="isCreate"
+        color="warning"
       >
-        <Banner
-          v-if="isEdit"
-          color="info"
-        >
-          {{ t('cluster.memberRoles.removeMessage') }}
-        </Banner>
-        <ClusterMembershipEditor
+        {{ t('harvesterManager.cluster.supportMessage') }}
+      </Banner>
+      <div class="mt-20">
+        <NameNsDescription
+          v-if="!isView"
+          v-model:value="normanCluster"
           :mode="mode"
-          :parent-id="normanCluster.id ? normanCluster.id : null"
-          @membership-update="onMembershipUpdate"
+          :namespaced="false"
+          :nameEditable="!isEdit"
+          nameKey="name"
+          descriptionKey="description"
+          name-label="cluster.name.label"
+          name-placeholder="cluster.name.placeholder"
+          description-label="cluster.description.label"
+          description-placeholder="cluster.description.placeholder"
         />
-      </Tab>
-      <AgentEnv
-        v-model:value="normanCluster"
-        :mode="mode"
-      />
-      <Labels
-        v-model:value="normanCluster"
-        :mode="mode"
-      />
-    </Tabbed>
+      </div>
+
+      <Tabbed :side-tabs="true">
+        <Tab
+          name="memberRoles"
+          label-key="cluster.tabs.memberRoles"
+          :weight="3"
+        >
+          <Banner
+            v-if="isEdit"
+            color="info"
+          >
+            {{ t('cluster.memberRoles.removeMessage') }}
+          </Banner>
+          <ClusterMembershipEditor
+            :mode="canManageMembers ? mode : VIEW"
+            :parent-id="normanCluster.id ? normanCluster.id : null"
+            @membership-update="onMembershipUpdate"
+          />
+        </Tab>
+        <Tab
+          name="agentEnv"
+          label-key="cluster.tabs.agentEnv"
+        >
+          <KeyValue
+            v-model:value="normanCluster.agentEnvVars"
+            :mode="mode"
+            key-name="name"
+            :as-map="false"
+            :preserve-keys="['valueFrom']"
+            :supported="(row) => typeof row.valueFrom === 'undefined'"
+            :read-allowed="true"
+            :value-can-be-empty="true"
+            :key-label="t('cluster.agentEnvVars.keyLabel')"
+            :parse-lines-from-file="true"
+          />
+        </Tab>
+        <Tab
+          name="labels"
+          label-key="generic.labelsAndAnnotations"
+        >
+          <Labels
+            v-model:value="normanCluster"
+            :mode="mode"
+          />
+        </Tab>
+      </Tabbed>
+    </div>
   </CruResource>
 </template>
