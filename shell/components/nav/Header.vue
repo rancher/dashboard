08d4b3442fd0ec0b39fd422923a85b5a15ef5677
Phillip Rak <rak.phillip@gmail.com>
2025-05-12 10:34:58 -0700

    Make width of page actions menu consistent with other page header buttons
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/components/nav/Header.vue
+++ shell/components/nav/Header.vue
@@ -859,6 +859,10 @@ export default {
       flex-direction: row;
       padding: 0;
 
+      :deep() .btn.role-tertiary {
+        width: 32px;
+      }
+
       > * {
         padding: 0 5px;
       }
