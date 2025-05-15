2aa9ebede9c2b98662a594493e9537a040e4d00d
Alexandre Alves <alexandre.alves@suse.com>
2025-05-15 17:38:45 +0100

    fix problem with space key on searchable selects + add unit tests

--- shell/components/form/Select.vue
+++ shell/components/form/Select.vue
@@ -124,7 +124,14 @@ export default {
       calculatePosition(dropdownList, component, width, this.placement);
     },
 
-    focusSearch() {
+    focusSearch(ev) {
+      const searchBox = document.querySelector('.vs__search');
+
+      // added to mitigate https://github.com/rancher/dashboard/issues/14361
+      if (!this.isSearchable || (searchBox && document.activeElement && !searchBox.contains(document.activeElement))) {
+        ev.preventDefault();
+      }
+
       this.$refs['select-input'].open = true;
 
       this.$nextTick(() => {
@@ -269,7 +276,7 @@ export default {
     @click="focusSearch"
     @keydown.enter="focusSearch"
     @keydown.down.prevent="focusSearch"
-    @keydown.space.prevent="focusSearch"
+    @keydown.space="focusSearch"
   >
     <v-select
       ref="select-input"
--- shell/components/form/__tests__/Select.test.ts
+++ shell/components/form/__tests__/Select.test.ts
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import { defineComponent } from 'vue';
 import Select from '@shell/components/form/Select.vue';
 
@@ -63,5 +63,37 @@ describe('select.vue', () => {
     // screen readers won't pick up the default "Select option" aria-label
     // from the library
     expect(vSelectInput.attributes('aria-label')).toBe('-');
+  });
+
+  it('pressing space key while focused on search should not prevent event propagation', async() => {
+    const value = 'value-1';
+    const options = [
+      { label: 'label-1', value: 'value-1' },
+      { label: 'label-2', value: 'value-2' },
+    ];
+
+    const wrapper = mount(SelectComponent, {
+      props: {
+        value,
+        label:      'some-label',
+        options,
+        searchable: true
+      }
+    });
+
+    const mockEvent = { preventDefault: jest.fn() };
+    const spyFocus = jest.spyOn(wrapper.vm, 'focusSearch');
+    const spyPreventDefault = jest.spyOn(mockEvent, 'preventDefault');
+
+    const input = wrapper.find('.unlabeled-select');
+
+    // open unlabeled-select first
+    await input.trigger('keydown.enter');
+
+    // mimic pressing space on search box inside v-select
+    await input.trigger('keydown.space', mockEvent);
+
+    expect(spyFocus).toHaveBeenCalled();
+    expect(spyPreventDefault).not.toHaveBeenCalled();
   });
 });
