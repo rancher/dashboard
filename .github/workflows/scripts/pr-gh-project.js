e76b325e13f06992867cfda1a8cf665d61055d6f
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-05-27 16:34:19 -0700

    removed console logs

--- .github/workflows/scripts/pr-gh-project.js
+++ .github/workflows/scripts/pr-gh-project.js
@@ -74,8 +74,6 @@ function getReferencedIssues(body) {
 
 function hasLabel(issue, label) {
   const labels = issue.labels || [];
-  console.log(labels)
-  console.log(label)
 
   return !!(labels.find(l => l.name.toLowerCase() === label.toLowerCase()));
 }
