f7537408b8b5acff12aa32baa33b9f7b3da355d0
yonasberhe23 <yonas.berhe@suse.com>
2025-05-29 17:15:35 -0700

    add Ingress tests for vai

--- cypress/e2e/po/pages/explorer/ingress.po.ts
+++ cypress/e2e/po/pages/explorer/ingress.po.ts
@@ -1,20 +1,23 @@
-import PagePo from '@/cypress/e2e/po/pages/page.po';
-import BaseResourceList from '@/cypress/e2e/po/lists/base-resource-list.po';
 import BurgerMenuPo from '@/cypress/e2e/po/side-bars/burger-side-menu.po';
 import ProductNavPo from '@/cypress/e2e/po/side-bars/product-side-nav.po';
-import IngressPageCreateEditPo from '@/cypress/e2e/po/edit/ingress.po';
+import { BaseListPagePo } from '@/cypress/e2e/po/pages/base/base-list-page.po';
+import { BaseDetailPagePo } from '@/cypress/e2e/po/pages/base/base-detail-page.po';
+import ResourceTablePo from '@/cypress/e2e/po/components/resource-table.po';
+import ArrayListPo from '@/cypress/e2e/po/components/array-list.po';
+import LabeledSelectPo from '@/cypress/e2e/po/components/labeled-select.po';
+import LabeledInputPo from '@/cypress/e2e/po/components/labeled-input.po';
 
-export class IngressPagePo extends PagePo {
+export class IngressListPagePo extends BaseListPagePo {
   private static createPath(clusterId: string) {
     return `/c/${ clusterId }/explorer/networking.k8s.io.ingress`;
   }
 
   urlPath(clusterId = 'local') {
-    return IngressPagePo.createPath(clusterId);
+    return IngressListPagePo.createPath(clusterId);
   }
 
   static goTo(clusterId: string): Cypress.Chainable<Cypress.AUTWindow> {
-    return super.goTo(IngressPagePo.createPath(clusterId));
+    return super.goTo(IngressListPagePo.createPath(clusterId));
   }
 
   static navTo(clusterId = 'local') {
@@ -27,22 +30,100 @@ export class IngressPagePo extends PagePo {
   }
 
   constructor(clusterId = 'local') {
-    super(IngressPagePo.createPath(clusterId));
+    super(IngressListPagePo.createPath(clusterId));
+  }
+}
+
+export class IngressCreateEditPo extends BaseDetailPagePo {
+  private static createPath(clusterId: string, namespace?: string, id?: string ) {
+    const root = `/c/${ clusterId }/explorer/networking.k8s.io.ingress`;
+
+    return id ? `${ root }/${ namespace }/${ id }` : `${ root }/create`;
   }
 
-  list() {
-    return new BaseResourceList(this.self());
+  static goTo(path: string): Cypress.Chainable<Cypress.AUTWindow> {
+    throw new Error('invalid');
   }
 
-  clickCreate() {
-    return this.list().masthead().create();
+  constructor(clusterId = 'local', namespace?: string, id?: string) {
+    super(IngressCreateEditPo.createPath(clusterId, namespace, id));
   }
 
-  listElementWithName(name:string) {
-    return this.list().resourceTable().sortableTable().rowElementWithName(name);
+  rulesList() {
+    return new ArrayListPo('section#rules .array-list-grouped');
   }
 
-  createIngressForm(id? : string): IngressPageCreateEditPo {
-    return new IngressPageCreateEditPo(id);
+  setRuleRequestHostValue(arrayListIndex: number, value: string) {
+    const item = this.rulesList().arrayListItem(arrayListIndex);
+
+    new LabeledInputPo(item.find('#host .labeled-input').type(value));
+  }
+
+  setPathTypeByLabel(arrayListIndex: number, value: string) {
+    const item = this.rulesList().arrayListItem(arrayListIndex);
+    const select = new LabeledSelectPo(item.find('.col:nth-of-type(1) .unlabeled-select'));
+
+    select.toggle();
+    select.clickOptionWithLabel(value);
+  }
+
+  setTargetServiceValueByLabel(arrayListIndex: number, value: string) {
+    const item = this.rulesList().arrayListItem(arrayListIndex);
+    const select = new LabeledSelectPo(item.find('.col:nth-of-type(2) .unlabeled-select'));
+
+    select.toggle();
+    select.clickOptionWithLabel(value);
+  }
+
+  setPortValueByLabel(arrayListIndex: number, value: string) {
+    const item = this.rulesList().arrayListItem(arrayListIndex);
+    const select = new LabeledSelectPo(item.find('.col:nth-of-type(3) .unlabeled-select'));
+
+    select.toggle();
+    select.clickOptionWithLabel(value);
+  }
+
+  certificatesList() {
+    return new ArrayListPo('section#certificates .array-list-grouped');
+  }
+
+  setSecretNameValueByLabel(arrayListIndex: number, value: string, parentIndex?: number) {
+    const item = this.certificatesList().arrayListItem(arrayListIndex, parentIndex);
+    const select = new LabeledSelectPo(item.find('.labeled-select'));
+
+    select.toggle();
+    select.clickOptionWithLabel(value);
+  }
+
+  setHostValueByIndex(arrayListIndex: number, value: string, parentIndex?: number) {
+    const item = this.certificatesList().arrayListItem(arrayListIndex, parentIndex);
+
+    new LabeledInputPo(item.find('.labeled-input').type(value));
+  }
+}
+
+export class IngressDetailPagePo extends BaseDetailPagePo {
+  private static createPath(clusterId: string, namespace: string, id: string) {
+    return `/c/${ clusterId }/explorer/networking.k8s.io.ingress/${ namespace }/${ id }`;
+  }
+
+  static goTo(path: string): Cypress.Chainable<Cypress.AUTWindow> {
+    throw new Error('invalid');
+  }
+
+  constructor(clusterId = 'local', namespace: string, id: string) {
+    super(IngressDetailPagePo.createPath(clusterId, namespace, id));
+  }
+
+  /**
+   * @param tabId - the id of the tab
+   * @param index - the index of the list (only used for 'related' tab)
+   * @returns the list of the tab
+   */
+  list(tabId: 'rules' | 'events' | 'related', index?: number) {
+    const baseSelector = `#${ tabId } [data-testid="sortable-table-list-container"]`;
+    const selector = tabId === 'related' ? `${ baseSelector }:nth-of-type(${ index })` : baseSelector;
+
+    return new ResourceTablePo(selector);
   }
 }
