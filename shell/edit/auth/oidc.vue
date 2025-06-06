6b7b4957afc7cf06f263bd4715fd54d068b56d66
Neil MacDougall <nwmac@users.noreply.github.com>
2025-06-06 17:01:01 +0100

    Add auth provider config page for Amazon Cognito (#14422)
    * Add auth provider config page for Amazon Cognito
    
    * Fix e2e test
    
    * Fix issue with cross-origin and the popup
    
    * Change name on login screen and reduce code change
    
    * Fix lint
    
    * Fix e2e tests - fix bugs in po change
    
    * Update auth-config.js
    
    * Remove rancher url from test
    
    * Address PR feedback
    
    * Use selection grid
    
    * Use selection grid po correctly
    
    * Address Phil's PR feedback

--- shell/edit/auth/oidc.vue
+++ shell/edit/auth/oidc.vue
@@ -5,6 +5,7 @@ import AuthConfig from '@shell/mixins/auth-config';
 import CruResource from '@shell/components/CruResource';
 import AllowedPrincipals from '@shell/components/auth/AllowedPrincipals';
 import FileSelector from '@shell/components/form/FileSelector';
+import { Banner } from '@components/Banner';
 import AuthBanner from '@shell/components/auth/AuthBanner';
 import AuthProviderWarningBanners from '@shell/edit/auth/AuthProviderWarningBanners';
 import AdvancedSection from '@shell/components/AdvancedSection.vue';
@@ -13,9 +14,11 @@ import { LabeledInput } from '@components/Form/LabeledInput';
 import { RadioGroup } from '@components/Form/Radio';
 import { Checkbox } from '@components/Form/Checkbox';
 import { BASE_SCOPES } from '@shell/store/auth';
+import CopyToClipboardText from '@shell/components/CopyToClipboardText.vue';
 
 export default {
   components: {
+    Banner,
     Loading,
     CruResource,
     AllowedPrincipals,
@@ -27,6 +30,7 @@ export default {
     LabeledInput,
     RadioGroup,
     Checkbox,
+    CopyToClipboardText,
   },
 
   mixins: [CreateEditView, AuthConfig],
@@ -85,7 +89,11 @@ export default {
         return false;
       }
 
-      if ( !this.customEndpoint.value ) {
+      if (this.isAmazonCognito) {
+        const { issuer } = this.model;
+
+        return !!(clientId && clientSecret && issuer);
+      } else if ( !this.customEndpoint.value ) {
         const { url, realm } = this.oidcUrls;
 
         return !!(clientId && clientSecret && url && realm);
@@ -107,6 +115,20 @@ export default {
      */
     requiredScopes() {
       return this.model.id ? (BASE_SCOPES[this.model.id] || []) ? (BASE_SCOPES[this.model.id] || [])[0].split(' ') : [] : [];
+    },
+
+    requiresCert() {
+      // We assume all do, apart from the ones here, which do not
+      return !(['cognito'].includes(this.model.id));
+    },
+
+    supportsGroupSearch() {
+      // We assume all do, apart from the ones here, which do not
+      return !(['cognito'].includes(this.model.id));
+    },
+
+    isAmazonCognito() {
+      return this.model?.id === 'cognito';
     }
   },
 
@@ -205,7 +227,9 @@ export default {
             <tr><td>{{ t(`authConfig.oidc.rancherUrl`) }}: </td><td>{{ model.rancherUrl }}</td></tr>
             <tr><td>{{ t(`authConfig.oidc.clientId`) }}: </td><td>{{ model.clientId }}</td></tr>
             <tr><td>{{ t(`authConfig.oidc.issuer`) }}: </td><td>{{ model.issuer }}</td></tr>
-            <tr><td>{{ t(`authConfig.oidc.authEndpoint`) }}: </td><td>{{ model.authEndpoint }}</td></tr>
+            <tr v-if="model.authEndpoint">
+              <td>{{ t(`authConfig.oidc.authEndpoint`) }}: </td><td>{{ model.authEndpoint }}</td>
+            </tr>
           </template>
         </AuthBanner>
 
@@ -225,6 +249,25 @@ export default {
         />
 
         <h3>{{ t(`authConfig.oidc.${NAME}`) }}</h3>
+
+        <Banner
+          v-if="!model.enabled && isAmazonCognito"
+          color="info"
+          class="mb-20 mt-0"
+          data-testid="oidc-cognito-banner"
+        >
+          <div>
+            <div
+              v-clean-html="t('authConfig.oidc.cognitoHelp', {}, true)"
+            />
+            <div class="mt-10">
+              <CopyToClipboardText
+                :plain="true"
+                :text="model.rancherUrl"
+              />
+            </div>
+          </div>
+        </Banner>
 
         <!-- Auth credentials -->
         <div class="row mb-20">
@@ -249,7 +292,10 @@ export default {
         </div>
 
         <!-- Key/Certificate -->
-        <div class="row mb-20">
+        <div
+          v-if="requiresCert"
+          class="row mb-20"
+        >
           <div class="col span-6">
             <LabeledInput
               v-model:value="model.privateKey"
@@ -283,7 +329,10 @@ export default {
         </div>
 
         <!-- Allow group search -->
-        <div class="row mb-20">
+        <div
+          v-if="supportsGroupSearch"
+          class="row mb-20"
+        >
           <div class="col span-6">
             <Checkbox
               v-model:value="model.groupSearchEnabled"
@@ -309,102 +358,59 @@ export default {
           </div>
         </div>
 
-        <!-- Generated vs Specific Endpoints -->
-        <div class="row mb-20">
-          <div class="col span-6">
-            <RadioGroup
-              v-model:value="customEndpoint.value"
-              name="customEndpoint"
-              label-key="authConfig.oidc.customEndpoint.label"
-              :labels="customEndpoint.labels"
-              :options="customEndpoint.options"
-              data-testid="oidc-custom-endpoint"
-            >
-              <template #label>
-                <h4>{{ t('authConfig.oidc.customEndpoint.label') }}</h4>
-              </template>
-            </RadioGroup>
+        <template v-if="!isAmazonCognito">
+          <!-- Generated vs Specific Endpoints -->
+          <div class="row mb-20">
+            <div class="col span-6">
+              <RadioGroup
+                v-model:value="customEndpoint.value"
+                name="customEndpoint"
+                label-key="authConfig.oidc.customEndpoint.label"
+                :labels="customEndpoint.labels"
+                :options="customEndpoint.options"
+                data-testid="oidc-custom-endpoint"
+              >
+                <template #label>
+                  <h4>{{ t('authConfig.oidc.customEndpoint.label') }}</h4>
+                </template>
+              </RadioGroup>
+            </div>
           </div>
-        </div>
 
-        <!-- Generated endpoints -->
-        <div class="row mb-20">
-          <div class="col span-6">
-            <LabeledInput
-              v-model:value="oidcUrls.url"
-              :label="t(`authConfig.oidc.url`)"
-              :mode="mode"
-              :required="!customEndpoint.value"
-              :disabled="customEndpoint.value"
-              data-testid="oidc-url"
-            />
-          </div>
-          <div class="col span-6">
-            <LabeledInput
-              v-model:value="oidcUrls.realm"
-              :label="t(`authConfig.oidc.realm`)"
-              :mode="mode"
-              :required="!customEndpoint.value"
-              :disabled="customEndpoint.value"
-              data-testid="oidc-realm"
-            />
-          </div>
-        </div>
-
-        <!-- Specific Endpoints -->
-        <div class="row mb-20">
-          <div class="col span-6">
-            <LabeledInput
-              v-model:value="model.rancherUrl"
-              :label="t(`authConfig.oidc.rancherUrl`)"
-              :mode="mode"
-              required
-              :disabled="!customEndpoint.value"
-              data-testid="oidc-rancher-url"
-            />
-          </div>
-        </div>
-
-        <div class="row mb-20">
-          <div class="col span-6">
-            <LabeledInput
-              v-model:value="model.issuer"
-              :label="t(`authConfig.oidc.issuer`)"
-              :mode="mode"
-              required
-              :disabled="!customEndpoint.value"
-              data-testid="oidc-issuer"
-            />
-          </div>
-          <div class="col span-6">
-            <LabeledInput
-              v-model:value="model.authEndpoint"
-              :label="t(`authConfig.oidc.authEndpoint`)"
-              :mode="mode"
-              :disabled="!customEndpoint.value"
-              :required="requiresAuthEndpoint"
-              data-testid="oidc-auth-endpoint"
-            />
-          </div>
-        </div>
-
-        <!-- Advanced section -->
-        <AdvancedSection :mode="mode">
+          <!-- Generated endpoints -->
           <div class="row mb-20">
             <div class="col span-6">
               <LabeledInput
-                v-model:value="model.jwksUrl"
-                :label="t(`authConfig.oidc.jwksUrl`)"
+                v-model:value="oidcUrls.url"
+                :label="t(`authConfig.oidc.url`)"
                 :mode="mode"
-                :disabled="!customEndpoint.value"
+                :required="!customEndpoint.value"
+                :disabled="customEndpoint.value"
+                data-testid="oidc-url"
               />
             </div>
             <div class="col span-6">
               <LabeledInput
-                v-model:value="model.tokenEndpoint"
-                :label="t(`authConfig.oidc.tokenEndpoint`)"
+                v-model:value="oidcUrls.realm"
+                :label="t(`authConfig.oidc.realm`)"
                 :mode="mode"
+                :required="!customEndpoint.value"
+                :disabled="customEndpoint.value"
+                data-testid="oidc-realm"
+              />
+            </div>
+          </div>
+
+          <!-- Specific Endpoints -->
+          <div class="row mb-20">
+            <div class="col span-6">
+              <LabeledInput
+                v-model:value="model.rancherUrl"
+                :label="t(`authConfig.oidc.rancherUrl`)"
+                :mode="mode"
+                required
                 :disabled="!customEndpoint.value"
+                data-testid="oidc-rancher-url"
               />
             </div>
           </div>
@@ -412,22 +418,82 @@ export default {
           <div class="row mb-20">
             <div class="col span-6">
               <LabeledInput
-                v-model:value="model.userInfoEndpoint"
-                :label="t(`authConfig.oidc.userInfoEndpoint`)"
+                v-model:value="model.issuer"
+                :label="t(`authConfig.oidc.issuer`)"
                 :mode="mode"
+                required
                 :disabled="!customEndpoint.value"
+                data-testid="oidc-issuer"
               />
             </div>
             <div class="col span-6">
               <LabeledInput
-                v-model:value="model.acrValue"
-                :label="t(`authConfig.oidc.acrValue`)"
+                v-model:value="model.authEndpoint"
+                :label="t(`authConfig.oidc.authEndpoint`)"
                 :mode="mode"
                 :disabled="!customEndpoint.value"
+                :required="requiresAuthEndpoint"
+                data-testid="oidc-auth-endpoint"
               />
             </div>
           </div>
-        </AdvancedSection>
+
+          <!-- Advanced section -->
+          <AdvancedSection :mode="mode">
+            <div class="row mb-20">
+              <div class="col span-6">
+                <LabeledInput
+                  v-model:value="model.jwksUrl"
+                  :label="t(`authConfig.oidc.jwksUrl`)"
+                  :mode="mode"
+                  :disabled="!customEndpoint.value"
+                />
+              </div>
+              <div class="col span-6">
+                <LabeledInput
+                  v-model:value="model.tokenEndpoint"
+                  :label="t(`authConfig.oidc.tokenEndpoint`)"
+                  :mode="mode"
+                  :disabled="!customEndpoint.value"
+                />
+              </div>
+            </div>
+
+            <div class="row mb-20">
+              <div class="col span-6">
+                <LabeledInput
+                  v-model:value="model.userInfoEndpoint"
+                  :label="t(`authConfig.oidc.userInfoEndpoint`)"
+                  :mode="mode"
+                  :disabled="!customEndpoint.value"
+                />
+              </div>
+              <div class="col span-6">
+                <LabeledInput
+                  v-model:value="model.acrValue"
+                  :label="t(`authConfig.oidc.acrValue`)"
+                  :mode="mode"
+                  :disabled="!customEndpoint.value"
+                />
+              </div>
+            </div>
+          </AdvancedSection>
+        </template>
+
+        <template v-if="isAmazonCognito">
+          <h3>{{ t(`authConfig.oidc.cognitoIssuer`) }}</h3>
+          <div class="row mb-20">
+            <div class="col span-6">
+              <LabeledInput
+                v-model:value="model.issuer"
+                :label="t(`authConfig.oidc.issuer`)"
+                :mode="mode"
+                required
+                data-testid="oidc-issuer"
+              />
+            </div>
+          </div>
+        </template>
       </template>
     </CruResource>
   </div>
