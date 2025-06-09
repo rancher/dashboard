dab0f6ebf55ca80b32cb60091733f1d1b51442a4
Phillip Rak <rak.phillip@gmail.com>
2025-06-09 16:19:55 -0700

    Add a modern theme/brand
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/store/prefs.js
+++ shell/store/prefs.js
@@ -57,7 +57,7 @@ export const PINNED_CLUSTERS = create('pinned-clusters', [], { parseJSON });
 export const GROUP_RESOURCES = create('group-by', 'namespace');
 export const DIFF = create('diff', 'unified', { options: ['unified', 'split'] });
 export const THEME = create('theme', 'auto', {
-  options:     ['light', 'light-modern', 'auto', 'dark', 'dark-modern'],
+  options:     ['light', 'auto', 'dark'],
   asCookie,
   parseJSON,
   mangleRead:  (x) => x.replace(/^ui-/, ''),
