c9984c1a2fc0a8bcdb6f6099b888607619fbc3d8
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2025-06-09 12:47:13 -0700

    revert removed data-testid

--- shell/pages/c/_cluster/apps/charts/index.vue
+++ shell/pages/c/_cluster/apps/charts/index.vue
@@ -396,7 +396,10 @@ export default {
   <Loading v-if="$fetchState.pending" />
   <div v-else>
     <div class="header">
-      <h1 class="m-0">
+      <h1
+        data-testid="charts-header-title"
+        class="m-0"
+      >
         {{ t('catalog.chart.header.charts') }}
       </h1>
       <AsyncButton
