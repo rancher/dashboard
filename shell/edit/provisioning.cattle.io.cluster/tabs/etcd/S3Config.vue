e2e47b57465b784505ff4b21d545dcc556b505b8
marielly freitas <mfreitas@localhost.localdomain>
2025-06-16 12:51:21 -0300

    disable edit when view mode

--- shell/edit/provisioning.cattle.io.cluster/tabs/etcd/S3Config.vue
+++ shell/edit/provisioning.cattle.io.cluster/tabs/etcd/S3Config.vue
@@ -3,6 +3,11 @@ import { Checkbox } from '@components/Form/Checkbox';
 import { LabeledInput } from '@components/Form/LabeledInput';
 import SelectOrCreateAuthSecret from '@shell/components/form/SelectOrCreateAuthSecret';
 import { NORMAN } from '@shell/config/types';
+import { _EDIT, _VIEW } from '@shell/config/query-params';
+import { isHttpsOrHttp } from '@shell/utils/validators/setting';
+import CreateEditView from '@shell/mixins/create-edit-view';
+import FormValidation from '@shell/mixins/form-validation';
+import CruResource from '@shell/components/CruResource.vue';
 
 export default {
   emits: ['update:value'],
@@ -11,7 +16,9 @@ export default {
     LabeledInput,
     Checkbox,
     SelectOrCreateAuthSecret,
+    CruResource,
   },
+  mixins: [CreateEditView, FormValidation],
 
   props: {
     mode: {
@@ -46,6 +53,20 @@ export default {
         region:              '',
         skipSSLVerify:       false,
         ...(this.value || {}),
+      },
+      fvRules: {
+        endpoint: [
+          (value) => {
+            if (!value) {
+              return true; // Assuming optional if empty
+            }
+            // If it's NOT HTTPS or HTTP, return an error message
+            if (isHttpsOrHttp(value)) {
+            // needs to change to correct way of returning errors
+              return 'Endpoint cannot start with http:// or https://';
+            }
+          },
+        ],
       }
     };
   },
@@ -62,6 +83,13 @@ export default {
 
       return {};
     },
+    isEdit() {
+      return this.mode === _EDIT;
+    },
+    isView() {
+      return this.mode === _VIEW;
+    },
+
   },
 
   methods: {
@@ -71,82 +99,104 @@ export default {
       this.$emit('update:value', out);
     },
   },
+
+  watch: {
+    fvFormIsValid: {
+      handler(newValue) {
+        this.$emit('update:configIsValid', !!newValue);
+      },
+      immediate: true,
+    },
+  }
 };
 </script>
 
 <template>
-  <div>
-    <SelectOrCreateAuthSecret
-      v-model:value="config.cloudCredentialName"
-      :register-before-hook="registerBeforeHook"
-      in-store="management"
-      :allow-ssh="false"
-      :allow-basic="false"
-      :allow-s3="true"
-      :namespace="namespace"
-      generate-name="etcd-backup-s3-"
-      :cache-secrets="true"
-      @update:value="update"
-    />
-
-    <div class="row mt-20">
-      <div class="col span-6">
-        <LabeledInput
-          v-model:value="config.bucket"
-          label="Bucket"
-          :placeholder="ccData.defaultBucket"
-          :required="!ccData.defaultBucket"
-          @update:value="update"
-        />
-      </div>
-      <div class="col span-6">
-        <LabeledInput
-          v-model:value="config.folder"
-          label="Folder"
-          :placeholder="ccData.defaultFolder"
-          @update:value="update"
-        />
-      </div>
-    </div>
-
-    <div class="row mt-20">
-      <div class="col span-6">
-        <LabeledInput
-          v-model:value="config.region"
-          label="Region"
-          :placeholder="ccData.defaultRegion"
-          @update:value="update"
-        />
-      </div>
-      <div class="col span-6">
-        <LabeledInput
-          v-model:value="config.endpoint"
-          label="Endpoint"
-          :placeholder="ccData.defaultEndpoint"
-          @update:value="update"
-        />
-      </div>
-    </div>
-
-    <div
-      v-if="!ccData.defaultSkipSSLVerify"
-      class="mt-20"
-    >
-      <Checkbox
-        v-model:value="config.skipSSLVerify"
-        :mode="mode"
-        label="Accept any certificate (insecure)"
+  <CruResource
+    ref="cruresource"
+    :mode="mode"
+    :resource="value"
+    :validation-passed="fvFormIsValid"
+    :errors="errors"
+  >
+    <div>
+      <SelectOrCreateAuthSecret
+        v-model:value="config.cloudCredentialName"
+        :register-before-hook="registerBeforeHook"
+        in-store="management"
+        :allow-ssh="false"
+        :allow-basic="false"
+        :allow-s3="true"
+        :namespace="namespace"
+        generate-name="etcd-backup-s3-"
+        :cache-secrets="true"
         @update:value="update"
       />
 
-      <LabeledInput
-        v-if="!config.skipSSLVerify"
-        v-model:value="config.endpointCA"
-        type="multiline"
-        label="Endpoint CA Cert"
-        :placeholder="ccData.defaultEndpointCA"
-        @update:value="update"
-      />
+      <div class="row mt-20">
+        <div class="col span-6">
+          <LabeledInput
+            v-model:value="config.bucket"
+            label="Bucket"
+            :disabled="isView"
+            :placeholder="ccData.defaultBucket"
+            :required="!ccData.defaultBucket"
+            @update:value="update"
+          />
+        </div>
+        <div class="col span-6">
+          <LabeledInput
+            v-model:value="config.folder"
+            label="Folder"
+            :disabled="isView"
+            :placeholder="ccData.defaultFolder"
+            @update:value="update"
+          />
+        </div>
+      </div>
+
+      <div class="row mt-20">
+        <div class="col span-6">
+          <LabeledInput
+            v-model:value="config.region"
+            label="Region"
+            :disabled="isView"
+            :placeholder="ccData.defaultRegion"
+            @update:value="update"
+          />
+        </div>
+        <div class="col span-6">
+          <LabeledInput
+            v-model:value="config.endpoint"
+            label="Endpoint"
+            :disabled="isView"
+            :placeholder="ccData.defaultEndpoint"
+            :rules="fvRules.endpoint"
+            @update:value="update"
+          />
+        </div>
+      </div>
+
+      <div
+        v-if="!ccData.defaultSkipSSLVerify"
+        class="mt-20"
+      >
+        <Checkbox
+          v-model:value="config.skipSSLVerify"
+          :mode="mode"
+          label="Accept any certificate (insecure)"
+          @update:value="update"
+        />
+
+        <LabeledInput
+          v-if="!config.skipSSLVerify"
+          v-model:value="config.endpointCA"
+          type="multiline"
+          label="Endpoint CA Cert"
+          :placeholder="ccData.defaultEndpointCA"
+          @update:value="update"
+        />
+      </div>
     </div>
-  </div>
+  </CruResource>
 </template>
--- shell/utils/validators/setting.js
+++ shell/utils/validators/setting.js
@@ -4,6 +4,8 @@ export const isServerUrl = (value) => value === 'server-url';
 
 export const isHttps = (value) => value.toLowerCase().startsWith('https://');
 
+export const isHttpsOrHttp = (value) => value.toLowerCase().startsWith('https://') || value.toLowerCase().startsWith('http://');
+
 export const isLocalhost = (value) => (/^(?:https?:\/\/)?(?:localhost|127\.0\.0\.1)/i).test(value);
 
 export const hasTrailingForwardSlash = (value) => isUrl(value) && value?.toLowerCase().endsWith('/');
