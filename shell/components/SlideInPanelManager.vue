e28101963f42c8aab503eeec7b90d463dbea0274
Alexandre Alves <alexandre.alves@suse.com>
2025-06-17 12:46:21 +0100

    fix item 2: All page content should be contained by landmarks

--- shell/components/SlideInPanelManager.vue
+++ shell/components/SlideInPanelManager.vue
@@ -111,7 +111,7 @@ function closePanel() {
         }"
         @click="closePanel"
       />
-      <div
+      <aside
         class="slide-in"
         :class="{ 'slide-in-open': isOpen }"
         :style="{
@@ -145,7 +145,7 @@ function closePanel() {
             class="dynamic-panel-content"
           />
         </div>
-      </div>
+      </aside>
     </div>
   </Teleport>
 </template>
