9010d914b8e258e1265c28141b06efd862faf12a
Phillip Rak <rak.phillip@gmail.com>
2025-06-20 11:14:18 -0700

    Fix unit test
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/components/form/__tests__/LabeledSelect.test.ts
+++ shell/components/form/__tests__/LabeledSelect.test.ts
@@ -267,7 +267,8 @@ describe('component: LabeledSelect', () => {
         value,
         label:      'some-label',
         options,
-        searchable: true
+        searchable: true,
+        loading:    false,
       }
     });
 
@@ -281,7 +282,9 @@ describe('component: LabeledSelect', () => {
     await input.trigger('keydown.enter');
 
     // mimic pressing space on search box inside v-select
-    await input.trigger('keydown.space', mockEvent);
+    const search = input.find('input');
+
+    await search.trigger('keydown.space', mockEvent);
 
     // eslint-disable-next-line
     expect(spyFocus).toHaveBeenCalled();
