9565e16efa416e456c48bc66a15a8071b907114f
Alexandre Alves <alexandre.alves@suse.com>
2025-06-23 16:41:41 +0100

    fix item 6. ARIA attributes must conform to valid values

--- shell/components/Tabbed/index.vue
+++ shell/components/Tabbed/index.vue
@@ -275,7 +275,7 @@ export default {
       >
         <a
           :data-testid="`btn-${tab.name}`"
-          :aria-controls="'#' + tab.name"
+          :aria-controls="tab.name"
           :aria-selected="tab.active"
           :aria-label="tab.labelDisplay || ''"
           role="tab"
