a35b81cbd379b726e506d6e56343c180f90f1c33
momesgin <mo.mesgin@suse.com>
2025-06-24 12:58:37 -0700

    Project scoped secrets (#13979)
    * projec scoped secrets page
    
    * project-scoped secret creation
    
    * refactor
    
    * use new label + refactor
    
    * refactor
    
    * always local cluster url for project-scoped secrets
    
    * grouping secrets by project name
    
    * fix grouping
    
    * fix namespace value
    
    * fix cancel redirect
    
    * fix done redirect
    
    * e2e tests
    
    * fix e2e test
    
    ---------
    
    Co-authored-by: Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>

--- shell/components/form/NameNsDescription.vue
+++ shell/components/form/NameNsDescription.vue
@@ -429,6 +429,7 @@ export default {
 
 <template>
   <div class="row mb-20">
+    <slot name="project-selector" />
     <div
       v-if="namespaced && !nameNsHidden && createNamespace"
       :data-testid="componentTestid + '-namespace-create'"
