920fc4ffcc736aac3679a72b2bb83ed288705bda
Neil MacDougall <nmacdougall@suse.com>
2025-06-25 21:57:08 +0100

    Fix lint

--- shell/config/product/compliance.js
+++ shell/config/product/compliance.js
@@ -14,9 +14,7 @@ export function init(store) {
     headers
   } = DSL(store, NAME);
 
-  product({
-    ifHaveGroup: /^(.*\.)*compliance\.cattle\.io$/,
-  });
+  product({ ifHaveGroup: /^(.*\.)*compliance\.cattle\.io$/ });
 
   weightType(COMPLIANCE.CLUSTER_SCAN, 3, true);
   weightType(COMPLIANCE.CLUSTER_SCAN_PROFILE, 2, true);
