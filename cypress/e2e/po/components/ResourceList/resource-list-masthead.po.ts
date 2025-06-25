de32d20bc935b78851c5969e48f0fe7e132b8e74
Cody Jackson <cody.jackson@suse.com>
2025-06-25 09:09:05 -0700

    Updating a title selector to get e2e tests to pass

--- cypress/e2e/po/components/ResourceList/resource-list-masthead.po.ts
+++ cypress/e2e/po/components/ResourceList/resource-list-masthead.po.ts
@@ -2,11 +2,11 @@ import ComponentPo from '@/cypress/e2e/po/components/component.po';
 
 export default class ResourceListMastheadPo extends ComponentPo {
   actions() {
-    return this.self().get('.actions-container .actions .btn');
+    return this.self().get('.actions-container .actions .btn, [data-testid="masthead-action-menu"]');
   }
 
   title() {
-    return this.self().get('.with-subheader .title h1, .primaryheader h1, .title-bar h1.title').invoke('text');
+    return this.self().get('.title h1, .primaryheader h1, .title-bar h1.title').invoke('text');
   }
 
   createYaml() {
