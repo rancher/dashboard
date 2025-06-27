ed0270f790c5a06540cc5a1db3506742083e7e1b
Julia Bier <juliabiersuriano@gmail.com>
2025-06-27 11:39:54 -0400

    Change location for encryption-provider-config.yaml

--- shell/utils/formatter.js
+++ shell/utils/formatter.js
@@ -3,7 +3,7 @@ import { SECRET_TYPES } from '@shell/config/secret';
 
 export function formatEncryptionSecretNames(secrets, chartNamespace) {
   return secrets
-    .filter((secret) => (secret.data || {})['encryption-provider-config.yaml'] &&
+    .filter((secret) => (secret.data || {})['/encryption/encryption-provider-config.yaml'] &&
       secret.metadata.namespace === chartNamespace &&
       !secret.metadata?.state?.error &&
       secret._type === SECRET_TYPES.OPAQUE
