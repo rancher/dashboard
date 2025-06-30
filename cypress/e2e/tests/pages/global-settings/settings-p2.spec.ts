b7b74b35234b4666fdf7eaf5ff574b45a645e647
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-06-30 09:02:15 -0700

    commented out test until we ccan use latest BE

--- cypress/e2e/tests/pages/global-settings/settings-p2.spec.ts
+++ cypress/e2e/tests/pages/global-settings/settings-p2.spec.ts
@@ -349,41 +349,42 @@ describe('Settings', { testIsolation: 'off' }, () => {
     settingsPage.settingsValue('k3s-based-upgrader-uninstall-concurrency').contains(settings['k3s-based-upgrader-uninstall-concurrency'].original);
   });
 
-  it('can update system-agent-upgrader-install-concurrency', { tags: ['@globalSettings', '@adminUser'] }, () => {
-    // Update setting
-    SettingsPagePo.navTo();
-    settingsPage.editSettingsByLabel('system-agent-upgrader-install-concurrency');
+  // GH https://github.com/rancher/rancher/issues/50883
+  // it('can update system-agent-upgrader-install-concurrency', { tags: ['@globalSettings', '@adminUser'] }, () => {
+  //     // Update setting
+  //     SettingsPagePo.navTo();
+  //     settingsPage.editSettingsByLabel('system-agent-upgrader-install-concurrency');
 
-    const settingsEdit = settingsPage.editSettings('local', 'system-agent-upgrader-install-concurrency');
+  //     const settingsEdit = settingsPage.editSettings('local', 'system-agent-upgrader-install-concurrency');
 
-    settingsEdit.waitForPage();
-    settingsEdit.title().contains('Setting: system-agent-upgrader-install-concurrency').should('be.visible');
-    settingsEdit.settingsInput().set(settings['system-agent-upgrader-install-concurrency'].new);
-    settingsEdit.saveAndWait('system-agent-upgrader-install-concurrency').then(({ request, response }) => {
-      expect(response?.statusCode).to.eq(200);
-      expect(request.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].new);
-      expect(response?.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].new);
-    });
-    settingsPage.waitForPage();
-    settingsPage.settingsValue('system-agent-upgrader-install-concurrency').contains(settings['system-agent-upgrader-install-concurrency'].new);
+  //     settingsEdit.waitForPage();
+  //     settingsEdit.title().contains('Setting: system-agent-upgrader-install-concurrency').should('be.visible');
+  //     settingsEdit.settingsInput().set(settings['system-agent-upgrader-install-concurrency'].new);
+  //     settingsEdit.saveAndWait('system-agent-upgrader-install-concurrency').then(({ request, response }) => {
+  //       expect(response?.statusCode).to.eq(200);
+  //       expect(request.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].new);
+  //       expect(response?.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].new);
+  //     });
+  //     settingsPage.waitForPage();
+  //     settingsPage.settingsValue('system-agent-upgrader-install-concurrency').contains(settings['system-agent-upgrader-install-concurrency'].new);
 
-    // Reset
-    SettingsPagePo.navTo();
-    settingsPage.waitForPage();
-    settingsPage.editSettingsByLabel('system-agent-upgrader-install-concurrency');
+  //     // Reset
+  //     SettingsPagePo.navTo();
+  //     settingsPage.waitForPage();
+  //     settingsPage.editSettingsByLabel('system-agent-upgrader-install-concurrency');
 
-    settingsEdit.waitForPage();
-    settingsEdit.title().contains('Setting: system-agent-upgrader-install-concurrency').should('be.visible');
-    settingsEdit.useDefaultButton().click();
-    settingsEdit.saveAndWait('system-agent-upgrader-install-concurrency').then(({ request, response }) => {
-      expect(response?.statusCode).to.eq(200);
-      expect(request.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].original);
-      expect(response?.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].original);
-    });
+  //     settingsEdit.waitForPage();
+  //     settingsEdit.title().contains('Setting: system-agent-upgrader-install-concurrency').should('be.visible');
+  //     settingsEdit.useDefaultButton().click();
+  //     settingsEdit.saveAndWait('system-agent-upgrader-install-concurrency').then(({ request, response }) => {
+  //       expect(response?.statusCode).to.eq(200);
+  //       expect(request.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].original);
+  //       expect(response?.body).to.have.property('value', settings['system-agent-upgrader-install-concurrency'].original);
+  //     });
 
-    settingsPage.waitForPage();
-    settingsPage.settingsValue('system-agent-upgrader-install-concurrency').contains(settings['system-agent-upgrader-install-concurrency'].original);
-  });
+  //     settingsPage.waitForPage();
+  //     settingsPage.settingsValue('system-agent-upgrader-install-concurrency').contains(settings['system-agent-upgrader-install-concurrency'].original);
+  //   });
 
   it('can update system-default-registry', { tags: ['@globalSettings', '@adminUser'] }, () => {
     // Update setting
