37489d9fec6abf37d3021d208534b49b02931beb
Francesco Torchia <francesco.torchia@suse.com>
2025-07-18 17:56:05 +0200

    Fleet show dash if source is empty
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- shell/components/formatter/FleetApplicationSource.vue
+++ shell/components/formatter/FleetApplicationSource.vue
@@ -40,27 +40,35 @@ export default {
 </script>
 
 <template>
-  <Link
-    v-if="row.source.showLink"
-    class="source-link"
-    label-key="source.display"
-    before-icon-key="source.icon"
-    url-key="source.value"
-    :row="row"
-    :value="row.source.value || ''"
-  />
-  <span v-else>
-    {{ row.source.value }}
-  </span>
-  <div
-    v-if="row.source.value && row.sourceSub.value"
-    class="text-label"
-  >
-    <Shortened
-      long-value-key="sourceSub.value"
+  <template v-if="row.source.value">
+    <Link
+      v-if="row.source.showLink"
+      class="source-link"
+      label-key="source.display"
+      before-icon-key="source.icon"
+      url-key="source.value"
       :row="row"
-      :value="row.sourceSub.display"
+      :value="row.source.value"
     />
+    <span v-else>
+      {{ row.source.value }}
+    </span>
+    <div
+      v-if="row.sourceSub.value"
+      class="text-label"
+    >
+      <Shortened
+        long-value-key="sourceSub.value"
+        :row="row"
+        :value="row.sourceSub.display"
+      />
+    </div>
+  </template>
+  <div
+    v-else
+    class="text-muted"
+  >
+    &mdash;
   </div>
 </template>
 
