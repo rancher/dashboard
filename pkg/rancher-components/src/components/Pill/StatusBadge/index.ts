3e0acdcb547100643697f367d9e71dbfdd619471
Cody Jackson <cody.jackson@suse.com>
2025-07-22 08:37:45 -0700

    status badge

---
+++ pkg/rancher-components/src/components/Pill/StatusBadge/index.ts
@@ -0,0 +1,2 @@
+export { default } from './StatusBadge.vue';
+export type { Shape, Status } from './types';
--- pkg/rancher-components/src/components/Pill/StatusIndicator/types.ts
+++ pkg/rancher-components/src/components/Pill/StatusIndicator/types.ts
@@ -1,5 +1,4 @@
-export type Shape = 'disc' | 'horizontal-bar' | 'vertical-bar';
-export type Status = 'info' | 'success' | 'warning' | 'error' | 'unknown' | 'none';
+import { Shape, Status } from '@components/Pill/types';
 
 export interface CardProps {
   shape: Shape;
