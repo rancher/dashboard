6058587b10d8265458775eacbe7d218faa17dad7
momesgin <mo.mesgin@suse.com>
2025-08-08 16:57:56 -0700

    Improve performance in apps/charts page by optimizing LazyImage component (#14906)
    * utilize observer in LazyImage to load the image only when it's in viewport
    
    * fix unit tests
    
    * display error image only when src is falsy or got an error
    
    * removed unnecessary data block
    
    * initialize non-reactive data
    
    * move non-reactive data
    
    ---------
    
    Co-authored-by: Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>

--- shell/components/LazyImage.vue
+++ shell/components/LazyImage.vue
@@ -1,9 +1,11 @@
 <script>
+import { BLANK_IMAGE } from '@shell/utils/style';
+
 export default {
   props: {
     initialSrc: {
       type:    String,
-      default: require('@shell/assets/images/generic-catalog.svg'),
+      default: BLANK_IMAGE,
     },
 
     errorSrc: {
@@ -20,29 +22,83 @@ export default {
   watch: {
     src(neu, old) {
       if (neu !== old) {
-        this.loadImage();
+        // Show error image if src is falsy
+        if (!neu) {
+          return this.onError();
+        }
+
+        if (this.intersected) {
+          // The component is in the viewport, load the new image right away
+          this.loadImage();
+        } else if (!this.observer) {
+          // The component is not in the viewport and not being observed,
+          // which can happen if `src` was null during mount.
+          // Start observing.
+          this.startObserver();
+        }
       }
     }
   },
 
+  created() {
+    // initialize non-reactive data
+    this.observer = null;
+    this.intersected = false;
+    this.boundError = null;
+  },
+
   mounted() {
-    this.loadImage();
+    // Show error image if src is falsy
+    if (!this.src) {
+      this.onError();
+    } else {
+      this.startObserver();
+    }
   },
 
   beforeUnmount() {
     const img = this.$refs.img;
 
-    if (img) {
+    if (img && this.boundError) {
       img.removeEventListener('error', this.boundError);
+    }
+
+    if (this.observer) {
+      this.observer.disconnect();
+      this.observer = null;
     }
   },
 
   methods: {
+    startObserver() {
+      if (this.src && !this.observer && this.$refs.img) {
+        this.observer = new IntersectionObserver((entries) => {
+          const image = entries[0];
+
+          if (image.isIntersecting) {
+            this.intersected = true;
+            this.loadImage();
+            // Once the image is loaded, we don't need the observer anymore
+            if (this.observer) {
+              this.observer.disconnect();
+              this.observer = null;
+            }
+          }
+        });
+        this.observer.observe(this.$refs.img);
+      }
+    },
+
     // Ensure we load the image when the source changes
     loadImage() {
       const img = this.$refs.img;
 
       if (this.src) {
+        // Remove previous error listener if any
+        if (img && this.boundError) {
+          img.removeEventListener('error', this.boundError);
+        }
+
         img.setAttribute('src', this.src);
         this.boundError = this.onError.bind(this);
 
