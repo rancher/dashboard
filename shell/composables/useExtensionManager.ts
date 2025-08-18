c039e1222144ff140142659796d5b366d708261f
Phillip Rak <rak.phillip@gmail.com>
2025-08-18 10:14:47 -0700

    Rename `usePlugin` to `useExtensionManager`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/composables/useExtensionManager.ts
+++ shell/composables/useExtensionManager.ts
@@ -1,11 +1,11 @@
 import { Plugin } from '@shell/types/plugin';
 import { getExtensionManager } from '~/shell/core/extension-manager-impl';
 
-export const usePlugin = (): Plugin => {
+export const useExtensionManager = (): Plugin => {
   const plugin = getExtensionManager();
 
   if (!plugin) {
-    throw new Error('usePlugin must be called after the plugin has been initialized');
+    throw new Error('useExtensionManager must be called after the plugin has been initialized');
   }
 
   return plugin;
