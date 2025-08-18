38ded99ac87603464042c6791f4d2237371d93cb
Phillip Rak <rak.phillip@gmail.com>
2025-08-18 14:11:41 -0700

    Add a deprecation warning to `this.$plugin`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/core/plugins.js
+++ shell/core/plugins.js
@@ -2,7 +2,26 @@ import { initExtensionManager } from './extension-manager-impl';
 
 export default function(context, inject) {
   const extensionManager = initExtensionManager(context);
+  const deprecationMessage = '[DEPRECATED] `this.$plugin` is deprecated and will be removed in a future version. Use `this.$extension` instead.';
 
-  inject('plugin', extensionManager);
+  inject('plugin', deprecationProxy(extensionManager, deprecationMessage));
   inject('extension', extensionManager);
 }
+
+/**
+ * Proxy to log a deprecation warning when target is accessed
+ * @param {*} target the object to proxy
+ * @param {*} message the deprecation warning to print to the console
+ * @returns The proxied target that prints a deprecation warning when target is
+ * accessed
+ */
+const deprecationProxy = (target, message) => {
+  return new Proxy(target, {
+    get(target, prop) {
+      // eslint-disable-next-line no-console
+      console.warn(message);
+
+      return Reflect.get(target, prop);
+    }
+  });
+};
