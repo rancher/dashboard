6abffc32e0f78de02376604facd88a61f7c4d167
Phillip Rak <rak.phillip@gmail.com>
2025-08-20 15:54:52 -0700

    Print deprecation messages only in dev builds
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/core/plugins.js
+++ shell/core/plugins.js
@@ -9,19 +9,25 @@ export default function(context, inject) {
 }
 
 /**
- * Proxy to log a deprecation warning when target is accessed
+ * Proxy to log a deprecation warning when target is accessed. Only prints
+ * deprecation warnings in dev builds.
  * @param {*} target the object to proxy
  * @param {*} message the deprecation warning to print to the console
  * @returns The proxied target that prints a deprecation warning when target is
  * accessed
  */
 const deprecationProxy = (target, message) => {
-  return new Proxy(target, {
+  const deprecationHandler = {
     get(target, prop) {
       // eslint-disable-next-line no-console
       console.warn(message);
 
       return Reflect.get(target, prop);
     }
-  });
+  };
+
+  // an empty handler allows the proxy to behave just like the original target
+  const proxyHandler = !!process.env.dev ? deprecationHandler : {};
+
+  return new Proxy(target, proxyHandler);
 };
