db2452132cd1f903b0bf4db2c5dd7ca7eaa0ecba
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-09-05 14:22:01 -0700

    skipped rke-template test

--- cypress/e2e/tests/pages/manager/rke-templates.spec.ts
+++ cypress/e2e/tests/pages/manager/rke-templates.spec.ts
@@ -3,7 +3,8 @@ import ClusterManagerCreateRke1CustomPagePo from '@/cypress/e2e/po/edit/provisio
 import RkeTemplatesPagePo from '@/cypress/e2e/po/pages/cluster-manager/rke-templates.po';
 import EmberModalPo from '@/cypress/e2e/po/components/ember/ember-modal.po';
 
-describe('RKE Templates', { testIsolation: 'off', tags: ['@manager', '@adminUser', '@ember'] }, () => {
+// Skipping due to CI failures. opened gh #15350 to undo
+describe.skip('RKE Templates', { testIsolation: 'off', tags: ['@manager', '@adminUser', '@ember'] }, () => {
   const rkeTemplatesPage = new RkeTemplatesPagePo();
   const clusterList = new ClusterManagerListPagePo();
   const promptRemove = new EmberModalPo();
