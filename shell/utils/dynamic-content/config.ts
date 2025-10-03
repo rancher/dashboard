abb599226e7b01a4a4b5c5145af7c2d61a5f4a8c
Neil MacDougall <nmacdougall@suse.com>
2025-10-03 11:41:23 +0100

    Use CDN endpoint

--- shell/utils/dynamic-content/config.ts
+++ shell/utils/dynamic-content/config.ts
@@ -4,10 +4,7 @@ import { Configuration, Distribution } from './types';
 import { MANAGEMENT } from '@shell/config/types';
 
 // Default endpoint ($dist is either 'community' or 'prime')
-// const DEFAULT_ENDPOINT = 'https://SERVER/rancher/$dist';
-
-// TODO: Update with the default production endpoint
-const DEFAULT_ENDPOINT = '';
+const DEFAULT_ENDPOINT = 'https://updates.rancher.io/rancher/$dist/index.yaml';
 
 // We only support retrieving content from secure endpoints
 const HTTPS_PREFIX = 'https://';
@@ -47,23 +44,12 @@ export function getConfig(getters: any): Configuration {
     const urlSetting = getters['management/byId'](MANAGEMENT.SETTING, SETTING.DYNAMIC_CONTENT_ENDPOINT);
 
     // Are we Prime, do we have a URL and does the URL start with the https prefix? If so, use it
-    // TODO: ====================================================================================
-    // TODO: Put this back in when the default URL is set
-    // if (prime && urlSetting?.value && urlSetting.value.startsWith(HTTPS_PREFIX)) {
-    // TODO: ====================================================================================
-    if (urlSetting?.value && urlSetting.value.startsWith(HTTPS_PREFIX)) {
+    if (prime && urlSetting?.value && urlSetting.value.startsWith(HTTPS_PREFIX)) {
       config.endpoint = urlSetting.value;
     }
   } catch (e) {
     console.error('Error reading dynamic content settings', e); // eslint-disable-line no-console
   }
-
-  // TODO: Remove this code ===================================================================================
-  // For testing, there is no default endpoint, so if not specified in config, then this feature is not enabled
-  if (!config.endpoint) {
-    config.enabled = false;
-  }
-  // END TODO: Remove this code ===============================================================================
 
   return config;
 }
