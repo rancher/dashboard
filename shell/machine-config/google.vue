0c3e06569ab23d2ded5bdc09e939801bd3ab8041
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-10-02 21:13:25 -0700

    [2.12] Fixed adding a new machine pool to existing GCE instance (#15568)

--- shell/machine-config/google.vue
+++ shell/machine-config/google.vue
@@ -78,7 +78,11 @@ export default {
     disabled: {
       type:    Boolean,
       default: false
-    }
+    },
+    poolCreateMode: {
+      type:    Boolean,
+      default: true
+    },
   },
 
   async fetch() {
@@ -122,7 +126,7 @@ export default {
     };
   },
   created() {
-    if (this.mode === _CREATE) {
+    if (this.poolCreateMode) {
       this.$emit('validationChanged', false);
       this.value.project = this.projectId;
       for (const key in this.defaultConfig) {
@@ -156,7 +160,7 @@ export default {
     'value.setExternalFirewallRulePrefix'(neu) {
       if (!neu) {
         this.value.openPort = [];
-      } else if (this.isCreate) {
+      } else if (this.poolCreateMode) {
         this.value.openPort.push('6443');
       } else {
         this.value.openPort = this.originalOpenPort.length > 0 ? this.originalOpenPort : ['6443'];
@@ -333,7 +337,7 @@ export default {
 
         if (!cur ) {
           // If default is not actually available, reset
-          if (this.isCreate) {
+          if (this.poolCreateMode) {
             this.value.diskType = '';
           }
         } else {
@@ -447,6 +451,7 @@ export default {
         :originalMachineImage="originalMachineImage"
         :mode="mode"
         :location="location"
+        :pool-create-mode="poolCreateMode"
         :rules="{machineImage: fvGetAndReportPathRules('machineImage')}"
         @min-disk-changed="(val)=>minDiskFromImage=val"
         @error="(val)=>errors.push(val)"
@@ -493,7 +498,7 @@ export default {
           label-key="cluster.machineConfig.gce.network.label"
           :mode="mode"
           :options="networkOptions"
-          :disabled="!isCreate"
+          :disabled="!poolCreateMode"
           option-key="name"
           option-label="label"
           :loading="loadingNetworks"
@@ -507,7 +512,7 @@ export default {
           label-key="cluster.machineConfig.gce.subnetwork.label"
           :mode="mode"
           :options="subnetworkOptions"
-          :disabled="!isCreate"
+          :disabled="!poolCreateMode"
           option-key="name"
           option-label="name"
           :loading="loadingNetworks"
