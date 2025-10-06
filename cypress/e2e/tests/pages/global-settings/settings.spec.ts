54d2591cf9b5d607685a35515734eb8af5577818
Alexandre Alves <alexandre.alves@suse.com>
2025-10-06 11:08:42 +0100

    fix e2e test

--- cypress/e2e/tests/pages/global-settings/settings.spec.ts
+++ cypress/e2e/tests/pages/global-settings/settings.spec.ts
@@ -91,7 +91,7 @@ describe('Settings', { testIsolation: 'off' }, () => {
       // we need to do this so that the timer on the modal doesn't go out too quickly
       // not giving enough time to assert the contents of the modal
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(5000);
+      cy.wait(10000);
 
       cy.wait('@getUpdatedUserActivity', { timeout: 15000 });
 
