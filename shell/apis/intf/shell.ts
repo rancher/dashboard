965356a103995aaaa2e4c1968ad713f898c9a590
Alexandre Alves <alexandre.alves@suse.com>
2025-10-10 14:12:43 +0100

    working on integrating notification center with shell API

--- shell/apis/intf/shell.ts
+++ shell/apis/intf/shell.ts
@@ -1,4 +1,5 @@
 import { Component } from 'vue';
+import { NotificationLevel } from '@shell/types/notifications';
 
 export type VALID_GROWL_TYPES = 'success' | 'info' | 'warning' | 'error';
 /**
@@ -209,29 +210,38 @@ export interface SlideInApi {
   open(component: Component, config?: SlideInConfig): void;
 }
 
-export interface NotificationsApi {
+export interface NotificationApi {
   /**
-   * Opens a slide in panel in Rancher UI
+   * Sends a notification to the Rancher UI Notification Center
    *
    * Example:
    * ```ts
-   * import MyCustomSlideIn from '@/components/MyCustomSlideIn.vue';
+   * import { NotificationLevel } from '@shell/types/notifications';
    *
-   * this.$shell.slideIn.open(MyCustomSlideIn, {
-   *   title: 'Hello from SlideIn panel!'
-   * });
+   * this.$shell.notification.send(NotificationLevel.Success, 'Some notification title', 'Hello world! Success!', {})
    * ```
    *
    * For usage with the Composition API check usage guide [here](../../shell-api#using-composition-api-in-vue).
    *
-   * @param component
-   * The Vue component to be displayed inside the slide in panel.
-   * This can be any SFC (Single-File Component) imported and passed in as a `Component`.
+   * @param level
+   * Notification level
+   * The `level` specifies the importance of the notification and determines the icon that is shown in the notification
    *
-   * @param config Slide-In configuration object
+   * |Level|Purpose|Icon|
+   * |---|---|---|
+   * |Announcement|An announcement. To be used when we want to inform on high-interest topics - news, updates, changes, scheduled maintenance, etc. E.g. “New version available!”|<img class="svg-blue" src="https://raw.githubusercontent.com/rancher/icons/refs/heads/master/svg/notify-announcement.svg"/>|
+   * |Task|A task that is underway. To be used when we want to inform on a process taking place - on-going actions that might take a while. E.g. “Cluster provisioning in progress”. The progress bar will also be shown if the `progress` field is set|<img class="svg-blue" src="https://raw.githubusercontent.com/rancher/icons/refs/heads/master/svg/notify-busy.svg"/>|
+   * |Info|Information notification. To be used when we want to inform on low-interest topics. E.g. “Welcome to Rancher v2.8"|<img class="svg-blue" src="https://raw.githubusercontent.com/rancher/icons/refs/heads/master/svg/notify-info.svg"/>|
+   * |Success|Notification that something has completed successfully. To be used when we want to confirm a successful action was completed. E.g. “Cluster provisioning completed”|<img class="svg-green" src="https://raw.githubusercontent.com/rancher/icons/refs/heads/master/svg/notify-tick.svg"/>|
+   * |Warning|Notification of a warning. To be used when we want to warn about a potential risk. E.g. “Nodes limitation warning”|<img class="svg-orange" src="https://raw.githubusercontent.com/rancher/icons/refs/heads/master/svg/notify-warning.svg"/>|
+   * |Error|Notification of an error. To be used when we want to alert on a confirmed risk. E.g. “Extension failed to load”|<img class="svg-red" src="https://raw.githubusercontent.com/rancher/icons/refs/heads/master/svg/notify-error.svg"/>|
+   *
+   * @param title The notification title
+   * @param message The notification message to be displayed
+   * @param config Notifications configuration object
    *
    */
-  send(notification: any): void;
+  send(level: NotificationLevel, title: string, message?:string, config?: any): void;
 }
 
 /**
@@ -257,5 +267,5 @@ export interface ShellApi {
   /**
    * Provides access to the Notification Center API
    */
-  get notifications(): NotificationsApi;
+  get notification(): NotificationApi;
 }
