af56af3cb1d7f6e65a230484bcd1505d71f7b62e
Phillip Rak <rak.phillip@gmail.com>
2025-10-10 18:03:57 -0700

    Prefer `all` in favor of `logoutAll`
    `logoutAll` exists only for backward compatibility as the same handler serves both old and new endpoints.
    
    Co-authored-by: Peter Matseykanets <pmatseykanets@gmail.com>

--- shell/store/auth.js
+++ shell/store/auth.js
@@ -408,7 +408,7 @@ export const actions = {
 
     // SLO - Single-sign logout - will logout auth provider from all places where it's logged in
     if (options.slo) {
-      logoutAction = '?logoutAll';
+      logoutAction = '?all';
       data.finalRedirectUrl = returnTo({ isSlo: true }, this);
     }
 
