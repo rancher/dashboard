5af897e34c2e764116ac2fd4647ae1e28c282515
Alexandre Alves <alexandre.alves@suse.com>
2025-10-15 09:39:45 +0100

    add system api to shell api

--- shell/pages/home.vue
+++ shell/pages/home.vue
@@ -458,16 +458,7 @@ export default defineComponent({
       }
 
       return pagination;
-    },
-    testmodal() {
-      this.$shell.modal.open(CommunityLinks);
-    },
-    testslide() {
-      this.$shell.slideIn.open(CommunityLinks, { showHeader: false });
-    },
-    testgrowl() {
-      this.$shell.growl.show('success', 'Hello!');
-    },
+    }
   }
 });
 
@@ -499,15 +490,6 @@ export default defineComponent({
               v-if="mcm"
               class="col span-12"
             >
-              <button @click="testmodal">
-                modal
-              </button>
-              <button @click="testslide">
-                slide
-              </button>
-              <button @click="testgrowl">
-                growl
-              </button>
               <PaginatedResourceTable
                 v-if="provClusterSchema"
                 :schema="provClusterSchema"
