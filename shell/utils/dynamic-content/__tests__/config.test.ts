69d8fe9ad91ebc6ba90dd348286e7f8131160028
Neil MacDougall <nmacdougall@suse.com>
2025-10-16 16:43:49 +0100

    Fix first round of PR feedback and update tests

--- shell/utils/dynamic-content/__tests__/config.test.ts
+++ shell/utils/dynamic-content/__tests__/config.test.ts
@@ -2,8 +2,10 @@ import { getConfig } from '../config';
 import { SETTING } from '@shell/config/settings';
 import * as version from '@shell/config/version';
 
+const DEFAULT_ENDPOINT = 'https://updates.rancher.io/rancher/$dist/updates';
+
 // Mock dependencies
-jest.mock('@shell/config/version', () => ({ getVersionData: jest.fn() }));
+jest.mock('@shell/config/version', () => ({ getVersionData: jest.fn(), isRancherPrime: jest.fn() }));
 
 describe('getConfig', () => {
   let mockGetters: any;
@@ -11,6 +13,7 @@ describe('getConfig', () => {
   beforeEach(() => {
     // Reset mocks before each test
     (version.getVersionData as jest.Mock).mockClear();
+    (version.isRancherPrime as jest.Mock).mockClear();
 
     // Default mock for getters
     mockGetters = { 'management/byId': jest.fn() };
@@ -19,6 +22,7 @@ describe('getConfig', () => {
   describe('community distribution', () => {
     beforeEach(() => {
       (version.getVersionData as jest.Mock).mockReturnValue({ RancherPrime: 'false' });
+      (version.isRancherPrime as jest.Mock).mockReturnValue(false);
     });
 
     it('should return default community config when no settings are present', () => {
@@ -27,9 +31,8 @@ describe('getConfig', () => {
 
       expect(config.prime).toBe(false);
       expect(config.distribution).toBe('community');
-      expect(config.endpoint).toBe('');
-      // Enabled is false because the default endpoint is empty
-      expect(config.enabled).toBe(false);
+      expect(config.endpoint).toBe(DEFAULT_ENDPOINT);
+      expect(config.enabled).toBe(true);
       expect(config.debug).toBe(false);
       expect(config.log).toBe(false);
     });
@@ -72,7 +75,7 @@ describe('getConfig', () => {
       expect(config.log).toBe(true);
     });
 
-    it('should use the endpoint from settings if it is a valid HTTPS URL', () => {
+    it('should not use the endpoint from settings when community', () => {
       mockGetters['management/byId'].mockImplementation((type: string, id: string) => {
         if (id === SETTING.DYNAMIC_CONTENT_ENDPOINT) {
           return { value: 'https://custom.endpoint/rancher/$dist' };
@@ -83,7 +86,7 @@ describe('getConfig', () => {
 
       const config = getConfig(mockGetters);
 
-      expect(config.endpoint).toBe('https://custom.endpoint/rancher/$dist');
+      expect(config.endpoint).toBe(DEFAULT_ENDPOINT);
       expect(config.enabled).toBe(true);
     });
   });
@@ -91,6 +94,7 @@ describe('getConfig', () => {
   describe('prime distribution', () => {
     beforeEach(() => {
       (version.getVersionData as jest.Mock).mockReturnValue({ RancherPrime: 'true' });
+      (version.isRancherPrime as jest.Mock).mockReturnValue(true);
     });
 
     it('should return default prime config when no settings are present', () => {
@@ -99,9 +103,8 @@ describe('getConfig', () => {
 
       expect(config.prime).toBe(true);
       expect(config.distribution).toBe('prime');
-      expect(config.endpoint).toBe('');
-      // Enabled is false because the default endpoint is empty
-      expect(config.enabled).toBe(false);
+      expect(config.endpoint).toBe(DEFAULT_ENDPOINT);
+      expect(config.enabled).toBe(true);
     });
 
     it('should be disabled when the setting is "false"', () => {
@@ -149,34 +152,18 @@ describe('getConfig', () => {
 
       const config = getConfig(mockGetters);
 
-      expect(config.endpoint).toBe(''); // Falls back to default
-      expect(config.enabled).toBe(false); // Disabled because no valid endpoint
+      expect(config.endpoint).toBe(DEFAULT_ENDPOINT); // Falls back to default
+      expect(config.enabled).toBe(true);
     });
   });
 
   describe('general', () => {
-    it('should be disabled if no endpoint is configured, regardless of other settings', () => {
-      (version.getVersionData as jest.Mock).mockReturnValue({ RancherPrime: 'true' });
-      mockGetters['management/byId'].mockImplementation((type: string, id: string) => {
-        if (id === SETTING.DYNAMIC_CONTENT_ENABLED) {
-          return { value: 'true' }; // Explicitly enabled
-        }
-
-        // No endpoint setting
-        return null;
-      });
-
-      const config = getConfig(mockGetters);
-
-      expect(config.endpoint).toBe('');
-      expect(config.enabled).toBe(false);
-    });
-
     it('should handle exceptions when reading settings and return defaults', () => {
       const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
       const testError = new Error('Something went wrong');
 
       (version.getVersionData as jest.Mock).mockReturnValue({ RancherPrime: 'false' });
+      (version.isRancherPrime as jest.Mock).mockReturnValue(false);
       mockGetters['management/byId'].mockImplementation(() => {
         throw testError;
       });
@@ -186,8 +173,8 @@ describe('getConfig', () => {
       // It should fall back to the default configuration
       expect(config.prime).toBe(false);
       expect(config.distribution).toBe('community');
-      expect(config.endpoint).toBe('');
-      expect(config.enabled).toBe(false); // Because endpoint is empty
+      expect(config.endpoint).toBe(DEFAULT_ENDPOINT);
+      expect(config.enabled).toBe(true);
       expect(config.debug).toBe(false);
       expect(config.log).toBe(false);
 
