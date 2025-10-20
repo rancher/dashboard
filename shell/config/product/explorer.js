f560346a234d6c5e8cf638f36d000efd51f01026
Phillip Rak <rak.phillip@gmail.com>
2025-10-20 14:27:47 -0700

    Add missing POD component for type
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/config/product/explorer.js
+++ shell/config/product/explorer.js
@@ -125,6 +125,8 @@ export function init(store) {
     componentForType(WORKLOAD_TYPES[key], WORKLOAD);
   }
 
+  componentForType(POD, WORKLOAD);
+
   ignoreType(MANAGEMENT.GLOBAL_DNS_PROVIDER); // Old, managed in multi-cluster-apps
   ignoreType('events.k8s.io.event'); // Old, moved into core
   ignoreType('extensions.ingress'); // Old, moved into networking
