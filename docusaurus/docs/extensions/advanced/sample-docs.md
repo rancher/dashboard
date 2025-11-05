c0f8d12b4fa760732ddbfe6266396142a537f216
Alexandre Alves <alexandre.alves@suse.com>
2025-11-05 12:32:32 +0000

    fix merge conflicts

---
+++ docusaurus/docs/extensions/advanced/sample-docs.md
@@ -0,0 +1,12 @@
+---
+id: sample-docs
+title: Sample API Reference
+---
+
+This page provides a reference for the custom functions available in our sample module.
+
+### Functions
+
+| Function | Description |
+| :--- | :--- |
+| [`minify()`](./functions/minify) | Responsible for minifying a given string containing JavaScript code. |
--- shell/apis/shell/growl.ts
+++ shell/apis/shell/growl.ts
@@ -8,6 +8,19 @@ export class GrowlApiImpl implements GrowlApi {
     this.store = store;
   }
 
+  /**
+   * Dispatches a growl notification.
+   *
+   * @param config - Configuration for the growl notification.
+   * - If `message` is a string, it is treated as the main content of the notification.
+   * - If `message` is a `DetailedMessage` object, `title` and `description` are extracted.
+   *
+   * Example:
+   * ```ts
+   * this.$shell.growl({ message: 'Operation successful!', type: 'success' });
+   * this.$shell.growl({ message: { title: 'Warning', description: 'Check your input.' }, type: 'warning' });
+   * ```
+   */
   public show(config: GrowlMessage): void {
     const { type = 'error' } = config;
 
