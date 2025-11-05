30bc4753b84596564beb8687038834e67fc76c2a
Alexandre Alves <alexandre.alves@suse.com>
2025-11-05 13:03:04 +0000

    fix merge conflicts

--- shell/apis/shell/index.ts
+++ shell/apis/shell/index.ts
@@ -1,18 +1,23 @@
 import { Store } from 'vuex';
-import { ModalApi, ShellApi, SlideInApi, NotificationApi } from '@shell/apis/intf/shell';
+import {
+  ModalApi, ShellApi, SlideInApi, NotificationApi, SystemApi
+} from '@shell/apis/intf/shell';
 import { ModalApiImpl } from './modal';
 import { SlideInApiImpl } from './slide-in';
 import { NotificationApiImpl } from './notifications';
+import { SystemApiImpl } from './system';
 
 export class ShellApiImpl implements ShellApi {
   private modalApi: ModalApi;
   private slideInApi: SlideInApi;
   private notificationApi: NotificationApi;
+  private systemApi: SystemApi;
 
   constructor(store: Store<any>) {
     this.modalApi = new ModalApiImpl(store);
     this.slideInApi = new SlideInApiImpl(store);
     this.notificationApi = new NotificationApiImpl(store);
+    this.systemApi = new SystemApiImpl(store);
   }
 
   get modal(): ModalApi {
@@ -25,5 +30,9 @@ export class ShellApiImpl implements ShellApi {
 
   get notification(): NotificationApi {
     return this.notificationApi;
+  }
+
+  get system(): SystemApi {
+    return this.systemApi;
   }
 }
