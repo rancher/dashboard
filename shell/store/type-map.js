ee7b1fa2565ded4e0e448c2ada8f3a5bba201fdd
Phillip Rak <rak.phillip@gmail.com>
2025-11-07 08:48:13 -0700

    Replace all instances of `rootState.$plugin`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/store/type-map.js
+++ shell/store/type-map.js
@@ -2026,7 +2026,7 @@ function hasCustom(state, rootState, kind, key, fallback) {
   }
 
   // Check to see if the custom kind is provided by a plugin (ignore booleans)
-  const pluginComponent = rootState.$plugin.getDynamic(kind, key);
+  const pluginComponent = rootState.$extension.getDynamic(kind, key);
 
   if (typeof pluginComponent !== 'boolean' && !!pluginComponent) {
     cache[key] = true;
@@ -2046,7 +2046,7 @@ function hasCustom(state, rootState, kind, key, fallback) {
 }
 
 function loadExtension(rootState, kind, key, fallback) {
-  const ext = rootState.$plugin.getDynamic(kind, key);
+  const ext = rootState.$extension.getDynamic(kind, key);
 
   if (ext) {
     if (typeof ext === 'function') {
