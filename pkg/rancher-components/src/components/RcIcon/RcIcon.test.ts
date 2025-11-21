853b5d7d08f04e4282fce7b24b587673a587cec4
Cody Jackson <cody.jackson@suse.com>
2025-11-21 14:21:30 -0700

    Updated the unit test

--- pkg/rancher-components/src/components/RcIcon/RcIcon.test.ts
+++ pkg/rancher-components/src/components/RcIcon/RcIcon.test.ts
@@ -13,4 +13,39 @@ describe('rcIcon.vue', () => {
     expect(wrapper.classes()).toContain('icon-search');
     expect(wrapper.classes()).toContain('medium');
   });
+
+  it('sets aria-hidden to true by default', () => {
+    const wrapper = shallowMount(RcIcon, {
+      props: {
+        size: 'medium',
+        type: 'search',
+      },
+    });
+
+    expect(wrapper.attributes('aria-hidden')).toBe('true');
+  });
+
+  it('sets aria-hidden to false when explicitly provided', () => {
+    const wrapper = shallowMount(RcIcon, {
+      props: {
+        size:       'medium',
+        type:       'search',
+        ariaHidden: false,
+      },
+    });
+
+    expect(wrapper.attributes('aria-hidden')).toBe('false');
+  });
+
+  it('sets aria-hidden to true when explicitly provided', () => {
+    const wrapper = shallowMount(RcIcon, {
+      props: {
+        size:       'medium',
+        type:       'search',
+        ariaHidden: true,
+      },
+    });
+
+    expect(wrapper.attributes('aria-hidden')).toBe('true');
+  });
 });
