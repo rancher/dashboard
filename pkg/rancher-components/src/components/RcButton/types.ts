6b067f9408932aa05edce83c8ac8465bbe93928d
Cody Jackson <cody.jackson@suse.com>
2025-11-24 17:51:42 -0700

    Attempting to use the RcButton without the RcIcon import.

--- pkg/rancher-components/src/components/RcButton/types.ts
+++ pkg/rancher-components/src/components/RcButton/types.ts
@@ -1,9 +1,20 @@
+import { RcIconType } from '@components/RcIcon/types';
+
 // TODO: 13211 Investigate why `InstanceType<typeof RcButton>` fails prod builds
 // export type RcButtonType = InstanceType<typeof RcButton>
 export type RcButtonType = {
   focus: () => void;
 }
 
+export type ButtonRole = 'primary' | 'secondary' | 'tertiary' | 'link' | 'multiAction' | 'ghost';
+
+export type ButtonRoleNewProps = {
+  role?: ButtonRole;
+}
+
+/**
+ * @deprecated Use the `role` property instead. These boolean props will be removed in a future version.
+ */
 export type ButtonRoleProps = {
   primary?: boolean;
   secondary?: boolean;
@@ -11,8 +22,23 @@ export type ButtonRoleProps = {
   link?: boolean;
   multiAction?: boolean;
   ghost?: boolean;
+
 }
 
+export type ButtonSize = 'small' | 'medium' | 'large';
+
+export type ButtonSizeNewProps = {
+  size?: ButtonSize;
+}
+
+/**
+ * @deprecated Use the `size` property instead. The `small` boolean prop will be removed in a future version.
+ */
 export type ButtonSizeProps = {
   small?: boolean;
+}
+
+export type IconProps = {
+  leftIcon?: RcIconType;
+  rightIcon?: RcIconType;
 }
