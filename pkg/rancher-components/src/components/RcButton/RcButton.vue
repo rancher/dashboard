972a4f93f2bf510afaf95373c70bc9793b7f7061
Cody Jackson <cody.jackson@suse.com>
2025-11-24 17:51:42 -0700

    Attempting to use the RcButton without the RcIcon import.

--- pkg/rancher-components/src/components/RcButton/RcButton.vue
+++ pkg/rancher-components/src/components/RcButton/RcButton.vue
@@ -5,10 +5,14 @@
  *
  * Example:
  *
- * <rc-button primary @click="doAction">Perform an Action</rc-button>
+ * <rc-button role="primary" @click="doAction">Perform an Action</rc-button>
  */
 import { computed, ref } from 'vue';
-import { ButtonRoleProps, ButtonSizeProps } from './types';
+import {
+  ButtonRoleProps, ButtonSizeProps, ButtonRoleNewProps, ButtonSizeNewProps, ButtonSize,
+  IconProps
+} from './types';
+// import RcIcon from '@components/RcIcon/RcIcon.vue';
 
 const buttonRoles: { role: keyof ButtonRoleProps, className: string }[] = [
   { role: 'primary', className: 'role-primary' },
@@ -23,18 +27,53 @@ const buttonSizes: { size: keyof ButtonSizeProps, className: string }[] = [
   { size: 'small', className: 'btn-sm' },
 ];
 
-const props = defineProps<ButtonRoleProps & ButtonSizeProps>();
+const buttonSizesNew: { size: ButtonSize, className: string }[] = [
+  { size: 'small', className: 'btn-small' },
+  { size: 'medium', className: 'btn-medium' },
+  { size: 'large', className: 'btn-large' },
+];
+
+const props = withDefaults(defineProps<ButtonRoleProps & ButtonSizeProps & ButtonRoleNewProps & ButtonSizeNewProps & IconProps>(), { size: 'medium' });
+
+const activeRoleClassName = computed(() => {
+  const activeRole = buttonRoles.find(({ role }) => props[role]);
+
+  if (activeRole) {
+    console.warn(
+      `[RcButton] The "${ activeRole.role }" prop is deprecated and will be removed in a future version. ` +
+      `Please use role="${ activeRole.role }" instead.`
+    );
+
+    return activeRole.className;
+  } else {
+    const roleConfig = buttonRoles.find(({ role }) => role === props.role);
+
+    return roleConfig?.className || 'role-primary';
+  }
+});
+
+const activeSizeClassName = computed(() => {
+  const activeSize = buttonSizes.find(({ size }) => props[size]);
+
+  if (activeSize) {
+    console.warn(
+      `[RcButton] The "${ activeSize.size }" prop is deprecated and will be removed in a future version. ` +
+      `Please use size="${ activeSize.size }" instead.`
+    );
+
+    return activeSize.className;
+  } else {
+    const sizeConfig = buttonSizesNew.find(({ size }) => size === props.size);
+
+    return sizeConfig?.className || '';
+  }
+});
 
 const buttonClass = computed(() => {
-  const activeRole = buttonRoles.find(({ role }) => props[role]);
-  const isButtonSmall = buttonSizes.some(({ size }) => props[size]);
-
   return {
-    btn: true,
-
-    [activeRole?.className || 'role-primary']: true,
-
-    'btn-sm': isButtonSmall,
+    btn:                         true,
+    [activeRoleClassName.value]: true,
+    [activeSizeClassName.value]: !!activeSizeClassName.value,
   };
 });
 
@@ -53,19 +92,45 @@ defineExpose({ focus });
     role="button"
     :class="{ ...buttonClass }"
   >
-    <slot name="before">
-      <!-- Empty Content -->
+    <slot
+      v-if="$slots.before || props.leftIcon"
+      name="before"
+    >
+      <!-- <RcIcon
+        v-if="props.leftIcon"
+        class="left-icon"
+        :type="props.leftIcon"
+        size="inherit"
+      /> -->
     </slot>
     <slot>
       <!-- Empty Content -->
     </slot>
-    <slot name="after">
-      <!-- Empty Content -->
+    <slot
+      v-if="$slots.after || props.rightIcon"
+      name="after"
+    >
+      <!-- <RcIcon
+        v-if="props.rightIcon"
+        class="right-icon"
+        :type="props.rightIcon"
+        size="inherit"
+      /> -->
     </slot>
   </button>
 </template>
 
 <style lang="scss" scoped>
+@mixin iconMargins($size) {
+  .left-icon {
+    margin-right: $size;
+  }
+
+  .right-icon {
+    margin-left: $size;
+  }
+}
+
 button {
   &.role-link {
     &:focus, &.focused {
@@ -91,6 +156,45 @@ button {
     &:focus-visible {
       @include focus-outline;
       outline-offset: 0;
+    }
+  }
+
+  &.btn.btn-small {
+    //:not(.btn-sm) is being used to make the style more specific to override global styles. We may want to get rid of those styles at some point.
+    &, &:not(.btn-sm) {
+      line-height: 15px;
+      font-size: 12px;
+      min-height: 24px;
+
+      padding: 0 8px;
+
+      @include iconMargins(8px);
+    }
+  }
+
+  &.btn.btn-medium {
+    //:not(.btn-sm) is being used to make the style more specific to override global styles. We may want to get rid of those styles at some point.
+    &, &:not(.btn-sm) {
+      line-height: 18px;
+      font-size: 14px;
+      min-height: 32px;
+
+      padding: 0 12px;
+
+      @include iconMargins(8px);
+    }
+  }
+
+  &.btn.btn-large {
+    // This is the default size brought by the global button styling
+    &, &:not(.btn-sm) {
+      line-height: 20px;
+      font-size: 16px;
+      min-height: 40px;
+
+      padding: 0 16px;
+
+      @include iconMargins(12px);
     }
   }
 }</style>
