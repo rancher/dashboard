4e23743aa63e4327cd7f6771cf233a7dff7ef0df
yonasberhe23 <yonas.berhe@suse.com>
2025-11-26 07:30:06 -0800

    add scale pools tests

--- cypress/e2e/po/prompts/genericPrompt.po.ts
+++ cypress/e2e/po/prompts/genericPrompt.po.ts
@@ -1,6 +1,7 @@
 import ComponentPo from '@/cypress/e2e/po/components/component.po';
 import CardPo from '@/cypress/e2e/po/components/card.po';
 import LabeledSelectPo from '@/cypress/e2e/po/components/labeled-select.po';
+import CheckboxInputPo from '@/cypress/e2e/po/components/checkbox-input.po';
 
 export default class GenericPrompt extends ComponentPo {
   card = new CardPo();
@@ -15,6 +16,10 @@ export default class GenericPrompt extends ComponentPo {
 
   labeledSelect(selector = '.labeled-select'): LabeledSelectPo {
     return new LabeledSelectPo(selector);
+  }
+
+  checkbox(selector = '[data-checkbox-ctrl]') {
+    return new CheckboxInputPo(this.self().get(selector));
   }
 
   clickActionButton(text: string) {
