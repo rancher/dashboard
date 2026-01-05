df49a79128a2e7ca715b6a059c7d9298ddd1064c
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2026-01-05 11:24:55 -0800

    make list and scroll to accordion

--- shell/components/TableOfContents/index.vue
+++ shell/components/TableOfContents/index.vue
@@ -1,24 +1,21 @@
 <script setup>
 import { randomStr } from 'utils/string';
-import { onMounted, getCurrentInstance } from 'vue';
+import { onMounted, getCurrentInstance, ref } from 'vue';
 
 const root = getCurrentInstance();
+
+const accordions = ref([]);
 
 const isAccordionVNode = (vNode) => {
   // TODO nb better way of doing this that doesn't rely on a class
   return (vNode?.el?.className || '').includes('accordion-container');
 };
 
-/**
- * Pass through parent? Need tree
- *
- */
-
 const findAccordionChildren = (accordions = [], node) => {
   let nextInput = accordions;
 
   if (isAccordionVNode(node)) {
-    const out = { self: node, children: [] };
+    const out = { node, children: [] };
 
     accordions.push(out);
     nextInput = out.children;
@@ -41,11 +38,18 @@ const findAccordionChildren = (accordions = [], node) => {
 
 const findAccordions = () => {
   const parent = root.parent || {};
-  const accordions = findAccordionChildren([], parent.vnode);
 
-  console.log('*** accordions found: ', accordions);
+  accordions.value = findAccordionChildren([], parent.vnode);
 };
 
+const scrollToAccordion = (accNode) => {
+  if (!accNode.el) {
+    return;
+  }
+  accNode.el.scrollIntoView(true);
+};
+
+// TODO nb need to run this after all other components have loaded, somehow
 onMounted(() => {
   findAccordions();
 });
@@ -61,4 +65,31 @@ onMounted(() => {
       click me
     </button>
   </div>
+  <div class="toc-container">
+    <ul>
+      <li
+        v-for="acc, i in accordions"
+        :key="i"
+        @click="scrollToAccordion(acc.node)"
+      >
+        {{ acc?.node?.el?.id }}
+        <template v-if="acc?.children?.length">
+          <ul>
+            <li
+              v-for="childAcc, j in acc.children"
+              :key="j"
+              @click="scrollToAccordion(acc.node)"
+            >
+              {{ childAcc?.node?.el?.id }}
+            </li>
+          </ul>
+        </template>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  .toc-container {
+  }
+</style>
