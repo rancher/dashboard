1f8e2d3e03bed11e172a48e4f0083006ff5913f2
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2026-01-05 13:59:37 -0800

    open accordion when scrolling to

--- pkg/imported/components/CruImported.vue
+++ pkg/imported/components/CruImported.vue
@@ -441,7 +441,7 @@ export default defineComponent({
       </div>
       <Accordion
         v-if="showBasics"
-        :title="providerTabKey"
+        :title="providerTabKey || 'abc'"
         :open-initially="true"
         class="mb-20 accordion"
       >
