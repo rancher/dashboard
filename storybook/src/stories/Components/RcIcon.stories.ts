9623b341d83fb3dd651eb7d9373f468f5e371048
Cody Jackson <cody.jackson@suse.com>
2026-01-06 07:04:59 -0700

    Fixed a storybook error

--- storybook/src/stories/Components/RcIcon.stories.ts
+++ storybook/src/stories/Components/RcIcon.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
 import { RcIcon } from '@components/RcIcon';
-import { RcIconType, RcIconTypeToClass, RcIconSizeToCSS } from '@components/RcIcon/types';
+import { RcIconTypeToClass, RcIconSizeToCSS } from '@components/RcIcon/types';
 import { StatusDefinitions } from '@components/utils/status';
 
 const meta: Meta<typeof RcIcon> = {
@@ -46,7 +46,7 @@ export const All: Story = {
   render: (args: any) => ({
     components: { RcIcon },
     setup() {
-      return { args, types: Object.keys(RcIconType) };
+      return { args, types: Object.keys(RcIconTypeToClass) };
     },
     template: `<div style="display: flex; max-width: 100%; flex-wrap: wrap; ">
       <div v-for="type in types" :key="type" style="display: inline-flex; justify-content: center; align-items: center; flex-direction: column; flex-basis: 25%; margin-bottom: 20px;">
