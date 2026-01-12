3a7b2513922bbd2f4f80037e5c5e48a0b8e6a401
Alexandre Alves <alexandre.alves@suse.com>
2026-01-12 16:22:12 +0000

    add current test pkg

---
+++ pkg/add-new-prod/components/Test.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name:     'TestPanel',
+  computed: {
+    routeName() {
+      return this.$route.name;
+    }
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Test render page! {{ routeName }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
