2c089c5653d6a42860cfc5572a692f345759e78f
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2026-01-15 15:34:03 -0800

    fixed test

--- shell/utils/__tests__/error.test.ts
+++ shell/utils/__tests__/error.test.ts
@@ -2,8 +2,8 @@ import { formatAWSError } from '@shell/utils/error';
 const SOME_ERROR = 'Some error';
 
 describe('formatAWSError', () => {
-  it('should return the extracted reason when error is a TypeError and message includes "reading \'Error\'"', () => {
-    const error = Object.assign(new TypeError("Cannot read property of undefined (reading 'Error')"), { $response: { reason: SOME_ERROR } });
+  it('should return the extracted reason when error is a TypeError and message includes "Deserialization error:"', () => {
+    const error = Object.assign(new TypeError("Cannot read property of undefined (reading 'Error'): Deserialization error:"), { $response: { reason: SOME_ERROR } });
 
     const result = formatAWSError(error);
 
@@ -11,14 +11,14 @@ describe('formatAWSError', () => {
   });
 
   it('should return the original error if it is not an instance of TypeError', () => {
-    const error = Object.assign(new Error("Cannot read property of undefined (reading 'Error')"), { $response: { reason: SOME_ERROR } });
+    const error = Object.assign(new Error('...Deserialization error:'), { $response: { reason: SOME_ERROR } });
 
     const result = formatAWSError(error);
 
     expect(result).toBe(error);
   });
 
-  it('should return the original error if the message does not contain "reading \'Error\'"', () => {
+  it('should return the original error if the message does not contain "Deserialization error:"', () => {
     const error = Object.assign(new TypeError('Some other type error occurred'), { $response: { reason: SOME_ERROR } });
 
     const result = formatAWSError(error);
@@ -28,7 +28,7 @@ describe('formatAWSError', () => {
 
   it('should return the original input if it is a plain object (not an Error instance)', () => {
     const error = {
-      message:   "reading 'Error'",
+      message:   'Deserialization error:',
       $response: { reason: 'Should not be returned' }
     };
 
