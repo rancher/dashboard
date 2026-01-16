b4e1536df79aa5ee8ab0597a76641aa5ce5c3777
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2026-01-15 16:28:51 -0800

    Improved error handling in aws and azure cluster provisioning (#16353)

--- shell/utils/azure.js
+++ shell/utils/azure.js
@@ -1,5 +1,6 @@
 const AZURE_ERROR_MSG_REGEX = /^.*Message=\"(.*)\"$/;
 const AZURE_ERROR_JSON_REGEX = /^.*Response body: ({.*})/;
+const AZURE_ERROR_JSON_REGEX_MULTILINE = /({[\s\S]*"error":[\s\S]*})/;
 
 export const parseAzureError = (err) => {
   // Try and parse the response from Azure a couple of ways
@@ -15,6 +16,17 @@ export const parseAzureError = (err) => {
         const errorObj = JSON.parse(jsonMatch[1]);
 
         return errorObj.error_description;
+      } catch (e) {}
+    }
+    const jsonMatchMultiline = err.match(AZURE_ERROR_JSON_REGEX_MULTILINE);
+
+    if (jsonMatchMultiline?.length === 2) {
+      try {
+        const errorObj = JSON.parse(jsonMatchMultiline[1]);
+
+        if (errorObj?.error?.message) {
+          return errorObj.error.message;
+        }
       } catch (e) {}
     }
   }
