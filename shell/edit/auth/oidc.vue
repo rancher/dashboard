b036f92567f03c991f2612c4ffb4b20f42f4c722
Phillip Rak <rak.phillip@gmail.com>
2026-01-27 15:38:56 -0700

    Display the PKCE method when enabled
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/edit/auth/oidc.vue
+++ shell/edit/auth/oidc.vue
@@ -316,6 +316,9 @@ export default {
             <tr v-if="model.authEndpoint">
               <td>{{ t('authConfig.oidc.authEndpoint') }}: </td><td>{{ model.authEndpoint }}</td>
             </tr>
+            <tr v-if="model.pkceMethod">
+              <td>{{ t('authConfig.pkceMethod.label') }}: </td><td>{{ model.pkceMethod }}</td>
+            </tr>
             <tr v-if="isLogoutAllSupported">
               <td>{{ t('authConfig.slo.sloTitle') }}: </td><td>{{ sloTypeText }}</td>
             </tr>
