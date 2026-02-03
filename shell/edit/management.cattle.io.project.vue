bd358487a66e919493a30be4c47b031da60f35f0
Phillip Rak <rak.phillip@gmail.com>
2026-02-03 10:27:14 -0700

    Remove quotas from list
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/edit/management.cattle.io.project.vue
+++ shell/edit/management.cattle.io.project.vue
@@ -6,7 +6,7 @@ import FormValidation from '@shell/mixins/form-validation';
 import CruResource from '@shell/components/CruResource';
 import Labels from '@shell/components/form/Labels';
 import ResourceQuota from '@shell/components/form/ResourceQuota/Project';
-import { HARVESTER_TYPES, RANCHER_TYPES } from '@shell/components/form/ResourceQuota/shared';
+import { HARVESTER_TYPES, RANCHER_TYPES, TYPES } from '@shell/components/form/ResourceQuota/shared';
 import Tab from '@shell/components/Tabbed/Tab';
 import Tabbed from '@shell/components/Tabbed';
 import NameNsDescription from '@shell/components/form/NameNsDescription';
@@ -157,12 +157,37 @@ export default {
     },
 
     removeQuota(key) {
+      const isExtended = key.startsWith(`${ TYPES.EXTENDED }.`);
+      let resourceKey = key;
+
+      if (isExtended) {
+        resourceKey = key.substring(`${ TYPES.EXTENDED }.`.length);
+      }
+
       ['resourceQuota', 'namespaceDefaultResourceQuota'].forEach((specProp) => {
-        if (this.value?.spec[specProp]?.limit && this.value?.spec[specProp]?.limit[key]) {
-          delete this.value?.spec[specProp]?.limit[key];
-        }
-        if (this.value?.spec[specProp]?.usedLimit && this.value?.spec[specProp]?.usedLimit[key]) {
-          delete this.value?.spec[specProp]?.usedLimit[key];
+        const limit = this.value?.spec[specProp]?.limit;
+        const usedLimit = this.value?.spec[specProp]?.usedLimit;
+
+        if (isExtended) {
+          if (limit?.extended && typeof limit.extended[resourceKey] !== 'undefined') {
+            delete limit.extended[resourceKey];
+            if (Object.keys(limit.extended).length === 0) {
+              delete limit.extended;
+            }
+          }
+          if (usedLimit?.extended && typeof usedLimit.extended[resourceKey] !== 'undefined') {
+            delete usedLimit.extended[resourceKey];
+            if (Object.keys(usedLimit.extended).length === 0) {
+              delete usedLimit.extended;
+            }
+          }
+        } else {
+          if (limit && typeof limit[resourceKey] !== 'undefined') {
+            delete limit[resourceKey];
+          }
+          if (usedLimit && typeof usedLimit[resourceKey] !== 'undefined') {
+            delete usedLimit[resourceKey];
+          }
         }
       });
     }
