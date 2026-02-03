e88dca3e0a7474b4907e97f542cd0705723d3c55
Cody Jackson <cody.jackson@suse.com>
2026-02-03 10:31:31 -0700

    Switched to typescript and added unit tests

--- shell/models/ext.cattle.io.kubeconfig.ts
+++ shell/models/ext.cattle.io.kubeconfig.ts
@@ -1,19 +1,35 @@
 import { CAPI, MANAGEMENT } from '@shell/config/types';
 import SteveModel from '@shell/plugins/steve/steve-class';
+import { Location } from 'vue-router';
+
+interface ReferencedCluster {
+  label: string;
+  location: Location | null;
+}
 
 export default class Kubeconfig extends SteveModel {
-  get _availableActions() {
+  declare spec: {
+    clusters?: string[];
+    ttl?: number;
+  };
+
+  declare metadata: {
+    name?: string;
+    creationTimestamp?: string;
+  };
+
+  get _availableActions(): object[] {
     const out = super._availableActions;
 
     // Remove element at index 1 (the first divider), the actions that don't make sense.
-    return out.filter((action, index) => index !== 1 && !['goToEdit', 'goToEditYaml', 'cloneYaml', 'download'].includes(action.action));
+    return out.filter((action: { action: string }, index: number) => index !== 1 && !['goToEdit', 'goToEditYaml', 'cloneYaml', 'download'].includes(action.action));
   }
 
   /**
    * Calculates the expiry timestamp from creationTimestamp + ttl.
    * Returns an ISO date string for use with LiveDate formatter.
    */
-  get expiresAt() {
+  get expiresAt(): string | null {
     const ttlSeconds = this.spec?.ttl;
     const creationTimestamp = this.metadata?.creationTimestamp;
 
@@ -31,14 +47,14 @@ export default class Kubeconfig extends SteveModel {
    * Returns cluster information for display and linking.
    * Each object contains {label, location} where location is null if cluster doesn't exist.
    */
-  get referencedClusters() {
+  get referencedClusters(): ReferencedCluster[] {
     const clusterIds = this.spec?.clusters || [];
     const provClusters = this.$rootGetters['management/all'](CAPI.RANCHER_CLUSTER) || [];
     const mgmtClusters = this.$rootGetters['management/all'](MANAGEMENT.CLUSTER) || [];
 
-    return clusterIds.map((id) => {
-      const provCluster = provClusters.find((c) => c.mgmt?.id === id || c.status?.clusterName === id);
-      const mgmtCluster = mgmtClusters.find((c) => c.id === id);
+    return clusterIds.map((id: string) => {
+      const provCluster = provClusters.find((c: any) => c.mgmt?.id === id || c.status?.clusterName === id);
+      const mgmtCluster = mgmtClusters.find((c: any) => c.id === id);
       const cluster = provCluster || mgmtCluster;
 
       return {
@@ -51,7 +67,7 @@ export default class Kubeconfig extends SteveModel {
   /**
    * Returns referenced clusters sorted: existing clusters first (by name), then deleted clusters.
    */
-  get sortedReferencedClusters() {
+  get sortedReferencedClusters(): ReferencedCluster[] {
     return this.referencedClusters.slice().sort((a, b) => {
       const aExists = a.location !== null;
       const bExists = b.location !== null;
@@ -73,7 +89,7 @@ export default class Kubeconfig extends SteveModel {
   /**
    * Returns a sortable string for the clusters column.
    */
-  get referencedClustersSortable() {
+  get referencedClustersSortable(): string {
     return this.sortedReferencedClusters
       .map((c) => c.label.toLowerCase())
       .join(',');
