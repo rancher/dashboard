fd3bd8e716f68561fb4fa2e49af0c346a19f1a61
Cody Jackson <cody.jackson@suse.com>
2026-02-09 16:30:03 -0700

    Converted to the component to the composable API.

--- shell/list/ext.cattle.io.kubeconfig.vue
+++ shell/list/ext.cattle.io.kubeconfig.vue
@@ -1,117 +1,111 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import PaginatedResourceTable from '@shell/components/PaginatedResourceTable.vue';
 import { CAPI, MANAGEMENT } from '@shell/config/types';
 import { ActionFindPageArgs } from '@shell/types/store/dashboard-store.types';
 import { FilterArgs, PaginationFilterField, PaginationParamFilter } from '@shell/types/store/pagination.types';
 import { PagTableFetchPageSecondaryResourcesOpts, PagTableFetchSecondaryResourcesOpts, PagTableFetchSecondaryResourcesReturns } from '@shell/types/components/paginatedResourceTable';
 
-export default defineComponent({
-  name: 'KubeconfigList',
-
-  components: { PaginatedResourceTable },
-
-  props: {
-    schema: {
-      type:     Object,
-      required: true
-    },
-
-    useQueryParamsForSimpleFiltering: {
-      type:    Boolean,
-      default: false
-    }
+defineProps({
+  schema: {
+    type:     Object,
+    required: true
   },
 
-  computed: {
-    canViewProvClusters(): boolean {
-      return !!this.$store.getters['management/canList'](CAPI.RANCHER_CLUSTER);
-    },
-    canViewMgmtClusters(): boolean {
-      return !!this.$store.getters['management/canList'](MANAGEMENT.CLUSTER);
-    }
-  },
-
-  methods: {
-    /**
-     * Fetch all clusters when not using pagination
-     */
-    async fetchSecondaryResources({ canPaginate }: PagTableFetchSecondaryResourcesOpts): PagTableFetchSecondaryResourcesReturns {
-      if (canPaginate) {
-        return;
-      }
-
-      const promises = [];
-
-      if (this.canViewProvClusters) {
-        promises.push(this.$store.dispatch('management/findAll', { type: CAPI.RANCHER_CLUSTER }));
-      }
-
-      if (this.canViewMgmtClusters) {
-        promises.push(this.$store.dispatch('management/findAll', { type: MANAGEMENT.CLUSTER }));
-      }
-
-      await Promise.all(promises);
-    },
-
-    /**
-     * Fetch only the clusters referenced by kubeconfigs on the current page
-     *
-     * NOTE: For the time being this isn't validated because ext.cattle.io.kubeconfig is not one of the indexed resources. I'm putting this in for future support since secondary resources are needed.
-     */
-    async fetchPageSecondaryResources({ force, page }: PagTableFetchPageSecondaryResourcesOpts) {
-      if (!page?.length) {
-        return;
-      }
-
-      const uniqueClusterIds = new Set<string>();
-
-      page.forEach((kubeconfig: any) => {
-        const ids = kubeconfig.spec?.clusters || [];
-
-        ids.forEach((id: string) => uniqueClusterIds.add(id));
-      });
-
-      if (uniqueClusterIds.size === 0) {
-        return;
-      }
-
-      const clusterIdArray = Array.from(uniqueClusterIds);
-
-      if (this.canViewProvClusters) {
-        const opt: ActionFindPageArgs = {
-          force,
-          pagination: new FilterArgs({
-            filters: PaginationParamFilter.createMultipleFields(
-              clusterIdArray.map((id) => new PaginationFilterField({
-                field: 'status.clusterName', // Verified it's one of the attribute fields listed in the schema, according to steve-pagination-utils that means it should be filterable
-                value: id
-              }))
-            )
-          })
-        };
-
-        this.$store.dispatch('management/findPage', { type: CAPI.RANCHER_CLUSTER, opt });
-      }
-
-      if (this.canViewMgmtClusters) {
-        const opt: ActionFindPageArgs = {
-          force,
-          pagination: new FilterArgs({
-            filters: PaginationParamFilter.createMultipleFields(
-              clusterIdArray.map((id) => new PaginationFilterField({
-                field: 'metadata.name', // Verified it's one of the attribute fields listed in the schema, according to steve-pagination-utils that means it should be filterable
-                value: id
-              }))
-            )
-          })
-        };
-
-        this.$store.dispatch('management/findPage', { type: MANAGEMENT.CLUSTER, opt });
-      }
-    }
+  useQueryParamsForSimpleFiltering: {
+    type:    Boolean,
+    default: false
   }
 });
+
+const store = useStore();
+
+const canViewProvClusters = computed<boolean>(() => {
+  return !!store.getters['management/canList'](CAPI.RANCHER_CLUSTER);
+});
+
+const canViewMgmtClusters = computed<boolean>(() => {
+  return !!store.getters['management/canList'](MANAGEMENT.CLUSTER);
+});
+
+/**
+ * Fetch all clusters when not using pagination
+ */
+async function fetchSecondaryResources({ canPaginate }: PagTableFetchSecondaryResourcesOpts): PagTableFetchSecondaryResourcesReturns {
+  if (canPaginate) {
+    return;
+  }
+
+  const promises = [];
+
+  if (canViewProvClusters.value) {
+    promises.push(store.dispatch('management/findAll', { type: CAPI.RANCHER_CLUSTER }));
+  }
+
+  if (canViewMgmtClusters.value) {
+    promises.push(store.dispatch('management/findAll', { type: MANAGEMENT.CLUSTER }));
+  }
+
+  await Promise.all(promises);
+}
+
+/**
+ * Fetch only the clusters referenced by kubeconfigs on the current page
+ *
+ * NOTE: For the time being this isn't validated because ext.cattle.io.kubeconfig is not one of the indexed resources. I'm putting this in for future support since secondary resources are needed.
+ */
+async function fetchPageSecondaryResources({ force, page }: PagTableFetchPageSecondaryResourcesOpts) {
+  if (!page?.length) {
+    return;
+  }
+
+  const uniqueClusterIds = new Set<string>();
+
+  page.forEach((kubeconfig: any) => {
+    const ids = kubeconfig.spec?.clusters || [];
+
+    ids.forEach((id: string) => uniqueClusterIds.add(id));
+  });
+
+  if (uniqueClusterIds.size === 0) {
+    return;
+  }
+
+  const clusterIdArray = Array.from(uniqueClusterIds);
+
+  if (canViewProvClusters.value) {
+    const opt: ActionFindPageArgs = {
+      force,
+      pagination: new FilterArgs({
+        filters: PaginationParamFilter.createMultipleFields(
+          clusterIdArray.map((id) => new PaginationFilterField({
+            field: 'status.clusterName', // Verified it's one of the attribute fields listed in the schema, according to steve-pagination-utils that means it should be filterable
+            value: id
+          }))
+        )
+      })
+    };
+
+    store.dispatch('management/findPage', { type: CAPI.RANCHER_CLUSTER, opt });
+  }
+
+  if (canViewMgmtClusters.value) {
+    const opt: ActionFindPageArgs = {
+      force,
+      pagination: new FilterArgs({
+        filters: PaginationParamFilter.createMultipleFields(
+          clusterIdArray.map((id) => new PaginationFilterField({
+            field: 'metadata.name', // Verified it's one of the attribute fields listed in the schema, according to steve-pagination-utils that means it should be filterable
+            value: id
+          }))
+        )
+      })
+    };
+
+    store.dispatch('management/findPage', { type: MANAGEMENT.CLUSTER, opt });
+  }
+}
 </script>
 
 <template>
