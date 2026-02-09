26accce7253c5194986444a10aea02d1c7f20e43
Cody Jackson <cody.jackson@suse.com>
2026-02-09 15:02:48 -0700

    We now differentiate the extensions/cluster tools links between admin and non-admin. We link the the places in the app if it's an admin and to the docs if it's not an admin.

--- shell/components/Resource/Detail/Card/ExtrasCard.vue
+++ shell/components/Resource/Detail/Card/ExtrasCard.vue
@@ -3,29 +3,42 @@ import Card from '@shell/components/Resource/Detail/Card/index.vue';
 import { useI18n } from '@shell/composables/useI18n';
 import { useStore } from 'vuex';
 import { BLANK_CLUSTER } from '@shell/store/store-types';
+import { isAdminUser } from '@shell/store/type-map';
+import { DOCS_BASE } from '@shell/config/private-label';
 </script>
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import { computed } from 'vue';
 
 const store = useStore();
 const router = useRouter();
 const i18n = useI18n(store);
+const isAdmin = computed(() => isAdminUser(store.getters));
 
-const extensionsUrl = router.resolve({
-  name:   'c-cluster-uiplugins',
-  params: { cluster: BLANK_CLUSTER }
-}).href;
+const extensionsUrl = computed(() => {
+  const pageUrl = router.resolve({
+    name:   'c-cluster-uiplugins',
+    params: { cluster: BLANK_CLUSTER }
+  }).href;
+  const docsUrl = `${ DOCS_BASE }/integrations-in-rancher/rancher-extensions`;
 
-const clusterToolsUrl = router.resolve({
-  name:   'c-cluster-apps-charts',
-  params: { cluster: BLANK_CLUSTER }
-}).href;
+  return isAdmin.value ? pageUrl : docsUrl;
+});
+
+const clusterToolsUrl = computed(() => {
+  const pageUrl = router.resolve({ name: 'c-cluster-explorer-tools' }).href;
+  const docsUrl = `${ DOCS_BASE }/reference-guides/rancher-cluster-tools`;
+
+  return isAdmin.value ? pageUrl : docsUrl;
+});
+
+const linkTarget = computed(() => isAdmin.value ? '_self' : '_blank');
 </script>
 
 <template>
   <Card :title="i18n.t('component.resource.detail.card.extrasCard.title')">
     <p
-      v-clean-html="i18n.t('component.resource.detail.card.extrasCard.message', { extensionsUrl, clusterToolsUrl }, true)"
+      v-clean-html="i18n.t('component.resource.detail.card.extrasCard.message', { extensionsUrl, clusterToolsUrl, linkTarget }, true)"
       class="message text-deemphasized"
     />
   </Card>
