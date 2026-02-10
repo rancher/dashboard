4e8a2a026047336835d6724a12430472fd25a27e
Marcelo Fukumoto <marcelo.fukumoto@suse.com>
2026-02-10 15:05:16 +0100

    [e2e][ai] Revert changes

--- cypress/e2e/po/components/action-menu-shell.po.ts
+++ cypress/e2e/po/components/action-menu-shell.po.ts
@@ -2,12 +2,7 @@ import ComponentPo from '@/cypress/e2e/po/components/component.po';
 
 export default class ActionMenuPo extends ComponentPo {
   constructor(arg?:any) {
-    if (arg) {
-      super(arg);
-    } else {
-      // Get the most recently opened/visible dropdown menu collection
-      super(cy.get('[dropdown-menu-collection]:visible').last());
-    }
+    super(arg || cy.get('[dropdown-menu-collection]'));
   }
 
   clickMenuItem(index: number) {
@@ -15,23 +10,11 @@ export default class ActionMenuPo extends ComponentPo {
   }
 
   getMenuItem(name: string) {
-    // First ensure the dropdown collection exists and is visible
-    cy.get('[dropdown-menu-collection]:visible').should('exist');
-
-    // Wait for the dropdown to be properly rendered and not show "No actions available"
-    cy.get('[dropdown-menu-collection]:visible').should('not.contain', 'No actions available');
-
-    // Wait for menu items to be populated - use retry logic for async loading
-    cy.get('[dropdown-menu-collection]:visible').within(() => {
-      cy.get('[dropdown-menu-item]:not([disabled])').should('have.length.greaterThan', 0);
-    });
-
-    // Now find the specific menu item using global selector for reliability
-    return cy.get('[dropdown-menu-collection]:visible [dropdown-menu-item]').contains(name).should('be.visible');
+    return this.self().get('[dropdown-menu-item]').contains(name);
   }
 
   menuItemNames() {
-    return this.self().find('[dropdown-menu-item]').then(($els) => {
+    return this.self().get('[dropdown-menu-item]').then(($els) => {
       return (
         Cypress.$.makeArray($els)
           // and extract inner text from each
