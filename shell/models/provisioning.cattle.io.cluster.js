805866f7b43e95962f0e32ac342ec96f3f1ca533
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2026-02-26 14:59:56 -0800

    Hostbusters dev (#16790)
    * Revert "update test to properly use po"
    
    This reverts commit d714bce1ae75d67846bc56f9dc0adc9dc4103a8f.
    
    * Revert "clean up spacing in provisioning cluster list template"
    
    This reverts commit 4f7c31c1cba39d4f1124c9fd9b07f69ef5ce0c9b.
    
    * Revert "fix e2e test"
    
    This reverts commit 3ccf727b1830b4c2cda08ecffee2f4d711887c31.
    
    * Revert "use additiona-sub-row instead of new sortabletable slot"
    
    This reverts commit 12315ce3a1e2d88b32b600aadcc7d32e362a81b8.
    
    * Revert "style tweaks and copilot changes"
    
    This reverts commit 0131c68eed1b6f12b7c76abad2f8c6695e3f6e86.
    
    * Revert "add tests"
    
    This reverts commit e66fe1955bab044af570ffbedc8f8de8f9937676.
    
    * Revert "block editing v2 prov capi clusters"
    
    This reverts commit 1d50bb1b098cbfce046b0e8bdc3fad990a17a6f9.

--- shell/models/provisioning.cattle.io.cluster.js
+++ shell/models/provisioning.cattle.io.cluster.js
@@ -101,16 +101,12 @@ export default class ProvCluster extends SteveModel {
   }
 
   get canEdit() {
-    // If the cluster is a KEV1 cluster, Harvester cluster, or v2 provisioning cluster that uses upstream capi infrastructure providers, then prevent edit
-    if (this.isKev1 || this.isHarvester || this.isCapiHybrid) {
+    // If the cluster is a KEV1 cluster or Harvester cluster then prevent edit
+    if (this.isKev1 || this.isHarvester) {
       return false;
     }
 
     return super.canEdit;
-  }
-
-  get canCustomEdit() {
-    return !this.isCapiHybrid && super.canCustomEdit;
   }
 
   get _availableActions() {
@@ -127,9 +123,9 @@ export default class ProvCluster extends SteveModel {
     }
     const ready = this.mgmt?.isReady;
 
-    const canEditRKE2cluster = this.isRke2 && ready && this.canUpdate && !this.isCapiHybrid;
+    const canEditRKE2cluster = this.isRke2 && ready && this.canUpdate;
 
-    const canSnapshot = ready && this.isRke2 && this.canUpdate && !this.isCapiHybrid;
+    const canSnapshot = ready && this.isRke2 && this.canUpdate;
 
     const actions = [
       // Note: Actions are not supported in the Steve API, so we check
@@ -392,19 +388,6 @@ export default class ProvCluster extends SteveModel {
     return !!this.mgmt?.isHarvester;
   }
 
-  // identify v2 provisioning clusters created using upstream capi infrastructure providers instead of rancher/machine
-  get isCapiHybrid() {
-    if (!this.isRke2) {
-      return false;
-    }
-
-    const machineReferences = (this.spec?.rkeConfig?.machinePools || []).map((pool) => pool.machineConfigRef);
-
-    const capiMachines = machineReferences.find((r) => r?.apiVersion?.includes('cluster.x-k8s.io'));
-
-    return !!capiMachines;
-  }
-
   get mgmtClusterId() {
     // when a cluster is created `this` instance isn't immediately updated with `status.clusterName`
     // Workaround - Get fresh copy from the store
@@ -553,7 +536,7 @@ export default class ProvCluster extends SteveModel {
 
     if (this.isHarvester) {
       return HARVESTER;
-    } else if ( this.isImported || this.isCapiHybrid ) {
+    } else if ( this.isImported ) {
       return null;
     } else if ( this.isRke2 ) {
       const kind = this.spec?.rkeConfig?.machinePools?.[0]?.machineConfigRef?.kind?.toLowerCase();
@@ -1070,7 +1053,7 @@ export default class ProvCluster extends SteveModel {
   }
 
   get disableResourceDetailDrawerConfigTab() {
-    return !!this.isHarvester || this.isCapiHybrid;
+    return !!this.isHarvester;
   }
 
   get fullDetailPageOverride() {
