f837b6f15bc494cebe7535982f257f4daa61105b
Cody Jackson <cody.jackson@suse.com>
2026-03-04 00:18:06 +0000

    Put in a max of 3 badges

--- pkg/rancher-components/src/components/RcSection/RcSectionBadges.vue
+++ pkg/rancher-components/src/components/RcSection/RcSectionBadges.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import RcStatusBadge from '@components/Pill/RcStatusBadge';
 
-defineProps<{
+const MAX_BADGES = 3;
+
+const props = defineProps<{
   badges: { label: string; status: 'info' | 'success' | 'warning' | 'error' | 'unknown' | 'none' }[];
 }>();
+
+const visibleBadges = computed(() => {
+  if (props.badges.length > MAX_BADGES) {
+    // eslint-disable-next-line no-console
+    console.warn(`[RcSectionBadges]: Received ${ props.badges.length } badges but only ${ MAX_BADGES } are allowed. Extra badges will be hidden.`);
+  }
+
+  return props.badges.slice(0, MAX_BADGES);
+});
 </script>
 
 <template>
   <RcStatusBadge
-    v-for="(badge, i) in badges"
+    v-for="(badge, i) in visibleBadges"
     :key="i"
     :status="badge.status"
   >
