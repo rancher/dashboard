af723a90057092c6ef439f1d6afb9ce373cc9847
Richard Cox <ricox@suse.com>
2026-03-05 11:03:38 +0000

    Fetch branches-metadata from `master` branch instead of using whatever's local

--- scripts/get-branch-metadata.sh
+++ scripts/get-branch-metadata.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Get the directory of the script to find the project root
-DIR=$(cd $(dirname $0)/..; pwd)
-
 # 1. discover the current github branch
 if [ -n "$1" ]; then
   BRANCH="$1"
@@ -13,23 +10,22 @@ fi
 
 echo "Current branch: ${BRANCH}" >&2
 
-# 2. find the relevant json object in branches-metadata.json and return it
-METADATA_FILE="${DIR}/branches-metadata.json"
-
-if [ ! -f "$METADATA_FILE" ]; then
-  echo "Error: branches-metadata.json not found at ${METADATA_FILE}" >&2
-  exit 1
-fi
-
 if ! command -v jq &> /dev/null; then
     echo "jq could not be found. Please install jq to run this script." >&2
     exit 1
 fi
 
-METADATA=$(jq -r --arg branch "$BRANCH" '.branches[$branch] // ""' "$METADATA_FILE" )
+# 2. find the relevant json object in `master` branches-metadata.json
+METADATA_JSON_URL=https://raw.githubusercontent.com/rancher/dashboard/master/branches-metadata.json
+METADATA_JSON=$(curl -s -f -L $METADATA_JSON_URL) || {
+  echo "Error: Failed to download $METADATA_JSON_URL" >&2
+  exit 1
+}
+
+METADATA=$(echo "$METADATA_JSON" | jq -r --arg branch "$BRANCH" '.branches[$branch] // ""')
 
 echo "Metadata for branch '${BRANCH}':"  >&2
 echo "$METADATA" >&2
 
-# Return it
+# 3. Return it
 echo "${METADATA}"
