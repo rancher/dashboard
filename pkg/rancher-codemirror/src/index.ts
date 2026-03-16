23f8858b0e4836a094748863ff0a2cdab80fecbb
Phillip Rak <rak.phillip@gmail.com>
2026-03-16 14:20:01 -0700

    Allow for custom folding
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-codemirror/src/index.ts
+++ pkg/rancher-codemirror/src/index.ts
@@ -1,4 +1,8 @@
 export { default as CodeMirror } from './components/CodeMirror.vue'
 export type { Props as CodeMirrorProps } from './components/CodeMirror.vue'
 export type { FoldOptions } from './extensions/fold'
-export { indentFoldService, bracketFoldService } from './extensions/fold'
+export {
+  indentFoldService, bracketFoldService,
+  foldByLineMatch, foldByYamlPath,
+  foldMatchingLines, foldYamlPath
+} from './extensions/fold'
