5dd2efeef051a1eb4f00310c5f56bbcdaeadcae7
Phillip Rak <rak.phillip@gmail.com>
2026-03-31 14:13:55 -0700

    Configure lint rules
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-codemirror/eslint.config.ts
+++ pkg/rancher-codemirror/eslint.config.ts
@@ -1,13 +1,14 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginOxlint from 'eslint-plugin-oxlint'
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import pluginOxlint from 'eslint-plugin-oxlint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{vue,ts,mts,tsx}'],
+    files: ['**/*.{vue,ts,mts,tsx}']
   },
 
   globalIgnores(['**/dist/**', '**/coverage/**']),
@@ -15,10 +16,23 @@ export default defineConfigWithVueTs(
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
 
+  stylistic.configs.customize({
+    semi: true,
+    quotes: 'single',
+    indent: 2,
+    commaDangle: 'never'
+  }),
+
   {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
+    rules: {
+      curly: ['error', 'all']
+    }
   },
 
-  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
-)
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*']
+  },
+
+  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json')
+);
