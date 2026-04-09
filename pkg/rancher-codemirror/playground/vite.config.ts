5d4eedff90d3901b7312b28a5d425b33dba8536e
Phillip Rak <rak.phillip@gmail.com>
2026-04-09 13:28:33 -0700

    Use Rancher theme variabled
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-codemirror/playground/vite.config.ts
+++ pkg/rancher-codemirror/playground/vite.config.ts
@@ -6,7 +6,18 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@rancher/codemirror': resolve(__dirname, '../src/index.ts')
+      '@rancher/codemirror': resolve(__dirname, '../src/index.ts'),
+      '~shell':              resolve(__dirname, '../../../shell')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Allow @import 'base/functions', 'themes/modern', etc. without prefix
+        loadPaths:    [resolve(__dirname, '../../../shell/assets/styles')],
+        // Suppress deprecation warnings originating from the dashboard's theme files
+        silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'abs-percent', 'if-function']
+      }
     }
   }
 });
