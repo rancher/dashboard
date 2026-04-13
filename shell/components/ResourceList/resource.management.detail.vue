dae07d69e7463ba65703c35c66c2eaf5fd631c1d
ed-thuando <thuando.work@gmail.com>
2026-04-13 15:51:06 +0700

    fix: resolve #17066 — Action menu needs tidying and terminology being made consistent
    Fixes #17066
    
    Signed-off-by: ed-thuando <231172918+ed-thuando@users.noreply.github.com>

---
+++ shell/components/ResourceList/resource.management.detail.vue
@@ -0,0 +1,38 @@
+<template>
+  <ResourceDetail
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <template #detail>
+      <slot name="detail" />
+    </template>
+
+    <template #detail-top>
+      <slot name="detail-top" />
+    </template>
+
+
+    <template #aside-footer>
+      <slot name="aside-footer">
+        <div class="aside-footer">
+          <slot name="aside-footer" />
+        </div>
+      </slot>
+    </template>
+
+    <template #related">
+      <slot name="related" />
+    </template>
+  </ResourceDetail>
+</template>
+
+<script lang="ts">nimport ResourceDetail from '@shell/components/ResourceDetail/index.vue';
+
+export default {
+  name: 'ResourceManagementDetail',
+  components: {
+    ResourceDetail,
+  },
+  inheritAttrs: false,
+};
+</script>
