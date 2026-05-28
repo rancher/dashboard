0290e498143fa3b1e587abe06d1281ad3eea9057
Neil MacDougall <nmacdougall@suse.com>
2026-05-28 11:19:25 +0100

    Ensure message part of growl has a restricted height and scrolls

--- shell/components/GrowlManager.vue
+++ shell/components/GrowlManager.vue
@@ -155,7 +155,7 @@ export default {
 
   .growl-list {
     max-height: calc(100vh - 50px);
-    overflow: scroll;
+    overflow: hidden;
   }
 
   .growl {
@@ -203,6 +203,10 @@ export default {
 
         > P {
           padding-top: 2px;
+
+          // Limit size of message and scroll just the message, not the entire growl, so that the title and icon are always visible
+          max-height: 200px;
+          overflow-y: scroll;
 
           &.has-title {
             margin-top: 5px;
