7922a2382ddd62f26d5900c848ee39a441a51894
Richard Cox <ricox@suse.com>
2026-06-10 13:52:21 +0100

    wip 6

--- shell/core/plugin-products-extending.ts
+++ shell/core/plugin-products-extending.ts
@@ -24,7 +24,7 @@ export class ExtendingPluginProduct extends BasePluginProduct {
       // If no config is provided, add a default empty page
       this.config = [{
         name:      'main',
-        label:     'Main',
+        display:   { label: 'Main' },
         component: EmptyProductPage,
       }];
     }
