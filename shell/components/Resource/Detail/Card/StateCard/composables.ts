c928b1d93856c29d5c83e9de138ac8a220b50a7a
Cody Jackson <cody.jackson@suse.com>
2026-06-12 21:02:47 +0000

    Found some more dead code and added a try catch to better handle a potential error.

--- shell/components/Resource/Detail/Card/StateCard/composables.ts
+++ shell/components/Resource/Detail/Card/StateCard/composables.ts
@@ -1,10 +1,5 @@
 import { Count, Props as ResourceRowProps } from '@shell/components/Resource/Detail/ResourceRow.types';
-import { useI18n } from '@shell/composables/useI18n';
-import { INGRESS, SERVICE } from '@shell/config/types';
 import { isHigherAlert, StateColor } from '@shell/utils/style';
-import { computed, Ref, toValue } from 'vue';
-import { useStore } from 'vuex';
-import { Props as StateCardProps } from '@shell/components/Resource/Detail/Card/StateCard/types';
 import { RouteLocationRaw } from 'vue-router';
 import { simpleColorForState, stateDisplay as stateDisplayFn } from '@shell/plugins/dashboard-store/resource-class';
 
@@ -115,96 +110,5 @@ export function useResourceCardRowFromRelationships(label: string, relationships
     color:  tuples.length ? tuples[0].color : undefined,
     counts: tuples.length ? tuples : undefined,
     to
-  };
-}
-
-export interface Pairs {
-    label: string;
-    to?: RouteLocationRaw;
-    resources: any[];
-}
-
-export function useDefaultResources(pairs: Ref<Pairs[]>) {
-  const rows = computed(() => toValue(pairs).map(({ label, resources, to }) => useResourceCardRow(label, resources, undefined, undefined, to)));
-
-  return rows;
-}
-
-export function useResourcePair(label: string, type: string, resources?: any[]) {
-  const store = useStore();
-
-  return computed(() => {
-    if (!resources) {
-      return undefined;
-    }
-
-    const resourcesValue = toValue(resources);
-
-    return {
-      label,
-      resources: resourcesValue,
-      to:        (resourcesValue?.length > 0) ? {
-        name:   'c-cluster-product-resource',
-        params: {
-          cluster:  store.getters.currentCluster.id,
-          product:  store.getters.currentProduct.name,
-          resource: type
-        }
-      } : undefined,
-    };
-  });
-}
-
-export function useDefaultWorkloadResources(services?: any[], ingresses?: any[], referredToBy?: any[], refersTo?: any[]): StateCardProps {
-  const store = useStore();
-  const i18n = useI18n(store);
-
-  const pairs: Ref<(Pairs | undefined)[]> = computed(() => [
-    useResourcePair(i18n.t('component.resource.detail.card.resourcesCard.rows.services'), SERVICE, services).value,
-    useResourcePair(i18n.t('component.resource.detail.card.resourcesCard.rows.ingresses'), INGRESS, ingresses).value,
-    referredToBy ? {
-      label:     i18n.t('component.resource.detail.card.resourcesCard.rows.referredToBy'),
-      resources: toValue(referredToBy || []),
-      to:        { hash: '#related' }
-    } : undefined,
-    refersTo ? {
-      label:     i18n.t('component.resource.detail.card.resourcesCard.rows.refersTo'),
-      resources: toValue(refersTo || []),
-      to:        { hash: '#related' }
-    } : undefined
-  ]);
-
-  const remainingPairs = computed(() => pairs.value.filter((p: (Pairs | undefined)): p is Pairs => p !== undefined));
-
-  const rows = useDefaultResources(remainingPairs);
-
-  return {
-    title: i18n.t('component.resource.detail.card.resourcesCard.title'),
-    rows:  rows.value
-  };
-}
-
-export function useDefaultWorkloadInsightsCardProps(): StateCardProps {
-  const store = useStore();
-  const i18n = useI18n(store);
-
-  const rows: ResourceRowProps[] = [
-    {
-      label:  i18n.t('component.resource.detail.card.insightsCard.rows.conditions'),
-      to:     '#conditions',
-      color:  'disabled',
-      counts: [{ label: 'Available', count: 1 }, { label: 'Progressing', count: 1 }]
-    },
-    {
-      label:  i18n.t('component.resource.detail.card.insightsCard.rows.events'),
-      to:     '#events',
-      color:  'disabled',
-      counts: [{ label: 'Normal', count: 2 }]
-    }
-  ];
-
-  return {
-    title: i18n.t('component.resource.detail.card.insightsCard.title'),
-    rows
   };
 }
