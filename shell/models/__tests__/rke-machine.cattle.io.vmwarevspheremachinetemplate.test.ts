e296ec9e50f6d5c051405f81c8966513e95db796
momesgin <mo.mesgin@suse.com>
2026-06-19 17:06:16 -0700

    more test cases

--- shell/models/__tests__/rke-machine.cattle.io.vmwarevspheremachinetemplate.test.ts
+++ shell/models/__tests__/rke-machine.cattle.io.vmwarevspheremachinetemplate.test.ts
@@ -5,10 +5,15 @@ describe('class VmwarevsphereMachineTemplate', () => {
     it.each([
       ['/DC1/host/ClusterA/Resources', 'ClusterA'],
       ['/DC1/host/ClusterA/Resources/PoolX', 'ClusterA'],
+      ['/DC1/host/ClusterA/Resources/', 'ClusterA'],
       ['/DC1/host/esxi01/Resources', 'esxi01'],
       ['/DC1/vm/FolderA', null],
+      ['/DC1/host/foo/ResourcesPool/bar', null],
+      ['/DC1/host//Resources/PoolX', null],
+      ['', null],
+      [null, null],
       [undefined, null],
-    ])('should derive provider location from pool path %p', (pool: string | undefined, expected: string | null) => {
+    ])('returns %p for pool %p', (pool: string | null | undefined, expected: string | null) => {
       const machineTemplate = new VmwarevsphereMachineTemplate({
         spec: {
           template: {
