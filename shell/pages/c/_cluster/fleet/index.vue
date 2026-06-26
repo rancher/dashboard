b415c7ceb0b77d8d13629acaba2734d02964bc99
Alexandre Alves <alexandre.alves@suse.com>
2026-06-26 12:30:22 +0100

    address various problems with `SlideIn` api (#18118)
    * address various problems with slideIn api
    
    * bump shell version
    
    * address pr comments

--- shell/pages/c/_cluster/fleet/index.vue
+++ shell/pages/c/_cluster/fleet/index.vue
@@ -322,19 +322,15 @@ export default {
       this.selectedCard = selected;
 
       this.$shell.slideIn.open(ResourceDetails, {
-        showHeader: false,
-        props:      {
+        props: {
           value,
           statePanel,
           workspace
         },
-        width:              '73%',
-        // We want this to be full viewport height top to bottom
-        height:             '100vh',
-        top:                '0',
-        'z-index':          101, // We want this to be above the main side menu
-        closeOnRouteChange: ['name', 'params', 'query'], // We want to ignore hash changes, tables in extensions can trigger the drawer to close while opening
-        triggerFocusTrap:   false,
+        width:              'wide',
+        height:             'full',
+        closeOnRouteChange: ['name', 'params', 'query'],
+        disableFocusTrap:   true,
       });
     },
 
