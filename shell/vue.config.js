088e61469e40cdd99622f57860dc97c1031c1d69
Alexandre Alves <alexandre.alves@suse.com>
2026-07-01 14:48:06 +0100

    fix lint errors

--- shell/vue.config.js
+++ shell/vue.config.js
@@ -224,7 +224,9 @@ function checkRancherVersionCompatibility(targetApi, shellRancherVersion) {
     (res) => {
       let data = '';
 
-      res.on('data', (chunk) => { data += chunk; });
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
       res.on('end', () => {
         try {
           const { Version } = JSON.parse(data);
