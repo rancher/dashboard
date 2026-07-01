4ca16306dcc8c31d107600e10c5703f1314a8c9e
Richard Cox <ricox@suse.com>
2026-07-01 15:37:23 +0100

    Show ling to and count of CR in CRD list

---
+++ shell/models/apiextensions.k8s.io.customresourcedefinition.js
@@ -0,0 +1,24 @@
+import SteveModel from '@shell/plugins/steve/steve-class';
+import { NAME as EXPLORER } from '@shell/config/product/explorer';
+import { COUNT } from '@shell/config/types';
+
+export default class CustomResourceDefinition extends SteveModel {
+  get resourceType() {
+    return `${ this.spec.group }.${ this.spec.names.singular }`;
+  }
+
+  get resourceLink() {
+    const to = { ...this.listLocation };
+
+    to.params.resource = this.resourceType;
+
+    return {
+      text: this.resourceType,
+      to
+    };
+  }
+
+  get resourceCount() {
+    return this.$getters.all(COUNT)[0].counts[this.resourceType]?.summary.count;
+  }
+}
