d3fcec679fe8e46956ab28f5c9e11d3af02008dd
Phillip Rak <rak.phillip@gmail.com>
2026-07-30 14:24:00 -0700

    Remove type-map and dashboard-store shims
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/types/rancher/index.d.ts
+++ shell/types/rancher/index.d.ts
@@ -2,13 +2,6 @@ declare module '@rancher/auto-import' {
   export function importTypes(ext: any): void;
 }
 
-declare module '@shell/store/type-map' {
-  export function DSL(store: any, name: string): any;
-  export function isAdminUser(getters: any): boolean;
-}
-
-declare module '@shell/plugins/dashboard-store';
-
 declare module '@shell/config/version' {
   export const CURRENT_RANCHER_VERSION: string;
   export function getVersionData(): any;
