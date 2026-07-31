702f867687dc1a999dffedb1c4fd6b2dd402f1c5
Francesco Torchia <francesco.torchia@suse.com>
2026-07-31 10:43:43 +0200

    @rancher/cypress: remove shell imports
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- cypress/e2e/po/pages/cluster-manager/machines.po.ts
+++ cypress/e2e/po/pages/cluster-manager/machines.po.ts
@@ -4,7 +4,7 @@ import MachinesListPo from '@/cypress/e2e/po/lists/machines-list.po';
 import CodeMirrorPo from '@/cypress/e2e/po/components/code-mirror.po';
 import ProductNavPo from '@/cypress/e2e/po/side-bars/product-side-nav.po';
 import BurgerMenuPo from '@/cypress/e2e/po/side-bars/burger-side-menu.po';
-import { BLANK_CLUSTER } from '@shell/store/store-types.js';
+import { BLANK_CLUSTER } from '@/cypress/support/utils/shell';
 
 export default class MachinesPagePo extends PagePo {
   private static createPath(clusterId: string = BLANK_CLUSTER) {
---
+++ cypress/support/utils/shell.ts
@@ -0,0 +1,65 @@
+/**
+ * Mirror of shell utilities for use in cypress tests
+ * This replicates essential exports from shell modules to avoid cypress dependencies on the main shell directory
+ */
+
+// From @shell/config/version.js
+export const CURRENT_RANCHER_VERSION = '2.15';
+
+// From @shell/store/store-types.js
+export const STORE = {
+  CLUSTER:    'cluster',
+  RANCHER:    'rancher',
+  MANAGEMENT: 'management',
+};
+
+export const BLANK_CLUSTER = '_';
+
+// From @shell/types/fleet.d.ts
+export type WorkloadType = 'workload' | 'pods' | 'apps.deployments' | 'replicasets' | 'daemonsets' | 'statefulsets' | 'jobs' | 'cronjobs';
+
+// From @shell/config/types.js
+export const DEFAULT_GRAFANA_STORAGE_SIZE = '10Gi';
+
+// From @shell/utils/crypto/index.js
+const NORMAL = 'normal';
+const URL = 'url';
+
+export function base64Encode(string: string | null | undefined, alphabet = NORMAL): string | null | undefined {
+  if (string === null || typeof string === 'undefined') {
+    return string;
+  }
+
+  const buf = Buffer.from(string);
+
+  if (alphabet === URL) {
+    const m: Record<string, string> = {
+      '+': '-',
+      '/': '_',
+    };
+
+    return buf.toString('base64').replace(/[+/]|=+$/g, (char) => m[char] || '');
+  }
+
+  return buf.toString('base64');
+}
+
+export function base64DecodeToBuffer(string: string | null | undefined): Buffer | string | null | undefined {
+  if (string === null || typeof string === 'undefined') {
+    return string;
+  }
+
+  if (typeof Buffer !== 'undefined' && typeof Buffer.from === 'function' && Buffer.from !== Uint8Array.from) {
+    return Buffer.from(string, 'base64');
+  } else {
+    return Buffer.from(string, 'base64');
+  }
+}
+
+export function base64Decode(string: string | null | undefined): string | null | undefined {
+  if (string) {
+    return base64DecodeToBuffer(string.replace(/[-_]/g, (char) => char === '-' ? '+' : '/'))?.toString();
+  }
+
+  return '';
+}
