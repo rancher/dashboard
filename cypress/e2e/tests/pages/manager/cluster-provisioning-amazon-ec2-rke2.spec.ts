c85c141d78ecd6b19d435da06ee36f85c027ba1f
Isabela Guimaraes <isabela.guimaraes@suse.com>
2026-08-10 11:30:17 -0700

    reduced the timeout in transient states of machine pool scale down test
    Signed-off-by: Isabela Guimaraes <isabela.guimaraes@suse.com>

--- cypress/e2e/tests/pages/manager/cluster-provisioning-amazon-ec2-rke2.spec.ts
+++ cypress/e2e/tests/pages/manager/cluster-provisioning-amazon-ec2-rke2.spec.ts
@@ -304,16 +304,16 @@ describe('Deploy RKE2 cluster using node driver on Amazon EC2', { tags: ['@manag
     // The mixed red/green state can transition too quickly to observe reliably in CI.
     // Rely on the settled assertions below instead.
 
-    // Verify the machine pool is scaled down to 1
-    clusterDetails.poolsList('machine').machinePoolReadyofDesiredCount(`${ this.rke2Ec2ClusterName }-pool1`, /^1$/, VERY_LONG_TIMEOUT_OPT);
-    // progress bar should contain green and no other color
-    clusterDetails.poolsList('machine').progressBarElements(`${ this.rke2Ec2ClusterName }-pool1`, '.bg-error', VERY_LONG_TIMEOUT_OPT).should('not.exist');
-    clusterDetails.poolsList('machine').progressBarElements(`${ this.rke2Ec2ClusterName }-pool1`, '.bg-success').should('exist');
-    clusterDetails.poolsList('machine').progressBarElements(`${ this.rke2Ec2ClusterName }-pool1`, '.piece').should('have.length', 1);
-
-    // Verify the cluster is active
+    // Verify the cluster reaches the settled state.
     clusterDetails.resourceDetail().masthead().resourceStatus().contains('Active', VERY_LONG_TIMEOUT_OPT);
     clusterDetails.poolsList('machine').resourceTable().sortableTable().checkRowCount(false, 1, VERY_LONG_TIMEOUT_OPT);
+
+    // Verify the machine pool is scaled down to 1
+    clusterDetails.poolsList('machine').machinePoolReadyofDesiredCount(`${ this.rke2Ec2ClusterName }-pool1`, /^1$/, MEDIUM_TIMEOUT_OPT);
+    // progress bar should contain green and no other color
+    clusterDetails.poolsList('machine').progressBarElements(`${ this.rke2Ec2ClusterName }-pool1`, '.bg-error', MEDIUM_TIMEOUT_OPT).should('not.exist');
+    clusterDetails.poolsList('machine').progressBarElements(`${ this.rke2Ec2ClusterName }-pool1`, '.bg-success', MEDIUM_TIMEOUT_OPT).should('exist');
+    clusterDetails.poolsList('machine').progressBarElements(`${ this.rke2Ec2ClusterName }-pool1`, '.piece').should('have.length', 1);
 
     // Verify the scale down button is now disabled (can't scale below 1)
     clusterDetails.poolsList('machine').scaleDownButton(`${ this.rke2Ec2ClusterName }-pool1`)
