7e923c08a5df6438461e0eda2aad4f5740e79065
Neil MacDougall <nwmac@users.noreply.github.com>
2026-08-25 16:37:04 +0100

    Ensure proxying errors close the stream and UI detects this error (#18901)
    * Ensure proxying errors close the stream and UI detects 504
    
    * Simpler change
    
    * Fix lint
    
    * Change error status code from 504 to 59Keep old error code
    
    * Change error handling status from 504 to 598

--- shell/vue-config-helper.js
+++ shell/vue-config-helper.js
@@ -116,7 +116,7 @@ function onProxyReqWs(proxyReq, req, socket, options, head) {
 function onError(err, req, res) {
   res.statusCode = 598;
   console.error('Proxy Error:', err); // eslint-disable-line no-console
-  res.write(JSON.stringify(err));
+  res.end(JSON.stringify(err));
 }
 
 module.exports = {
