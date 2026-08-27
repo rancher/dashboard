1a94be2be9dba0355ce4947a06bf860c1d7264ab
Phillip Rak <rak.phillip@gmail.com>
2026-08-27 11:36:51 -0700

    Migrate the provider login buttons to RcButton
    Each provider's login control rolled its own button: a bare `<button
    class="btn bg-primary">` with an inline `font-size: 18px`. Swap them for
    RcButton so they pick up the design system's primary style and sizing,
    and give them a shared test id so the page has one handle on "the button
    that signs you in", whichever provider is on screen.
    
    The LDAP form's inputs drop their `span-6 offset-3` grid at the same
    time, leaving the login page to size the column its fields sit in.
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/components/auth/login/oauth.vue
+++ shell/components/auth/login/oauth.vue
@@ -1,8 +1,10 @@
 <script>
+import { RcButton } from '@components/RcButton';
 import Login from '@shell/mixins/login';
 
 export default {
-  mixins: [Login],
+  components: { RcButton },
+  mixins:     [Login],
 
   methods: {
     login() {
@@ -14,13 +16,14 @@ export default {
 
 <template>
   <div class="text-center">
-    <button
+    <RcButton
       ref="btn"
-      class="btn bg-primary"
-      style="font-size: 18px;"
+      variant="primary"
+      size="large"
+      data-testid="login-provider-submit"
       @click="login"
     >
       {{ t('login.loginWithProvider', {provider: displayName}) }}
-    </button>
+    </RcButton>
   </div>
 </template>
