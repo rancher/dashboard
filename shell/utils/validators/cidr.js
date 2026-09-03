bf77269535ed1e3de47bfbc4db63194d604c3f49
Yunare <yunare@gmail.com>
2026-09-03 11:49:39 +0000

    fix: support IPv6 CIDR in isValidCIDR validator
    Issue #9589 requested IPv6 support for the ipaddr question type
    and chart questions that validate IP addresses with CIDR notation.
    isValidIP already supported IPv6 via ipaddr.js, but isValidCIDR
    used an IPv4-only regex. This adds IPv6 CIDR validation using
    ipaddr.js parse and prefix length checks (0-128 for IPv6,
    0-32 for IPv4).

--- shell/utils/validators/cidr.js
+++ shell/utils/validators/cidr.js
@@ -3,7 +3,28 @@ import ipaddr from 'ipaddr.js';
 const validCIDRregex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/(3[0-2]|2[0-9]|1[0-9]|[0-9])$/;
 
 export function isValidCIDR(cidr) {
-  return !!cidr.match(validCIDRregex);
+  if (!validCIDRregex.test(cidr)) {
+    // Try IPv6 CIDR using ipaddr.js
+    try {
+      const [ip, prefixLen] = cidr.split('/');
+      const ipObj = ipaddr.parse(ip);
+      const prefix = parseInt(prefixLen, 10);
+
+      if (ipObj instanceof ipaddr.IPv6) {
+        return prefix >= 0 && prefix <= 128;
+      }
+
+      if (ipObj instanceof ipaddr.IPv4) {
+        return prefix >= 0 && prefix <= 32;
+      }
+    } catch {
+      return false;
+    }
+
+    return false;
+  }
+
+  return true;
 }
 
 export function isValidIP(ip) {
